8b7570cbad015af9119f1728817508997eeaaa3f
David Scicluna <dave_nostra@hotmail.com>
2021-05-13 09:33:41 +0200

    Eslint & Prettier
    Implemented eslint & prettier into the application

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
 
-import App from "./App";
+import ReactDOM from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
