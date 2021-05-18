085ee316d76294c18d784d392b72ce8da0c20400
davidscicluna <dave_nostra@hotmail.com>
2021-05-18 19:39:07 +0200

    Feature: Theme part 1
    Partially implemented theme.

---
+++ src/themes/foundations/radius.ts
@@ -0,0 +1,11 @@
+const radii = {
+  none: '0',
+  sm: '5px',
+  base: '10px',
+  md: '12.5px',
+  lg: '15px',
+  xl: '20px',
+  full: '9999px'
+};
+
+export default radii;
---
+++ src/themes/foundations/transition.ts
@@ -0,0 +1,31 @@
+const transitionProperty = {
+  common: 'background-color, border-color, color, fill, stroke, opacity, box-shadow, transform',
+  colors: 'background-color, border-color, color, fill, stroke',
+  dimensions: 'width, height',
+  position: 'left, right, top, bottom',
+  background: 'background-color, background-image, background-position'
+};
+
+const transitionTimingFunction = {
+  'ease-in': 'cubic-bezier(0.5, 0, 0.75, 0)',
+  'ease-out': 'cubic-bezier(0.25, 1, 0.5, 1)',
+  'ease-in-out': 'cubic-bezier(0.76, 0, 0.24, 1)'
+};
+
+const transitionDuration = {
+  'ultra-fast': '50ms',
+  'faster': '100ms',
+  'fast': '200ms',
+  'normal': '250ms',
+  'slow': '500ms',
+  'slower': '750ms',
+  'ultra-slow': '1000ms'
+};
+
+const transition = {
+  property: transitionProperty,
+  easing: transitionTimingFunction,
+  duration: transitionDuration
+};
+
+export default transition;
