5fb66117c23e92a8e7947306ae4ced42e6d87499
David Scicluna <dave_nostra@hotmail.com>
2021-08-08 17:36:03 +0200

    Fix: Person Bio Loading

--- src/pages/Person/components/Bio/index.tsx
+++ src/pages/Person/components/Bio/index.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement } from 'react';
 
 import { useColorMode, useBoolean, VStack, HStack, Text, Collapse } from '@chakra-ui/react';
+import _ from 'lodash';
 
 import useSelector from '../../../../common/hooks/useSelectorTyped';
 import utils from '../../../../common/utils/utils';
@@ -9,11 +10,12 @@ import Button from '../../../../components/Clickable/Button';
 import SkeletonText from '../../../../components/Skeleton/Text';
 import { BioProps } from './types';
 
-const Bio = ({ biography, isLoading }: BioProps): ReactElement => {
+const Bio = ({ biography, isLoading = false }: BioProps): ReactElement => {
   const { colorMode } = useColorMode();
-  const [isExpanded, setIsExpanded] = useBoolean();
 
   const color = useSelector((state) => state.user.ui.theme.color);
+
+  const [isExpanded, setIsExpanded] = useBoolean();
 
   return (
     <Card minWidth='100%' px={2} pt={1.5} pb={2}>
@@ -35,22 +37,35 @@ const Bio = ({ biography, isLoading }: BioProps): ReactElement => {
           </Text>
         </HStack>
 
-        <Collapse startingHeight={44} in={isExpanded}>
-          <SkeletonText isLoaded={!isLoading} noOfLines={5}>
-            <VStack spacing={2}>
-              {biography.split('\n'[0]).map((paragraph, index) => (
-                <Text
-                  key={index}
-                  align='left'
-                  color={colorMode === 'light' ? 'gray.900' : 'gray.50'}
-                  fontSize='md'
-                  fontWeight='medium'>
-                  {paragraph}
+        {!isLoading ? (
+          <Collapse startingHeight={44} in={isExpanded}>
+            <SkeletonText isLoaded={!isLoading} noOfLines={5}>
+              <VStack width='100%' spacing={2}>
+                {biography.split('\n'[0]).map((paragraph, index) => (
+                  <Text
+                    key={index}
+                    align='left'
+                    color={colorMode === 'light' ? 'gray.900' : 'gray.50'}
+                    fontSize='md'
+                    fontWeight='medium'>
+                    {paragraph}
+                  </Text>
+                ))}
+              </VStack>
+            </SkeletonText>
+          </Collapse>
+        ) : (
+          <VStack width='100%' spacing={2}>
+            {_.range(0, 3).map((_dummy, index) => (
+              <SkeletonText key={index} width='100%' isLoaded={!isLoading}>
+                <Text align='left' fontSize='xs'>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                  dolore magna aliqua.
                 </Text>
-              ))}
-            </VStack>
-          </SkeletonText>
-        </Collapse>
+              </SkeletonText>
+            ))}
+          </VStack>
+        )}
 
         <Button
           color={utils.handleReturnColor(color)}
