acaec08900f83bb67f0607bae8bbe7c56fae2fde
David Scicluna <dave_nostra@hotmail.com>
2021-09-18 16:28:16 +0200

    Feature: Cast
    Implemented Top Cast HorizontalGrid in Movie Home Tab

---
+++ src/pages/Movie/components/Cast/types.ts
@@ -0,0 +1,9 @@
+import { Cast } from '../../../../common/types/movie';
+
+export type CastProps = {
+  cast?: Cast[];
+  name?: string;
+  isError?: boolean;
+  isSuccess?: boolean;
+  isLoading?: boolean;
+};
