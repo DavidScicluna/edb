7ba44aca0555dea2a6c377fed99a73465368b72d
David Scicluna <dave_nostra@hotmail.com>
2021-10-22 20:14:57 +0200

    Fix: Movie Actions
    Moved Movie Actions folder to View/components folder to be used for both Movie & Show

---
+++ src/pages/View/components/Actions/types.ts
@@ -0,0 +1,10 @@
+import { PartialMovie } from '../../../../common/types/movie';
+import { PartialTV } from '../../../../common/types/tv';
+import { MediaType } from '../../../../common/types/types';
+
+export type ActionsProps = {
+  mediaItem?: PartialMovie | PartialTV;
+  mediaType: MediaType;
+  title?: string;
+  isLoading?: boolean;
+};
