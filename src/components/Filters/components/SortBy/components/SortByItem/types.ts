f4e110a166d3823280b622ca752f6a4baeacfab0
David Scicluna <dave_nostra@hotmail.com>
2021-12-31 16:40:11 +0100

    Fix: Removed Filter SortBy
    Removed Filter SortBy since it has been extracted into its own component

--- src/components/Filters/components/SortBy/components/SortByItem/types.ts
+++
@@ -1,8 +0,0 @@
-import { SortBy } from '../../../../../../common/types';
-import { SortDirection } from '../../../../../../store/slices/App/types';
-
-export type SortByItemProps = {
-  direction: SortDirection;
-  onSortChange: (sortBy: SortBy) => void;
-  onDirectionChange: (direction: SortDirection) => void;
-} & SortBy;
