bf6c6aaea40945ad4a93e28da40f5ea8ffaae7ec
David Scicluna <dave_nostra@hotmail.com>
2021-12-31 16:38:56 +0100

    Fix: Filters. Removed Not Supported filters

--- src/components/Filters/components/DisplayMode/components/DisplayModeItem/types.ts
+++
@@ -1,7 +0,0 @@
-import { Form } from '../../../../types';
-import { DisplayMode } from '../../types';
-
-export type DisplayModeItemProps = {
-  isActive?: boolean;
-  onClick?: (mode: Form['displayMode']) => void;
-} & DisplayMode;
--- src/components/Filters/components/DisplayMode/types.ts
+++
@@ -1,9 +0,0 @@
-import { Icon } from '../../../../common/types';
-import { Form } from '../../types';
-
-export type DisplayMode = {
-  label: string;
-  value: Form['displayMode'];
-  iconActive: Icon;
-  icon: Icon;
-};
