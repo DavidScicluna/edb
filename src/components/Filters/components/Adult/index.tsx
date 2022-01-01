f195d6773c04ed10e498e0a878a13657e6daa512
David Scicluna <dave_nostra@hotmail.com>
2022-01-01 16:49:38 +0100

    Feature: Filter By Adult

---
+++ src/components/Filters/components/Adult/index.tsx
@@ -0,0 +1,69 @@
+import React, { ReactElement } from 'react';
+
+import { HStack } from '@chakra-ui/react';
+import {
+  RadioButtonUncheckedOutlined as RadioButtonUncheckedOutlinedIcon,
+  RadioButtonCheckedOutlined as RadioButtonCheckedOutlinedIcon
+} from '@material-ui/icons';
+import { Controller } from 'react-hook-form';
+
+import { useSelector } from '../../../../common/hooks';
+import Button from '../../../Clickable/Button';
+import Panel from '../../../Panel';
+import { AdultProps } from './types';
+
+const Adult = ({ form, mediaType }: AdultProps): ReactElement => {
+  const color = useSelector((state) => state.user.ui.theme.color);
+
+  return (
+    <Controller
+      control={form.control}
+      name='adult'
+      render={({ field: { value } }) => (
+        <Panel isFullWidth>
+          {{
+            header: {
+              title: mediaType === 'movie' ? 'Show Adult Movies' : 'Show Adult TV Shows'
+            },
+            body: (
+              <HStack width='100%' spacing={2}>
+                <Button
+                  color={!value ? color : 'gray'}
+                  renderLeftIcon={({ fontSize }) =>
+                    !value ? (
+                      <RadioButtonCheckedOutlinedIcon style={{ fontSize }} />
+                    ) : (
+                      <RadioButtonUncheckedOutlinedIcon style={{ fontSize }} />
+                    )
+                  }
+                  isFullWidth
+                  onClick={value ? () => form.setValue('adult', false, { shouldDirty: true }) : undefined}
+                  variant='outlined'
+                >
+                  No
+                </Button>
+                <Button
+                  color={value ? color : 'gray'}
+                  renderLeftIcon={({ fontSize }) =>
+                    value ? (
+                      <RadioButtonCheckedOutlinedIcon style={{ fontSize }} />
+                    ) : (
+                      <RadioButtonUncheckedOutlinedIcon style={{ fontSize }} />
+                    )
+                  }
+                  isFullWidth
+                  onClick={!value ? () => form.setValue('adult', true, { shouldDirty: true }) : undefined}
+                  variant='outlined'
+                >
+                  Yes
+                </Button>
+              </HStack>
+            )
+          }}
+        </Panel>
+      )}
+    />
+  );
+};
+
+export default Adult;
---
+++ src/components/Filters/components/Adult/types.ts
@@ -0,0 +1,9 @@
+import { UseFormReturn } from 'react-hook-form';
+
+import { MediaType } from '../../../../common/types';
+import { Form } from '../../types';
+
+export type AdultProps = {
+  form: UseFormReturn<Form>;
+  mediaType: Omit<MediaType, 'person'>;
+};
