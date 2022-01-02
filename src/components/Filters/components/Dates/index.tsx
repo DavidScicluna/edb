6e7c16027b8cb292c83087d3dfc53c86bdf2ea54
David Scicluna <dave_nostra@hotmail.com>
2022-01-02 17:15:10 +0100

    Fix: Dates Responsiveness issues

--- src/components/Filters/components/Dates/index.tsx
+++ src/components/Filters/components/Dates/index.tsx
@@ -1,6 +1,6 @@
 import React, { ReactElement } from 'react';
 
-import { useColorMode, useMediaQuery, Stack, HStack, Text, ScaleFade } from '@chakra-ui/react';
+import { useColorMode, useMediaQuery, Stack, Text, ScaleFade } from '@chakra-ui/react';
 import _ from 'lodash';
 import moment from 'moment';
 import { Controller } from 'react-hook-form';
@@ -9,6 +9,7 @@ import { useSelector } from '../../../../common/hooks';
 import Button from '../../../Clickable/Button';
 import DatePicker from '../../../Forms/DatePicker';
 import Panel from '../../../Panel';
+import Header from '../Header';
 import { DatesProps } from './types';
 
 const minDate = new Date(1900, 1);
@@ -30,128 +31,95 @@ const Dates = ({ form, mediaType }: DatesProps): ReactElement => {
       render={({ field: { value } }) => (
         <Panel isFullWidth>
           {{
-            header: {
-              title: mediaType === 'movie' ? 'Release Date' : 'First Air Date',
-              actions: (
-                <HStack spacing={2}>
+            header: (
+              <Header
+                label={mediaType === 'movie' ? 'Release Date' : 'First Air Date'}
+                renderMessage={({ color, fontSize, fontWeight }) => (
                   <ScaleFade in={value.every((date) => !_.isNil(date))} unmountOnExit>
-                    <Text color={colorMode === 'light' ? 'gray.900' : 'gray.50'} fontSize='md' fontWeight='medium'>
+                    <Text color={color} fontSize={fontSize} fontWeight={fontWeight}>
                       {moment(value[0]).isSame(moment(value[1]), 'date')
                         ? moment(value[0]).format(visibleFormat)
                         : value.map((date) => moment(date).format(visibleFormat)).join(' - ')}
                     </Text>
                   </ScaleFade>
+                )}
+                renderButton={({ color, size, variant }) => (
                   <Button
                     color={color}
                     isDisabled={value.every((date) => _.isNil(date))}
                     onClick={() => form.setValue('date', [undefined, undefined], { shouldDirty: true })}
-                    size='sm'
-                    variant='text'
+                    size={size}
+                    variant={variant}
                   >
                     Clear
                   </Button>
-                </HStack>
-              )
-            },
+                )}
+              />
+            ),
             body: (
               <Stack
                 width='100%'
                 direction={isSm ? 'column' : 'row'}
                 alignItems='center'
                 justifyContent='center'
+                // spacing={isSm ? 1 : 2}
                 spacing={2}
               >
-                <DatePicker
-                  renderToggleModal={
-                    ({ color, icon, onClick }) => (
-                      // isSm ? (
-                      <Button
-                        color={color}
-                        // renderLeftIcon={({ fontSize }) => <Icon style={{ fontSize }} />}
-                        isFullWidth
-                        onClick={onClick}
-                        variant='outlined'
-                      >
+                <span style={{ width: '100%' }}>
+                  <DatePicker
+                    renderToggleModal={({ color, onClick }) => (
+                      <Button color={color} isFullWidth onClick={onClick} variant='outlined'>
                         {value[0] ? moment(value[0]).format(visibleFormat) : 'Select Start Date'}
                       </Button>
-                      // )
-                      //  : (
-                      // <IconButton
-                      //   aria-label='Open Filters'
-                      //   color={color}
-                      //   isDisabled={moviesQuery.isFetching || moviesQuery.isLoading || moviesQuery.isError}
-                      //   onClick={onClick}
-                      //   variant='outlined'
-                      // >
-                      //   {icon}
-                      // </IconButton>
-                    )
-                    // )
-                  }
-                  color={color}
-                  minDate={minDate}
-                  maxDate={maxDate}
-                  firstDayOfWeek={1}
-                  value={value[0] ? moment(value[0], dataFormat).toDate() : undefined}
-                  onSetDate={(date) =>
-                    form.setValue(
-                      'date',
-                      [
-                        moment(date).format(dataFormat),
-                        moment(moment(value[1]).format(dataFormat) || moment(date).add(1, 'days')).format(dataFormat)
-                      ],
-                      {
-                        shouldDirty: true
-                      }
-                    )
-                  }
-                />
-                <Text
+                    )}
+                    color={color}
+                    minDate={minDate}
+                    maxDate={maxDate}
+                    firstDayOfWeek={1}
+                    value={value[0] ? moment(value[0], dataFormat).toDate() : undefined}
+                    onSetDate={(date) =>
+                      form.setValue(
+                        'date',
+                        [
+                          moment(date).format(dataFormat),
+                          moment(value[1] ? moment(value[1]).format(dataFormat) : moment(date).add(1, 'days')).format(
+                            dataFormat
+                          )
+                        ],
+                        {
+                          shouldDirty: true
+                        }
+                      )
+                    }
+                  />
+                </span>
+                {/* <Text
                   align='center'
                   color={colorMode === 'light' ? 'gray.400' : 'gray.500'}
                   fontSize='md'
                   fontWeight='semibold'
                 >
                   TO
-                </Text>
-                <DatePicker
-                  renderToggleModal={
-                    ({ color, icon, onClick }) => (
-                      // isSm ? (
-                      <Button
-                        color={color}
-                        // renderLeftIcon={({ fontSize }) => <Icon style={{ fontSize }} />}
-                        isFullWidth
-                        onClick={onClick}
-                        variant='outlined'
-                      >
+                </Text> */}
+                <span style={{ width: '100%' }}>
+                  <DatePicker
+                    renderToggleModal={({ color, onClick }) => (
+                      <Button color={color} isFullWidth onClick={onClick} variant='outlined'>
                         {value[1] ? moment(value[1]).format(visibleFormat) : 'Select To Date'}
                       </Button>
-                      // )
-                      //  : (
-                      // <IconButton
-                      //   aria-label='Open Filters'
-                      //   color={color}
-                      //   isDisabled={moviesQuery.isFetching || moviesQuery.isLoading || moviesQuery.isError}
-                      //   onClick={onClick}
-                      //   variant='outlined'
-                      // >
-                      //   {icon}
-                      // </IconButton>
-                    )
-                    // )
-                  }
-                  color={color}
-                  minDate={moment(value[0], dataFormat).toDate() || minDate}
-                  maxDate={maxDate}
-                  firstDayOfWeek={1}
-                  value={value[1] ? moment(value[1], dataFormat).toDate() : undefined}
-                  onSetDate={(date) =>
-                    form.setValue('date', [moment(value[0]).format(dataFormat), moment(date).format(dataFormat)], {
-                      shouldDirty: true
-                    })
-                  }
-                />
+                    )}
+                    color={color}
+                    minDate={moment(value[0], dataFormat).toDate() || minDate}
+                    maxDate={maxDate}
+                    firstDayOfWeek={1}
+                    value={value[1] ? moment(value[1], dataFormat).toDate() : undefined}
+                    onSetDate={(date) =>
+                      form.setValue('date', [moment(value[0]).format(dataFormat), moment(date).format(dataFormat)], {
+                        shouldDirty: true
+                      })
+                    }
+                  />
+                </span>
               </Stack>
             )
           }}
