3867c069e497fefd32bcf4a4e38965d61d2bb4f6
David Scicluna <dave_nostra@hotmail.com>
2022-01-15 19:21:14 +0100

    Moved Liked to User folder

--- src/pages/Liked/components/Divider/types.ts
+++
@@ -1,7 +0,0 @@
-import { BoxProps } from '@chakra-ui/react';
-
-import { Orientation } from '../../../../common/types';
-
-export type DividerProps = {
-  orientation?: Orientation;
-} & BoxProps;
--- src/pages/Liked/components/Inactive/types.ts
+++
@@ -1,6 +0,0 @@
-import { MediaType } from '../../../../common/types';
-
-export type InactiveProps = {
-  mediaTypes: MediaType[];
-  onSetMediaType: (mediaType: MediaType) => void;
-};
