1509d0b5c7f36edd637e14a59189192f1208159c
David Scicluna <dave_nostra@hotmail.com>
2022-01-15 19:22:08 +0100

    Feature: Created a common TV for Lists & Liked

---
+++ src/pages/User/components/TV/index.tsx
@@ -0,0 +1,41 @@
+import React, { ReactElement, useState } from 'react';
+
+import { useMediaQuery, VStack, ScaleFade } from '@chakra-ui/react';
+
+import LoadMore from '../../../../components/Clickable/LoadMore';
+import VerticalTV from '../../../TV/components/Orientation/Vertical';
+import { TVProps } from './types';
+
+const incrementBy = 20;
+
+const TV = ({ shows }: TVProps): ReactElement => {
+  const [isSm] = useMediaQuery('(max-width: 600px)');
+
+  const [totalVisible, setTotalVisible] = useState<number>(incrementBy);
+
+  return (
+    <VStack width='100%' spacing={4}>
+      <VerticalTV
+        isError={shows.length === 0}
+        isSuccess={shows.length > 0}
+        isLoading={false}
+        shows={shows.filter((_show, index) => index < totalVisible)}
+      />
+
+      <ScaleFade
+        in={shows.length > 0 && shows.length > incrementBy}
+        unmountOnExit
+        style={{ width: isSm ? '100%' : 'auto' }}
+      >
+        <LoadMore
+          amount={totalVisible}
+          total={shows.length}
+          label='TV Shows'
+          onClick={() => setTotalVisible(totalVisible + incrementBy)}
+        />
+      </ScaleFade>
+    </VStack>
+  );
+};
+
+export default TV;
