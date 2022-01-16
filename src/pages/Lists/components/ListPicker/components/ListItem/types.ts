df73f55255aa14f75a9ffb72ea82381936d8b5b6
David Scicluna <dave_nostra@hotmail.com>
2022-01-16 16:52:32 +0100

    Removed Old Lists Page (Since moved to User folder)

--- src/pages/Lists/components/ListPicker/components/ListItem/types.ts
+++
@@ -1,9 +0,0 @@
-import { List } from '../../../../../../store/slices/User/types';
-
-export type ListItemProps = {
-  isActive?: boolean;
-  isSelectable?: boolean;
-  isSelected?: boolean;
-  onSelected?: (id: List['id']) => void;
-  onClose?: () => void;
-} & List;
