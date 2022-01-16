1c9ee2195dbf1053da876adcfb589ba3573ced20
David Scicluna <dave_nostra@hotmail.com>
2022-01-16 16:49:51 +0100

    Fix: EditList

---
+++ src/pages/User/pages/Lists/components/EditList/index.tsx
@@ -0,0 +1,164 @@
+import { ReactElement, useEffect } from 'react';
+
+import { useMediaQuery, useDisclosure, VStack } from '@chakra-ui/react';
+import { yupResolver } from '@hookform/resolvers/yup';
+import moment from 'moment';
+import { useForm, useFormState, Controller } from 'react-hook-form';
+import { useDispatch } from 'react-redux';
+
+import { useSelector } from '../../../../../../common/hooks';
+import Button from '../../../../../../components/Clickable/Button';
+import ConfirmModal from '../../../../../../components/ConfirmModal';
+import Input from '../../../../../../components/Forms/Input';
+import Textarea from '../../../../../../components/Forms/Textarea';
+import Modal from '../../../../../../components/Modal';
+import { setLists } from '../../../../../../store/slices/User';
+import { EditListProps, Form } from './types';
+import { defaultValues, schema } from './validation';
+
+const placeholders = [
+  'Action Movies',
+  'DC Movies',
+  'Leonardo DiCaprio',
+  'Classics',
+  'Comedy',
+  'Mafia Movies & TV Shows',
+  'Jennifer Lawrence',
+  'Johnny Depp',
+  'Angelina Jolie'
+];
+const placeholder = placeholders[Math.floor(Math.random() * placeholders.length)];
+
+const EditList = ({ list, isOpen, onClose }: EditListProps): ReactElement => {
+  const [isSm] = useMediaQuery('(max-width: 600px)');
+
+  const { isOpen: isConfirmOpen, onOpen: onOpenConfirm, onClose: onCloseConfirm } = useDisclosure();
+
+  const dispatch = useDispatch();
+  const lists = useSelector((state) => state.user.data.lists);
+  const color = useSelector((state) => state.user.ui.theme.color);
+
+  const form = useForm<Form>({
+    defaultValues,
+    reValidateMode: 'onSubmit',
+    resolver: yupResolver(schema)
+  });
+
+  const { isDirty } = useFormState({ control: form.control });
+
+  const handleSubmit = (values: Form): void => {
+    dispatch(
+      setLists(
+        lists.map((paramList) =>
+          paramList.id === list.id
+            ? {
+                ...list,
+                label: values.label,
+                description: values?.description || '',
+                date: moment(new Date()).toISOString(),
+                results: {
+                  ...list.results
+                }
+              }
+            : { ...paramList }
+        )
+      )
+    );
+
+    onClose();
+  };
+
+  const handleCloseConfirm = (): void => {
+    onCloseConfirm();
+    onClose();
+  };
+
+  const handleCheckClose = (): void => {
+    if (!isDirty) {
+      onClose();
+    } else {
+      onOpenConfirm();
+    }
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      form.reset({
+        label: list.label,
+        description: list.description
+      });
+    }
+  }, [isOpen]);
+
+  return (
+    <>
+      <Modal
+        title={isSm ? 'Edit List' : `Edit "${list.label}" List`}
+        renderActions={({ color, colorMode, size }) => (
+          <Button
+            color={color}
+            colorMode={colorMode}
+            isDisabled={!isDirty}
+            onClick={form.handleSubmit((values) => handleSubmit(values))}
+            size={size}
+          >
+            {isSm ? 'Save' : 'Save List'}
+          </Button>
+        )}
+        isOpen={isOpen}
+        onClose={handleCheckClose}
+        isCentered
+        size='lg'
+      >
+        <VStack spacing={3} p={2}>
+          <Controller
+            control={form.control}
+            name='label'
+            render={({ field: { onChange, value, name }, fieldState: { error } }) => (
+              <Input
+                color={color}
+                label='Label'
+                error={error}
+                name={name}
+                placeholder={`Try "${placeholder}"`}
+                onChange={onChange}
+                isFullWidth
+                isRequired
+                value={value}
+              />
+            )}
+          />
+          <Controller
+            control={form.control}
+            name='description'
+            render={({ field: { onChange, value, name }, fieldState: { error } }) => (
+              <Textarea
+                color={color}
+                label='Description'
+                error={error}
+                name={name}
+                onChange={onChange}
+                isFullWidth
+                value={value}
+              />
+            )}
+          />
+        </VStack>
+      </Modal>
+
+      <ConfirmModal
+        renderActions={({ color, colorMode, size }) => (
+          <Button color={color} colorMode={colorMode} onClick={() => handleCloseConfirm()} size={size}>
+            Close
+          </Button>
+        )}
+        title='Unsaved data!'
+        description='Are you sure you want to close the modal, the data inserted will be lost unless you save it!'
+        isOpen={isConfirmOpen}
+        onClose={onCloseConfirm}
+      />
+    </>
+  );
+};
+
+export default EditList;
