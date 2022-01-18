e6b29e8c8f602cf18c4d95a471c833adaf5847c3
David Scicluna <dave_nostra@hotmail.com>
2022-01-18 18:08:11 +0100

    Fix: Page Header in Home

--- src/containers/Page/index.tsx
+++ src/containers/Page/index.tsx
@@ -1,16 +1,19 @@
 import { ReactElement } from 'react';
 
 import { VStack, Box } from '@chakra-ui/react';
+import { useLocation } from 'react-router-dom';
 
 import Divider from './components/Divider';
 import Header from './components/Header';
 import { PageProps } from './types';
 
 const Page = ({ children, title }: PageProps): ReactElement => {
+  const location = useLocation();
+
   return (
     <VStack width='100%' divider={<Divider />} spacing={0}>
       {/* Header */}
-      <Header title={title} actions={children.actions} />
+      {location.pathname !== '/' ? <Header title={title} actions={children.actions} /> : null}
 
       {/* Body */}
       <Box width='100%'>{children.body}</Box>
