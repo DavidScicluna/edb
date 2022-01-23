01895568da4f2b32a07cbcba837f13569434f285
David Scicluna <dave_nostra@hotmail.com>
2022-01-23 19:30:14 +0100

    Fix: Search Movies

--- src/pages/Search/components/All/components/Movies/index.tsx
+++ src/pages/Search/components/All/components/Movies/index.tsx
@@ -1,68 +1,37 @@
 import { ReactElement } from 'react';
 
-import { useColorMode, useMediaQuery, Text } from '@chakra-ui/react';
+import { useMediaQuery } from '@chakra-ui/react';
 import qs from 'query-string';
 
 import { useSelector } from '../../../../../../common/hooks';
+import { PartialMovie } from '../../../../../../common/types/movie';
 import Button from '../../../../../../components/Clickable/Button';
 import Link from '../../../../../../components/Clickable/Link';
-import HorizontalGrid from '../../../../../../components/Grid/Horizontal';
-import HorizontalMovies from '../../../../../Movies/components/HorizontalMovies';
+import HorizontalGrid from '../../../../../../components/Grid/Horizontal/Default';
+import VerticalMoviePoster from '../../../../../Movies/components/Poster/Vertical';
 import { MoviesProps } from './types';
 
-const Movies = (props: MoviesProps): ReactElement => {
-  const { colorMode } = useColorMode();
+const Movies = ({ query, movies = [], total = 0 }: MoviesProps): ReactElement => {
   const [isSm] = useMediaQuery('(max-width: 600px)');
 
   const color = useSelector((state) => state.user.ui.theme.color);
 
-  const { query, results, total_results, isFetching, isLoading, isError, isSuccess, refetch } = props;
-
   return (
     <HorizontalGrid
-      title={
-        <Text
-          align='left'
-          color={colorMode === 'light' ? 'gray.900' : 'gray.50'}
-          fontSize={['xl', 'xl', '2xl', '2xl', '2xl', '2xl']}
-          fontWeight='semibold'
-          textTransform='capitalize'
-        >
-          {`Found ${total_results || 0} movie${
-            total_results ? (total_results === 0 || total_results > 1 ? 's' : '') : ''
-          } with "${query}"`}
-        </Text>
-      }
+      title={`Found ${total} movie${total === 0 || total > 1 ? 's' : ''} with "${query}"`}
       footer={
-        (total_results || 0) > 20 ? (
-          <Link
-            to={{ pathname: '/search', search: qs.stringify({ query, mediaType: 'movie' }) }}
-            isFullWidth
-            isDisabled={isFetching || isLoading}
-          >
-            <Button
-              color={color}
-              isFullWidth
-              isDisabled={isFetching || isLoading}
-              onClick={() => refetch()}
-              size={isSm ? 'sm' : 'md'}
-              variant='text'
-            >
-              {`View all ${total_results || 0} movie${
-                total_results ? (total_results === 0 || total_results > 1 ? 's' : '') : ''
-              } with "${query}"`}
+        total > 20 ? (
+          <Link to={{ pathname: '/search/movies', search: qs.stringify({ query }) }} isFullWidth>
+            <Button color={color} isFullWidth size={isSm ? 'sm' : 'md'} variant='text'>
+              {`View all ${total} movie${total === 0 || total > 1 ? 's' : ''} with "${query}"`}
             </Button>
           </Link>
         ) : undefined
       }
-      isLoading={isFetching || isLoading}
     >
-      <HorizontalMovies
-        isError={isError}
-        isSuccess={isSuccess}
-        isLoading={isFetching || isLoading}
-        movies={results || []}
-      />
+      {movies.map((movie: PartialMovie) => (
+        <VerticalMoviePoster key={movie.id} width={['185px', '205px', '230px']} movie={movie} isLoading={false} />
+      ))}
     </HorizontalGrid>
   );
 };
--- src/pages/Search/components/All/components/Movies/types.ts
+++ src/pages/Search/components/All/components/Movies/types.ts
@@ -2,11 +2,6 @@ import { PartialMovie } from '../../../../../../common/types/movie';
 
 export type MoviesProps = {
   query: string;
-  results?: PartialMovie[];
-  total_results?: number;
-  isFetching: boolean;
-  isLoading: boolean;
-  isError: boolean;
-  isSuccess: boolean;
-  refetch: () => void;
+  movies?: PartialMovie[];
+  total?: number;
 };
