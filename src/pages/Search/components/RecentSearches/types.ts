d8e86e9f4858cef070593eb10a3d11ac3df81a8c
David Scicluna <dave_nostra@hotmail.com>
2022-01-23 16:53:56 +0100

    Feature: RecentSearches List

---
+++ src/pages/Search/components/RecentSearches/types.ts
@@ -0,0 +1,5 @@
+import { Search, SearchType } from '../../../../store/slices/User/types';
+
+export type RecentSearchesProps = {
+  onSearchClick: (label: Search['label'], searchTypes?: SearchType[]) => void;
+};
