3108294850c52a97e5fe725cf26a38a2da1d914a
David Scicluna <dave_nostra@hotmail.com>
2022-01-23 16:54:42 +0100

    Feature: Keywords

---
+++ src/pages/Search/components/Keywords/index.tsx
@@ -0,0 +1,75 @@
+import React, { ReactElement } from 'react';
+
+import { useMediaQuery, VStack, ScaleFade } from '@chakra-ui/react';
+import _ from 'lodash';
+
+import LoadMore from '../../../../components/Clickable/LoadMore';
+import Empty from '../../../../components/Empty';
+import Error from '../../../../components/Error';
+import List from '../List';
+import ListItem from '../List/components/ListItem';
+import { KeywordsProps } from './types';
+
+const dummies = _.range(4, 8);
+
+const Keywords = (props: KeywordsProps): ReactElement => {
+  const [isSm] = useMediaQuery('(max-width: 600px)');
+
+  const {
+    keywords,
+    isLoading = true,
+    isError = false,
+    isSuccess = false,
+    hasNextPage = true,
+    onKeywordClick,
+    onFetchNextPage
+  } = props;
+
+  return (
+    <VStack width='100%' spacing={4}>
+      <List title='Keywords'>
+        {!isLoading && isError ? (
+          <Error
+            hasIllustration={false}
+            label='Oh no! Something went wrong'
+            description='Failed to fetch keywords!'
+            size='xs'
+          />
+        ) : !isLoading && isSuccess && keywords?.results && keywords.results.length === 0 ? (
+          <Empty hasIllustration={false} label='Oh no!' description='No keywords found!' size='xs' />
+        ) : !isLoading && isSuccess && keywords?.results && keywords.results.length > 0 ? (
+          <>
+            {keywords.results.map((keyword) => (
+              <ListItem
+                key={keyword.id}
+                title={keyword.name}
+                isLoading={false}
+                onClick={() => onKeywordClick(keyword.name)}
+                variant='transparent'
+              />
+            ))}
+          </>
+        ) : (
+          <>
+            {_.range(0, _.sample(dummies)).map((_dummy, index) => (
+              <ListItem key={index} id={String(index)} title='Lorem Ipsum' isLoading variant='transparent' />
+            ))}
+          </>
+        )}
+      </List>
+
+      <ScaleFade in={hasNextPage && !isError && !isLoading} unmountOnExit style={{ width: isSm ? '100%' : 'auto' }}>
+        <LoadMore
+          amount={keywords?.results?.length || 0}
+          total={keywords?.total_results || 0}
+          label='Keywords'
+          isLoading={isLoading}
+          isButtonVisible={hasNextPage && !isError}
+          onClick={onFetchNextPage}
+        />
+      </ScaleFade>
+    </VStack>
+  );
+};
+
+export default Keywords;
