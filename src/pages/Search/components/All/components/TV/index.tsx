d534d8b317ad8a6f5eab97f3a7a5b5b68198364d
David Scicluna <dave_nostra@hotmail.com>
2022-01-23 19:30:27 +0100

    Fix: Search TV

--- src/pages/Search/components/All/components/TV/index.tsx
+++ src/pages/Search/components/All/components/TV/index.tsx
@@ -1,63 +1,37 @@
 import { ReactElement } from 'react';
 
-import { useColorMode, useMediaQuery, Text } from '@chakra-ui/react';
+import { useMediaQuery } from '@chakra-ui/react';
 import qs from 'query-string';
 
 import { useSelector } from '../../../../../../common/hooks';
+import { PartialTV } from '../../../../../../common/types/tv';
 import Button from '../../../../../../components/Clickable/Button';
 import Link from '../../../../../../components/Clickable/Link';
-import HorizontalGrid from '../../../../../../components/Grid/Horizontal';
-import HorizontalTV from '../../../../../TV/components/HorizontalTV';
+import HorizontalGrid from '../../../../../../components/Grid/Horizontal/Default';
+import VerticalTVShowPoster from '../../../../../TV/components/Poster/Vertical';
 import { TVProps } from './types';
 
-const TV = (props: TVProps): ReactElement => {
-  const { colorMode } = useColorMode();
+const TV = ({ query, shows = [], total = 0 }: TVProps): ReactElement => {
   const [isSm] = useMediaQuery('(max-width: 600px)');
 
   const color = useSelector((state) => state.user.ui.theme.color);
 
-  const { query, results, total_results, isFetching, isLoading, isError, isSuccess, refetch } = props;
-
   return (
     <HorizontalGrid
-      title={
-        <Text
-          align='left'
-          color={colorMode === 'light' ? 'gray.900' : 'gray.50'}
-          fontSize={['xl', 'xl', '2xl', '2xl', '2xl', '2xl']}
-          fontWeight='semibold'
-          textTransform='capitalize'
-        >
-          {`Found ${total_results || 0} TV show${
-            total_results ? (total_results === 0 || total_results > 1 ? 's' : '') : ''
-          } with "${query}"`}
-        </Text>
-      }
+      title={`Found ${total} TV show${total === 0 || total > 1 ? 's' : ''} with "${query}"`}
       footer={
-        (total_results || 0) > 20 ? (
-          <Link
-            to={{ pathname: '/search', search: qs.stringify({ query, page: 1, mediaType: 'tv' }) }}
-            isFullWidth
-            isDisabled={isFetching || isLoading}
-          >
-            <Button
-              color={color}
-              isFullWidth
-              isDisabled={isFetching || isLoading}
-              onClick={() => refetch()}
-              size={isSm ? 'sm' : 'md'}
-              variant='text'
-            >
-              {`View all ${total_results || 0} TV show${
-                total_results ? (total_results === 0 || total_results > 1 ? 's' : '') : ''
-              } with "${query}"`}
+        total > 20 ? (
+          <Link to={{ pathname: '/search/tv', search: qs.stringify({ query }) }} isFullWidth>
+            <Button color={color} isFullWidth size={isSm ? 'sm' : 'md'} variant='text'>
+              {`View all ${total} TV show${total === 0 || total > 1 ? 's' : ''} with "${query}"`}
             </Button>
           </Link>
         ) : undefined
       }
-      isLoading={isFetching || isLoading}
     >
-      <HorizontalTV isError={isError} isSuccess={isSuccess} isLoading={isFetching || isLoading} tv={results || []} />
+      {shows.map((show: PartialTV) => (
+        <VerticalTVShowPoster key={show.id} width={['185px', '205px', '230px']} show={show} isLoading={false} />
+      ))}
     </HorizontalGrid>
   );
 };
--- src/pages/Search/components/All/components/TV/types.ts
+++ src/pages/Search/components/All/components/TV/types.ts
@@ -2,11 +2,6 @@ import { PartialTV } from '../../../../../../common/types/tv';
 
 export type TVProps = {
   query: string;
-  results?: PartialTV[];
-  total_results?: number;
-  isFetching: boolean;
-  isLoading: boolean;
-  isError: boolean;
-  isSuccess: boolean;
-  refetch: () => void;
+  shows?: PartialTV[];
+  total?: number;
 };
