4562043bcfe153c9fafac72bcf1286445bfa833e
David Scicluna <dave_nostra@hotmail.com>
2022-01-23 19:30:21 +0100

    Fix: Search People

--- src/pages/Search/components/All/components/People/index.tsx
+++ src/pages/Search/components/All/components/People/index.tsx
@@ -1,68 +1,37 @@
 import { ReactElement } from 'react';
 
-import { useColorMode, useMediaQuery, Text } from '@chakra-ui/react';
+import { useMediaQuery } from '@chakra-ui/react';
 import qs from 'query-string';
 
 import { useSelector } from '../../../../../../common/hooks';
+import { PartialPerson } from '../../../../../../common/types/person';
 import Button from '../../../../../../components/Clickable/Button';
 import Link from '../../../../../../components/Clickable/Link';
-import HorizontalGrid from '../../../../../../components/Grid/Horizontal';
-import HorizontalPeople from '../../../../../People/components/HorizontalPeople';
+import HorizontalGrid from '../../../../../../components/Grid/Horizontal/Default';
+import VerticalPersonPoster from '../../../../../People/components/Poster/Vertical';
 import { PeopleProps } from './types';
 
-const People = (props: PeopleProps): ReactElement => {
-  const { colorMode } = useColorMode();
+const People = ({ query, people = [], total = 0 }: PeopleProps): ReactElement => {
   const [isSm] = useMediaQuery('(max-width: 600px)');
 
   const color = useSelector((state) => state.user.ui.theme.color);
 
-  const { query, results, total_results, isFetching, isLoading, isError, isSuccess, refetch } = props;
-
   return (
     <HorizontalGrid
-      title={
-        <Text
-          align='left'
-          color={colorMode === 'light' ? 'gray.900' : 'gray.50'}
-          fontSize={['xl', 'xl', '2xl', '2xl', '2xl', '2xl']}
-          fontWeight='semibold'
-          textTransform='capitalize'
-        >
-          {`Found ${total_results || 0} ${
-            total_results ? (total_results === 0 || total_results > 1 ? 'people' : 'person') : ''
-          } with "${query}"`}
-        </Text>
-      }
+      title={`Found ${total || 0} ${total === 0 || total > 1 ? 'people' : 'person'} with "${query}"`}
       footer={
-        (total_results || 0) > 20 ? (
-          <Link
-            to={{ pathname: '/search', search: qs.stringify({ query, page: 1, mediaType: 'person' }) }}
-            isFullWidth
-            isDisabled={isFetching || isLoading}
-          >
-            <Button
-              color={color}
-              isFullWidth
-              isDisabled={isFetching || isLoading}
-              onClick={() => refetch()}
-              size={isSm ? 'sm' : 'md'}
-              variant='text'
-            >
-              {`View all ${total_results || 0} ${
-                total_results ? (total_results === 0 || total_results > 1 ? 'people' : 'person') : ''
-              } with "${query}"`}
+        total > 20 ? (
+          <Link to={{ pathname: '/search/people', search: qs.stringify({ query }) }} isFullWidth>
+            <Button color={color} isFullWidth size={isSm ? 'sm' : 'md'} variant='text'>
+              {`View all ${total || 0} ${total === 0 || total > 1 ? 'people' : 'person'} with "${query}"`}
             </Button>
           </Link>
         ) : undefined
       }
-      isLoading={isFetching || isLoading}
     >
-      <HorizontalPeople
-        isError={isError}
-        isSuccess={isSuccess}
-        isLoading={isFetching || isLoading}
-        people={results || []}
-      />
+      {people.map((person: PartialPerson) => (
+        <VerticalPersonPoster key={person.id} width={['185px', '205px', '230px']} person={person} isLoading={false} />
+      ))}
     </HorizontalGrid>
   );
 };
--- src/pages/Search/components/All/components/People/types.ts
+++ src/pages/Search/components/All/components/People/types.ts
@@ -2,11 +2,6 @@ import { PartialPerson } from '../../../../../../common/types/person';
 
 export type PeopleProps = {
   query: string;
-  results?: PartialPerson[];
-  total_results?: number;
-  isFetching: boolean;
-  isLoading: boolean;
-  isError: boolean;
-  isSuccess: boolean;
-  refetch: () => void;
+  people?: PartialPerson[];
+  total?: number;
 };
