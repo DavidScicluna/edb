eb2211ec5bb81147eff8ee9a4f79c2492dc22eef
David Scicluna <dave_nostra@hotmail.com>
2022-01-23 19:30:32 +0100

    Fix: Search All

--- src/pages/Search/components/All/index.tsx
+++ src/pages/Search/components/All/index.tsx
@@ -2,65 +2,92 @@ import { ReactElement } from 'react';
 
 import { VStack, Fade } from '@chakra-ui/react';
 
+import Collections from './components/Collections';
+import Companies from './components/Companies';
 import Movies from './components/Movies';
 import People from './components/People';
 import TV from './components/TV';
 import { AllProps } from './types';
 
-const All = ({ query, movies, tv, people }: AllProps): ReactElement => {
+const All = (props: AllProps): ReactElement => {
+  const { query, moviesQuery, tvQuery, peopleQuery, collectionsQuery, companiesQuery } = props;
+
   return (
-    <VStack width='100%' spacing={6}>
+    <VStack width='100%' spacing={4}>
       {/* Movies */}
       <Fade
-        in={(movies.data?.pages[movies.data?.pages.length - 1]?.results.length || 0) > 0}
+        in={
+          moviesQuery.isSuccess &&
+          (moviesQuery.data?.pages[moviesQuery.data?.pages.length - 1]?.results || []).length > 0
+        }
         unmountOnExit
         style={{ width: '100%' }}
       >
         <Movies
           query={query}
-          results={movies.data?.pages[movies.data?.pages.length - 1].results || []}
-          total_results={movies.data?.pages[movies.data?.pages.length - 1].total_results || 0}
-          isFetching={movies.isFetching}
-          isLoading={movies.isLoading}
-          isError={movies.isError}
-          isSuccess={movies.isSuccess}
-          refetch={movies.refetch}
+          movies={moviesQuery.data?.pages[moviesQuery.data?.pages.length - 1]?.results || []}
+          total={moviesQuery.data?.pages[moviesQuery.data?.pages.length - 1]?.total_results || 0}
         />
       </Fade>
 
-      {/* TV */}
+      {/* TV Shows */}
       <Fade
-        in={(tv.data?.pages[tv.data?.pages.length - 1]?.results.length || 0) > 0}
+        in={tvQuery.isSuccess && (tvQuery.data?.pages[tvQuery.data?.pages.length - 1]?.results || []).length > 0}
         unmountOnExit
         style={{ width: '100%' }}
       >
         <TV
           query={query}
-          results={tv.data?.pages[tv.data?.pages.length - 1].results || []}
-          total_results={tv.data?.pages[tv.data?.pages.length - 1].total_results || 0}
-          isFetching={tv.isFetching}
-          isLoading={tv.isLoading}
-          isError={tv.isError}
-          isSuccess={tv.isSuccess}
-          refetch={tv.refetch}
+          shows={tvQuery.data?.pages[tvQuery.data?.pages.length - 1]?.results || []}
+          total={tvQuery.data?.pages[tvQuery.data?.pages.length - 1]?.total_results || 0}
         />
       </Fade>
 
       {/* People */}
       <Fade
-        in={(people.data?.pages[people.data?.pages.length - 1]?.results.length || 0) > 0}
+        in={
+          peopleQuery.isSuccess &&
+          (peopleQuery.data?.pages[peopleQuery.data?.pages.length - 1]?.results || []).length > 0
+        }
         unmountOnExit
         style={{ width: '100%' }}
       >
         <People
           query={query}
-          results={people.data?.pages[people.data?.pages.length - 1].results || []}
-          total_results={people.data?.pages[people.data?.pages.length - 1].total_results || 0}
-          isFetching={people.isFetching}
-          isLoading={people.isLoading}
-          isError={people.isError}
-          isSuccess={people.isSuccess}
-          refetch={people.refetch}
+          people={peopleQuery.data?.pages[peopleQuery.data?.pages.length - 1]?.results || []}
+          total={peopleQuery.data?.pages[peopleQuery.data?.pages.length - 1]?.total_results || 0}
+        />
+      </Fade>
+
+      {/* Collections */}
+      <Fade
+        in={
+          collectionsQuery.isSuccess &&
+          (collectionsQuery.data?.pages[collectionsQuery.data?.pages.length - 1]?.results || []).length > 0
+        }
+        unmountOnExit
+        style={{ width: '100%' }}
+      >
+        <Collections
+          query={query}
+          collections={collectionsQuery.data?.pages[collectionsQuery.data?.pages.length - 1]?.results || []}
+          total={collectionsQuery.data?.pages[collectionsQuery.data?.pages.length - 1]?.total_results || 0}
+        />
+      </Fade>
+
+      {/* Companies */}
+      <Fade
+        in={
+          companiesQuery.isSuccess &&
+          (companiesQuery.data?.pages[companiesQuery.data?.pages.length - 1]?.results || []).length > 0
+        }
+        unmountOnExit
+        style={{ width: '100%' }}
+      >
+        <Companies
+          query={query}
+          companies={companiesQuery.data?.pages[companiesQuery.data?.pages.length - 1]?.results || []}
+          total={companiesQuery.data?.pages[companiesQuery.data?.pages.length - 1]?.total_results || 0}
         />
       </Fade>
     </VStack>
--- src/pages/Search/components/All/types.ts
+++ src/pages/Search/components/All/types.ts
@@ -1,13 +1,16 @@
 import { UseInfiniteQueryResult } from 'react-query';
 
-import { Response } from '../../../../common/types';
+import { Company, Response } from '../../../../common/types';
 import { PartialMovie } from '../../../../common/types/movie';
 import { PartialPerson } from '../../../../common/types/person';
 import { PartialTV } from '../../../../common/types/tv';
+import { Collection } from '../../types';
 
 export type AllProps = {
   query: string;
-  movies: UseInfiniteQueryResult<Response<PartialMovie[]>, unknown>;
-  tv: UseInfiniteQueryResult<Response<PartialTV[]>, unknown>;
-  people: UseInfiniteQueryResult<Response<PartialPerson[]>, unknown>;
+  moviesQuery: UseInfiniteQueryResult<Response<PartialMovie[]>, unknown>;
+  tvQuery: UseInfiniteQueryResult<Response<PartialTV[]>, unknown>;
+  peopleQuery: UseInfiniteQueryResult<Response<PartialPerson[]>, unknown>;
+  companiesQuery: UseInfiniteQueryResult<Response<Company[]>, unknown>;
+  collectionsQuery: UseInfiniteQueryResult<Response<Collection[]>, unknown>;
 };
