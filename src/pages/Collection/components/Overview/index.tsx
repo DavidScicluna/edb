7fea56877b9541b4faa63c6c69a276e250b834b1
David Scicluna <dave_nostra@hotmail.com>
2022-01-27 19:34:36 +0100

    Feature: Collection Overview

---
+++ src/pages/Collection/components/Overview/index.tsx
@@ -0,0 +1,59 @@
+import React, { ReactElement, useState } from 'react';
+
+import { useDisclosure, VStack } from '@chakra-ui/react';
+
+import MediaViewer from '../../../../components/MediaViewer';
+import { MediaViewerProps, MediaViewerType } from '../../../../components/MediaViewer/types';
+import Overview from './components/Overview';
+import Photos from './components/Photos';
+import { OverviewTabProps } from './types';
+
+const OverviewTab = ({ collectionQuery, imagesQuery }: OverviewTabProps): ReactElement => {
+  const { isOpen: isMediaViewerOpen, onOpen: onMediaViewerOpen, onClose: onMediaViewerClose } = useDisclosure();
+
+  const [selectedAsset, setSelectedAsset] = useState<MediaViewerProps['selected']>();
+
+  const handleImageClick = (asset: string, type: MediaViewerType): void => {
+    setSelectedAsset({ type, asset: asset });
+    onMediaViewerOpen();
+  };
+
+  return (
+    <>
+      <VStack width='100%' spacing={2}>
+        {collectionQuery.data?.overview || collectionQuery.isFetching || collectionQuery.isLoading ? (
+          <Overview
+            overview={collectionQuery.data?.overview || ''}
+            isLoading={collectionQuery.isFetching || collectionQuery.isLoading}
+          />
+        ) : null}
+
+        <Photos
+          data={{
+            photos: imagesQuery.data?.posters || [],
+            backdrops: imagesQuery.data?.backdrops || []
+          }}
+          isError={imagesQuery.isError}
+          isSuccess={imagesQuery.isSuccess}
+          isLoading={imagesQuery.isFetching || imagesQuery.isLoading}
+          onClickImage={handleImageClick}
+        />
+      </VStack>
+
+      {imagesQuery.isSuccess ? (
+        <MediaViewer
+          isOpen={isMediaViewerOpen}
+          selected={{
+            type: selectedAsset?.type,
+            asset: selectedAsset?.asset || ''
+          }}
+          photos={[...(imagesQuery.data?.posters || [])]}
+          backdrops={[...(imagesQuery.data?.backdrops || [])]}
+          onClose={onMediaViewerClose}
+        />
+      ) : null}
+    </>
+  );
+};
+
+export default OverviewTab;
