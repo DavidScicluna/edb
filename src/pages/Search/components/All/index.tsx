6736e6c77bcc94c6b4a3238f80916e6dc39e7660
David Scicluna <dave_nostra@hotmail.com>
2022-01-27 19:41:56 +0100

    Fix: Search All MediaTypes

--- src/pages/Search/components/All/index.tsx
+++ src/pages/Search/components/All/index.tsx
@@ -1,95 +1,107 @@
 import { ReactElement } from 'react';
 
 import { VStack, Fade } from '@chakra-ui/react';
+import { useLocation } from 'react-router-dom';
 
-import Collections from './components/Collections';
+import HorizontalSearchCollections from './components/Collections/Horizontal';
+import VerticalSearchCollections from './components/Collections/Vertical';
 import Companies from './components/Companies';
-import Movies from './components/Movies';
-import People from './components/People';
-import TV from './components/TV';
+import HorizontalSearchMovies from './components/Movies/Horizontal';
+import VerticalSearchMovies from './components/Movies/Vertical';
+import HorizontalSearchPeople from './components/People/Horizontal';
+import VerticalSearchPeople from './components/People/Vertical';
+import HorizontalSearchTV from './components/TV/Horizontal';
+import VerticalSearchTV from './components/TV/Vertical';
 import { AllProps } from './types';
 
 const All = (props: AllProps): ReactElement => {
-  const { query, moviesQuery, tvQuery, peopleQuery, collectionsQuery, companiesQuery } = props;
+  const location = useLocation();
+
+  const {
+    query,
+    searchTypes,
+    movies,
+    moviesQuery,
+    shows,
+    showsQuery,
+    people,
+    peopleQuery,
+    collections,
+    collectionsQuery
+    // companies,
+    // companiesQuery
+  } = props;
 
   return (
     <VStack width='100%' spacing={4}>
       {/* Movies */}
       <Fade
         in={
-          moviesQuery.isSuccess &&
-          (moviesQuery.data?.pages[moviesQuery.data?.pages.length - 1]?.results || []).length > 0
+          (movies?.total_results || 0) > 0 && (searchTypes.length === 0 || searchTypes.some((type) => type === 'movie'))
         }
         unmountOnExit
         style={{ width: '100%' }}
       >
-        <Movies
-          query={query}
-          movies={moviesQuery.data?.pages[moviesQuery.data?.pages.length - 1]?.results || []}
-          total={moviesQuery.data?.pages[moviesQuery.data?.pages.length - 1]?.total_results || 0}
-        />
+        {searchTypes.length === 1 || (location.hash && location.hash === '#movie') ? (
+          <VerticalSearchMovies query={query} movies={movies} moviesQuery={moviesQuery} />
+        ) : (
+          <HorizontalSearchMovies query={query} movies={movies?.results || []} total={movies?.total_results || 0} />
+        )}
       </Fade>
 
       {/* TV Shows */}
       <Fade
-        in={tvQuery.isSuccess && (tvQuery.data?.pages[tvQuery.data?.pages.length - 1]?.results || []).length > 0}
+        in={(shows?.total_results || 0) > 0 && (searchTypes.length === 0 || searchTypes.some((type) => type === 'tv'))}
         unmountOnExit
         style={{ width: '100%' }}
       >
-        <TV
-          query={query}
-          shows={tvQuery.data?.pages[tvQuery.data?.pages.length - 1]?.results || []}
-          total={tvQuery.data?.pages[tvQuery.data?.pages.length - 1]?.total_results || 0}
-        />
+        {searchTypes.length === 1 || (location.hash && location.hash === '#tv') ? (
+          <VerticalSearchTV query={query} shows={shows} showsQuery={showsQuery} />
+        ) : (
+          <HorizontalSearchTV query={query} shows={shows?.results || []} total={shows?.total_results || 0} />
+        )}
       </Fade>
 
       {/* People */}
       <Fade
         in={
-          peopleQuery.isSuccess &&
-          (peopleQuery.data?.pages[peopleQuery.data?.pages.length - 1]?.results || []).length > 0
+          (people?.total_results || 0) > 0 &&
+          (searchTypes.length === 0 || searchTypes.some((type) => type === 'person'))
         }
         unmountOnExit
         style={{ width: '100%' }}
       >
-        <People
-          query={query}
-          people={peopleQuery.data?.pages[peopleQuery.data?.pages.length - 1]?.results || []}
-          total={peopleQuery.data?.pages[peopleQuery.data?.pages.length - 1]?.total_results || 0}
-        />
+        {searchTypes.length === 1 || (location.hash && location.hash === '#person') ? (
+          <VerticalSearchPeople query={query} people={people} peopleQuery={peopleQuery} />
+        ) : (
+          <HorizontalSearchPeople query={query} people={people?.results || []} total={people?.total_results || 0} />
+        )}
       </Fade>
 
       {/* Collections */}
       <Fade
         in={
-          collectionsQuery.isSuccess &&
-          (collectionsQuery.data?.pages[collectionsQuery.data?.pages.length - 1]?.results || []).length > 0
+          (collections?.total_results || 0) > 0 &&
+          (searchTypes.length === 0 || searchTypes.some((type) => type === 'collection'))
         }
         unmountOnExit
         style={{ width: '100%' }}
       >
-        <Collections
-          query={query}
-          collections={collectionsQuery.data?.pages[collectionsQuery.data?.pages.length - 1]?.results || []}
-          total={collectionsQuery.data?.pages[collectionsQuery.data?.pages.length - 1]?.total_results || 0}
-        />
+        {searchTypes.length === 1 || (location.hash && location.hash === '#collection') ? (
+          <VerticalSearchCollections query={query} collections={collections} collectionsQuery={collectionsQuery} />
+        ) : (
+          <HorizontalSearchCollections
+            query={query}
+            collections={collections?.results || []}
+            total={collections?.total_results || 0}
+          />
+        )}
       </Fade>
 
       {/* Companies */}
-      <Fade
-        in={
-          companiesQuery.isSuccess &&
-          (companiesQuery.data?.pages[companiesQuery.data?.pages.length - 1]?.results || []).length > 0
-        }
-        unmountOnExit
-        style={{ width: '100%' }}
-      >
-        <Companies
-          query={query}
-          companies={companiesQuery.data?.pages[companiesQuery.data?.pages.length - 1]?.results || []}
-          total={companiesQuery.data?.pages[companiesQuery.data?.pages.length - 1]?.total_results || 0}
-        />
-      </Fade>
+      {/* <Fade in={(companies?.total_results || 0) > 0 &&   (searchTypes.length === 0 || searchTypes.every((type) => type === 'collection'))} unmountOnExit style={{ width: '100%' }}>
+        <Companies query={query} companies={companies?.results || []} total={companies?.total_results || 0} />
+      </Fade> */}
     </VStack>
   );
 };
--- src/pages/Search/components/All/types.ts
+++ src/pages/Search/components/All/types.ts
@@ -4,13 +4,20 @@ import { Company, Response } from '../../../../common/types';
 import { PartialMovie } from '../../../../common/types/movie';
 import { PartialPerson } from '../../../../common/types/person';
 import { PartialTV } from '../../../../common/types/tv';
+import { SearchType } from '../../../../store/slices/User/types';
 import { Collection } from '../../types';
 
 export type AllProps = {
   query: string;
-  moviesQuery: UseInfiniteQueryResult<Response<PartialMovie[]>, unknown>;
-  tvQuery: UseInfiniteQueryResult<Response<PartialTV[]>, unknown>;
-  peopleQuery: UseInfiniteQueryResult<Response<PartialPerson[]>, unknown>;
-  companiesQuery: UseInfiniteQueryResult<Response<Company[]>, unknown>;
-  collectionsQuery: UseInfiniteQueryResult<Response<Collection[]>, unknown>;
+  searchTypes: SearchType[];
+  movies?: Response<PartialMovie[]>;
+  moviesQuery: UseInfiniteQueryResult<Response<PartialMovie[]>>;
+  shows?: Response<PartialTV[]>;
+  showsQuery: UseInfiniteQueryResult<Response<PartialTV[]>>;
+  people?: Response<PartialPerson[]>;
+  peopleQuery: UseInfiniteQueryResult<Response<PartialPerson[]>>;
+  companies?: Response<Company[]>;
+  companiesQuery: UseInfiniteQueryResult<Response<Company[]>>;
+  collections?: Response<Collection[]>;
+  collectionsQuery: UseInfiniteQueryResult<Response<Collection[]>>;
 };
