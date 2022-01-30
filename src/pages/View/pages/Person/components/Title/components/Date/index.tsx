115d3ee27289323e38a32973596155ce0a8d01c0
David Scicluna <dave_nostra@hotmail.com>
2022-01-30 20:38:20 +0100

    Feature: Person Title Date

---
+++ src/pages/View/pages/Person/components/Title/components/Date/index.tsx
@@ -0,0 +1,45 @@
+import { ReactElement } from 'react';
+
+import { useColorMode, useMediaQuery, Text, ScaleFade } from '@chakra-ui/react';
+import _ from 'lodash';
+import moment from 'moment';
+
+import SkeletonText from '../../../../../../../../components/Skeleton/Text';
+import { DateProps } from './types';
+
+const PersonDate = (props: DateProps): ReactElement => {
+  const { colorMode } = useColorMode();
+  const [isSm] = useMediaQuery('(max-width: 600px)');
+
+  const { birthday, place_of_birth, deathday, isLoading = false } = props;
+
+  const handleReturnDates = (): string => {
+    const birthDate = moment(birthday || '', 'YYYY-MM-DD').format('LL');
+    const birthPlace = place_of_birth ? `in ${place_of_birth}` : undefined;
+    const deathDate = deathday ? `- ${moment(deathday || '', 'YYYY-MM-DD').format('LL')}` : undefined;
+    const yearsOld = deathday
+      ? `(${moment(deathday || new Date()).diff(moment(birthday || '', 'YYYY-MM-DD'), 'years')} years old)`
+      : undefined;
+
+    return ['Born', 'on', birthDate, birthPlace, deathDate, yearsOld].filter((string) => string).join(' ');
+  };
+
+  return (
+    <ScaleFade in={isLoading || !_.isEmpty(birthday) || !_.isNil(birthday)} unmountOnExit style={{ width: '100%' }}>
+      <SkeletonText width='100%' fontSize='sm' isLoaded={!isLoading}>
+        <Text
+          align='left'
+          color={colorMode === 'light' ? 'gray.400' : 'gray.500'}
+          fontSize={isSm ? 'xs' : 'sm'}
+          isTruncated
+          overflow='hidden'
+          whiteSpace='nowrap'
+        >
+          {!isLoading ? handleReturnDates() : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,'}
+        </Text>
+      </SkeletonText>
+    </ScaleFade>
+  );
+};
+
+export default PersonDate;
---
+++ src/pages/View/pages/Person/components/Title/components/Date/types.ts
@@ -0,0 +1,8 @@
+import { FullPerson } from '../../../../../../../../common/types/person';
+
+export type DateProps = {
+  birthday?: FullPerson['birthday'];
+  place_of_birth?: FullPerson['place_of_birth'];
+  deathday?: FullPerson['deathday'];
+  isLoading?: boolean;
+};
