34bfd3b1e79d302b7a9181a3f441d6cce168b5b3
David Scicluna <dave_nostra@hotmail.com>
2022-01-31 02:52:13 +0100

    Fix: Rating size Type

--- src/components/Rating/index.tsx
+++ src/components/Rating/index.tsx
@@ -12,7 +12,7 @@ const Rating = (props: RatingProps): ReactElement => {
   const theme = useTheme<Theme>();
   const { colorMode } = useColorMode();
 
-  const { children, isLoading = false, size = 'md' } = props;
+  const { children, inView = true, isLoading = false, size = 'md' } = props;
 
   /**
    * This method will return the appropriate font-size from theme depending on size prop
@@ -58,24 +58,27 @@ const Rating = (props: RatingProps): ReactElement => {
           fontSize: handleReturnIconSize()
         }}
       />
-      <SkeletonText isLoaded={!isLoading} ml={0.5}>
-        <VStack spacing={0.25}>
-          <Text
-            color={colorMode === 'light' ? 'gray.900' : 'gray.50'}
-            fontSize={size}
-            fontWeight='medium'
-            lineHeight='normal'
-          >
-            {children ? _.round(Number(children)) : 'N/A'}
-          </Text>
-          {/* TODO: Find a way to better display count */}
-          {/* {count ? (
+      {inView || isLoading ? (
+        <SkeletonText fontSize={size} isLoaded={!isLoading} ml={0.5}>
+          <VStack spacing={0.25}>
+            <Text
+              align='left'
+              fontSize={size}
+              fontWeight='semibold'
+              color={colorMode === 'light' ? 'gray.900' : 'gray.50'}
+              whiteSpace='nowrap'
+            >
+              {children && !isLoading ? _.round(Number(children)) : 'N/A'}
+            </Text>
+            {/* TODO: Find a way to better display count */}
+            {/* {count ? (
             <Text color={colorMode === 'light' ? 'gray.400' : 'gray.500'} fontSize={handleReturnCountSize()}>
               {count}
             </Text>
           ) : null} */}
-        </VStack>
-      </SkeletonText>
+          </VStack>
+        </SkeletonText>
+      ) : null}
     </Center>
   );
 };
--- src/components/Rating/types.ts
+++ src/components/Rating/types.ts
@@ -1,4 +1,6 @@
-type Size = 'sm' | 'md' | 'lg' | 'xl' | string;
+import { FontSizes } from '../../theme/types';
+
+type Size = keyof Omit<FontSizes, '4xl' | '5xl' | '6xl' | '7xl' | '8xl' | '9xl'>;
 
 export type Rating = number | string | undefined | null;
 
@@ -7,6 +9,7 @@ export type Count = number | undefined | null;
 export type RatingProps = {
   children?: Rating;
   count?: Count;
+  inView?: boolean;
   isLoading?: boolean;
   size?: Size;
 } & Omit<Rating, 'rating'>;
