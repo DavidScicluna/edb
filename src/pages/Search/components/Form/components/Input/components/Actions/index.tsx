c0bd6e8e503217b6d1d37cfa19ea0c1f20a0e51c
David Scicluna <dave_nostra@hotmail.com>
2022-02-03 19:11:41 +0100

    Fix: Search Form Input Actions

--- src/pages/Search/components/Form/components/Input/components/Actions/index.tsx
+++ src/pages/Search/components/Form/components/Input/components/Actions/index.tsx
@@ -1,21 +1,17 @@
 import { ReactElement } from 'react';
 
-import { useBoolean, HStack, Fade } from '@chakra-ui/react';
+import { HStack, Fade } from '@chakra-ui/react';
 import { ClearOutlined as ClearOutlinedIcon, SendOutlined as SendOutlinedIcon } from '@material-ui/icons';
 import { useElementSize } from 'usehooks-ts';
 
 import IconButton from '../../../../../../../../components/Clickable/IconButton';
 import Divider from '../../../../../../../../components/Divider';
-import Tooltip from '../../../../../../../../components/Tooltip';
 import { ActionsProps } from './types';
 
 const Actions = (props: ActionsProps): ReactElement => {
   const [ref, { height }] = useElementSize();
 
   const { hasQuery = false, isDisabled = false, onClear, onSubmit } = props;
-
-  const [isHoveringClear, setIsHoveringClear] = useBoolean();
-  const [isHoveringSubmit, setIsHoveringSubmit] = useBoolean();
 
   return (
     <HStack
@@ -27,45 +23,26 @@ const Actions = (props: ActionsProps): ReactElement => {
       }
     >
       <Fade in={hasQuery} unmountOnExit>
-        <Tooltip
-          aria-label='Clear search'
-          label='Clear search'
-          isOpen={!isDisabled && isHoveringClear}
-          isDisabled={isDisabled}
-          placement='top'
-        >
-          <IconButton
-            aria-label='Clear search'
-            isDisabled={isDisabled}
-            onClick={() => onClear()}
-            onMouseEnter={() => setIsHoveringClear.on()}
-            onMouseLeave={() => setIsHoveringClear.off()}
-            size='sm'
-            variant='icon'
-          >
-            <ClearOutlinedIcon />
-          </IconButton>
-        </Tooltip>
-      </Fade>
-      <Tooltip
-        aria-label='Submit Search'
-        label='Submit Search'
-        isOpen={!(isDisabled || !hasQuery) && isHoveringSubmit}
-        isDisabled={isDisabled}
-        placement='top'
-      >
         <IconButton
-          aria-label='Submit Search'
-          isDisabled={isDisabled || !hasQuery}
-          onClick={() => onSubmit()}
-          onMouseEnter={() => setIsHoveringSubmit.on()}
-          onMouseLeave={() => setIsHoveringSubmit.off()}
+          aria-label='Clear search'
+          isDisabled={isDisabled}
+          onClick={() => onClear()}
           size='sm'
           variant='icon'
         >
-          <SendOutlinedIcon />
+          <ClearOutlinedIcon />
         </IconButton>
-      </Tooltip>
+      </Fade>
+
+      <IconButton
+        aria-label='Submit Search'
+        isDisabled={isDisabled || !hasQuery}
+        onClick={() => onSubmit()}
+        size='sm'
+        variant='icon'
+      >
+        <SendOutlinedIcon />
+      </IconButton>
     </HStack>
   );
 };
