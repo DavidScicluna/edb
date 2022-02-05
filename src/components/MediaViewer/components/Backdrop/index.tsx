79737de25d3efedc4744a02cd3a048ced64b453a
David Scicluna <dave_nostra@hotmail.com>
2022-02-05 15:47:54 +0100

    Feature: Backdrop when hovering Toolkit

---
+++ src/components/MediaViewer/components/Backdrop/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { useTheme, useColorMode, Box, Fade } from '@chakra-ui/react';
+
+import { Theme } from '../../../../theme/types';
+import { BackdropProps } from './types';
+
+const Backdrop = ({ isHovering = false }: BackdropProps) => {
+  const theme = useTheme<Theme>();
+  const { colorMode } = useColorMode();
+
+  return (
+    <Box
+      as={Fade}
+      in={isHovering}
+      width='100%'
+      height='100vh'
+      position='absolute'
+      zIndex={3}
+      unmountOnExit
+      sx={{
+        backdropFilter: 'blur(12px)',
+        backgroundColor: colorMode === 'light' ? 'rgba(255, 255, 255, 0.25)' : 'rgba(0, 0, 0, 0.5)',
+        transition: `${theme.transition.duration.faster} ${theme.transition.easing['ease-in-out']}`
+      }}
+    />
+  );
+};
+
+export default Backdrop;
