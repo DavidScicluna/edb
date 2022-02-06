4e33365c77dd9b4eccde9dd37a559127d837214b
David Scicluna <dave_nostra@hotmail.com>
2022-02-06 18:27:32 +0100

    Feature: Movie OverviewTab Collection

---
+++ src/pages/View/pages/Movie/components/OverviewTab/components/Collection/index.tsx
@@ -0,0 +1,30 @@
+import React, { ReactElement } from 'react';
+
+import _ from 'lodash';
+
+import Panel from '../../../../../../../../components/Panel';
+import HorizontalCollectionPoster from '../../../../../../../Search/components/All/components/Collections/components/Poster/Horizontal';
+import { CollectionProps } from './types';
+
+const Collection = ({ collection }: CollectionProps): ReactElement => {
+  const handleReturnFormattedTitle = (name: string): string => {
+    return _.compact(name.toLowerCase().replace('collection', '').split(' ')).join(' ');
+  };
+
+  return (
+    <Panel isFullWidth>
+      {{
+        header: {
+          title: `Part of the ${
+            collection?.name
+              ? `"${_.capitalize(handleReturnFormattedTitle(collection.name))}" Collection`
+              : 'Collection'
+          }`
+        },
+        body: <HorizontalCollectionPoster collection={collection} isLoading={false} />
+      }}
+    </Panel>
+  );
+};
+
+export default Collection;
