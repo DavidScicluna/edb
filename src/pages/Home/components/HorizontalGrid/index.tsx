d58082d3952d286d77f6a38d68dcbed21a2e4848
David Scicluna <dave_nostra@hotmail.com>
2022-02-06 20:02:23 +0100

    Fix: Home HorizontalGrid Width

--- src/pages/Home/components/HorizontalGrid/index.tsx
+++ src/pages/Home/components/HorizontalGrid/index.tsx
@@ -17,7 +17,7 @@ import VerticalPersonPoster from '../../../People/components/Poster/Vertical';
 import VerticalTVShowPoster from '../../../TV/components/Poster/Vertical';
 import { HomeHorizontalGridProps } from './types';
 
-const widths = ['185px', '205px', '230px'];
+const width = ['185px', '205px', '230px'];
 
 const HomeHorizontalGrid = (props: HomeHorizontalGridProps): ReactElement => {
   const [isSm] = useMediaQuery('(max-width: 600px)');
@@ -103,10 +103,10 @@ const HomeHorizontalGrid = (props: HomeHorizontalGridProps): ReactElement => {
           />
         ) : !isLoading.movie && isSuccess.movie && data.movie && data.movie.length > 0 ? (
           data.movie.map((movie: PartialMovie) => (
-            <VerticalMoviePoster key={movie.id} width={widths} movie={movie} isLoading={false} />
+            <VerticalMoviePoster key={movie.id} width={width} movie={movie} isLoading={false} />
           ))
         ) : (
-          _.range(0, 20).map((_dummy, index: number) => <VerticalMoviePoster key={index} width={widths} isLoading />)
+          _.range(0, 20).map((_dummy, index: number) => <VerticalMoviePoster key={index} width={width} isLoading />)
         )}
       </>
 
@@ -122,10 +122,10 @@ const HomeHorizontalGrid = (props: HomeHorizontalGridProps): ReactElement => {
           <Empty label={`${title ? `"${title}"` : ''} TV Shows list is currently empty!`} variant='transparent' />
         ) : !isLoading.tv && isSuccess.tv && data.tv && data.tv.length > 0 ? (
           data.tv.map((show: PartialTV) => (
-            <VerticalTVShowPoster key={show.id} width={widths} show={show} isLoading={false} />
+            <VerticalTVShowPoster key={show.id} width={width} show={show} isLoading={false} />
           ))
         ) : (
-          _.range(0, 20).map((_dummy, index: number) => <VerticalTVShowPoster key={index} width={widths} isLoading />)
+          _.range(0, 20).map((_dummy, index: number) => <VerticalTVShowPoster key={index} width={width} isLoading />)
         )}
       </>
 
@@ -141,10 +141,10 @@ const HomeHorizontalGrid = (props: HomeHorizontalGridProps): ReactElement => {
           <Empty label={`${title ? `"${title}"` : ''} People list is currently empty!`} variant='transparent' />
         ) : !isLoading.person && isSuccess.person && data.person && data.person.length > 0 ? (
           data.person.map((person: PartialPerson) => (
-            <VerticalPersonPoster key={person.id} width={widths} person={person} isLoading={false} />
+            <VerticalPersonPoster key={person.id} width={width} person={person} isLoading={false} />
           ))
         ) : (
-          _.range(0, 20).map((_dummy, index: number) => <VerticalPersonPoster key={index} width={widths} isLoading />)
+          _.range(0, 20).map((_dummy, index: number) => <VerticalPersonPoster key={index} width={width} isLoading />)
         )}
       </>
     </HorizontalTabbedGrid>
