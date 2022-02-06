02425cb7358ad9434eac5147a9b3854e0bb0291c
David Scicluna <dave_nostra@hotmail.com>
2022-02-06 20:01:47 +0100

    Removed Unused View Media components

--- src/pages/View/components/Media/components/Photos/types.ts
+++
@@ -1,7 +0,0 @@
-import { MediaProps } from '../../types';
-
-export type PhotosProps = {
-  isError?: boolean;
-  isSuccess?: boolean;
-  isLoading?: boolean;
-} & Omit<MediaProps, 'isError' | 'isSuccess' | 'isLoading' | 'backdrops' | 'videos'>;
--- src/pages/View/components/Media/components/Videos/components/Video/types.ts
+++
@@ -1,7 +0,0 @@
-import { Video } from '../../../../../../../../common/types';
-import { VideosProps } from '../../types';
-
-export type VideoProps = {
-  video?: Video | number;
-  isLoading?: boolean;
-} & Omit<VideosProps, 'isError' | 'isSuccess' | 'isLoading' | 'videos'>;
