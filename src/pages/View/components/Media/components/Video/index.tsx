90dd2fc59afe356a3030b9b0fe8616e0b3d6d8b1
David Scicluna <dave_nostra@hotmail.com>
2022-02-06 20:01:21 +0100

    Feature: View Media Video

---
+++ src/pages/View/components/Media/components/Video/index.tsx
@@ -0,0 +1,56 @@
+import { ReactElement } from 'react';
+
+import { Box } from '@chakra-ui/react';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import _ from 'lodash';
+import YouTube, { Options } from 'react-youtube';
+
+import ClickableImage from '../../../../../../components/Clickable/Image';
+import Skeleton from '../../../../../../components/Skeleton';
+import { MediaVideoProps } from './types';
+import './styles.css';
+
+const width = ['185px', '205px', '230px'];
+
+const opts: Options = {
+  playerVars: {
+    autoplay: 0,
+    controls: 0,
+    color: 'white',
+    enablejsapi: 1,
+    disablekb: 1,
+    mute: 1,
+    fs: 0,
+    loop: 1,
+    modestbranding: 1,
+    showinfo: 0
+  }
+};
+
+const MediaVideo = (props: MediaVideoProps): ReactElement => {
+  const { alt, videoId, isLoading = true, onClick } = props;
+
+  return (
+    <Box alt={`${alt ? `"${alt}"` : ''} video`} borderRadius='lg'>
+      <ClickableImage
+        width={width}
+        ratio={1 / 1}
+        borderRadius='lg'
+        isDisabled={isLoading || _.isNil(videoId) || _.isEmpty(videoId)}
+        renderIcon={({ color, fontSize }) => <PlayArrowIcon style={{ color, fontSize }} />}
+        onClick={onClick}
+      >
+        <Skeleton borderRadius='lg' isLoaded={!isLoading}>
+          <YouTube
+            videoId={videoId}
+            className='VideoGalleryFrame'
+            containerClassName='VideoGalleryContainer'
+            opts={opts}
+          />
+        </Skeleton>
+      </ClickableImage>
+    </Box>
+  );
+};
+
+export default MediaVideo;
