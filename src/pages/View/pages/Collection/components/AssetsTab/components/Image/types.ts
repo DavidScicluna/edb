16c70fd23dbe0c9e804d1495688aa62cea3ba649
David Scicluna <dave_nostra@hotmail.com>
2022-02-08 21:03:29 +0100

    Removed Unused Collection folder

--- src/pages/View/pages/Collection/components/AssetsTab/components/Image/types.ts
+++
@@ -1,8 +0,0 @@
-import { Image } from '../../../../../../../../common/types';
-import { AssetsTabProps } from '../../types';
-
-export type AssetImageProps = {
-  file_path?: Image['file_path'];
-  srcSize: [string, string];
-  onClickImage?: (path: string) => void;
-} & Omit<AssetsTabProps, 'images' | 'isError' | 'isSuccess' | 'onClickImage'>;
--- src/pages/View/pages/Collection/components/PartsTab/types.ts
+++
@@ -1,9 +0,0 @@
-import { PartialMovie } from '../../../../../../common/types/movie';
-
-export type PartsTabProps = {
-  name?: string;
-  isError?: boolean;
-  isSuccess?: boolean;
-  isLoading?: boolean;
-  parts: PartialMovie[];
-};
