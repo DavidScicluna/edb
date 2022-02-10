86f8469871b9c5343db5458ccbc75789d19bb738
David Scicluna <dave_nostra@hotmail.com>
2022-02-10 20:10:52 +0100

    Feature: Common Quick View Actions

---
+++ src/containers/Layout/components/Modals/QuickView/components/Actions/types.ts
@@ -0,0 +1,11 @@
+import { MediaType } from '../../../../../../../common/types';
+import { PartialMovie } from '../../../../../../../common/types/movie';
+import { PartialTV } from '../../../../../../../common/types/tv';
+
+export type ActionsProps = {
+  mediaItem?: PartialMovie | PartialTV;
+  mediaType: Omit<MediaType, 'company'>;
+  title?: string;
+  isLoading?: boolean;
+  isError?: boolean;
+};
