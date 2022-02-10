8bb5e8906ed927781495795703bc7033c24f6511
David Scicluna <dave_nostra@hotmail.com>
2022-02-10 16:47:24 +0100

    Removed unused Person PhotosTab
    Since a common Asset component was created

--- src/pages/View/pages/Person/components/PhotosTab/components/Photo/types.ts
+++
@@ -1,8 +0,0 @@
-import { Image } from '../../../../../../../../common/types';
-import { PhotosTabProps } from '../../types';
-
-export type PhotoProps = {
-  file_path?: Image['file_path'];
-  isLoading: boolean;
-  onClickImage?: (path: string) => void;
-} & Omit<PhotosTabProps, 'images' | 'isError' | 'isSuccess' | 'isLoading' | 'onClickImage'>;
