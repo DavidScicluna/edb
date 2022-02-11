8756dfe0067ead9d9d56b238ea36931b30aacd0e
David Scicluna <dave_nostra@hotmail.com>
2022-02-11 14:37:41 +0100

    Removed unused View Movie since it was moved to View pages folder

--- src/pages/View/Movie/components/HomeTab/components/Recommendations/types.ts
+++
@@ -1,9 +0,0 @@
-import { PartialMovie } from '../../../../../../../common/types/movie';
-
-export type RecommendationsProps = {
-  recommendations?: PartialMovie[];
-  title?: string;
-  isError?: boolean;
-  isSuccess?: boolean;
-  isLoading?: boolean;
-};
