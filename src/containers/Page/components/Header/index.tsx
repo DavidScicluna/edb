bb040eab39052843bd866a601d48a2d104ec2584
David Scicluna <dave_nostra@hotmail.com>
2022-02-12 19:09:30 +0100

    Fix: Page Header Responsiveness

--- src/containers/Page/components/Header/index.tsx
+++ src/containers/Page/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement } from 'react';
 
-import { useMediaQuery, VStack, HStack, Center } from '@chakra-ui/react';
+import { useMediaQuery, VStack, Center, Stack } from '@chakra-ui/react';
+import { useElementSize } from 'usehooks-ts';
 
 import Breadcrumbs from './components/Breadcrumbs';
 import Title from './components/Title';
@@ -9,24 +10,19 @@ import { HeaderProps } from './types';
 const Header = ({ title, actions }: HeaderProps): ReactElement => {
   const [isSm] = useMediaQuery('(max-width: 600px)');
 
-  return isSm ? (
-    <VStack width='100%' p={2} spacing={2}>
-      <VStack width='100%' alignItems='flex-start' spacing={0}>
+  const [ref, { width }] = useElementSize();
+
+  return (
+    <Stack width='100%' direction={isSm ? 'column' : 'row'} p={2} spacing={2}>
+      <VStack width={isSm ? '100%' : `calc(100% - ${width + 16}px)`} alignItems='flex-start' spacing={0}>
         <Breadcrumbs />
         {title ? <Title title={title} /> : null}
       </VStack>
 
-      <Center width='100%'>{actions || null}</Center>
-    </VStack>
-  ) : (
-    <HStack width='100%' justifyContent='space-between' p={2}>
-      <VStack width='100%' alignItems='flex-start' spacing={0}>
-        <Breadcrumbs />
-        {title ? <Title title={title} /> : null}
-      </VStack>
-
-      <Center>{actions || null}</Center>
-    </HStack>
+      <Center ref={ref} width={isSm ? '100%' : 'auto'}>
+        {actions || null}
+      </Center>
+    </Stack>
   );
 };
 
