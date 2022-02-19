3e8f67670a5140e036534627497a68c6a3a6fef0
David Scicluna <dave_nostra@hotmail.com>
2022-02-19 19:26:31 +0100

    Fix: Person Credits Header responsiveness

--- src/pages/View/pages/Person/components/CreditsTab/components/MediaItems/components/Header/index.tsx
+++ src/pages/View/pages/Person/components/CreditsTab/components/MediaItems/components/Header/index.tsx
@@ -1,7 +1,7 @@
 import { ReactElement } from 'react';
 import CountUp from 'react-countup';
 
-import { HStack } from '@chakra-ui/react';
+import { HStack, Center } from '@chakra-ui/react';
 
 import { useElementSize } from 'usehooks-ts';
 
@@ -16,32 +16,34 @@ import TabList from '../../../../../../../../../../components/Tabs/components/Ta
 const Header = ({ movies = 0, shows = 0 }: HeaderProps): ReactElement => {
 	const color = useSelector((state) => state.user.ui.theme.color);
 
-	const [ref, { height }] = useElementSize();
+	const [ref, { width, height }] = useElementSize();
 
 	return (
 		<HStack width='100%' divider={<Divider orientation='vertical' height={`${height}px`} mx={2} />} spacing={2}>
-			<TabList color={color}>
-				{[
-					{
-						label: 'Movies',
-						isDisabled: movies === 0,
-						renderRight: ({ isSelected, size }) => (
-							<Badge color={isSelected ? color : 'gray'} isLight={!isSelected} size={size}>
-								<CountUp duration={1} end={movies} />
-							</Badge>
-						)
-					},
-					{
-						label: 'TV Shows',
-						isDisabled: shows === 0,
-						renderRight: ({ isSelected, size }) => (
-							<Badge color={isSelected ? color : 'gray'} isLight={!isSelected} size={size}>
-								<CountUp duration={1} end={shows} />
-							</Badge>
-						)
-					}
-				]}
-			</TabList>
+			<Center width={`calc(100% - ${width + 34}px)`}>
+				<TabList color={color}>
+					{[
+						{
+							label: 'Movies',
+							isDisabled: movies === 0,
+							renderRight: ({ isSelected, size }) => (
+								<Badge color={isSelected ? color : 'gray'} isLight={!isSelected} size={size}>
+									<CountUp duration={1} end={movies} />
+								</Badge>
+							)
+						},
+						{
+							label: 'TV Shows',
+							isDisabled: shows === 0,
+							renderRight: ({ isSelected, size }) => (
+								<Badge color={isSelected ? color : 'gray'} isLight={!isSelected} size={size}>
+									<CountUp duration={1} end={shows} />
+								</Badge>
+							)
+						}
+					]}
+				</TabList>
+			</Center>
 
 			<DisplayMode ref={ref} />
 		</HStack>
