3038ae4604ba61c0559aa9f32003ec86c39d9056
David Scicluna <dave_nostra@hotmail.com>
2022-02-20 18:34:08 +0100

    Fix: Divider transition

--- src/components/Divider/index.tsx
+++ src/components/Divider/index.tsx
@@ -1,13 +1,18 @@
 import { ReactElement } from 'react';
 
-import { ColorMode, useColorMode, Box } from '@chakra-ui/react';
+import { ColorMode, useTheme, useColorMode, Box } from '@chakra-ui/react';
+
+import _ from 'lodash';
 
 import { DividerProps } from './types';
 
+import { Theme } from '../../theme/types';
+
 const Divider = (props: DividerProps): ReactElement => {
+	const theme = useTheme<Theme>();
 	const { colorMode: colorModeHook } = useColorMode();
 
-	const { colorMode: colorModeProp, orientation = 'horizontal', ...rest } = props;
+	const { colorMode: colorModeProp, orientation = 'horizontal', sx, ...rest } = props;
 
 	const colorMode: ColorMode = colorModeProp || colorModeHook;
 
@@ -18,6 +23,14 @@ const Divider = (props: DividerProps): ReactElement => {
 			backgroundColor={colorMode === 'light' ? 'gray.200' : 'gray.700'}
 			{...rest}
 			border='none'
+			sx={{
+				..._.merge(
+					{
+						transition: `${theme.transition.duration.faster} ${theme.transition.easing['ease-out']} !important`
+					},
+					sx
+				)
+			}}
 		/>
 	);
 };
