e0e80b51a44a608dd1608755c25db0b176b5ed9f
David Scicluna <dave_nostra@hotmail.com>
2022-02-26 19:05:57 +0100

    Fix: Arrow styles

--- src/components/HorizontalScroll/components/Arrow/styles.ts
+++ src/components/HorizontalScroll/components/Arrow/styles.ts
@@ -31,97 +31,98 @@ export default (theme: Theme, { isDisabled = false }: StyleArrowProps): ArrowSty
 	},
 	light: {
 		left: {
-			'backgroundColor': 'gray.50',
-			'-moz-background': !isDisabled
+			backgroundColor: 'gray.50',
+
+			MozBackground: !isDisabled
 				? `-moz-linear-gradient(90deg, ${handleConvertHexToRGB(
 						theme.colors.gray[50],
 						1
 				  )} 0%, rgba(0, 0, 0, 0) 100%)`
 				: 'transparent',
-			'-webkit-linear-background': !isDisabled
+			WebkitLinearBackground: !isDisabled
 				? `-webkit-linear-gradient(90deg, ${handleConvertHexToRGB(
 						theme.colors.gray[50],
 						1
 				  )} 0%, rgba(0, 0, 0, 0) 100%)`
 				: 'transparent',
-			'background': !isDisabled
+			background: !isDisabled
 				? `linear-gradient(90deg, ${handleConvertHexToRGB(theme.colors.gray[50], 1)} 0%, rgba(0, 0, 0, 0) 100%)`
 				: 'transparent',
-			'filter': !isDisabled
+			filter: !isDisabled
 				? `progid:DXImageTransform.Microsoft.gradient(startColorstr="${theme.colors.gray[50]}",endColorstr="transparent",GradientType=1)`
 				: 'transparent'
 		},
 		right: {
-			'backgroundColor': 'gray.50',
-			'-moz-background': !isDisabled
+			backgroundColor: 'gray.50',
+			MozBackground: !isDisabled
 				? `-moz-linear-gradient(270deg, ${handleConvertHexToRGB(
 						theme.colors.gray[50],
 						1
 				  )} 0%, rgba(0, 0, 0, 0) 100%)`
 				: 'transparent',
-			'-webkit-linear-background': !isDisabled
+			WebkitLinearBackground: !isDisabled
 				? `-webkit-linear-gradient(270deg, ${handleConvertHexToRGB(
 						theme.colors.gray[50],
 						1
 				  )} 0%, rgba(0, 0, 0, 0) 100%)`
 				: 'transparent',
-			'background': !isDisabled
+			background: !isDisabled
 				? `linear-gradient(270deg, ${handleConvertHexToRGB(
 						theme.colors.gray[50],
 						1
 				  )} 0%, rgba(0, 0, 0, 0) 100%)`
 				: 'transparent',
-			'filter': !isDisabled
+			filter: !isDisabled
 				? `progid:DXImageTransform.Microsoft.gradient(startColorstr="${theme.colors.gray[50]}",endColorstr="transparent",GradientType=1)`
 				: 'transparent'
 		}
 	},
 	dark: {
 		left: {
-			'backgroundColor': 'gray.900',
-			'-moz-background': !isDisabled
+			backgroundColor: 'gray.900',
+			MozBackground: !isDisabled
 				? `-moz-linear-gradient(90deg, ${handleConvertHexToRGB(
 						theme.colors.gray[900],
 						1
 				  )} 0%, rgba(0, 0, 0, 0) 100%)`
 				: 'transparent',
-			'-webkit-linear-background': !isDisabled
+			WebkitLinearBackground: !isDisabled
 				? `-webkit-linear-gradient(90deg, ${handleConvertHexToRGB(
 						theme.colors.gray[900],
 						1
 				  )} 0%, rgba(0, 0, 0, 0) 100%)`
 				: 'transparent',
-			'background': !isDisabled
+			background: !isDisabled
 				? `linear-gradient(90deg, ${handleConvertHexToRGB(
 						theme.colors.gray[900],
 						1
 				  )} 0%, rgba(0, 0, 0, 0) 100%)`
 				: 'transparent',
-			'filter': !isDisabled
+			filter: !isDisabled
 				? `progid:DXImageTransform.Microsoft.gradient(startColorstr="${theme.colors.gray[900]}",endColorstr="transparent",GradientType=1)`
 				: 'transparent'
 		},
 		right: {
-			'backgroundColor': 'gray.900',
-			'-moz-background': !isDisabled
+			backgroundColor: 'gray.900',
+			MozBackground: !isDisabled
 				? `-moz-linear-gradient(270deg, ${handleConvertHexToRGB(
 						theme.colors.gray[900],
 						1
 				  )} 0%, rgba(0, 0, 0, 0) 100%)`
 				: 'transparent',
-			'-webkit-linear-background': !isDisabled
+			WebkitLinearBackground: !isDisabled
 				? `-webkit-linear-gradient(270deg, ${handleConvertHexToRGB(
 						theme.colors.gray[900],
 						1
 				  )} 0%, rgba(0, 0, 0, 0) 100%)`
 				: 'transparent',
-			'background': !isDisabled
+			background: !isDisabled
 				? `linear-gradient(270deg, ${handleConvertHexToRGB(
 						theme.colors.gray[900],
 						1
 				  )} 0%, rgba(0, 0, 0, 0) 100%)`
 				: 'transparent',
-			'filter': !isDisabled
+			filter: !isDisabled
 				? `progid:DXImageTransform.Microsoft.gradient(startColorstr="${theme.colors.gray[900]}",endColorstr="transparent",GradientType=1)`
 				: 'transparent'
 		}
