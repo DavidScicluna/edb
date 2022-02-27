e1d33c8f2a459a416b2ae9fcd04cb4f58a9a868e
David Scicluna <dave_nostra@hotmail.com>
2022-02-27 16:24:25 +0100

    Fix: TV Route Breadcrumb

--- src/containers/Layout/components/Routes/components/Breadcrumb/index.tsx
+++ src/containers/Layout/components/Routes/components/Breadcrumb/index.tsx
@@ -3,14 +3,21 @@ import { useQueryClient } from 'react-query';
 
 import { Center } from '@chakra-ui/react';
 
+import _ from 'lodash';
+
 import { BreadcrumbProps, Data } from './types';
 
 const Breadcrumb = ({ match, mediaType }: BreadcrumbProps): ReactElement => {
 	const queryClient = useQueryClient();
 
-	const data: Data | undefined = queryClient.getQueryData([`${mediaType}-${match.params.id}`, match.params.id]);
+	const data: Data | undefined = queryClient.getQueryData([
+		`${mediaType === 'tv' ? 'tv-show' : mediaType}-${match.params.id}`,
+		match.params.id
+	]);
 
-	return <Center>{data?.title || data?.name || match.params.id || ''}</Center>;
+	return (
+		<Center>{data?.title || data?.name || _.startCase(mediaType === 'tv' ? 'tv-show' : mediaType) || ''}</Center>
+	);
 };
 
 export default Breadcrumb;
