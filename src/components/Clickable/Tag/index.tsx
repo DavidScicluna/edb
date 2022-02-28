9f1cda47fc0ba50f1bfb47dc81fd7d6b7d046250
David Scicluna <dave_nostra@hotmail.com>
2022-02-28 18:15:03 +0100

    Feature: Tag

---
+++ src/components/Clickable/Tag/index.tsx
@@ -0,0 +1,129 @@
+import React, { ReactElement, forwardRef } from 'react';
+
+import { ColorMode, useTheme, useColorMode, Tag as CUITag, HStack } from '@chakra-ui/react';
+
+import _ from 'lodash';
+
+import useStyles from './styles';
+import { TagRef, TagProps } from './types';
+
+import { handleConvertREMToPixels, handleConvertStringToNumber } from '../../../common/utils';
+import { Theme, Space } from '../../../theme/types';
+import Icon from '../../Icon';
+import IconButton from '../IconButton';
+
+const Tag = forwardRef<TagRef, TagProps>(function Tag(props, ref): ReactElement {
+	const theme = useTheme<Theme>();
+	const { colorMode: colorModeHook } = useColorMode();
+
+	const {
+		children,
+		color = 'gray',
+		colorMode: colorModeProp,
+		renderLeft,
+		renderRight,
+		isClickable = false,
+		isDisabled = false,
+		isFullWidth = false,
+		onDelete,
+		size = 'md',
+		variant = 'contained',
+		sx,
+		...rest
+	} = props;
+
+	const colorMode: ColorMode = colorModeProp || colorModeHook;
+
+	const style = useStyles(theme, {
+		color,
+		isClickable,
+		isFullWidth,
+		hasIcon: !!onDelete,
+		size,
+		variant
+	});
+
+	/**
+	 * This method will return the appropriate spacing depending on the size passed
+	 *
+	 * @returns - number: Spacing value
+	 */
+	const handleReturnSpacing = (): keyof Space => {
+		switch (size) {
+			case 'sm':
+				return 0.5;
+			case 'lg':
+				return 2;
+			default:
+				return 1;
+		}
+	};
+
+	/**
+	 * This method will return the appropriate font-size in PX depending on size prop
+	 *
+	 * @returns - number: Font-size in PX
+	 */
+	const handleReturnIconSize = (): number => {
+		switch (size) {
+			case 'sm':
+				return handleConvertREMToPixels(handleConvertStringToNumber(theme.fontSizes.xs, 'rem')) + 2;
+			case 'lg':
+				return handleConvertREMToPixels(handleConvertStringToNumber(theme.fontSizes.md, 'rem')) + 3;
+			default:
+				return handleConvertREMToPixels(handleConvertStringToNumber(theme.fontSizes.sm, 'rem')) + 2;
+		}
+	};
+
+	const iconSize = `${handleReturnIconSize()}px`;
+
+	return (
+		<CUITag
+			{...rest}
+			ref={ref}
+			variant='unstyled'
+			sx={{ ..._.merge(style.tag.default, style.tag[size], style[colorMode][variant], sx) }}
+			_disabled={{ ...style.tag.disabled }}
+		>
+			<HStack width='100%' spacing={handleReturnSpacing()}>
+				{renderLeft
+					? renderLeft({
+							color,
+							colorMode,
+							width: iconSize,
+							height: iconSize,
+							fontSize: iconSize
+					  })
+					: null}
+				<span className='tag_children'>{children}</span>
+				{renderRight
+					? renderRight({
+							color,
+							colorMode,
+							width: iconSize,
+							height: iconSize,
+							fontSize: iconSize
+					  })
+					: null}
+				{onDelete ? (
+					<IconButton
+						aria-label='Delete Tag'
+						isDisabled={isDisabled}
+						onClick={(event) => {
+							event.preventDefault();
+							event.stopPropagation();
+
+							onDelete();
+						}}
+						size={size}
+						variant='icon'
+					>
+						<Icon icon='clear' type='outlined' />
+					</IconButton>
+				) : null}
+			</HStack>
+		</CUITag>
+	);
+});
+
+export default Tag;
---
+++ src/components/Clickable/Tag/styles.ts
@@ -0,0 +1,300 @@
+import { TagProps, Variant, Size } from './types';
+
+import { Style } from '../../../common/types';
+import { handleIsTouchDevice } from '../../../common/utils';
+import { Theme } from '../../../theme/types';
+
+type VariantStyle = { [key in Variant]: Style };
+
+type SizeStyle = { [key in Size]: Style };
+
+type TagDefaultStyle = {
+	default: Style;
+	disabled: Style;
+} & SizeStyle;
+
+type TagStyle = {
+	tag: TagDefaultStyle;
+	light: VariantStyle;
+	dark: VariantStyle;
+};
+
+type StyleTagProps = {
+	color: TagProps['color'];
+	isClickable: TagProps['isClickable'];
+	isFullWidth: TagProps['isFullWidth'];
+	hasIcon: boolean;
+	size: TagProps['size'];
+	variant: TagProps['variant'];
+};
+
+const isTouchDevice = handleIsTouchDevice();
+
+export default (
+	theme: Theme,
+	{
+		color = 'gray',
+		isClickable = false,
+		isFullWidth = false,
+		hasIcon = false,
+		size = 'md',
+		variant = 'contained'
+	}: StyleTagProps
+): TagStyle => ({
+	tag: {
+		default: {
+			'cursor': isClickable ? 'pointer' : 'default',
+
+			'position': 'relative',
+
+			'width': isFullWidth ? '100%' : 'auto',
+			'height': 'auto',
+
+			'minWidth': 'auto',
+			'minHeight': 'auto',
+			'maxWidth': 'none',
+			'maxHeight': 'none',
+
+			'overflow': 'hidden',
+
+			'userSelect': 'none',
+
+			'opacity': 1,
+
+			'borderStyle': 'solid',
+			'outline': !isTouchDevice ? '0px auto' : 'none !important',
+
+			'WebkitTapHighlightColor': 'transparent',
+
+			'transition': `${theme.transition.duration.faster} ${theme.transition.easing['ease-out']} !important`,
+
+			'& .tag_children': {
+				fontWeight: 'semibold',
+				textTransform: 'uppercase',
+				whiteSpace: 'nowrap',
+				lineHeight: 'normal',
+				textOverflow: 'ellipsis',
+				overflow: 'hidden'
+			},
+
+			'&:focus:not(:focus-visible)': {
+				outline: !isTouchDevice ? '0px auto' : 'none !important'
+			},
+
+			'&:focus': {
+				boxShadow: 'none',
+				outline: !isTouchDevice ? '0px auto' : 'none !important'
+			},
+
+			'&:active': {
+				outline: !isTouchDevice ? '0px auto' : 'none !important'
+			},
+
+			'& .edb-icon': {
+				transition: `${theme.transition.duration.faster} ${theme.transition.easing['ease-out']} !important`
+			}
+		},
+		sm: {
+			fontSize: 'xs',
+
+			borderRadius: 'sm',
+			borderWidth: variant !== 'text' ? '1px' : '0',
+
+			padding: `${theme.space[hasIcon ? 0 : 0.5]} ${theme.space[1]}`
+		},
+		md: {
+			fontSize: 'sm',
+
+			borderRadius: 'base',
+			borderWidth: variant !== 'text' ? '2px' : '0',
+
+			padding: `${theme.space[hasIcon ? 0 : 1]} ${theme.space[2]}`
+		},
+		lg: {
+			fontSize: 'md',
+
+			borderRadius: 'lg',
+			borderWidth: variant !== 'text' ? '2px' : '0',
+
+			padding: `${theme.space[hasIcon ? 0.5 : 1.5]} ${theme.space[3]}`
+		},
+		disabled: {
+			cursor: 'not-allowed',
+			pointerEvents: 'none',
+
+			opacity: 0.5
+		}
+	},
+	light: {
+		contained: {
+			'borderColor': `${color}.${color === 'gray' ? 400 : 500}`,
+			'backgroundColor': `${color}.${color === 'gray' ? 400 : 500}`,
+			'color': 'gray.50',
+
+			'&:hover': {
+				borderColor: `${color}.${isClickable ? (color === 'gray' ? 500 : 600) : color === 'gray' ? 400 : 500}`,
+				backgroundColor: `${color}.${
+					isClickable ? (color === 'gray' ? 500 : 600) : color === 'gray' ? 400 : 500
+				}`,
+				color: 'gray.50'
+			},
+
+			'&:active': {
+				borderColor: `${
+					theme.colors[color][isClickable ? (color === 'gray' ? 600 : 700) : color === 'gray' ? 400 : 500]
+				} !important`,
+				backgroundColor: `${
+					theme.colors[color][isClickable ? (color === 'gray' ? 600 : 700) : color === 'gray' ? 400 : 500]
+				} !important`,
+				color: `${theme.colors.gray[50]} !important`
+			},
+
+			'&:focus-visible': {
+				outline: !isTouchDevice
+					? `${size === 'sm' ? 1 : 2}px auto ${theme.colors[color][color === 'gray' ? 400 : 500]}`
+					: 'none',
+				outlineOffset: !isTouchDevice ? `${size === 'sm' ? 2 : size === 'md' ? 3 : 4}px` : 0
+			}
+		},
+		outlined: {
+			'borderColor': `${color}.${color === 'gray' ? 400 : 500}`,
+			'backgroundColor': 'gray.50',
+			'color': `${color}.${color === 'gray' ? 400 : 500}`,
+
+			'&:hover': {
+				borderColor: `${color}.${isClickable ? (color === 'gray' ? 500 : 600) : color === 'gray' ? 400 : 500}`,
+				backgroundColor: 'gray.50',
+				color: `${color}.${isClickable ? (color === 'gray' ? 500 : 600) : color === 'gray' ? 400 : 500}`
+			},
+
+			'&:active': {
+				borderColor: `${
+					theme.colors[color][isClickable ? (color === 'gray' ? 600 : 700) : color === 'gray' ? 400 : 500]
+				} !important`,
+				backgroundColor: `${theme.colors.gray[50]} !important`,
+				color: `${
+					theme.colors[color][isClickable ? (color === 'gray' ? 600 : 700) : color === 'gray' ? 400 : 500]
+				} !important`
+			},
+
+			'&:focus-visible': {
+				outline: !isTouchDevice
+					? `${size === 'sm' ? 1 : 2}px auto ${theme.colors[color][color === 'gray' ? 400 : 500]}`
+					: 'none',
+				outlineOffset: !isTouchDevice ? `${size === 'sm' ? 2 : size === 'md' ? 3 : 4}px` : 0
+			}
+		},
+		text: {
+			'borderColor': 'transparent',
+			'backgroundColor': 'transparent',
+			'color': `${color}.${color === 'gray' ? 400 : 500}`,
+
+			'&:hover': {
+				borderColor: 'transparent',
+				backgroundColor: 'transparent',
+				color: `${color}.${isClickable ? (color === 'gray' ? 500 : 600) : color === 'gray' ? 400 : 500}`
+			},
+
+			'&:active': {
+				borderColor: 'transparent !important',
+				backgroundColor: 'transparent !important',
+				color: `${
+					theme.colors[color][isClickable ? (color === 'gray' ? 600 : 700) : color === 'gray' ? 400 : 500]
+				} !important`
+			},
+
+			'&:focus-visible': {
+				outline: !isTouchDevice
+					? `${size === 'sm' ? 1 : 2}px auto ${theme.colors[color][color === 'gray' ? 400 : 500]}`
+					: 'none',
+				outlineOffset: !isTouchDevice ? `${size === 'sm' ? 2 : size === 'md' ? 3 : 4}px` : 0
+			}
+		}
+	},
+	dark: {
+		contained: {
+			'borderColor': `${color}.${color === 'gray' ? 500 : 400}`,
+			'backgroundColor': `${color}.${color === 'gray' ? 500 : 400}`,
+			'color': 'gray.900',
+
+			'&:hover': {
+				borderColor: `${color}.${isClickable ? (color === 'gray' ? 400 : 300) : color === 'gray' ? 500 : 400}`,
+				backgroundColor: `${color}.${
+					isClickable ? (color === 'gray' ? 400 : 300) : color === 'gray' ? 500 : 400
+				}`,
+				color: 'gray.900'
+			},
+
+			'&:active': {
+				borderColor: `${
+					theme.colors[color][isClickable ? (color === 'gray' ? 300 : 200) : color === 'gray' ? 500 : 400]
+				} !important`,
+				backgroundColor: `${
+					theme.colors[color][isClickable ? (color === 'gray' ? 300 : 200) : color === 'gray' ? 500 : 400]
+				} !important`,
+				color: `${theme.colors.gray[900]} !important`
+			},
+
+			'&:focus-visible': {
+				outline: !isTouchDevice
+					? `${size === 'sm' ? 1 : 2}px auto ${theme.colors[color][color === 'gray' ? 500 : 400]}`
+					: 'none',
+				outlineOffset: !isTouchDevice ? `${size === 'sm' ? 2 : size === 'md' ? 3 : 4}px` : 0
+			}
+		},
+		outlined: {
+			'borderColor': `${color}.${color === 'gray' ? 500 : 400}`,
+			'backgroundColor': 'gray.900',
+			'color': `${color}.${color === 'gray' ? 500 : 400}`,
+
+			'&:hover': {
+				borderColor: `${color}.${isClickable ? (color === 'gray' ? 400 : 300) : color === 'gray' ? 500 : 400}`,
+				backgroundColor: 'gray.900',
+				color: `${color}.${isClickable ? (color === 'gray' ? 400 : 300) : color === 'gray' ? 500 : 400}`
+			},
+
+			'&:active': {
+				borderColor: `${
+					theme.colors[color][isClickable ? (color === 'gray' ? 300 : 200) : color === 'gray' ? 500 : 400]
+				} !important`,
+				backgroundColor: `${theme.colors.gray[900]} !important`,
+				color: `${
+					theme.colors[color][isClickable ? (color === 'gray' ? 300 : 200) : color === 'gray' ? 500 : 400]
+				} !important`
+			},
+
+			'&:focus-visible': {
+				outline: !isTouchDevice
+					? `${size === 'sm' ? 1 : 2}px auto ${theme.colors[color][color === 'gray' ? 500 : 400]}`
+					: 'none',
+				outlineOffset: !isTouchDevice ? `${size === 'sm' ? 2 : size === 'md' ? 3 : 4}px` : 0
+			}
+		},
+		text: {
+			'borderColor': 'transparent',
+			'backgroundColor': 'transparent',
+			'color': `${color}.${color === 'gray' ? 500 : 400}`,
+
+			'&:hover': {
+				borderColor: 'transparent',
+				backgroundColor: 'transparent',
+				color: `${color}.${isClickable ? (color === 'gray' ? 400 : 300) : color === 'gray' ? 500 : 400}`
+			},
+
+			'&:active': {
+				borderColor: 'transparent !important',
+				backgroundColor: 'transparent !important',
+				color: `${
+					theme.colors[color][isClickable ? (color === 'gray' ? 300 : 200) : color === 'gray' ? 500 : 400]
+				} !important`
+			},
+
+			'&:focus-visible': {
+				outline: !isTouchDevice
+					? `${size === 'sm' ? 1 : 2}px auto ${theme.colors[color][color === 'gray' ? 500 : 400]}`
+					: 'none',
+				outlineOffset: !isTouchDevice ? `${size === 'sm' ? 2 : size === 'md' ? 3 : 4}px` : 0
+			}
+		}
+	}
+});
