7b77eaeabd9358d47c432fdf31e5306a8e1de34a
David Scicluna <dave_nostra@hotmail.com>
2022-03-05 19:50:15 +0100

    Fix: Page Header

--- src/containers/Page/index.tsx
+++ src/containers/Page/index.tsx
@@ -1,7 +1,7 @@
-import { ReactElement } from 'react';
+import { ReactElement, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
-import { VStack, Box } from '@chakra-ui/react';
+import { useBoolean, VStack, Box, Collapse } from '@chakra-ui/react';
 
 import Header from './components/Header';
 import { PageProps } from './types';
@@ -11,12 +11,22 @@ import Divider from '../../components/Divider';
 const Page = ({ children, title, direction }: PageProps): ReactElement => {
 	const location = useLocation();
 
+	const [isHome, setIsHome] = useBoolean(true);
+
+	useEffect(() => {
+		if (location.pathname !== '/') {
+			setTimeout(() => setIsHome.off(), 2500);
+		} else {
+			setTimeout(() => setIsHome.on(), 2500);
+		}
+	}, [location.pathname]);
+
 	return (
 		<VStack width='100%' divider={<Divider />} spacing={0}>
 			{/* Header */}
-			{location.pathname !== '/' ? (
-				<Header title={title} actions={children.actions} direction={direction} />
-			) : null}
+			<Collapse in={!isHome} unmountOnExit style={{ width: '100%' }}>
+				<Header title={title} actions={children.actions} direction={direction} />{' '}
+			</Collapse>
 
 			{/* Body */}
 			<Box width='100%'>{children.body}</Box>
