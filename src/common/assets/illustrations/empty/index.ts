84e7e75667fa4d94a7d1a44a1eae6b9a27124163
David Scicluna <dave_nostra@hotmail.com>
2022-03-06 16:06:16 +0100

    Fix: illustrations with new colors

--- src/common/assets/illustrations/empty/index.ts
+++ src/common/assets/illustrations/empty/index.ts
@@ -1,6 +1,13 @@
 import blue from './blue.svg';
+import brown from './brown.svg';
 import cyan from './cyan.svg';
+import deep_orange from './deep_orange.svg';
+import deep_purple from './deep_purple.svg';
 import green from './green.svg';
+import indigo from './indigo.svg';
+import light_blue from './light_blue.svg';
+import light_green from './light_green.svg';
+import lime from './lime.svg';
 import orange from './orange.svg';
 import pink from './pink.svg';
 import purple from './purple.svg';
@@ -9,8 +16,15 @@ import yellow from './yellow.svg';
 
 export default {
 	blue,
+	brown,
 	cyan,
+	deep_orange,
+	deep_purple,
 	green,
+	indigo,
+	light_blue,
+	light_green,
+	lime,
 	orange,
 	pink,
 	purple,
