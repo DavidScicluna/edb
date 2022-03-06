09a4c6537223aa5661b8e7e7cafac5bbc9451f45
David Scicluna <dave_nostra@hotmail.com>
2022-03-06 14:04:21 +0100

    Fix: Page Header

--- src/containers/Page/index.tsx
+++ src/containers/Page/index.tsx
@@ -1,7 +1,9 @@
-import { ReactElement, useEffect } from 'react';
+import { ReactElement } from 'react';
 import { useLocation } from 'react-router-dom';
 
 import { useBoolean, VStack, Box, Collapse } from '@chakra-ui/react';
+
+import { useEffectOnce } from 'usehooks-ts';
 
 import Header from './components/Header';
 import { PageProps } from './types';
@@ -13,16 +15,16 @@ const Page = ({ children, title, direction }: PageProps): ReactElement => {
 
 	const [isHome, setIsHome] = useBoolean(true);
 
-	useEffect(() => {
-		if (location.pathname !== '/') {
-			setTimeout(() => setIsHome.off(), 2500);
-		} else {
+	useEffectOnce(() => {
+		if (location.pathname === '/') {
 			setTimeout(() => setIsHome.on(), 2500);
+		} else {
+			setIsHome.off();
 		}
-	}, [location.pathname]);
+	});
 
 	return (
-		<VStack width='100%' divider={<Divider />} spacing={0}>
+		<VStack width='100%' divider={!isHome ? <Divider /> : undefined} spacing={0}>
 			{/* Header */}
 			<Collapse in={!isHome} unmountOnExit style={{ width: '100%' }}>
 				<Header title={title} actions={children.actions} direction={direction} />{' '}
