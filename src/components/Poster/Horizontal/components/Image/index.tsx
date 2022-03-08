e07eb85344885272eeb07cc3a8106fc56ecbf282
David Scicluna <dave_nostra@hotmail.com>
2022-03-08 19:06:19 +0100

    Fix: Horizontal Poster Image performance improvements

--- src/components/Poster/Horizontal/components/Image/index.tsx
+++ src/components/Poster/Horizontal/components/Image/index.tsx
@@ -1,33 +1,60 @@
 import { ReactElement } from 'react';
-import { useDispatch } from 'react-redux';
 
-import { AspectRatio, Fade } from '@chakra-ui/react';
+import { useColorMode, AspectRatio, Center, Image as CUIImage, Fade } from '@chakra-ui/react';
+
+import { AnimatePresence } from 'framer-motion';
 
 import { PosterImageProps } from './types';
 
+import * as fallback from '../../../../../common/assets/fallback';
 import { MediaType } from '../../../../../common/types';
 import { handleReturnBoringTypeByMediaType, handleReturnRatio } from '../../../../../common/utils';
 import Skeleton from '../../../../../components/Skeleton';
 import Image from '../../../../Image';
 
+const commonStyleProps = {
+	width: 'inherit',
+	borderRadius: 'inherit'
+};
+
 const width = ['100px', '116px', '152px', '188px', '188px', '224px'];
 
 const PosterImage = <MT extends MediaType>(props: PosterImageProps<MT>): ReactElement => {
+	const { colorMode } = useColorMode();
+
 	const { mediaType, image, isLoading = true, inView = false } = props;
 
 	return (
-		<AspectRatio as={Fade} in={inView} width={width} borderRadius='base' ratio={handleReturnRatio('portrait')}>
-			<Skeleton borderRadius='base' isLoaded={!isLoading && inView}>
-				<Image
-					alt={image?.alt || ''}
-					borderRadius='base'
-					boringType={handleReturnBoringTypeByMediaType(mediaType)}
-					thumbnailSrc={`${process.env.REACT_APP_IMAGE_URL}/${image?.size.thumbnail || ''}${
-						image?.src || ''
-					}`}
-					fullSrc={`${process.env.REACT_APP_IMAGE_URL}/${image?.size.full || ''}${image?.src || ''}`}
-				/>
-			</Skeleton>
+		<AspectRatio width={width} borderRadius='base' ratio={handleReturnRatio('portrait')}>
+			<AnimatePresence exitBeforeEnter initial={false}>
+				{inView ? (
+					<Center {...commonStyleProps} as={Fade} key='image' in unmountOnExit>
+						<Skeleton {...commonStyleProps} isLoaded={!isLoading}>
+							<Image
+								{...commonStyleProps}
+								alt={image?.alt || ''}
+								boringType={handleReturnBoringTypeByMediaType(mediaType)}
+								thumbnailSrc={`${process.env.REACT_APP_IMAGE_URL}/${image?.size.thumbnail || ''}${
+									image?.src || ''
+								}`}
+								fullSrc={`${process.env.REACT_APP_IMAGE_URL}/${image?.size.full || ''}${
+									image?.src || ''
+								}`}
+							/>
+						</Skeleton>
+					</Center>
+				) : (
+					<Center {...commonStyleProps} as={Fade} key='dummy-image' width='100%' in unmountOnExit>
+						<CUIImage
+							{...commonStyleProps}
+							alt='dummy-clickable-image'
+							width='auto'
+							maxWidth='none'
+							src={colorMode === 'light' ? fallback.default.light : fallback.default.dark}
+						/>
+					</Center>
+				)}
+			</AnimatePresence>
 		</AspectRatio>
 	);
 };
