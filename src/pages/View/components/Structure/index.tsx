948d6d887b93c76ccecbbde0afdba3656a61d3d0
David Scicluna <dave_nostra@hotmail.com>
2022-03-11 20:32:15 +0100

    Fix: View Structure - Omitted Page, Title & Actions

--- src/pages/View/components/Structure/index.tsx
+++ src/pages/View/components/Structure/index.tsx
@@ -7,46 +7,34 @@ import { useElementSize } from 'usehooks-ts';
 import { StructureProps } from './types';
 
 import Divider from '../../../../components/Divider';
-import Page from '../../../../containers/Page';
 
 const Structure = ({ children }: StructureProps): ReactElement => {
 	const [ref, { width, height }] = useElementSize();
 
 	return (
-		<Page title={children.title}>
-			{{
-				actions: children.actions,
-				body: (
-					<VStack
-						width='100%'
-						alignItems='stretch'
-						justifyContent='stretch'
-						divider={<Divider my={2} />}
-						spacing={2}
-						p={2}
-					>
-						<HStack
-							width='100%'
-							justifyContent={children.socials ? 'space-between' : 'flex-start'}
-							divider={
-								children.socials ? <Divider orientation='vertical' height={`${height}px`} /> : undefined
-							}
-							spacing={2}
-						>
-							<Center width={`calc(100% - ${children.socials ? width + 34 : 0}px)`}>
-								{children.tabList}
-							</Center>
+		<VStack
+			width='100%'
+			alignItems='stretch'
+			justifyContent='stretch'
+			divider={<Divider my={2} />}
+			spacing={2}
+			p={2}
+		>
+			<HStack
+				width='100%'
+				justifyContent={children.socials ? 'space-between' : 'flex-start'}
+				divider={<Divider orientation='vertical' height={`${height}px`} />}
+				spacing={2}
+			>
+				<Center width={`calc(100% - ${children.socials ? width + 34 : 0}px)`}>{children.tabList}</Center>
 
-							{children.socials ? <Center ref={ref}>{children.socials}</Center> : null}
-						</HStack>
+				{children.socials ? <Center ref={ref}>{children.socials}</Center> : null}
+			</HStack>
 
-						<VStack alignItems='stretch' justifyContent='stretch' spacing={0}>
-							{children.tabPanels}
-						</VStack>
-					</VStack>
-				)
-			}}
-		</Page>
+			<VStack alignItems='stretch' justifyContent='stretch' spacing={0}>
+				{children.tabPanels}
+			</VStack>
+		</VStack>
 	);
 };
 
--- src/pages/View/components/Structure/types.ts
+++ src/pages/View/components/Structure/types.ts
@@ -2,8 +2,6 @@ import { ReactElement } from 'react';
 
 export type StructureProps = {
 	children: {
-		title: ReactElement;
-		actions: ReactElement;
 		tabList: ReactElement;
 		socials?: ReactElement;
 		tabPanels: ReactElement;
