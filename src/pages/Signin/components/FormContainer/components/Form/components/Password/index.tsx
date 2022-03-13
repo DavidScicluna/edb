f55208e8afe7b74662a2a3a5e446924c0b554a5a
David Scicluna <dave_nostra@hotmail.com>
2022-03-13 13:40:04 +0100

    Fix: Signin Password - Added Hide/Show password

--- src/pages/Signin/components/FormContainer/components/Form/components/Password/index.tsx
+++ src/pages/Signin/components/FormContainer/components/Form/components/Password/index.tsx
@@ -1,11 +1,19 @@
 import React, { ReactElement } from 'react';
 import { Controller } from 'react-hook-form';
 
+import { useBoolean } from '@chakra-ui/react';
+
 import { color } from '../../../..';
+import IconButton from '../../../../../../../../components/Clickable/IconButton';
 import Input from '../../../../../../../../components/Forms/Input';
+import Icon from '../../../../../../../../components/Icon';
+import Tooltip from '../../../../../../../../components/Tooltip';
 import { FormProps as PasswordProps } from '../../types';
 
 const Password = ({ form }: Omit<PasswordProps, 'onSubmit'>): ReactElement => {
+	const [isPasswordVisible, setIsPasswordVisible] = useBoolean();
+	const [isHovering, setIsHovering] = useBoolean();
+
 	return (
 		<Controller
 			control={form.control}
@@ -19,7 +27,26 @@ const Password = ({ form }: Omit<PasswordProps, 'onSubmit'>): ReactElement => {
 					onChange={onChange}
 					isFullWidth
 					isRequired
-					type='password'
+					renderInputRightPanel={() => (
+						<Tooltip
+							aria-label={isPasswordVisible ? 'Hide password (tooltip)' : 'Show Password (tooltip)'}
+							label={isPasswordVisible ? 'Hide password' : 'Show Password'}
+							placement='top'
+							isOpen={isHovering}
+						>
+							<IconButton
+								aria-label={isPasswordVisible ? 'Hide password' : 'Show Password'}
+								onClick={() => setIsPasswordVisible.toggle()}
+								onMouseEnter={() => setIsHovering.on()}
+								onMouseLeave={() => setIsHovering.off()}
+								size='sm'
+								variant='icon'
+							>
+								<Icon icon={isPasswordVisible ? 'visibility_off' : 'visibility'} type='outlined' />
+							</IconButton>
+						</Tooltip>
+					)}
+					type={isPasswordVisible ? 'text' : 'password'}
 					value={value}
 				/>
 			)}
