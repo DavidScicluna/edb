7253c71406c822beff29daa8a0fc5bfaf4f761d3
David Scicluna <dave_nostra@hotmail.com>
2022-03-13 18:13:56 +0100

    Feature: Register Genres Step

---
+++ src/pages/Register/components/Genres/components/Genres/types.ts
@@ -0,0 +1,6 @@
+import { MediaType } from '../../../../../../common/types';
+import { GenresProps as GenresStepProps } from '../../types';
+
+export type GenresProps = {
+	mediaType: Omit<MediaType, 'person' | 'company' | 'collection'>;
+} & GenresStepProps;
---
+++ src/pages/Register/components/Genres/index.tsx
@@ -0,0 +1,17 @@
+import React, { ReactElement } from 'react';
+
+import { VStack } from '@chakra-ui/react';
+
+import Genres from './components/Genres';
+import { GenresProps } from './types';
+
+const GenresStep = ({ form, ...rest }: GenresProps): ReactElement => {
+	return (
+		<VStack width='100%' spacing={4}>
+			<Genres {...rest} form={form} mediaType='movie' />
+			<Genres {...rest} form={form} mediaType='tv' />
+		</VStack>
+	);
+};
+
+export default GenresStep;
