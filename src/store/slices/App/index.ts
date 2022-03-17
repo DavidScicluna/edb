ef8d66dad46785463f3633e404e9cd2b3b660a57
David Scicluna <dave_nostra@hotmail.com>
2022-03-17 22:24:01 +0100

    Fix: App slice reducers & added setUser reducer

--- src/store/slices/App/index.ts
+++ src/store/slices/App/index.ts
@@ -16,18 +16,21 @@ const appSlice = createSlice({
 	name: 'app',
 	initialState,
 	reducers: {
+		setUser: (state: StateProps, action: PayloadAction<string | undefined>) => {
+			state.data.user = action.payload;
+		},
+		toggleHasLoadedIcons: (state: StateProps, action: PayloadAction<boolean>) => {
+			state.data.hasLoadedIcons = action.payload;
+		},
 		toggleDisplayMode: (state: StateProps, action: PayloadAction<DisplayMode>) => {
 			state.ui.displayMode = action.payload;
 		},
 		toggleSidebarMode: (state: StateProps, action: PayloadAction<SidebarMode>) => {
 			state.ui.sidebarMode = action.payload;
-		},
-		toggleHasLoadedIcons: (state: StateProps, action: PayloadAction<boolean>) => {
-			state.data.hasLoadedIcons = action.payload;
 		}
 	}
 });
 
-export const { toggleDisplayMode, toggleSidebarMode, toggleHasLoadedIcons } = appSlice.actions;
+export const { setUser, toggleHasLoadedIcons, toggleDisplayMode, toggleSidebarMode } = appSlice.actions;
 
 export default appSlice.reducer;
