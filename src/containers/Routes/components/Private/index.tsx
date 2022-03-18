e2065be59478e0e88c40264b9a3855df909f39b6
David Scicluna <dave_nostra@hotmail.com>
2022-03-18 19:05:26 +0100

    Feature: Private Route

---
+++ src/containers/Routes/components/Private/index.tsx
@@ -0,0 +1,14 @@
+import React, { ReactElement } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+
+import { isEmpty, isNil } from 'lodash';
+
+import { useSelector } from '../../../../common/hooks';
+
+const Private = (): ReactElement => {
+	const user = useSelector((state) => state.app.data.user);
+
+	return !(isNil(user) || isEmpty(user)) ? <Outlet /> : <Navigate to='/signin' />;
+};
+
+export default Private;
