f647dbdbaeaeede28fd9e9f95f6bdb4c605cbdb3
David Scicluna <dave_nostra@hotmail.com>
2022-03-18 19:05:20 +0100

    Feature: Public Route

---
+++ src/containers/Routes/components/Public/index.tsx
@@ -0,0 +1,14 @@
+import React, { ReactElement } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+
+import { isEmpty, isNil } from 'lodash';
+
+import { useSelector } from '../../../../common/hooks';
+
+const Public = (): ReactElement => {
+	const user = useSelector((state) => state.app.data.user);
+
+	return isNil(user) || isEmpty(user) ? <Outlet /> : <Navigate to='/' />;
+};
+
+export default Public;
