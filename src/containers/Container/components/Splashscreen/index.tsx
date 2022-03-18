4afd74fc42388d598a17fe9e6caee6d1abb8b55e
David Scicluna <dave_nostra@hotmail.com>
2022-03-18 19:10:43 +0100

    Removed Splashscreen check location logic

--- src/containers/Container/components/Splashscreen/index.tsx
+++ src/containers/Container/components/Splashscreen/index.tsx
@@ -1,10 +1,8 @@
 import { ReactElement } from 'react';
 import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router';
 
 import { useColorMode } from '@chakra-ui/react';
 
-import { isEmpty, isNil } from 'lodash';
 import { useTernaryDarkMode, useEffectOnce } from 'usehooks-ts';
 
 import { SplashscreenProps } from './types';
@@ -17,10 +15,7 @@ import { defaultUser, getUser } from '../../../../store/slices/Users';
 const Splashscreen = ({ isOpen = false }: SplashscreenProps): ReactElement => {
 	const { setColorMode } = useColorMode();
 
-	const navigate = useNavigate();
-
 	const dispatch = useDispatch();
-	const user = useSelector((state) => state.app.data.user);
 	const colorMode = useSelector(
 		(state) =>
 			getUser(state.users.data.users, state.app.data.user)?.ui.theme.colorMode || defaultUser.ui.theme.colorMode
@@ -35,12 +30,6 @@ const Splashscreen = ({ isOpen = false }: SplashscreenProps): ReactElement => {
 			setColorMode(isDarkMode ? 'dark' : 'light');
 
 			setTimeout(() => dispatch(toggleSplashscreen(false)), 5000);
-		}
-
-		if (isNil(user) || isEmpty(user)) {
-			navigate('/signin');
-		} else if (location.pathname === '/signin') {
-			navigate('/');
 		}
 	});
 
