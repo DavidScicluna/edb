cd8746a015b4a60eddfda3517f5e19e335a03791
David Scicluna <dave_nostra@hotmail.com>
2022-07-09 20:38:38 +0200

    Fix: useSelectorTyped hook

--- src/common/hooks/useSelectorTyped.ts
+++ src/common/hooks/useSelectorTyped.ts
@@ -1,7 +1,7 @@
 import { TypedUseSelectorHook, useSelector } from 'react-redux';
 
-import { RootState } from '../../store';
+import { State } from '../../store/types';
 
-const useSelectorTyped: TypedUseSelectorHook<RootState> = useSelector;
+const useSelectorTyped: TypedUseSelectorHook<State> = useSelector;
 
 export default useSelectorTyped;
