2b60a62e731a566848a6bed09a1c1866c898efde
David Scicluna <dave_nostra@hotmail.com>
2022-07-09 20:45:51 +0200

    Fix: Routes - NoMatch

--- src/containers/Routes/components/NoMatch/index.tsx
+++ src/containers/Routes/components/NoMatch/index.tsx
@@ -1,16 +1,32 @@
-import { ReactElement } from 'react';
+import { FC } from 'react';
 
-import Error from '../../../../pages/Error';
+import { InternalLink, Button } from '@davidscicluna/component-library';
 
-import Actions from './components/Actions';
+import { useMediaQuery } from '@chakra-ui/react';
 
-const NoMatch = (): ReactElement => {
+import Error from '../Error';
+
+const NoMatch: FC = () => {
+	const [isSm] = useMediaQuery('(max-width: 600px)');
+
 	return (
 		<Error
 			code={404}
 			title='Page not found!'
 			subtitle='Please check the URL in the address bar and try again.'
-			renderActions={(props) => <Actions {...props} />}
+			renderActions={(props) => (
+				<>
+					<InternalLink to='/' isFullWidth={isSm}>
+						<Button {...props} isFullWidth>
+							Go back home
+						</Button>
+					</InternalLink>
+
+					<Button {...props} isFullWidth={isSm} onClick={() => window.location.reload()} variant='outlined'>
+						Try again
+					</Button>
+				</>
+			)}
 		/>
 	);
 };
