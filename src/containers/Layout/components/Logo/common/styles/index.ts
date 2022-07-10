e77346b0926c0bc3b2fa6628604bb4c41be6db09
David Scicluna <dave_nostra@hotmail.com>
2022-07-10 13:28:04 +0200

    Feature: Layout - Logo - Styling

---
+++ src/containers/Layout/components/Logo/common/styles/index.ts
@@ -0,0 +1,17 @@
+import { memoize, merge } from 'lodash';
+
+import { color as defaultColor, colorMode as defaultColorMode } from '../../../../../../common/data/defaultPropValues';
+import { size as defaultSize } from '../data/defaultPropValues';
+
+import logo from './logo';
+import dark from './dark';
+import light from './light';
+import { LogoStyleProps, LogoStyleReturn } from './types';
+
+export default memoize((props: LogoStyleProps): LogoStyleReturn => {
+	const { theme, color = defaultColor, colorMode = defaultColorMode, size = defaultSize } = props;
+
+	const scheme = colorMode === 'light' ? light : dark;
+
+	return { logo: merge(logo({ theme, size }), scheme({ theme, color })) };
+});
---
+++ src/containers/Layout/components/Logo/common/styles/types.ts
@@ -0,0 +1,16 @@
+import { Style, Theme } from '@davidscicluna/component-library';
+
+import { ColorMode } from '@chakra-ui/react';
+
+import { UserThemeColor } from '../../../../../../store/slices/Users/types';
+import { LogoProps } from '../../types';
+
+export type LogoStyleProps = {
+	theme: Theme;
+	color: UserThemeColor;
+	colorMode: ColorMode;
+} & Pick<LogoProps, 'isClickable' | 'size'>;
+
+export type LogoStyleReturn = {
+	logo: Style;
+};
