cb735fb5296229a43129af5ac9897e23c93b039c
David Scicluna <dave_nostra@hotmail.com>
2022-07-10 13:26:44 +0200

    Feature: Common - Data - defaultPropValues

---
+++ src/common/data/defaultPropValues.ts
@@ -0,0 +1,7 @@
+import { ColorMode } from '@chakra-ui/react';
+
+import { UserThemeColor } from '../../store/slices/Users/types';
+
+export const color: UserThemeColor = 'light_blue';
+
+export const colorMode: ColorMode = 'light';
