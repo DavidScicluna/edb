e1c9368e1d99bc072ea4d6e30ee686b0800d6e83
David Scicluna <dave_nostra@hotmail.com>
2022-07-16 15:40:18 +0200

    Fix: Router

--- src/containers/Router/index.tsx
+++ src/containers/Router/index.tsx
@@ -1,9 +1,22 @@
-import { FC } from 'react';
+import { FC, useCallback } from 'react';
 
 import { BrowserRouter, BrowserRouterProps as RouterProps } from 'react-router-dom';
 
+import { useEffectOnce } from 'usehooks-ts';
+
+const basename = process.env.PUBLIC_URL;
+
 const Router: FC<RouterProps> = ({ children }) => {
-	return <BrowserRouter basename={process.env.PUBLIC_URL}>{children}</BrowserRouter>;
+	const handleRedirectToBasename = useCallback(() => {
+		if (!window.location.pathname.includes(basename)) {
+			window.history.replaceState('', '', `${basename}${window.location.pathname}`);
+			window.location.reload();
+		}
+	}, [window, basename]);
+
+	useEffectOnce(() => handleRedirectToBasename());
+
+	return <BrowserRouter basename={basename}>{children}</BrowserRouter>;
 };
 
 export default Router;
