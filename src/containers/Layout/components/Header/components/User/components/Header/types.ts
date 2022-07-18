63adf7721f56b507f967f8b92b998ef033e08f31
David Scicluna <dave_nostra@hotmail.com>
2022-07-18 19:45:25 +0200

    Fix: Layout - Header - Removed User

--- src/containers/Layout/components/Header/components/User/components/Header/types.ts
+++
@@ -1,4 +0,0 @@
-import { Credentials, Info } from '../../../../../../../../store/slices/Users/types';
-
-export type HeaderProps = Omit<Credentials, 'password' | 'rememberMe'> &
-	Omit<Info, 'bio' | 'background_path' | 'prefers'>;
