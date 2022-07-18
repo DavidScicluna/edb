8979f0a4cb3c649de184c3210d0319685e8a770e
David Scicluna <dave_nostra@hotmail.com>
2022-07-18 19:44:25 +0200

    Feature: Layout - Structure - UserPopper - Header

---
+++ src/containers/Layout/components/Structure/components/UserPopper/components/Header/index.tsx
@@ -0,0 +1,59 @@
+import { FC, useCallback } from 'react';
+
+import { Space, useTheme, utils } from '@davidscicluna/component-library';
+
+import { HStack, VStack, Text } from '@chakra-ui/react';
+
+import { useElementSize } from 'usehooks-ts';
+
+import { useSelector, useUserTheme } from '../../../../../../../../common/hooks';
+import Avatar from '../../../../../../../../components/Avatar';
+
+const { convertREMToPixels, convertStringToNumber, getColor } = utils;
+
+const spacing: Space = 2;
+
+const Header: FC = () => {
+	const theme = useTheme();
+	const { colorMode } = useUserTheme();
+
+	const user = useSelector((state) => state.users.data.activeUser);
+	const { name, avatar_path } = user.data.info;
+	const { username } = user.data.credentials;
+
+	const [avatarRef, { width: avatarWidth }] = useElementSize();
+
+	const handleContentWidth = useCallback(() => {
+		const spacingWidth = convertREMToPixels(convertStringToNumber(theme.space[spacing], 'rem'));
+
+		return `calc(100% - ${avatarWidth + spacingWidth}px)`;
+	}, [theme, spacing, avatarWidth]);
+
+	return (
+		<HStack width='100%' spacing={spacing}>
+			<Avatar ref={avatarRef} alt={name} borderRadius='base' src={{ full: avatar_path }} />
+
+			<VStack width={handleContentWidth()} alignItems='flex-start' spacing={0.5}>
+				<Text
+					align='left'
+					color={getColor({ theme, colorMode, type: 'text.primary' })}
+					fontSize='xl'
+					fontWeight='bold'
+					noOfLines={1}
+				>
+					{name}
+				</Text>
+				<Text
+					align='left'
+					color={getColor({ theme, colorMode, type: 'text.secondary' })}
+					fontSize='sm'
+					noOfLines={1}
+				>
+					{`@${username}`}
+				</Text>
+			</VStack>
+		</HStack>
+	);
+};
+
+export default Header;
