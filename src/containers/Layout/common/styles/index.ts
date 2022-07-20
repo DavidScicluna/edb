058bb1aee01d53b86a3f199e3af98c80e3e34c67
David Scicluna <dave_nostra@hotmail.com>
2022-07-20 23:13:54 +0200

    Fix: Layout - Styles

--- src/containers/Layout/common/styles/index.ts
+++ src/containers/Layout/common/styles/index.ts
@@ -6,9 +6,17 @@ import { LayoutStyleProps } from './types';
 
 export default memoize(({ theme }: LayoutStyleProps): Style => {
 	const transition = 'none';
-	const transitionProperty = ['width', 'min-width', 'max-width', 'height', 'min-height', 'max-height'].join(', ');
-	const transitionDuration = theme.transition.duration.normal;
-	const transitionDelay = theme.transition.duration.normal;
+	const transitionProperty = [
+		'width',
+		'min-width',
+		'max-width',
+		'height',
+		'min-height',
+		'max-height',
+		'padding'
+	].join(', ');
+	const transitionDuration = theme.transition.duration.slow;
+	const transitionDelay = theme.transition.duration.slow;
 	const transitionTimingFunction = theme.transition.easing['ease-in-out'];
 
 	return {
