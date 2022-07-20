3445cad2a97fc828c7d9addd71aa0d7dc834a4fd
David Scicluna <dave_nostra@hotmail.com>
2022-07-20 23:32:55 +0200

    Fix: Layout - ProgressBar

--- src/containers/Layout/components/ProgressBar/index.tsx
+++ src/containers/Layout/components/ProgressBar/index.tsx
@@ -1,11 +1,12 @@
-import { ReactElement } from 'react';
+import { FC } from 'react';
 
-import { ColorHues } from '@davidscicluna/component-library';
+import { ColorHues, useTheme, Collapse, utils } from '@davidscicluna/component-library';
 
-import { useColorMode, Progress } from '@chakra-ui/react';
+import { Progress } from '@chakra-ui/react';
 
-import { useSelector } from '../../../../common/hooks';
-import { defaultUser, getUser } from '../../../../store/slices/Users';
+import { useIsFetching, useIsMutating } from 'react-query';
+
+import { useSelector, useUserTheme } from '../../../../common/hooks';
 
 const lightShades: ColorHues[] = [
 	50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 800, 700, 600, 500, 400, 300, 200, 100, 50
@@ -14,30 +15,40 @@ const darkShades: ColorHues[] = [
 	900, 800, 700, 600, 500, 400, 300, 200, 100, 50, 100, 200, 300, 400, 500, 600, 700, 800, 900
 ];
 
-const ProgressBar = (): ReactElement => {
-	const { colorMode } = useColorMode();
+const { getColor } = utils;
 
-	const color = useSelector(
-		(state) => getUser(state.users.data.users, state.app.data.user)?.ui.theme.color || defaultUser.ui.theme.color
-	);
+const ProgressBar: FC = () => {
+	const theme = useTheme();
+	const { color, colorMode } = useUserTheme();
+
+	const isQuickViewOpen = useSelector((state) => state.modals.ui.quickViewModal.isOpen);
+
+	const isFetching = useIsFetching();
+	const isMutating = useIsMutating();
 
 	return (
-		<Progress
-			width='100%'
-			height='4px'
-			background={`gray.${colorMode === 'light' ? 200 : 700}`}
-			borderRadius='none'
-			isIndeterminate
-			hasStripe
-			variant='unstyled'
-			sx={{
-				'& div': {
-					bgGradient: `linear(to-r, ${(colorMode === 'light' ? lightShades : darkShades)
-						.map((shade) => `${color}.${shade}`)
-						.join(', ')})`
-				}
-			}}
-		/>
+		<Collapse
+			in={!isQuickViewOpen && (isFetching > 0 || isMutating) > 0}
+			unmountOnExit
+			style={{ position: 'fixed', top: 0, zIndex: 950, width: '100%' }}
+		>
+			<Progress
+				width='100%'
+				height={theme.space['0.5']}
+				background={getColor({ theme, colorMode, type: 'divider' })}
+				borderRadius='none'
+				isIndeterminate
+				hasStripe
+				variant='unstyled'
+				sx={{
+					'& div': {
+						bgGradient: `linear(to-r, ${(colorMode === 'light' ? lightShades : darkShades)
+							.map((shade) => `${color}.${shade}`)
+							.join(', ')})`
+					}
+				}}
+			/>
+		</Collapse>
 	);
 };
 
