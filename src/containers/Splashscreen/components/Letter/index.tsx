fc4e8d8120555eb4f58a607f8e6be0dc510958d3
David Scicluna <dave_nostra@hotmail.com>
2022-08-04 21:42:28 +0200

    Fix: Splashscreen - Letter

--- src/containers/Splashscreen/components/Letter/index.tsx
+++ src/containers/Splashscreen/components/Letter/index.tsx
@@ -2,19 +2,24 @@ import { FC } from 'react';
 
 import { useTheme, utils } from '@davidscicluna/component-library';
 
-import { Center } from '@chakra-ui/react';
+import { useBoolean, Center } from '@chakra-ui/react';
 
 import { lighten, darken } from 'color2k';
+import { useTimeout } from 'usehooks-ts';
 import { range } from 'lodash';
 
 import { LetterProps } from './types';
 
 const { getColor } = utils;
 
-const Letter: FC<LetterProps> = ({ letter, color: colorProp, colorMode }) => {
+const Letter: FC<LetterProps> = ({ color: colorProp, colorMode, letter, delay }) => {
 	const theme = useTheme();
 
+	const [hasShadow, setHasShadow] = useBoolean();
+
 	const color = getColor({ theme, colorMode, color: colorProp, type: 'color' });
+
+	useTimeout(() => setHasShadow.on(), delay * 1000 + 250);
 
 	return (
 		<Center
@@ -25,12 +30,16 @@ const Letter: FC<LetterProps> = ({ letter, color: colorProp, colorMode }) => {
 				fontWeight: 400,
 				textTransform: 'lowercase',
 				color,
-				textShadow: range(1, 13)
-					.map(
-						(num) =>
-							`0px ${num}px 0px ${colorMode === 'light' ? lighten(color, 0.25) : darken(color, 0.25)}`
-					)
-					.join(', ')
+				textShadow: hasShadow
+					? range(1, 13)
+							.map(
+								(num) =>
+									`0px ${num}px 0px ${
+										colorMode === 'light' ? lighten(color, 0.25) : darken(color, 0.25)
+									}`
+							)
+							.join(', ')
+					: undefined
 			}}
 		>
 			{letter}
--- src/containers/Splashscreen/components/Letter/types.ts
+++ src/containers/Splashscreen/components/Letter/types.ts
@@ -3,7 +3,8 @@ import { ColorMode } from '@chakra-ui/react';
 import { SplashscreenColor } from '../../types';
 
 export type LetterProps = {
-	letter: string;
 	color: SplashscreenColor;
 	colorMode: ColorMode;
+	letter: string;
+	delay: number;
 };
