ff3aa3ade7f7f380edb7821361e3592d659e224e
David Scicluna <dave_nostra@hotmail.com>
2022-08-06 19:35:50 +0200

    Fix: Layout - Structure

--- src/containers/Layout/components/Structure/index.tsx
+++ src/containers/Layout/components/Structure/index.tsx
@@ -1,18 +1,31 @@
 import { FC } from 'react';
 
+import { useBoolean } from '@chakra-ui/react';
+
+import { useUpdateEffect } from 'usehooks-ts';
+
+import { useSelector } from '../../../../common/hooks';
+import { guest } from '../../../../store/slices/Users';
+
 import { StructureProps } from './types';
 import StructureTablet from './components/StructureTablet';
 import StructureMobile from './components/StructureMobile';
 import StructureDesktop from './components/StructureDesktop';
 
 const Structure: FC<StructureProps> = ({ type }) => {
+	const activeUser = useSelector((state) => state.users.data.activeUser);
+
+	const [isGuest, setIsGuest] = useBoolean(guest.data.id === activeUser.data.id);
+
+	useUpdateEffect(() => setIsGuest[guest.data.id === activeUser.data.id ? 'on' : 'off'](), [activeUser]);
+
 	switch (type) {
 		case 'mobile':
-			return <StructureMobile />;
+			return <StructureMobile isGuest={isGuest} />;
 		case 'tablet':
-			return <StructureTablet />;
+			return <StructureTablet isGuest={isGuest} />;
 		default:
-			return <StructureDesktop />;
+			return <StructureDesktop isGuest={isGuest} />;
 	}
 };
 
