993458e8f1f3a96b8a2094196f15b4c84c68020a
David Scicluna <dave_nostra@hotmail.com>
2022-08-07 15:57:10 +0200

    Feature: UserThemeCustomization

---
+++ src/components/User/UserThemeCustomization/index.tsx
@@ -0,0 +1,23 @@
+import React, { FC } from 'react';
+
+import { VStack } from '@chakra-ui/react';
+
+import { color as defaultColor, colorMode as defaultColorMode } from '../../../common/data/defaultPropValues';
+
+import Color from './components/Color';
+import ColorMode from './components/ColorMode';
+import { UserThemeCustomizationProps } from './types';
+
+const UserThemeCustomization: FC<UserThemeCustomizationProps> = (props) => {
+	const { form, color = defaultColor, colorMode = defaultColorMode } = props;
+
+	return (
+		<VStack width='100%' spacing={4}>
+			<ColorMode form={form} color={color} colorMode={colorMode} />
+
+			<Color form={form} color={color} colorMode={colorMode} />
+		</VStack>
+	);
+};
+
+export default UserThemeCustomization;
---
+++ src/components/User/UserThemeCustomization/types.ts
@@ -0,0 +1,7 @@
+import { UseFormReturn } from 'react-hook-form';
+
+import { UserTheme } from '../../../store/slices/Users/types';
+
+export type Form = UserTheme;
+
+export type UserThemeCustomizationProps = { form: UseFormReturn<Form> } & UserTheme;
