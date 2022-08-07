29788f7882a741f0c6de01964955dfc3265831fb
David Scicluna <dave_nostra@hotmail.com>
2022-08-07 19:16:11 +0200

    Fix: Layout - Structure - Navigation

--- src/containers/Layout/components/Structure/components/Navigation/index.tsx
+++ src/containers/Layout/components/Structure/components/Navigation/index.tsx
@@ -4,13 +4,8 @@ import { useLocation } from 'react-router';
 
 import { NavItemType, useTheme, SideNavigation, NavItem, Icon } from '@davidscicluna/component-library';
 
-import { useDispatch } from 'react-redux';
-
 import useStyles from '../../../../common/styles';
 import { useSelector, useUserTheme } from '../../../../../../common/hooks';
-import { isGuest as defaultIsGuest } from '../../common/data/defaultPropValues';
-import { StructureCommonProps as NavigationProps } from '../../common/types';
-import { toggleUserThemeModal } from '../../../../../../store/slices/Modals';
 
 const navItems: NavItemType[] = [
 	{ title: 'Home', path: { pathname: '/' }, renderLeftIcon: (props) => <Icon {...props} icon='home' /> },
@@ -41,13 +36,12 @@ const navItems: NavItemType[] = [
 	}
 ];
 
-const Navigation: FC<NavigationProps> = ({ isGuest = defaultIsGuest }) => {
+const Navigation: FC = () => {
 	const theme = useTheme();
 	const { color, colorMode } = useUserTheme();
 
 	const location = useLocation();
 
-	const dispatch = useDispatch();
 	const sidebarMode = useSelector((state) => state.app.ui.sidebarMode);
 	const isUserThemeModalOpen = useSelector((state) => state.modals.ui.isUserThemeModalOpen);
 
@@ -56,20 +50,12 @@ const Navigation: FC<NavigationProps> = ({ isGuest = defaultIsGuest }) => {
 	return (
 		<SideNavigation color={color} colorMode={colorMode} mode={sidebarMode} spacing={1} sx={{ ...style }}>
 			{navItems.map((navItem) => (
-				<NavItem key={navItem.title} {...navItem} isActive={location.pathname === navItem.path.pathname} />
-			))}
-
-			{isGuest && (
 				<NavItem
-					title='Display'
-					path={{}}
-					isActive={isUserThemeModalOpen}
-					renderLeftIcon={(props) => (
-						<Icon {...props} icon='palette' category={isUserThemeModalOpen ? 'filled' : 'outlined'} />
-					)}
-					onClick={() => dispatch(toggleUserThemeModal(true))}
+					key={navItem.title}
+					{...navItem}
+					isActive={!isUserThemeModalOpen && location.pathname === navItem.path.pathname}
 				/>
-			)}
+			))}
 		</SideNavigation>
 	);
 };
