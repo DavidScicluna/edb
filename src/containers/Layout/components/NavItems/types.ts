21995bfdac4675784ff8beba2f85b171909ee06d
David Scicluna <dave_nostra@hotmail.com>
2022-08-12 21:05:34 +0200

    Fix: Layout - Removed NavItems

--- src/containers/Layout/components/NavItems/types.ts
+++
@@ -1,7 +0,0 @@
-import { NavItem } from '../../../../components/NavItem/types';
-import { SidebarMode } from '../../../../store/slices/App/types';
-
-export type NavItemsProps = {
-	navItems: NavItem[];
-	sidebarMode?: SidebarMode;
-};
