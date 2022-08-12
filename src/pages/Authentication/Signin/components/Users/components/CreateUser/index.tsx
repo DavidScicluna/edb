f693824bf715555e7a0336e82e60d9ef386dd241
David Scicluna <dave_nostra@hotmail.com>
2022-08-12 20:59:51 +0200

    Feature:  Authentication - Signin - Users - CreateUser

---
+++ src/pages/Authentication/Signin/components/Users/components/CreateUser/index.tsx
@@ -0,0 +1,47 @@
+import React, { FC } from 'react';
+
+import { useTheme, InternalLink, Card, CardBody, Icon, utils } from '@davidscicluna/component-library';
+
+import { VStack, Center, Text } from '@chakra-ui/react';
+
+import { useUserTheme } from '../../../../../../../common/hooks';
+
+const { getColor } = utils;
+
+const CreateUser: FC = () => {
+	const theme = useTheme();
+	const { color, colorMode } = useUserTheme();
+
+	return (
+		<InternalLink to='/register'>
+			<Card color='gray' colorMode={colorMode} isClickable p={1}>
+				<CardBody>
+					<VStack width='100%' spacing={1}>
+						<Center width={theme.space[5]} height={theme.space[5]} background={theme.colors.transparent}>
+							<Icon
+								color={getColor({ theme, colorMode, color, type: 'color' })}
+								colorMode={colorMode}
+								width={theme.fontSizes['2xl']}
+								height={theme.fontSizes['2xl']}
+								fontSize={theme.fontSizes['2xl']}
+								icon='add_box'
+								skeletonColor={color}
+							/>
+						</Center>
+
+						<Text
+							align='center'
+							color={getColor({ theme, colorMode, type: 'text.primary' })}
+							fontSize='md'
+							fontWeight='semibold'
+						>
+							Create New Account
+						</Text>
+					</VStack>
+				</CardBody>
+			</Card>
+		</InternalLink>
+	);
+};
+
+export default CreateUser;
