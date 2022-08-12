e38c523b7f25b9b43cdbb23f86cc7b0695177685
David Scicluna <dave_nostra@hotmail.com>
2022-08-12 20:58:59 +0200

    Feature:  Authentication - Signin - Footer

---
+++ src/pages/Authentication/Signin/components/Footer/index.tsx
@@ -0,0 +1,27 @@
+import { FC } from 'react';
+
+import { InternalLink, Button } from '@davidscicluna/component-library';
+
+import { useMediaQuery } from '@chakra-ui/react';
+
+import { compact } from 'lodash';
+
+import { useSelector, useUserTheme } from '../../../../../common/hooks';
+
+const Footer: FC = () => {
+	const { colorMode } = useUserTheme();
+
+	const [isSm] = useMediaQuery('(max-width: 600px)');
+
+	const activeUser = useSelector((state) => state.users.data.activeUser);
+
+	return (
+		<InternalLink to='/register' isFullWidth>
+			<Button color={activeUser.ui.theme.color} colorMode={colorMode} isFullWidth size='xs' variant='text'>
+				{compact([!isSm ? "Don't have an account yet?" : null, 'Create New Account!']).join(' ')}
+			</Button>
+		</InternalLink>
+	);
+};
+
+export default Footer;
