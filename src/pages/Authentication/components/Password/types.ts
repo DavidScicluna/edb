0a0210f8771fa7af8b0a54850dd40b08014968c4
David Scicluna <dave_nostra@hotmail.com>
2022-08-13 23:03:05 +0200

    Fix: Authentication - Removed Password

--- src/pages/Authentication/components/Password/types.ts
+++
@@ -1,9 +0,0 @@
-import { ControllerRenderProps, ControllerFieldState } from 'react-hook-form';
-
-import { AuthenticationForm } from '../../common/types';
-
-export type PasswordProps = {
-	field: ControllerRenderProps<AuthenticationForm, 'password'>;
-	fieldState: ControllerFieldState;
-	label?: string;
-};
