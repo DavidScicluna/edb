4998a86f1cca701043a0637f38de8e605e78e158
David Scicluna <dave_nostra@hotmail.com>
2022-08-13 16:10:48 +0200

    Fix: Authentication - Signin - Header

--- src/pages/Authentication/Signin/components/Header/index.tsx
+++ src/pages/Authentication/Signin/components/Header/index.tsx
@@ -2,9 +2,12 @@ import { FC } from 'react';
 
 import { useTheme, utils } from '@davidscicluna/component-library';
 
-import { VStack, Show, Text } from '@chakra-ui/react';
+import { VStack, Text } from '@chakra-ui/react';
 
-import { useUserTheme } from '../../../../../common/hooks';
+import { compact } from 'lodash';
+import { useWindowSize } from 'usehooks-ts';
+
+import { useSelector, useUserTheme } from '../../../../../common/hooks';
 import Logo from '../Logo';
 
 const { getColor } = utils;
@@ -12,6 +15,10 @@ const { getColor } = utils;
 const Header: FC = () => {
 	const theme = useTheme();
 	const { colorMode } = useUserTheme();
+
+	const { height: windowHeight } = useWindowSize();
+
+	const users = useSelector((state) => state.users.data.users.length);
 
 	return (
 		<VStack width='100%' spacing={2}>
@@ -21,18 +28,24 @@ const Header: FC = () => {
 				<Text
 					align='center'
 					color={getColor({ theme, colorMode, type: 'text.primary' })}
-					fontSize={['3xl', '4xl', '6xl', '6xl']}
+					fontSize={['3xl', '4xl', '5xl', '6xl']}
 					lineHeight='base'
 					whiteSpace='nowrap'
 				>
 					Hello there!
 				</Text>
-				<Show breakpoint='(min-width: 992px)'>
-					<Text align='center' color={getColor({ theme, colorMode, type: 'text.secondary' })} fontSize='sm'>
-						Welcome to The Entertainment Database application or EDB for short. The one-stop app for all
-						things Movies, TV Shows, and the people that make it happen!
-					</Text>
-				</Show>
+				<Text
+					align='center'
+					color={getColor({ theme, colorMode, type: 'text.secondary' })}
+					fontSize={['xs', 'xs', 'xs', 'sm']}
+				>
+					{compact([
+						'Welcome to The Entertainment Database application or EDB for short.',
+						users === 0 || windowHeight > 960
+							? 'The one-stop app for all things Movies, TV Shows, and the people that make it happen!'
+							: null
+					]).join(' ')}
+				</Text>
 			</VStack>
 		</VStack>
 	);
