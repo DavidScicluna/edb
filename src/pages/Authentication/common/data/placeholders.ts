a14beb75c73554452d1413956a5d5b9812b156ef
David Scicluna <dave_nostra@hotmail.com>
2022-08-13 23:07:15 +0200

    Feature: Authentication - placeholders

---
+++ src/pages/Authentication/common/data/placeholders.ts
@@ -0,0 +1,27 @@
+const names: string[] = [
+	'James Smith',
+	'Mary Johnson',
+	'Robert Williams',
+	'Patricia Brown',
+	'John Jones',
+	'Jennifer Garcia',
+	'Michael Miller',
+	'Linda Davis',
+	'David Rodriguez',
+	'Elizabeth Martinez'
+];
+
+const usernames: string[] = [
+	'jamessmith',
+	'maryjohnson',
+	'robertwilliams',
+	'patriciabrown',
+	'johnjones',
+	'jennifergarcia',
+	'michaelmiller',
+	'lindadavis',
+	'davidrodriguez',
+	'elizabethmartinez'
+];
+
+export { names, usernames };
