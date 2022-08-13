172b850b333bdb78e7bba44c6a08e020e380e853
David Scicluna <dave_nostra@hotmail.com>
2022-08-13 23:03:14 +0200

    Fix: Authentication - Removed Username

--- src/pages/Authentication/components/Username/types.ts
+++
@@ -1,8 +0,0 @@
-import { ControllerRenderProps, ControllerFieldState } from 'react-hook-form';
-
-import { AuthenticationForm } from '../../common/types';
-
-export type UsernameProps = {
-	field: ControllerRenderProps<AuthenticationForm, 'username'>;
-	fieldState: ControllerFieldState;
-};
