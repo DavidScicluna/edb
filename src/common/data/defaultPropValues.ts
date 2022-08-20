c7d37f8c93b3280ec4a3b068acef247641cff450
David Scicluna <dave_nostra@hotmail.com>
2022-08-20 21:29:10 +0200

    Fix: defaultPropValues

--- src/common/data/defaultPropValues.ts
+++ src/common/data/defaultPropValues.ts
@@ -2,6 +2,6 @@ import { ColorMode } from '@chakra-ui/react';
 
 import { UserThemeColor } from '../../store/slices/Users/types';
 
-export const color: UserThemeColor = 'light_blue';
+export const color: UserThemeColor = 'blue';
 
 export const colorMode: ColorMode = 'light';
