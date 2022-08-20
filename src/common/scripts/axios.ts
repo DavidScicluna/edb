d118bca0f19b1b921d689f92f06904f265755860
David Scicluna <dave_nostra@hotmail.com>
2022-08-20 21:29:18 +0200

    Fix: axios

--- src/common/scripts/axios.ts
+++ src/common/scripts/axios.ts
@@ -1,14 +1,15 @@
 import axios from 'axios';
+import { memoize } from 'lodash';
 
-export const handleDelay = <T>(ms: number, value: T): Promise<T> => {
+type GetAxiosDelayProps<T> = { ms: number; value: T };
+
+export const getAxiosDelay = memoize(<T>({ ms, value }: GetAxiosDelayProps<T>): Promise<T> => {
 	return new Promise<T>((resolve) => setTimeout(resolve, ms, value));
-};
+});
 
 const instance = axios.create({
 	baseURL: process.env.REACT_APP_BASE_URL,
-	headers: {
-		Authorization: `Bearer ${process.env.REACT_APP_KEY}`
-	}
+	headers: { Authorization: `Bearer ${process.env.REACT_APP_KEY}` }
 });
 
 export default instance;
