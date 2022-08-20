87108adc0b725ba0001c3ff5122f35ae029d03f6
David Scicluna <dave_nostra@hotmail.com>
2022-08-20 22:04:35 +0200

    Fix: Register - Customization & Renamed to CustomizationStep

---
+++ src/pages/Authentication/Register/components/CustomizationStep/index.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+import UserThemeCustomization from '../../../../../components/User/UserThemeCustomization';
+
+import { CustomizationStepProps } from './types';
+
+const CustomizationStep: FC<CustomizationStepProps> = (props) => {
+	return <UserThemeCustomization {...props} />;
+};
+
+export default CustomizationStep;
--- src/pages/Authentication/Register/components/CustomizationStep/types.ts
+++ src/pages/Authentication/Register/components/CustomizationStep/types.ts
@@ -1,7 +1,8 @@
 import { UseFormReturn } from 'react-hook-form';
 
+import { RegisterCommonProps } from '../../common/types';
 import { CustomizationForm } from '../../types';
 
-export type CustomizationProps = {
+export type CustomizationStepProps = RegisterCommonProps & {
 	form: UseFormReturn<CustomizationForm>;
 };
