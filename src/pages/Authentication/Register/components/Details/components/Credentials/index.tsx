b4bda9a3a11f14e0f8cc43aef69944c2b93de817
David Scicluna <dave_nostra@hotmail.com>
2022-08-20 16:17:52 +0200

    Fix: Register - Details - Credentials

--- src/pages/Authentication/Register/components/Details/components/Credentials/index.tsx
+++ src/pages/Authentication/Register/components/Details/components/Credentials/index.tsx
@@ -1,37 +1,56 @@
-import { ReactElement } from 'react';
+import { FC } from 'react';
 
-import { Card, CardHeader, CardBody, Button } from '@davidscicluna/component-library';
+import { Card, CardHeader, CardBody, Button, Input, Icon } from '@davidscicluna/component-library';
 
-import { VStack, Text } from '@chakra-ui/react';
+import { useBoolean, VStack, Text } from '@chakra-ui/react';
 
-import { Controller } from 'react-hook-form';
-import { isEmpty, isNil } from 'lodash';
+import { useWatch, Controller } from 'react-hook-form';
 
-import Password from '../../../../../components/Password';
-import Username from '../../../../../components/Username';
+import {
+	color as defaultColor,
+	colorMode as defaultColorMode
+} from '../../../../../../../common/data/defaultPropValues';
 import { detailsDefaultValues as defaultValues } from '../../../../defaults';
-import { DetailsProps as CredentialsProps } from '../../types';
+import { PasswordIcon } from '../../../../../components';
 
-const Credentials = ({ form, color, colorMode }: CredentialsProps): ReactElement => {
-	const username = form.watch('username');
-	const password = form.watch('password');
+import { CredentialsProps } from './types';
+
+const Credentials: FC<CredentialsProps> = (props) => {
+	const { form, color = defaultColor, colorMode = defaultColorMode, placeholder } = props;
+	const { control, getValues, reset } = form;
+
+	const watchUsername = useWatch({ control, name: 'username' });
+	const watchPassword = useWatch({ control, name: 'password' });
+
+	const [isPassVisible, setIsPassVisible] = useBoolean();
+	const [isHoveringPass, setIsHoveringPass] = useBoolean();
 
 	const handleClear = (): void => {
-		form.setValue('username', defaultValues.username, { shouldDirty: false });
-		form.setValue('password', defaultValues.password, { shouldDirty: false });
+		reset(
+			{ ...getValues(), username: defaultValues.username, password: defaultValues.password },
+			{
+				keepDirty: true,
+				keepDirtyValues: true,
+				keepErrors: true,
+				keepIsSubmitted: true,
+				keepIsValid: true,
+				keepSubmitCount: true,
+				keepTouched: true
+			}
+		);
 	};
 
 	return (
-		<Card colorMode={colorMode} isFullWidth variant='outlined'>
+		<Card colorMode={colorMode} isFullWidth variant='outlined' p={2}>
 			<CardHeader
 				renderTitle={(props) => <Text {...props}>Credentials</Text>}
 				actions={
 					<Button
 						color={color}
 						colorMode={colorMode}
-						isDisabled={(isNil(username) || isEmpty(username)) && (isNil(password) || isEmpty(password))}
+						isDisabled={!watchUsername && !watchPassword}
 						onClick={() => handleClear()}
-						size='sm'
+						size='xs'
 						variant='text'
 					>
 						Clear
@@ -41,17 +60,59 @@ const Credentials = ({ form, color, colorMode }: CredentialsProps): ReactElement
 			<CardBody>
 				<VStack width='100%' spacing={2}>
 					<Controller
-						control={form.control}
+						control={control}
 						name='username'
-						render={({ field, fieldState }) => (
-							<Username field={field} fieldState={fieldState} color={color} colorMode={colorMode} />
+						render={({ field: { onChange, onBlur, value, name }, fieldState: { error } }) => (
+							<Input
+								color={color}
+								colorMode={colorMode}
+								label='Username'
+								id={name}
+								name={name}
+								helper={error ? error.message : undefined}
+								placeholder={placeholder}
+								onBlur={onBlur}
+								onChange={onChange}
+								isError={!!error}
+								isFullWidth
+								isRequired
+								renderLeftPanel={({ color, ...rest }) => (
+									<Icon {...rest} icon='alternate_email' category='outlined' skeletonColor={color} />
+								)}
+								value={value}
+							/>
 						)}
 					/>
 					<Controller
-						control={form.control}
+						control={control}
 						name='password'
-						render={({ field, fieldState }) => (
-							<Password field={field} fieldState={fieldState} color={color} colorMode={colorMode} />
+						render={({ field: { onChange, onBlur, value, name }, fieldState: { error } }) => (
+							<Input
+								color={color}
+								colorMode={colorMode}
+								label='Password'
+								name={name}
+								helper={error ? error.message : undefined}
+								placeholder={isPassVisible ? 'password' : '••••••••'}
+								onBlur={onBlur}
+								onChange={onChange}
+								isError={!!error}
+								isFullWidth
+								isRequired
+								renderRightPanel={({ color, ...rest }) => (
+									<PasswordIcon
+										{...rest}
+										label='Password'
+										isVisible={isPassVisible}
+										isHovering={isHoveringPass}
+										setIsVisible={setIsPassVisible}
+										setIsHovering={setIsHoveringPass}
+										iconProps={{ ...rest, skeletonColor: color }}
+									/>
+								)}
+								type={isPassVisible ? 'text' : 'password'}
+								value={value}
+							/>
 						)}
 					/>
 				</VStack>
---
+++ src/pages/Authentication/Register/components/Details/components/Credentials/types.ts
@@ -0,0 +1,5 @@
+import { DetailsProps } from '../../types';
+
+export type CredentialsProps = DetailsProps & {
+	placeholder: string;
+};
