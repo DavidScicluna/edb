755d2696d256bfb47626d11ee69279c85109da78
David Scicluna <dave_nostra@hotmail.com>
2022-08-20 21:28:47 +0200

    Fix: useUserTheme

--- src/common/hooks/useUserTheme.tsx
+++ src/common/hooks/useUserTheme.tsx
@@ -1,30 +1,16 @@
 import { ColorMode, useColorMode } from '@chakra-ui/react';
 
 // import { useDispatch } from 'react-redux';
-import { memoize } from 'lodash';
 
 import { guest } from '../../store/slices/Users';
-import { UserTheme, UserThemeColorMode } from '../../store/slices/Users/types';
+import { UserTheme } from '../../store/slices/Users/types';
 // import { toggleSpinnerModal } from '../../store/slices/Modals';
 
 import { useSelector } from '.';
 
-type GetModeProps = {
-	colorMode: UserThemeColorMode;
-	isDarkMode: boolean;
-};
-
 type UseUserThemeReturn = {
 	colorMode: ColorMode;
 } & Pick<UserTheme, 'color'>;
-
-export const getMode = memoize(({ colorMode, isDarkMode = false }: GetModeProps): ColorMode => {
-	if (colorMode === 'system') {
-		return isDarkMode ? 'dark' : 'light';
-	} else {
-		return colorMode;
-	}
-});
 
 const useUserTheme = (): UseUserThemeReturn => {
 	const { colorMode } = useColorMode();
