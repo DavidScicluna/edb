6e626ae0020461f3a37ac38b6f8d3a7fff879202
David Scicluna <dave_nostra@hotmail.com>
2022-08-20 16:05:37 +0200

    Fix: Signin - Footer

--- src/pages/Authentication/Signin/components/Footer/index.tsx
+++ src/pages/Authentication/Signin/components/Footer/index.tsx
@@ -6,18 +6,16 @@ import { useMediaQuery } from '@chakra-ui/react';
 
 import { compact } from 'lodash';
 
-import { useSelector, useUserTheme } from '../../../../../common/hooks';
+import { useUserTheme } from '../../../../../common/hooks';
 
 const Footer: FC = () => {
-	const { colorMode } = useUserTheme();
+	const { color, colorMode } = useUserTheme();
 
 	const [isSm] = useMediaQuery('(max-width: 600px)');
 
-	const activeUser = useSelector((state) => state.users.data.activeUser);
-
 	return (
 		<InternalLink to='/register' isFullWidth>
-			<Button color={activeUser.ui.theme.color} colorMode={colorMode} isFullWidth size='xs' variant='text'>
+			<Button color={color} colorMode={colorMode} isFullWidth size='xs' variant='text'>
 				{compact([!isSm ? "Don't have an account yet?" : null, 'Create New Account!']).join(' ')}
 			</Button>
 		</InternalLink>
