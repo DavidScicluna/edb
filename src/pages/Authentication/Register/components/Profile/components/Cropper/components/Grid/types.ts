84ec7b5aef31ccd8b65aeaca06d4d26ef14000b3
David Scicluna <dave_nostra@hotmail.com>
2022-09-06 21:33:24 +0200

    Fix: Authentication - Register - ProfileStep - Removed Cropper

--- src/pages/Authentication/Register/components/Profile/components/Cropper/components/Grid/types.ts
+++
@@ -1,6 +0,0 @@
-import { CropperProps } from '../../types';
-
-export type GridProps = {
-	isActive?: boolean;
-	onToggle: () => void;
-} & Omit<CropperProps, 'type' | 'image' | 'isOpen' | 'onCrop' | 'onClose' | 'color'>;
--- src/pages/Authentication/Register/components/Profile/components/Cropper/components/Zoom/types.ts
+++
@@ -1,6 +0,0 @@
-import { CropperProps } from '../../types';
-
-export type ZoomProps = {
-	zoom: number;
-	onSetZoom: (zoom: number) => void;
-} & Omit<CropperProps, 'type' | 'image' | 'isOpen' | 'onCrop' | 'onClose' | 'color'>;
