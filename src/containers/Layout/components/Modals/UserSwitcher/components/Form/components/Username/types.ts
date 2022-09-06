e8ec0d49f92c7d2f9ca75e579aa17e24ede73084
David Scicluna <dave_nostra@hotmail.com>
2022-09-06 22:31:40 +0200

    Fix: Layout - Modals - Removed UserSwitcher Modal

--- src/containers/Layout/components/Modals/UserSwitcher/components/Form/components/Username/types.ts
+++
@@ -1,5 +0,0 @@
-import { UseFormReturn } from 'react-hook-form';
-
-import { Form } from '../../types';
-
-export type UsernameProps = { form: UseFormReturn<Form> };
--- src/containers/Layout/components/Modals/UserSwitcher/components/User/types.ts
+++
@@ -1,7 +0,0 @@
-import { User } from '../../../../../../../store/slices/Users/types';
-
-export type UserProps = {
-	user: User;
-	isActive?: boolean;
-	onClick: () => void;
-};
