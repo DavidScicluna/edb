df5c3b8db330ae71d4966cf1ab70b823a4244924
David Scicluna <dave_nostra@hotmail.com>
2022-09-06 21:34:40 +0200

    Fix: Authentication - GenresStep

--- src/pages/Authentication/Register/components/GenresStep/components/Genre/index.tsx
+++ src/pages/Authentication/Register/components/GenresStep/components/Genre/index.tsx
@@ -2,6 +2,8 @@ import { FC } from 'react';
 
 import { Button, Icon } from '@davidscicluna/component-library';
 
+import { omit } from 'lodash';
+
 import DummyGenre from '../DummyGenre';
 import {
 	color as defaultColor,
@@ -17,7 +19,20 @@ const Genre: FC<GenreProps> = (props) => {
 		<Button
 			color={isActive ? color : 'gray'}
 			colorMode={colorMode}
-			renderRight={isActive ? (props) => <Icon {...props} icon='check' category='outlined' /> : undefined}
+			renderRight={
+				isActive
+					? ({ colorMode, height }) => (
+							<Icon
+								colorMode={colorMode}
+								width={`${height}px`}
+								height={`${height}px`}
+								fontSize={`${height}px`}
+								icon='check'
+								category='outlined'
+							/>
+					  )
+					: undefined
+			}
 			onClick={() => onClick()}
 			variant='outlined'
 		>
