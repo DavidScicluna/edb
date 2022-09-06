a04c48449733b6ab6d2ba4aa2ab3311d79746d9d
David Scicluna <dave_nostra@hotmail.com>
2022-09-06 22:04:08 +0200

    Fix: Authentication - Signin

--- src/pages/Authentication/pages/Signin/index.tsx
+++ src/pages/Authentication/pages/Signin/index.tsx
@@ -1,17 +1,17 @@
 import { FC, useState, useCallback } from 'react';
 
-import { useNavigate } from 'react-router';
+import { useNavigate, useOutletContext } from 'react-router';
 
 import { useTheme, utils } from '@davidscicluna/component-library';
 
-import { useMediaQuery, Center, Container, VStack } from '@chakra-ui/react';
+import { useMediaQuery, HStack, VStack } from '@chakra-ui/react';
 
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useForm, useWatch } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
-import { useElementSize, useUpdateEffect } from 'usehooks-ts';
-import { debounce, merge } from 'lodash';
-import { SHA256 } from 'crypto-js';
+import { useElementSize, useUpdateEffect, useDebounce, useWindowSize } from 'usehooks-ts';
+import { debounce } from 'lodash';
+import sha256 from 'crypto-js/sha256';
 import { sort } from 'fast-sort';
 import dayjs from 'dayjs';
 
@@ -20,7 +20,9 @@ import { useSelector, useUserTheme } from '../../../../common/hooks';
 import { User } from '../../../../store/slices/Users/types';
 import { guest, setUser, setUsers } from '../../../../store/slices/Users';
 import { toggleSpinnerModal } from '../../../../store/slices/Modals';
+import { color as defaultColor, colorMode as defaultColorMode } from '../../../../common/data/defaultPropValues';
 import { getBoringAvatarSrc } from '../../../../common/utils';
+import { AuthenticationOutletContext } from '../../types';
 
 import Form from './components/Form';
 import Header from './components/Header';
@@ -29,7 +31,7 @@ import { schema } from './validation';
 import Footer from './components/Footer';
 import Users from './components/Users';
 
-const { getHue } = utils;
+const { getHue, getColorMode } = utils;
 
 export const defaultValues: FormType = {
 	username: '',
@@ -40,15 +42,25 @@ export const defaultValues: FormType = {
 const SignIn: FC = () => {
 	const theme = useTheme();
 	const [isLg] = useMediaQuery(`(min-width: ${theme.breakpoints.lg})`);
+	const [isXl] = useMediaQuery(`(min-width: ${theme.breakpoints.xl})`);
 
-	const { colorMode } = useUserTheme();
+	const userTheme = useUserTheme();
 
 	const navigate = useNavigate();
+	const {
+		color = defaultColor,
+		colorMode = defaultColorMode,
+		setColor,
+		setColorMode
+	} = useOutletContext<AuthenticationOutletContext>();
 
 	const dispatch = useDispatch();
 	const users = useSelector((state) => state.users.data.users);
 
-	const [illustrationRef, { width: illustrationWidth }] = useElementSize();
+	const { width: windowWidth } = useWindowSize();
+
+	const [containerRef, { width: containerWidth = 0 }] = useElementSize();
+	const debouncedContainerWidth = useDebounce<number>(containerWidth, 0);
 
 	const [selectedUserID, setSelectedUserID] = useState<string>('');
 
@@ -62,9 +74,15 @@ const SignIn: FC = () => {
 	const watchPassword = useWatch({ control, name: 'password' });
 
 	const handleUserClick = (user: User): void => {
+		const { color, colorMode } = user.ui.theme;
+
 		setSelectedUserID(user.data.id);
 
+		setColor(color);
+		setColorMode(colorMode === 'system' ? getColorMode() : colorMode);
+
 		setValue('username', user.data.credentials.username, { shouldDirty: true });
+		setValue('rememberMe', user.data.credentials.rememberMe, { shouldDirty: true });
 	};
 
 	const handleCheckForm = useCallback(
@@ -77,9 +95,12 @@ const SignIn: FC = () => {
 				user.data.credentials.password !== watchPassword
 			) {
 				setSelectedUserID('');
+
+				setColor(userTheme.color);
+				setColorMode(userTheme.colorMode);
 			}
 		}, 500),
-		[users, selectedUserID, watchUsername, watchPassword]
+		[users, selectedUserID, watchUsername, watchPassword, userTheme]
 	);
 
 	const handleSubmitAsGuest = (): void => {
@@ -88,22 +109,20 @@ const SignIn: FC = () => {
 		// TODO: Check if avatar is re generated
 		dispatch(
 			setUser({
-				...merge(guest, {
-					...guest,
-					data: {
-						...guest.data,
-						info: {
-							...guest.data.info,
-							avatar_path: getBoringAvatarSrc({
-								id: guest.data.id,
-								colors: theme.colors,
-								hue: getHue({ colorMode, type: 'color' }),
-								size: 500,
-								variant: 'beam'
-							})
-						}
+				...guest,
+				data: {
+					...guest.data,
+					info: {
+						...guest.data.info,
+						avatar_path: getBoringAvatarSrc({
+							id: guest.data.id,
+							colors: theme.colors,
+							hue: getHue({ colorMode, type: 'color' }),
+							size: 500,
+							variant: 'beam'
+						})
 					}
-				})
+				}
 			})
 		);
 
@@ -115,22 +134,21 @@ const SignIn: FC = () => {
 	const handleSubmitForm = (credentials: FormType): void => {
 		const user = users.find((user) => user.data.id === selectedUserID);
 
-		if (user && SHA256(credentials.password).toString() === user.data.credentials.password) {
+		// TODO: Implement global toast system and add success alert
+
+		if (user && sha256(credentials.password).toString() === user.data.credentials.password) {
 			dispatch(toggleSpinnerModal(true));
 
 			const { rememberMe } = getValues();
 
-			const updatedUser: User = merge(
-				{ ...user },
-				{
-					...user,
-					data: {
-						...user.data,
-						credentials: { ...user.data.credentials, rememberMe },
-						signedInAt: dayjs().toISOString()
-					}
+			const updatedUser: User = {
+				...user,
+				data: {
+					...user.data,
+					credentials: { ...user.data.credentials, rememberMe },
+					signedInAt: dayjs().toISOString()
 				}
-			);
+			};
 
 			const updatedUsers: User[] = sort([
 				...users.filter((u) => u.data.id !== updatedUser.data.id),
@@ -144,46 +162,64 @@ const SignIn: FC = () => {
 
 			setTimeout(() => dispatch(toggleSpinnerModal(false)), 2500);
 		} else {
-			// TODO: Implement global toast system
+			// TODO: Implement global toast system and add error alert
 		}
 	};
 
 	useUpdateEffect(() => handleCheckForm(), [watchUsername, watchPassword]);
 
 	return (
-		<Center width='100%' minHeight='100vh' position='relative' overflowX='hidden' overflowY='auto'>
-			{isLg && <Illustration ref={illustrationRef} position='fixed' top={0} left={0} />}
+		<HStack
+			ref={containerRef}
+			width='100%'
+			minHeight='100vh'
+			position='relative'
+			alignItems='center'
+			justifyContent='flex-end'
+			spacing={0}
+		>
+			{isLg && (
+				<Illustration
+					width={`${debouncedContainerWidth / 2}px`}
+					height='100vh'
+					position='fixed'
+					top={0}
+					left={`${isXl ? (windowWidth - containerWidth) / 2 : 0}px`}
+					colorMode={colorMode}
+				/>
+			)}
 
-			<Center
-				width={isLg ? `calc(100% - ${illustrationWidth}px)` : '100%'}
+			<VStack
+				width={isLg ? `${debouncedContainerWidth / 2}px` : '100%'}
 				minHeight='100vh'
-				position='absolute'
-				top={0}
-				right={0}
-				alignItems='center'
-				justifyContent='center'
+				alignItems='stretch'
+				justifyContent='space-between'
+				spacing={[3, 3, 4, 4]}
+				px={[2, 2, 3, 3]}
+				py={[3, 3, 4, 4]}
 			>
-				<Container
-					as={VStack}
-					width='100%'
-					minHeight='inherit'
-					maxWidth='container.lg'
-					centerContent
-					justifyContent='space-between'
-					spacing={[3, 3, 4, 4]}
-					px={[2, 2, 3, 3]}
-					py={[3, 3, 4, 4]}
-				>
-					<Header />
+				<Header colorMode={colorMode} />
 
-					{users.length > 0 && <Users selectedUserID={selectedUserID} onUserClick={handleUserClick} />}
+				{users.length > 0 && (
+					<Users
+						color={color}
+						colorMode={colorMode}
+						selectedUserID={selectedUserID}
+						onUserClick={handleUserClick}
+					/>
+				)}
 
-					<Form form={form} onSubmitAsGuest={handleSubmitAsGuest} onSubmit={handleSubmitForm} />
+				<Form
+					color={color}
+					colorMode={colorMode}
+					form={form}
+					onSubmitAsGuest={handleSubmitAsGuest}
+					onSubmit={handleSubmitForm}
+				/>
 
-					<Footer />
-				</Container>
-			</Center>
-		</Center>
+				<Footer color={color} colorMode={colorMode} />
+			</VStack>
+		</HStack>
 	);
 };
 
