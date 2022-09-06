bf590ed7c7d0181cfec249e8853e1dddec711137
David Scicluna <dave_nostra@hotmail.com>
2022-09-06 22:04:27 +0200

    Feature: Authentication

---
+++ src/pages/Authentication/index.tsx
@@ -0,0 +1,43 @@
+import { FC, useState } from 'react';
+
+import { Outlet } from 'react-router';
+
+import { useTheme, utils } from '@davidscicluna/component-library';
+
+import { ColorMode, useMediaQuery, Center, Container } from '@chakra-ui/react';
+
+import { color as defaultColor, colorMode as defaultColorMode } from '../../common/data/defaultPropValues';
+import { useUserTheme } from '../../common/hooks';
+import { UserThemeColor } from '../../store/slices/Users/types';
+
+const { getColor } = utils;
+
+const Authentication: FC = () => {
+	const theme = useTheme();
+	const userTheme = useUserTheme();
+
+	const [isXl] = useMediaQuery(`(min-width: ${theme.breakpoints['2xl']})`);
+
+	const [color, setColor] = useState<UserThemeColor>(userTheme.color || defaultColor);
+	const [colorMode, setColorMode] = useState<ColorMode>(userTheme.colorMode || defaultColorMode);
+
+	return (
+		<Center width='100%' minHeight='100vh' sx={{ background: getColor({ theme, colorMode, type: 'background' }) }}>
+			<Container
+				width='100%'
+				maxWidth={theme.breakpoints['2xl']}
+				minHeight='100vh'
+				centerContent
+				borderLeftWidth={isXl ? '2px' : 0}
+				borderRightWidth={isXl ? '2px' : 0}
+				borderStyle='solid'
+				borderColor={getColor({ theme, colorMode, type: 'divider' })}
+				p={0}
+			>
+				<Outlet context={{ color, colorMode, setColor, setColorMode }} />
+			</Container>
+		</Center>
+	);
+};
+
+export default Authentication;
