7bab7a3d0c4421c68c0251385a9108454d5421af
David Scicluna <dave_nostra@hotmail.com>
2022-09-07 19:58:55 +0200

    Fix: Store - Users Slice

--- src/store/slices/Users/index.ts
+++ src/store/slices/Users/index.ts
@@ -1,30 +1,43 @@
+import { sort } from 'fast-sort';
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import dayjs from 'dayjs';
+import { memoize, sample } from 'lodash';
 import { v4 as uuid } from 'uuid';
 
-import { StateProps, UserAction, User, Info, Search, MediaItems, List, UserReview, OtherReview, Theme } from './types';
+import { color as defaultColor } from '../../../common/data/defaultPropValues';
 
-export const guest: User = {
-	data: {
-		id: 'guest',
-		credentials: {
-			username: 'guest',
-			password: '',
-			rememberMe: false
-		},
-		info: {
-			name: 'Guest',
-			avatar_path: '',
-			background_path: ''
-		}
-	},
-	ui: {
-		theme: {
-			color: 'light_blue',
-			colorMode: 'light'
-		}
-	}
-};
+import {
+	GetUserProps,
+	UpdateUsersProps,
+	StateProps,
+	UserAction,
+	User,
+	Info,
+	Search,
+	MediaItems,
+	List,
+	// UserReview,
+	// OtherReview,
+	UserTheme,
+	UserThemeColor
+} from './types';
+
+const colors: UserThemeColor[] = [
+	'pink',
+	'purple',
+	'deep_purple',
+	'indigo',
+	'blue',
+	'light_blue',
+	'cyan',
+	'teal',
+	'light_green',
+	'lime',
+	'orange',
+	'deep_orange'
+];
+
+const color: UserThemeColor = sample(colors) || defaultColor;
 
 export const defaultUser: User = {
 	data: {
@@ -36,8 +49,13 @@ export const defaultUser: User = {
 		},
 		info: {
 			name: '',
+			bio: '',
 			avatar_path: '',
-			background_path: ''
+			background_path: '',
+			prefers: {
+				movie: [],
+				tv: []
+			}
 		},
 		recentSearches: [],
 		recentlyViewed: {
@@ -59,21 +77,53 @@ export const defaultUser: User = {
 				label: 'Watchlist',
 				description:
 					"A collection of movies and tv shows that I'm looking forward to watching and hopefully re-watch 🥳 🤓",
-				date: dayjs(new Date()).toISOString(),
+				updatedAt: dayjs(new Date()).toISOString(),
+				createdAt: dayjs(new Date()).toISOString(),
 				results: {
 					movies: [],
 					tv: []
 				}
 			}
 		],
-		reviews: {
-			user: [],
-			other: []
-		}
+		// reviews: {
+		// 	user: [],
+		// 	other: []
+		// },
+		signedInAt: dayjs().toISOString(),
+		updatedAt: dayjs().toISOString(),
+		createdAt: dayjs().toISOString()
 	},
 	ui: {
 		theme: {
-			color: 'light_blue',
+			color: color,
+			colorMode: 'system'
+		}
+	}
+};
+
+// TODO: Set User Boring Avatar src on render if user is not logged in
+export const guest: User = {
+	...defaultUser,
+	data: {
+		...defaultUser.data,
+		id: 'ds-edb-guest-user',
+		info: {
+			...defaultUser.data.info,
+			name: 'Guest',
+			avatar_path:
+				'https://source.boringavatars.com/beam/500/ds-edb-guest-user?colors=%23ef5350%2C%23ec407a%2C%23ab47bc%2C%237e57c2%2C%235c6bc0%2C%2342a5f5%2C%2329b6f6%2C%2326c6da%2C%2326a69a%2C%2366bb6a%2C%239ccc65%2C%23d4e157%2C%23ffca28%2C%23ffa726%2C%23ff7043&square=true'
+		},
+		lists: []
+		// reviews: {
+		// 	user: [],
+		// 	other: []
+		// },
+	},
+	ui: {
+		...defaultUser.ui,
+		theme: {
+			...defaultUser.ui.theme,
+			color: color,
 			colorMode: 'light'
 		}
 	}
@@ -81,157 +131,216 @@ export const defaultUser: User = {
 
 const initialState: StateProps = {
 	data: {
-		users: [guest]
+		activeUser: { ...guest },
+		users: []
 	}
 };
+
+const getUser = memoize(({ users, user }: GetUserProps): User => {
+	return users.find((u) => u.data.id === user) || { ...guest };
+});
+
+const updateUsers = memoize(({ users, user }: UpdateUsersProps): User[] => {
+	return sort([...users.filter((u) => u.data.id !== user.data.id), user]).desc((u) => u.data.updatedAt);
+});
 
 const usersSlice = createSlice({
 	name: 'users',
 	initialState,
 	reducers: {
+		setUser: (state: StateProps, action: PayloadAction<User>) => {
+			state.data.activeUser = action.payload;
+		},
 		setUsers: (state: StateProps, action: PayloadAction<User[]>) => {
 			state.data.users = action.payload;
 		},
 		setUserInfo: (state: StateProps, action: UserAction<Info>) => {
-			state.data.users = state.data.users.map((user) =>
-				user.data.id === action.payload.id
-					? {
-							...user,
-							data: {
-								...user.data,
-								info: {
-									...user.data.info,
-									...action.payload.data
-								}
-							}
-					  }
-					: { ...user }
-			);
+			const user = getUser({ users: state.data.users, user: action.payload.id });
+			const updatedUser: User = {
+				...user,
+				data: {
+					...user.data,
+					info: {
+						...user.data.info,
+						...action.payload.data
+					},
+					updatedAt: dayjs().toISOString()
+				}
+			};
+
+			state.data.users = updateUsers({
+				users: state.data.users,
+				user: { ...updatedUser }
+			});
+
+			if (state.data.activeUser.data.id === user.data.id) {
+				state.data.activeUser = { ...updatedUser };
+			}
 		},
 		setUserRecentSearches: (state: StateProps, action: UserAction<Search[]>) => {
-			state.data.users = state.data.users.map((user) =>
-				user.data.id === action.payload.id
-					? {
-							...user,
-							data: {
-								...user.data,
-								recentSearches: [...(user.data.recentSearches || []), ...action.payload.data]
-							}
-					  }
-					: { ...user }
-			);
+			const user = getUser({ users: state.data.users, user: action.payload.id });
+			const updatedUser: User = {
+				...user,
+				data: {
+					...user.data,
+					recentSearches: [...user.data.recentSearches, ...action.payload.data],
+					updatedAt: dayjs().toISOString()
+				}
+			};
+
+			state.data.users = updateUsers({
+				users: state.data.users,
+				user: { ...updatedUser }
+			});
+
+			if (state.data.activeUser.data.id === user.data.id) {
+				state.data.activeUser = { ...updatedUser };
+			}
 		},
 		setUserRecentlyViewed: (state: StateProps, action: UserAction<Omit<MediaItems, 'companies'>>) => {
-			state.data.users = state.data.users.map((user) =>
-				user.data.id === action.payload.id
-					? {
-							...user,
-							data: {
-								...user.data,
-								recentlyViewed: {
-									...user.data.recentlyViewed,
-									...action.payload.data
-								}
-							}
-					  }
-					: { ...user }
-			);
+			const user = getUser({ users: state.data.users, user: action.payload.id });
+			const updatedUser: User = {
+				...user,
+				data: {
+					...user.data,
+					recentlyViewed: {
+						...user.data.recentlyViewed,
+						...action.payload.data
+					},
+					updatedAt: dayjs().toISOString()
+				}
+			};
+
+			state.data.users = updateUsers({
+				users: state.data.users,
+				user: { ...updatedUser }
+			});
+
+			if (state.data.activeUser.data.id === user.data.id) {
+				state.data.activeUser = { ...updatedUser };
+			}
 		},
 		setUserLiked: (state: StateProps, action: UserAction<MediaItems>) => {
-			state.data.users = state.data.users.map((user) =>
-				user.data.id === action.payload.id
-					? {
-							...user,
-							data: {
-								...user.data,
-								liked: {
-									...user.data.liked,
-									...action.payload.data
-								}
-							}
-					  }
-					: { ...user }
-			);
+			const user = getUser({ users: state.data.users, user: action.payload.id });
+			const updatedUser: User = {
+				...user,
+				data: {
+					...user.data,
+					liked: {
+						...user.data.liked,
+						...action.payload.data
+					},
+					updatedAt: dayjs().toISOString()
+				}
+			};
+
+			state.data.users = updateUsers({
+				users: state.data.users,
+				user: { ...updatedUser }
+			});
+
+			if (state.data.activeUser.data.id === user.data.id) {
+				state.data.activeUser = { ...updatedUser };
+			}
 		},
+		// TODO: Maybe add more reducers to update specific lists with utils like user
 		setUserLists: (state: StateProps, action: UserAction<List[]>) => {
-			state.data.users = state.data.users.map((user) =>
-				user.data.id === action.payload.id
-					? {
-							...user,
-							data: {
-								...user.data,
-								lists: [...(user.data.lists || []), ...action.payload.data]
-							}
-					  }
-					: { ...user }
-			);
+			const user = getUser({ users: state.data.users, user: action.payload.id });
+			const updatedUser: User = {
+				...user,
+				data: {
+					...user.data,
+					lists: [...user.data.lists, ...action.payload.data],
+					updatedAt: dayjs().toISOString()
+				}
+			};
+
+			state.data.users = updateUsers({
+				users: state.data.users,
+				user: { ...updatedUser }
+			});
+
+			if (state.data.activeUser.data.id === user.data.id) {
+				state.data.activeUser = { ...updatedUser };
+			}
 		},
-		setUserReviews: (state: StateProps, action: UserAction<UserReview[]>) => {
-			state.data.users = state.data.users.map((user) =>
-				user.data.id === action.payload.id
-					? {
-							...user,
-							data: {
-								...user.data,
-								reviews: {
-									...(user.data.reviews || {}),
-									user: [...action.payload.data],
-									other: [...(user.data.reviews?.other || [])]
-								}
-							}
-					  }
-					: { ...user }
-			);
-		},
-		setUserOtherReviews: (state: StateProps, action: UserAction<OtherReview[]>) => {
-			state.data.users = state.data.users.map((user) =>
-				user.data.id === action.payload.id
-					? {
-							...user,
-							data: {
-								...user.data,
-								reviews: {
-									...(user.data.reviews || {}),
-									user: [...(user.data.reviews?.user || [])],
-									other: [...action.payload.data]
-								}
-							}
-					  }
-					: { ...user }
-			);
-		},
-		setUserTheme: (state: StateProps, action: UserAction<Theme>) => {
-			state.data.users = state.data.users.map((user) =>
-				user.data.id === action.payload.id
-					? {
-							...user,
-							ui: {
-								...user.ui,
-								theme: {
-									...user.ui.theme,
-									...action.payload.data
-								}
-							}
-					  }
-					: { ...user }
-			);
+		// setUserReviews: (state: StateProps, action: UserAction<UserReview[]>) => {
+		// const user = getUser({ users: state.data.users, user: action.payload.id });
+		//
+		// 	state.data.users = state.data.users.map((user) =>
+		// 		user.data.id === action.payload.id
+		// 			? {
+		// 					...user,
+		// 					data: {
+		// 						...user.data,
+		// 						reviews: {
+		// 							...(user.data.reviews || {}),
+		// 							user: [...action.payload.data],
+		// 							other: [...(user.data.reviews?.other || [])]
+		// 						}
+		// 					}
+		// 			  }
+		// 			: { ...user }
+		// 	);
+		// },
+		// setUserOtherReviews: (state: StateProps, action: UserAction<OtherReview[]>) => {
+		// const user = getUser({ users: state.data.users, user: action.payload.id });
+		//
+		// 	state.data.users = state.data.users.map((user) =>
+		// 		user.data.id === action.payload.id
+		// 			? {
+		// 					...user,
+		// 					data: {
+		// 						...user.data,
+		// 						reviews: {
+		// 							...(user.data.reviews || {}),
+		// 							user: [...(user.data.reviews?.user || [])],
+		// 							other: [...action.payload.data]
+		// 						}
+		// 					}
+		// 			  }
+		// 			: { ...user }
+		// 	);
+		// },
+		setUserTheme: (state: StateProps, action: UserAction<UserTheme>) => {
+			const user = getUser({ users: state.data.users, user: action.payload.id });
+			const updatedUser: User = {
+				...user,
+				data: {
+					...user.data,
+					updatedAt: dayjs().toISOString()
+				},
+				ui: {
+					...user.ui,
+					theme: {
+						...user.ui.theme,
+						...action.payload.data
+					}
+				}
+			};
+
+			state.data.users = updateUsers({
+				users: state.data.users,
+				user: { ...updatedUser }
+			});
+
+			if (state.data.activeUser.data.id === user.data.id) {
+				state.data.activeUser = { ...updatedUser };
+			}
 		}
 	}
 });
 
-export const getUser = (users: User[], id?: string): User | undefined => {
-	return users.find((user) => user.data.id === id);
-};
-
 export const {
+	setUser,
 	setUsers,
 	setUserInfo,
 	setUserRecentSearches,
 	setUserRecentlyViewed,
 	setUserLiked,
 	setUserLists,
-	setUserReviews,
-	setUserOtherReviews,
+	// setUserReviews,
+	// setUserOtherReviews,
 	setUserTheme
 } = usersSlice.actions;
 
--- src/store/slices/Users/types.ts
+++ src/store/slices/Users/types.ts
@@ -4,10 +4,14 @@ import { ColorMode } from '@chakra-ui/react';
 
 import { PayloadAction } from '@reduxjs/toolkit';
 
-import { MediaType, Review, FullCompany, Genre } from '../../../common/types';
+import { MediaType, FullCompany, Genre } from '../../../common/types';
 import { FullMovie, Collection } from '../../../common/types/movie';
 import { FullPerson } from '../../../common/types/person';
 import { FullTV } from '../../../common/types/tv';
+
+export type GetUserProps = { users: User[]; user: string };
+
+export type UpdateUsersProps = { users: User[]; user: User };
 
 export type Credentials = {
 	username: string;
@@ -22,19 +26,19 @@ export type InfoGenres = {
 
 export type Info = {
 	name: string;
-	bio?: string;
+	bio: string;
 	avatar_path: string;
 	background_path: string;
-	prefers?: InfoGenres;
+	prefers: InfoGenres;
 };
 
-export type SearchType = MediaType | 'collection' | string;
+export type SearchType = MediaType & string;
 
 export type Search = {
 	id: string;
 	label: string;
 	date: string;
-	searchTypes?: SearchType[];
+	searchTypes: SearchType[];
 };
 
 export type GetMediaType<MT extends MediaType> = MT extends 'movie'
@@ -48,7 +52,7 @@ export type GetMediaType<MT extends MediaType> = MT extends 'movie'
 	: Collection;
 
 export type MediaItem<MT extends MediaType> = {
-	dateAdded?: string;
+	dateAdded: string;
 } & GetMediaType<MT>;
 
 export type MediaItems = {
@@ -63,48 +67,50 @@ export type List = {
 	id: string;
 	label: string;
 	description?: string;
-	date: string;
+	updatedAt: string;
+	createdAt: string;
 	results: Omit<MediaItems, 'people' | 'companies' | 'collections'>;
 };
 
-export type ReviewState = 'isLiked' | 'isDisliked';
+// export type ReviewState = 'isLiked' | 'isDisliked';
 
-export type UserReview = {
-	mediaItem: { mediaType: Omit<MediaType, 'person' | 'company' | 'collection'> } & (FullMovie | FullTV);
-} & Omit<Review, 'media_id' | 'media_title' | 'media_type'>;
+// export type UserReview = {
+// 	mediaItem: { mediaType: Omit<MediaType, 'person' | 'company' | 'collection'> } & (FullMovie | FullTV);
+// } & Omit<Review, 'media_id' | 'media_title' | 'media_type'>;
 
-export type OtherReview = {
-	state?: ReviewState;
-} & Omit<Review, 'media_id' | 'media_title' | 'media_type'>;
+// export type OtherReview = {
+// 	state?: ReviewState;
+// } & Omit<Review, 'media_id' | 'media_title' | 'media_type'>;
 
-export type UserReviews = {
-	user: UserReview[];
-	other: OtherReview[];
-};
+// export type UserReviews = {
+// 	user: UserReview[];
+// 	other: OtherReview[];
+// };
 
-export type ThemeColor = Exclude<Color, 'gray' | 'red' | 'green' | 'yellow'>;
-export type ThemeColorMode = ColorMode | 'system';
+export type UserThemeColor = Exclude<Color, 'transparent' | 'black' | 'white' | 'gray' | 'red' | 'green' | 'yellow'>;
+export type UserThemeColorMode = ColorMode | 'system';
 
-export type Theme = {
-	// color: ThemeColor;
-	color: any;
-	colorMode: ThemeColorMode;
+export type UserTheme = {
+	color: UserThemeColor;
+	colorMode: UserThemeColorMode;
 };
 
 export type User = {
 	data: {
 		id: string;
-		credentials?: Credentials;
+		credentials: Credentials;
 		info: Info;
-		recentSearches?: Search[];
-		recentlyViewed?: Omit<MediaItems, 'companies'>;
-		liked?: MediaItems;
-		lists?: List[];
-		reviews?: UserReviews;
-		signedInAt?: string;
+		recentSearches: Search[];
+		recentlyViewed: Omit<MediaItems, 'companies'>;
+		liked: MediaItems;
+		lists: List[];
+		// reviews: UserReviews;
+		signedInAt: string;
+		updatedAt: string;
+		createdAt: string;
 	};
 	ui: {
-		theme: Theme;
+		theme: UserTheme;
 	};
 };
 
@@ -112,6 +118,7 @@ export type UserAction<P> = PayloadAction<{ id: string; data: P }>;
 
 export type StateProps = {
 	data: {
+		activeUser: User;
 		users: User[];
 	};
 };
