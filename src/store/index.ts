5715bb2cdcd9347f86337bc4880ffa94173c759c
David Scicluna <dave_nostra@hotmail.com>
2022-09-07 19:59:03 +0200

    Fix: Store

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,21 +2,20 @@ import { configureStore } from '@reduxjs/toolkit';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
-import rootReducer from './rootReducer';
+import slices from './slices';
 
 const persistConfig = {
 	key: 'root',
 	storage,
+	whitelist: ['app', 'users'],
 	blacklist: ['modals', 'options']
 };
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+const persistedReducer = persistReducer(persistConfig, slices);
 
 const store = configureStore({
 	reducer: persistedReducer,
 	devTools: process.env.NODE_ENV !== 'production'
 });
-
-export type RootState = ReturnType<typeof rootReducer>;
 
 export default store;
---
+++ src/store/types.ts
@@ -0,0 +1,3 @@
+import slices from './slices';
+
+export type State = ReturnType<typeof slices>;
