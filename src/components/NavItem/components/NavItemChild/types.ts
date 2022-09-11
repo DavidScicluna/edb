7b451737c0df4941edcef36f55ff771407514c31
David Scicluna <dave_nostra@hotmail.com>
2022-09-11 15:21:16 +0200

    Fix: Removed NavItem

--- src/components/NavItem/components/NavItemChild/types.ts
+++
@@ -1,7 +0,0 @@
-import { NavItem } from '../../types';
-
-export type NavItemChild = { renderChild: boolean } & Omit<NavItem, 'renderIcon' | 'children'>;
-
-export type NavItemChildProps = {
-	isLastChild?: boolean;
-} & Omit<NavItemChild, 'renderChild'>;
