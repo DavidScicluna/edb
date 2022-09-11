62aaa8b15f3a5df095c82191900c4b51b5718572
David Scicluna <dave_nostra@hotmail.com>
2022-09-11 18:12:21 +0200

    Fix: Page

--- src/containers/Page/index.tsx
+++ src/containers/Page/index.tsx
@@ -1,41 +1,15 @@
-import { ReactElement } from 'react';
+import { FC } from 'react';
 
-import { useLocation } from 'react-router-dom';
+import { Divider } from '@davidscicluna/component-library';
 
-import { useBoolean, VStack, Box, Collapse } from '@chakra-ui/react';
+import { VStack } from '@chakra-ui/react';
 
-import { useEffectOnce } from 'usehooks-ts';
-
-import Divider from '../../components/Divider';
-
-import Header from './components/Header';
 import { PageProps } from './types';
 
-const Page = (props: PageProps): ReactElement => {
-	const location = useLocation();
-
-	const { children, title, direction } = props;
-	const { body, ...rest } = children;
-
-	const [isHome, setIsHome] = useBoolean(true);
-
-	useEffectOnce(() => {
-		if (location.pathname === '/') {
-			setTimeout(() => setIsHome.on(), 2500);
-		} else {
-			setIsHome.off();
-		}
-	});
-
+const Page: FC<PageProps> = ({ children, ...rest }) => {
 	return (
-		<VStack width='100%' divider={!isHome ? <Divider /> : undefined} spacing={0}>
-			{/* Header */}
-			<Collapse in={!isHome} unmountOnExit style={{ width: '100%' }}>
-				<Header {...rest} title={title} direction={direction} />{' '}
-			</Collapse>
-
-			{/* Body */}
-			<Box width='100%'>{body}</Box>
+		<VStack {...rest} width='100%' divider={<Divider />} spacing={0}>
+			{children}
 		</VStack>
 	);
 };
--- src/containers/Page/types.ts
+++ src/containers/Page/types.ts
@@ -1,21 +1,3 @@
-import { ReactElement } from 'react';
-
 import { StackProps } from '@chakra-ui/react';
 
-export type RenderProps = {
-	width: number;
-	height: number;
-};
-
-export type PageChildren = {
-	renderLeftHeaderPanel?: (props: RenderProps) => ReactElement;
-	renderRightHeaderPanel?: (props: RenderProps) => ReactElement;
-	actions?: ReactElement;
-	body: ReactElement;
-};
-
-export type PageProps = {
-	children: PageChildren;
-	title?: string | ReactElement;
-	direction?: StackProps['direction'];
-};
+export type PageProps = Omit<StackProps, 'direction'>;
