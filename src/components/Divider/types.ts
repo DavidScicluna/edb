b58455f6bd115161779de0f77e1196fbdf8ce1d1
David Scicluna <dave_nostra@hotmail.com>
2022-09-11 15:21:54 +0200

    Fix: Divider - Replaced local Divider with Component-Library Divider

--- src/components/Divider/types.ts
+++
@@ -1,8 +0,0 @@
-import { BoxProps, ColorMode } from '@chakra-ui/react';
-
-import { Orientation } from '../../common/types';
-
-export type DividerProps = {
-	colorMode?: ColorMode;
-	orientation?: Orientation;
-} & BoxProps;
