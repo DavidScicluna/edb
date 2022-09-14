a5f8f6704c2cd1f0dbef13651d6f1ed6f4280f34
David Scicluna <dave_nostra@hotmail.com>
2022-09-14 21:55:23 +0200

    Feature: Layout - Modals - BookmarkModal - AddBookmark

---
+++ src/containers/Layout/components/Modals/BookmarkModal/components/AddBookmark/index.tsx
@@ -0,0 +1,170 @@
+import { FC, useState, useCallback, useEffect } from 'react';
+
+import {
+	useTheme,
+	Modal,
+	ModalHeader,
+	ModalBody,
+	ModalFooter,
+	Button,
+	IconButton,
+	IconButtonIcon
+} from '@davidscicluna/component-library';
+
+import { useMediaQuery, useDisclosure, VStack, Text } from '@chakra-ui/react';
+
+import { useDispatch } from 'react-redux';
+import dayjs from 'dayjs';
+import { includes } from 'lodash';
+import { sort } from 'fast-sort';
+
+import { CommonBookmarkModalProps as AddBookmarkProps } from '../../common/types';
+import { useSelector, useUserTheme } from '../../../../../../../common/hooks';
+import CreateList from '../../../../../../../pages/User/pages/Lists/components/CreateList';
+import { setUserLists } from '../../../../../../../store/slices/Users';
+import { formatMediaTypeLabel } from '../../../../../../../common/utils';
+import { FullMovie } from '../../../../../../../common/types/movie';
+import { FullTV } from '../../../../../../../common/types/tv';
+import { UserList as UserList, UserListMediaItems, MediaItem } from '../../../../../../../store/slices/Users/types';
+
+import { BookmarkListSelected } from './types';
+import List from './components/List';
+import { OnListClickProps } from './components/List/types';
+
+const AddBookmark: FC<AddBookmarkProps> = (props) => {
+	const theme = useTheme();
+	const { color, colorMode } = useUserTheme();
+
+	const [isSm] = useMediaQuery(`(max-width: ${theme.breakpoints.sm})`);
+
+	const { isOpen: isCreateListOpen, onOpen: onCreateListOpen, onClose: onCreateListClose } = useDisclosure();
+
+	const dispatch = useDispatch();
+	const {
+		data: { id, lists = [] }
+	} = useSelector((state) => state.users.data.activeUser);
+
+	const { mediaType, mediaItem, title, isOpen = false, onClose } = props;
+
+	const [selected, setSelected] = useState<BookmarkListSelected>([]);
+
+	const handleOnListClick = useCallback(
+		({ id, isSelected }: OnListClickProps): void => {
+			if (isSelected) {
+				setSelected(selected.filter((list) => list !== id));
+			} else {
+				setSelected([...selected, id]);
+			}
+		},
+		[selected]
+	);
+
+	const handleSaveItem = useCallback((): void => {
+		const updatedLists: UserList[] = lists.map((list) => {
+			if (includes([...selected], list.id)) {
+				const mediaItems: UserListMediaItems = { ...list.mediaItems };
+
+				switch (mediaType) {
+					case 'movie': {
+						mediaItems.movies = sort([
+							...mediaItems.movies,
+							{
+								mediaItem: { ...(mediaItem as FullMovie) },
+								mediaType: 'movie',
+								addedAt: dayjs(new Date()).toISOString()
+							} as MediaItem<'movie'>
+						]).by({ desc: (movie) => movie.addedAt });
+						break;
+					}
+					case 'tv': {
+						mediaItems.tv = sort([
+							...mediaItems.tv,
+							{
+								mediaItem: { ...(mediaItem as FullTV) },
+								mediaType: 'tv',
+								addedAt: dayjs(new Date()).toISOString()
+							} as MediaItem<'tv'>
+						]).by({ desc: (show) => show.addedAt });
+						break;
+					}
+				}
+
+				return {
+					...list,
+					updatedAt: dayjs(new Date()).toISOString(),
+					mediaItems: { ...mediaItems }
+				};
+			} else {
+				return { ...list };
+			}
+		});
+
+		dispatch(setUserLists({ id, data: sort([...updatedLists]).by({ desc: (list) => list.updatedAt }) }));
+	}, [id, lists, selected, mediaType, mediaItem]);
+
+	const handleResetModal = useCallback((): void => {
+		setSelected([]);
+	}, []);
+
+	useEffect(() => handleResetModal(), [isOpen]);
+
+	return (
+		<>
+			<Modal colorMode={colorMode} isOpen={isOpen} onClose={onClose} size='2xl'>
+				<ModalHeader
+					renderTitle={(props) => (
+						<Text {...props}>{isSm || !!title ? 'Add to a list/s' : `Add "${title}" to a list/s`}</Text>
+					)}
+					renderSubtitle={(props) => (
+						<Text {...props}>{`Create a new list or add the ${formatMediaTypeLabel({
+							type: 'single',
+							mediaType
+						})} to an existing list by selecting a list below!`}</Text>
+					)}
+					renderCancel={({ icon, category, ...rest }) => (
+						<IconButton {...rest}>
+							<IconButtonIcon icon={icon} category={category} />
+						</IconButton>
+					)}
+				/>
+				<ModalBody>
+					<VStack spacing={2} p={2}>
+						{lists.map((list) => (
+							<List
+								key={list.id}
+								list={list}
+								isSelected={includes([...selected], list.id)}
+								onClick={handleOnListClick}
+							/>
+						))}
+					</VStack>
+				</ModalBody>
+				<ModalFooter
+					renderCancel={(props) => <Button {...props}>Cancel</Button>}
+					renderAction={(props) => (
+						<Button
+							{...props}
+							color={color}
+							isDisabled={!!id}
+							onClick={selected.length > 0 ? () => handleSaveItem() : () => onCreateListOpen()}
+						>
+							{selected.length > 0
+								? `Save ${formatMediaTypeLabel({ type: 'single', mediaType })} to List${
+										selected.length === 1 ? '' : 's'
+								  }`
+								: 'Create a new List'}
+						</Button>
+					)}
+				/>
+			</Modal>
+
+			<CreateList
+				isOpen={isCreateListOpen}
+				onClose={() => onCreateListClose()}
+				onSubmit={({ id }) => handleOnListClick({ id, isSelected: false })}
+			/>
+		</>
+	);
+};
+
+export default AddBookmark;
---
+++ src/containers/Layout/components/Modals/BookmarkModal/components/AddBookmark/types.ts
@@ -0,0 +1,4 @@
+import { UserList } from '../../../../../../../store/slices/Users/types';
+
+export type BookmarkListSelectID = UserList['id'];
+export type BookmarkListSelected = BookmarkListSelectID[];
