fc0045e3e962a68f27a5ab7e468b01a981c1cc68
David Scicluna <dave_nostra@hotmail.com>
2022-09-14 21:50:23 +0200

    Fix: Store - App Slice

--- src/store/slices/App/index.ts
+++ src/store/slices/App/index.ts
@@ -11,7 +11,7 @@ const initialState: StateProps = {
 
 const appSlice = createSlice({
 	name: 'app',
-	initialState,
+	initialState: { ...initialState },
 	reducers: {
 		toggleDisplayMode: (state: StateProps, action: PayloadAction<DisplayMode>) => {
 			state.ui.displayMode = action.payload;
