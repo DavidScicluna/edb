dd2d312781fec2af1014724cb9bad7b813a16567
David Scicluna <dave_nostra@hotmail.com>
2022-09-14 21:54:41 +0200

    Fix: Layout - Modals - Removed Lists Modal

--- src/containers/Layout/components/Modals/Lists/components/List/types.ts
+++
@@ -1,7 +0,0 @@
-import { List } from '../../../../../../../store/slices/Users/types';
-
-export type ListProps = {
-	list: List;
-	isSelected?: boolean;
-	onClick: (id: List['id'], isSelected: boolean) => void;
-};
