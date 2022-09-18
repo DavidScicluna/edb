2436cb8d92487a7d63161aa009221b95a1385ac8
David Scicluna <dave_nostra@hotmail.com>
2022-09-18 18:58:03 +0200

    Fix: Home - Popular

--- src/pages/Home/components/Popular/index.tsx
+++ src/pages/Home/components/Popular/index.tsx
@@ -1,95 +1,89 @@
-import { ReactElement, useState, useEffect } from 'react';
+import { FC, useState } from 'react';
+
+import { useBoolean, Center } from '@chakra-ui/react';
 
 import { useInView } from 'react-cool-inview';
-import { useQuery } from 'react-query';
-import axios from 'axios';
 import qs from 'query-string';
+import { useDebounce, useTimeout } from 'usehooks-ts';
 
-import axiosInstance, { handleDelay } from '../../../../common/scripts/axios';
-import { Response } from '../../../../common/types';
-import { PartialMovie } from '../../../../common/types/movie';
-import { PartialTV } from '../../../../common/types/tv';
-import HomeHorizontalGrid from '../HorizontalGrid';
+import { usePopularQuery } from '../../../../common/queries';
+import HomeHorizontalGrid from '../HomeHorizontalGrid';
 
-const Popular = (): ReactElement => {
-	const source = axios.CancelToken.source();
-
+const Popular: FC = () => {
 	const { observe: ref, inView } = useInView<HTMLDivElement>({
 		threshold: [0.2, 0.4, 0.6, 0.8, 1],
 		unobserveOnEnter: true
 	});
 
 	const [activeTab, setActiveTab] = useState<number>(0);
+	const activeTabDebounced = useDebounce<number>(activeTab, 250);
+
+	const [isMoviesQueryEnabled, setIsMoviesQueryEnabled] = useBoolean();
+	const [isShowsQueryEnabled, setIsShowsQueryEnabled] = useBoolean();
 
 	// Fetching Popular Movies
-	const popularMoviesQuery = useQuery(
-		'popular-movies',
-		async () => {
-			const { data } = await axiosInstance
-				.get<Response<PartialMovie[]>>('/movie/popular', {
-					cancelToken: source.token
-				})
-				.then((response) => handleDelay(2500, response));
-			return data;
-		},
-		{ enabled: activeTab === 0 && inView }
-	);
+	const {
+		data: movies,
+		isFetching: isFetchingMovies = false,
+		isLoading: isMoviesLoading = false,
+		isError: isMoviesError = false,
+		isSuccess: isMoviesSuccess = false
+	} = usePopularQuery({
+		props: { mediaType: 'movie' },
+		options: { enabled: isMoviesQueryEnabled }
+	});
 
 	// Fetching Popular TV Shows
-	const popularTVQuery = useQuery(
-		'popular-tv-shows',
-		async () => {
-			const { data } = await axiosInstance
-				.get<Response<PartialTV[]>>('/tv/popular', {
-					cancelToken: source.token
-				})
-				.then((response) => handleDelay(2500, response));
-			return data;
-		},
-		{ enabled: activeTab === 1 && inView }
-	);
+	const {
+		data: shows,
+		isFetching: isFetchingShows = false,
+		isLoading: isShowsLoading = false,
+		isError: isShowsError = false,
+		isSuccess: isShowsSuccess = false
+	} = usePopularQuery({
+		props: { mediaType: 'tv' },
+		options: { enabled: isShowsQueryEnabled }
+	});
 
-	useEffect(() => {
-		return () => source.cancel();
-	}, []);
+	useTimeout(() => setIsMoviesQueryEnabled.on(), activeTabDebounced === 0 && inView ? 1000 : null);
+	useTimeout(() => setIsShowsQueryEnabled.on(), activeTabDebounced === 1 && inView ? 1000 : null);
 
 	return (
-		<HomeHorizontalGrid
-			ref={ref}
-			activeTab={activeTab}
-			title='Popular'
-			to={({ mediaType }) => {
-				if (mediaType === 'movie') {
+		<Center ref={ref} width='100%'>
+			<HomeHorizontalGrid
+				activeTab={activeTabDebounced}
+				title='Popular'
+				subtitle='A list containing the most popular Movies & TV Shows at the moment.'
+				to={({ mediaType }) => {
 					return {
-						pathname: '/movies',
+						pathname: mediaType === 'movie' ? '/movies' : '/tvshows',
 						search: qs.stringify({ sort_by: 'popularity.desc' })
 					};
-				} else {
-					return {
-						pathname: '/tvshows',
-						search: qs.stringify({ sort_by: 'popularity.desc' })
-					};
-				}
-			}}
-			mediaTypes={['movie', 'tv']}
-			data={{
-				movie: popularMoviesQuery.data?.results || [],
-				tv: popularTVQuery.data?.results || []
-			}}
-			isLoading={{
-				movie: popularMoviesQuery.isFetching || popularMoviesQuery.isLoading,
-				tv: popularTVQuery.isFetching || popularTVQuery.isLoading
-			}}
-			isError={{
-				movie: popularMoviesQuery.isError,
-				tv: popularTVQuery.isError
-			}}
-			isSuccess={{
-				movie: popularMoviesQuery.isSuccess,
-				tv: popularTVQuery.isSuccess
-			}}
-			onTabChange={(index: number) => setActiveTab(index)}
-		/>
+				}}
+				mediaTypes={['movie', 'tv']}
+				data={{
+					movie: movies?.results || [],
+					tv: shows?.results || [],
+					person: []
+				}}
+				isLoading={{
+					movie: isFetchingMovies || isMoviesLoading,
+					tv: isFetchingShows || isShowsLoading,
+					person: false
+				}}
+				isError={{
+					movie: isMoviesError,
+					tv: isShowsError,
+					person: false
+				}}
+				isSuccess={{
+					movie: isMoviesSuccess,
+					tv: isShowsSuccess,
+					person: false
+				}}
+				onChange={({ index }) => setActiveTab(index)}
+			/>
+		</Center>
 	);
 };
 
