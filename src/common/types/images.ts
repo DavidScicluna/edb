1c3f8674288bfa3e7378ba6ed680a07954c6bf1b
David Scicluna <dave_nostra@hotmail.com>
2022-09-18 19:09:35 +0200

    Fix: Image Types

--- src/common/types/images.ts
+++ src/common/types/images.ts
@@ -1,13 +1,13 @@
-export type Backdrop = 'w300' | 'w780' | 'w1280' | 'original';
+export type BackdropSizes = 'w300' | 'w780' | 'w1280' | 'original';
 
-export type Logo = 'w45' | 'w92' | 'w154' | 'w185' | 'w300' | 'w500' | 'original';
+export type LogoSizes = 'w45' | 'w92' | 'w154' | 'w185' | 'w300' | 'w500' | 'original';
 
-export type Poster = 'w92' | 'w154' | 'w185' | 'w342' | 'w500' | 'w780' | 'original';
+export type PosterSizes = 'w92' | 'w154' | 'w185' | 'w342' | 'w500' | 'w780' | 'original';
 
-export type Profile = 'w45' | 'w185' | 'h632' | 'original';
+export type ProfileSizes = 'w45' | 'w185' | 'h632' | 'original';
 
-export type Still = 'w92' | 'w185' | 'w300' | 'original';
+export type StillSizes = 'w92' | 'w185' | 'w300' | 'original';
 
-export type Image = 'backdrop' | 'logo' | 'poster' | 'profile' | 'still';
+export type ImageType = 'backdrop' | 'logo' | 'poster' | 'profile' | 'still';
 
-export type Images = Backdrop | Logo | Poster | Profile | Still;
+export type ImageSizes = BackdropSizes | LogoSizes | PosterSizes | ProfileSizes | StillSizes;
