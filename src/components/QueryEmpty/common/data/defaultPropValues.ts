29abad06d048e7258971ab69c41b09855bec2afe
David Scicluna <dave_nostra@hotmail.com>
2022-09-18 19:06:14 +0200

    Feature: QueryEmpty - defaultPropValues

---
+++ src/components/QueryEmpty/common/data/defaultPropValues.ts
@@ -0,0 +1,13 @@
+import { Space } from '@davidscicluna/component-library';
+
+import { ColorMode } from '@chakra-ui/react';
+
+import { QueryEmptyColor } from '../../types';
+
+export const color: QueryEmptyColor = 'blue';
+
+export const colorMode: ColorMode = 'light';
+
+export const spacing: Space = 4;
+
+export const padding: Space = 4;
