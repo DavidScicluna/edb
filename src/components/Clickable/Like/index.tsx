1d9ce3e08a33ac7d734868d934bc15dc4815e114
David Scicluna <dave_nostra@hotmail.com>
2022-09-21 15:52:59 +0200

    Fix: Like

--- src/components/Clickable/Like/index.tsx
+++ src/components/Clickable/Like/index.tsx
@@ -4,8 +4,8 @@ import { useBoolean } from '@chakra-ui/react';
 
 import { useDispatch } from 'react-redux';
 import dayjs from 'dayjs';
-import { useDebounce } from 'usehooks-ts';
 import { sort } from 'fast-sort';
+import { debounce, uniqBy } from 'lodash';
 
 import { useSelector } from '../../../common/hooks';
 import { setUserLiked } from '../../../store/slices/Users';
@@ -27,7 +27,6 @@ const Like = <MT extends MediaType>(props: LikeProps<MT>): ReactElement => {
 	const { renderAction, mediaType, mediaItem } = props;
 
 	const [isLiked, setIsLiked] = useBoolean();
-	const isLikedDebounced = useDebounce(isLiked, 500);
 
 	const handleIsLiked = useCallback((): void => {
 		let isLiked = false;
@@ -58,129 +57,181 @@ const Like = <MT extends MediaType>(props: LikeProps<MT>): ReactElement => {
 		setIsLiked[isLiked ? 'on' : 'off']();
 	}, [mediaType, mediaItem, liked]);
 
-	const handleUnlike = useCallback((): void => {
-		const updatedLiked: MediaItems = {
-			movies: [...(liked.movies || [])],
-			tv: [...(liked.tv || [])],
-			people: [...(liked.people || [])],
-			companies: [...(liked.companies || [])],
-			collections: [...(liked.collections || [])]
-		};
+	const handleUnlike = useCallback(
+		debounce((): void => {
+			const updatedLiked: MediaItems = {
+				movies: [...(liked.movies || [])],
+				tv: [...(liked.tv || [])],
+				people: [...(liked.people || [])],
+				companies: [...(liked.companies || [])],
+				collections: [...(liked.collections || [])]
+			};
 
-		switch (mediaType) {
-			case 'movie': {
-				updatedLiked.movies = sort([
-					...updatedLiked.movies.filter((movie) => movie.mediaItem.id !== mediaItem.id)
-				]).by({ desc: (movie) => movie.addedAt });
-				break;
+			switch (mediaType) {
+				case 'movie': {
+					updatedLiked.movies = sort(
+						uniqBy(
+							[...updatedLiked.movies.filter((movie) => movie.mediaItem.id !== mediaItem.id)],
+							'mediaItem.id'
+						)
+					).by({ desc: (movie) => movie.addedAt });
+					break;
+				}
+				case 'tv': {
+					updatedLiked.tv = sort(
+						uniqBy(
+							[...updatedLiked.tv.filter((show) => show.mediaItem.id !== mediaItem.id)],
+							'mediaItem.id'
+						)
+					).by({
+						desc: (show) => show.addedAt
+					});
+					break;
+				}
+				case 'person': {
+					updatedLiked.people = sort(
+						uniqBy(
+							[...updatedLiked.people.filter((person) => person.mediaItem.id !== mediaItem.id)],
+							'mediaItem.id'
+						)
+					).by({ desc: (person) => person.addedAt });
+					break;
+				}
+				case 'company': {
+					updatedLiked.companies = sort(
+						uniqBy(
+							[...updatedLiked.companies.filter((company) => company.mediaItem.id !== mediaItem.id)],
+							'mediaItem.id'
+						)
+					).by({ desc: (company) => company.addedAt });
+					break;
+				}
+				case 'collection': {
+					updatedLiked.collections = sort(
+						uniqBy(
+							[
+								...updatedLiked.collections.filter(
+									(collection) => collection.mediaItem.id !== mediaItem.id
+								)
+							],
+							'mediaItem.id'
+						)
+					).by({ desc: (collection) => collection.addedAt });
+					break;
+				}
 			}
-			case 'tv': {
-				updatedLiked.tv = sort([...updatedLiked.tv.filter((show) => show.mediaItem.id !== mediaItem.id)]).by({
-					desc: (show) => show.addedAt
-				});
-				break;
-			}
-			case 'person': {
-				updatedLiked.people = sort([
-					...updatedLiked.people.filter((person) => person.mediaItem.id !== mediaItem.id)
-				]).by({ desc: (person) => person.addedAt });
-				break;
-			}
-			case 'company': {
-				updatedLiked.companies = sort([
-					...updatedLiked.companies.filter((company) => company.mediaItem.id !== mediaItem.id)
-				]).by({ desc: (company) => company.addedAt });
-				break;
-			}
-			case 'collection': {
-				updatedLiked.collections = sort([
-					...updatedLiked.collections.filter((collection) => collection.mediaItem.id !== mediaItem.id)
-				]).by({ desc: (collection) => collection.addedAt });
-				break;
-			}
-		}
 
-		dispatch(setUserLiked({ id, data: { ...updatedLiked } }));
-	}, [mediaType, mediaItem, liked, id]);
+			dispatch(setUserLiked({ id, data: { ...updatedLiked } }));
+		}, 1000),
+		[mediaType, mediaItem, liked, id]
+	);
 
-	const handleLike = useCallback((): void => {
-		const updatedLiked: MediaItems = {
-			movies: [...(liked.movies || [])],
-			tv: [...(liked.tv || [])],
-			people: [...(liked.people || [])],
-			companies: [...(liked.companies || [])],
-			collections: [...(liked.collections || [])]
-		};
+	const handleLike = useCallback(
+		debounce((): void => {
+			const updatedLiked: MediaItems = {
+				movies: [...(liked.movies || [])],
+				tv: [...(liked.tv || [])],
+				people: [...(liked.people || [])],
+				companies: [...(liked.companies || [])],
+				collections: [...(liked.collections || [])]
+			};
 
-		switch (mediaType) {
-			case 'movie': {
-				updatedLiked.movies = sort([
-					...updatedLiked.movies,
-					{
-						mediaItem: { ...(mediaItem as FullMovie) },
-						mediaType: 'movie',
-						addedAt: dayjs(new Date()).toISOString()
-					} as MediaItem<'movie'>
-				]).by({ desc: (movie) => movie.addedAt });
-				break;
+			switch (mediaType) {
+				case 'movie': {
+					updatedLiked.movies = sort(
+						uniqBy(
+							[
+								...updatedLiked.movies,
+								{
+									mediaItem: { ...(mediaItem as FullMovie) },
+									mediaType: 'movie',
+									addedAt: dayjs(new Date()).toISOString()
+								} as MediaItem<'movie'>
+							],
+							'mediaItem.id'
+						)
+					).by({ desc: (movie) => movie.addedAt });
+					break;
+				}
+				case 'tv': {
+					updatedLiked.tv = sort(
+						uniqBy(
+							[
+								...updatedLiked.tv,
+								{
+									mediaItem: { ...(mediaItem as FullTV) },
+									mediaType: 'tv',
+									addedAt: dayjs(new Date()).toISOString()
+								} as MediaItem<'tv'>
+							],
+							'mediaItem.id'
+						)
+					).by({ desc: (show) => show.addedAt });
+					break;
+				}
+				case 'person': {
+					updatedLiked.people = sort(
+						uniqBy(
+							[
+								...updatedLiked.people,
+								{
+									mediaItem: { ...(mediaItem as FullPerson) },
+									mediaType: 'person',
+									addedAt: dayjs(new Date()).toISOString()
+								} as MediaItem<'person'>
+							],
+							'mediaItem.id'
+						)
+					).by({ desc: (person) => person.addedAt });
+					break;
+				}
+				case 'company': {
+					updatedLiked.companies = sort(
+						uniqBy(
+							[
+								...updatedLiked.companies,
+								{
+									mediaItem: { ...(mediaItem as FullCompany) },
+									mediaType: 'company',
+									addedAt: dayjs(new Date()).toISOString()
+								} as MediaItem<'company'>
+							],
+							'mediaItem.id'
+						)
+					).by({ desc: (company) => company.addedAt });
+					break;
+				}
+				case 'collection': {
+					updatedLiked.collections = sort(
+						uniqBy(
+							[
+								...updatedLiked.collections,
+								{
+									mediaItem: { ...(mediaItem as Collection) },
+									mediaType: 'collection',
+									addedAt: dayjs(new Date()).toISOString()
+								} as MediaItem<'collection'>
+							],
+							'mediaItem.id'
+						)
+					).by({ desc: (collection) => collection.addedAt });
+					break;
+				}
 			}
-			case 'tv': {
-				updatedLiked.tv = sort([
-					...updatedLiked.tv,
-					{
-						mediaItem: { ...(mediaItem as FullTV) },
-						mediaType: 'tv',
-						addedAt: dayjs(new Date()).toISOString()
-					} as MediaItem<'tv'>
-				]).by({ desc: (show) => show.addedAt });
-				break;
-			}
-			case 'person': {
-				updatedLiked.people = sort([
-					...updatedLiked.people,
-					{
-						mediaItem: { ...(mediaItem as FullPerson) },
-						mediaType: 'person',
-						addedAt: dayjs(new Date()).toISOString()
-					} as MediaItem<'person'>
-				]).by({ desc: (person) => person.addedAt });
-				break;
-			}
-			case 'company': {
-				updatedLiked.companies = sort([
-					...updatedLiked.companies,
-					{
-						mediaItem: { ...(mediaItem as FullCompany) },
-						mediaType: 'company',
-						addedAt: dayjs(new Date()).toISOString()
-					} as MediaItem<'company'>
-				]).by({ desc: (company) => company.addedAt });
-				break;
-			}
-			case 'collection': {
-				updatedLiked.collections = sort([
-					...updatedLiked.collections,
-					{
-						mediaItem: { ...(mediaItem as Collection) },
-						mediaType: 'collection',
-						addedAt: dayjs(new Date()).toISOString()
-					} as MediaItem<'collection'>
-				]).by({ desc: (collection) => collection.addedAt });
-				break;
-			}
-		}
 
-		dispatch(setUserLiked({ id, data: { ...updatedLiked } }));
-	}, [mediaType, mediaItem, liked, id]);
+			dispatch(setUserLiked({ id, data: { ...updatedLiked } }));
+		}, 1000),
+		[mediaType, mediaItem, liked, id]
+	);
 
 	useEffect(() => handleIsLiked(), [liked]);
 
 	return renderAction({
-		iconType: isLikedDebounced ? 'favorite' : 'favorite_border',
-		iconCategory: 'outlined',
-		isDisabled: !!activeUser,
-		isLiked: isLikedDebounced,
-		onClick: isLikedDebounced ? () => handleUnlike() : () => handleLike()
+		iconType: isLiked ? 'favorite' : 'favorite_border',
+		iconCategory: 'filled',
+		isDisabled: !activeUser,
+		isLiked: isLiked,
+		onClick: isLiked ? () => handleUnlike() : () => handleLike()
 	});
 };
 
