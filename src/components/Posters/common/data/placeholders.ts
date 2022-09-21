997ef379c09a3c3f1aa3b4536722045d0423c926
David Scicluna <dave_nostra@hotmail.com>
2022-09-21 16:44:44 +0200

    Feature: Posters - placeholders

---
+++ src/components/Posters/common/data/placeholders.ts
@@ -0,0 +1,29 @@
+const placeholders: string[] = [
+	'in',
+	'mollis',
+	'nunc',
+	'sed',
+	'id',
+	'semper',
+	'risus',
+	'in',
+	'hendrerit',
+	'gravida',
+	'rutrum',
+	'quisque',
+	'non',
+	'tellus',
+	'orci',
+	'ac',
+	'auctor',
+	'augue',
+	'mauris',
+	'augue',
+	'neque',
+	'gravida',
+	'in',
+	'fermentum',
+	'et'
+];
+
+export default placeholders;
