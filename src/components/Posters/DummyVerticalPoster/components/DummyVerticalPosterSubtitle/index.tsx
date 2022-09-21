0abe8db0224c5e077b4218375daedce51de068ae
David Scicluna <dave_nostra@hotmail.com>
2022-09-21 16:42:58 +0200

    Fix: Posters - DummyVerticalPoster - DummyVerticalPosterSubtitle

--- src/components/Posters/DummyVerticalPoster/components/DummyVerticalPosterSubtitle/index.tsx
+++ src/components/Posters/DummyVerticalPoster/components/DummyVerticalPosterSubtitle/index.tsx
@@ -1,16 +1,29 @@
 import { FC } from 'react';
 
-import { Skeleton } from '@davidscicluna/component-library';
+import { useTheme, Skeleton } from '@davidscicluna/component-library';
 
-import { Text } from '@chakra-ui/react';
+import { Box, Text } from '@chakra-ui/react';
+
+import { useDummyText } from '../../../common/hooks';
+import { getFontSizeHeight } from '../../../../../common/utils';
 
 const DummyVerticalPosterSubtitle: FC = () => {
+	const theme = useTheme();
+
+	const subtitle = useDummyText();
+
 	return (
-		<Skeleton isLoaded={false} variant='text'>
-			<Text align='left' fontSize='xs' fontWeight='normal' noOfLines={1}>
-				Poster Subtitle
-			</Text>
-		</Skeleton>
+		<Box
+			width='100%'
+			maxWidth='100%'
+			height={`${getFontSizeHeight({ theme, fontSize: 'xs', lineHeight: 'normal' })}px`}
+		>
+			<Skeleton isLoaded={false} variant='text'>
+				<Text align='left' fontSize='xs' fontWeight='normal' lineHeight='normal' noOfLines={1}>
+					{subtitle || 'Date • All Poster Genres • Subtitle'}
+				</Text>
+			</Skeleton>
+		</Box>
 	);
 };
 
