c522a979bcbb059726de1a546f0cd2387351bcbe
David Scicluna <dave_nostra@hotmail.com>
2022-09-21 16:43:06 +0200

    Fix: Posters - DummyVerticalPoster - DummyVerticalPosterTitle

--- src/components/Posters/DummyVerticalPoster/components/DummyVerticalPosterTitle/index.tsx
+++ src/components/Posters/DummyVerticalPoster/components/DummyVerticalPosterTitle/index.tsx
@@ -1,16 +1,29 @@
 import { FC } from 'react';
 
-import { Skeleton } from '@davidscicluna/component-library';
+import { useTheme, Skeleton } from '@davidscicluna/component-library';
 
-import { Text } from '@chakra-ui/react';
+import { Box, Text } from '@chakra-ui/react';
+
+import { getFontSizeHeight } from '../../../../../common/utils';
+import { useDummyText } from '../../../common/hooks';
 
 const DummyVerticalPosterTitle: FC = () => {
+	const theme = useTheme();
+
+	const title = useDummyText();
+
 	return (
-		<Skeleton isLoaded={false} variant='text'>
-			<Text align='left' fontSize='sm' fontWeight='semibold' noOfLines={1}>
-				Poster Title
-			</Text>
-		</Skeleton>
+		<Box
+			width='100%'
+			maxWidth='100%'
+			height={`${getFontSizeHeight({ theme, fontSize: 'sm', lineHeight: 'normal' })}px`}
+		>
+			<Skeleton isLoaded={false} variant='text'>
+				<Text align='left' fontSize='sm' fontWeight='semibold' lineHeight='normal' noOfLines={1}>
+					{title || 'Poster Title'}
+				</Text>
+			</Skeleton>
+		</Box>
 	);
 };
 
