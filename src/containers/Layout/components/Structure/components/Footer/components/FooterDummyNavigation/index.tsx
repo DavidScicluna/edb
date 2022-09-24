de1cb2f2e65a90c443362ebaf66443afc9afe7aa
David Scicluna <dave_nostra@hotmail.com>
2022-09-24 18:34:03 +0200

    Feature: Layout - Structure - Footer - FooterDummyNavigation

---
+++ src/containers/Layout/components/Structure/components/Footer/components/FooterDummyNavigation/index.tsx
@@ -0,0 +1,35 @@
+import { FC } from 'react';
+
+import { useTheme, Skeleton } from '@davidscicluna/component-library';
+
+import { useMediaQuery, Stack, Text } from '@chakra-ui/react';
+
+import { range } from 'lodash';
+
+import navItems from '../../common/data/navItems';
+
+const FooterDummyNavigation: FC = () => {
+	const theme = useTheme();
+
+	const [isSm] = useMediaQuery(`(max-width: ${theme.breakpoints.sm})`);
+
+	return (
+		<Stack
+			width='100%'
+			direction={isSm ? 'column' : 'row'}
+			alignItems='center'
+			justifyContent='space-between'
+			spacing={isSm ? 4 : 2}
+		>
+			{range(navItems.length - 1).map((_dummy, index) => (
+				<Skeleton key={index} width='100%' isLoaded={false} variant='text'>
+					<Text align='left' fontSize='md' fontWeight='semibold' textTransform='uppercase'>
+						This is dummy text
+					</Text>
+				</Skeleton>
+			))}
+		</Stack>
+	);
+};
+
+export default FooterDummyNavigation;
