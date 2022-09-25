8de803f89e6d6753640fd94105da7a8e03b57223
David Scicluna <dave_nostra@hotmail.com>
2022-09-25 14:06:48 +0200

    Feature: Layout - Structure - DummyStructureDesktop - DummySidebar - DummyUser

---
+++ src/containers/Layout/components/Structure/components/DummyStructureDesktop/components/DummySidebar/components/DummyUser/index.tsx
@@ -0,0 +1,62 @@
+import { FC, useCallback } from 'react';
+
+import { Space, useTheme, DummyCard, CardBody, Skeleton, utils } from '@davidscicluna/component-library';
+
+import { VStack, HStack, Center, Text } from '@chakra-ui/react';
+
+import { useElementSize } from 'usehooks-ts';
+
+import { useSelector, useUserTheme } from '../../../../../../../../../../common/hooks';
+
+const { convertREMToPixels, convertStringToNumber } = utils;
+
+const spacing: Space = 2;
+
+const DummyUser: FC = () => {
+	const theme = useTheme();
+	const { colorMode } = useUserTheme();
+
+	const [avatarRef, { width: avatarWidth }] = useElementSize();
+
+	const sidebarMode = useSelector((state) => state.app.ui.sidebarMode);
+
+	const handleAvatarWidth = useCallback(() => {
+		const spacingWidth = convertREMToPixels(convertStringToNumber(theme.space[spacing], 'rem'));
+
+		return `calc(100% - ${avatarWidth + spacingWidth}px)`;
+	}, [theme, spacing, avatarWidth]);
+
+	return (
+		<DummyCard colorMode={colorMode} isFullWidth isLight p={sidebarMode === 'expanded' ? 2 : 1}>
+			<CardBody>
+				<HStack width='100%' alignItems='center' justifyContent='stretch' spacing={spacing}>
+					<Center ref={avatarRef}>
+						<Skeleton
+							colorMode={colorMode}
+							width={theme.space[sidebarMode === 'expanded' ? 7 : 4.25]}
+							height={theme.space[sidebarMode === 'expanded' ? 7 : 4.25]}
+							variant='rectangle'
+						/>
+					</Center>
+
+					{sidebarMode === 'expanded' && (
+						<VStack width={handleAvatarWidth()} alignItems='flex-start' spacing={0.5}>
+							<Skeleton colorMode={colorMode} variant='text'>
+								<Text align='left' fontSize='2xl' fontWeight='bold' lineHeight='normal' noOfLines={1}>
+									Full Dummy User Name & Surname
+								</Text>
+							</Skeleton>
+							<Skeleton colorMode={colorMode} variant='text'>
+								<Text align='left' fontSize='sm' lineHeight='normal' noOfLines={1}>
+									@Dummy User Name
+								</Text>
+							</Skeleton>
+						</VStack>
+					)}
+				</HStack>
+			</CardBody>
+		</DummyCard>
+	);
+};
+
+export default DummyUser;
