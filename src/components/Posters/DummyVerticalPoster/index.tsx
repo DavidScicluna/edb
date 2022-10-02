6652cb3255d79b812703a37bb878062368383db6
David Scicluna <dave_nostra@hotmail.com>
2022-10-02 16:30:30 +0200

    Fix: Posters - DummyVerticalPoster

--- src/components/Posters/DummyVerticalPoster/index.tsx
+++ src/components/Posters/DummyVerticalPoster/index.tsx
@@ -1,20 +1,15 @@
-import { ReactElement, useContext } from 'react';
+import { ReactElement } from 'react';
 
 import { DummyCard, CardBody } from '@davidscicluna/component-library';
 
 import { VStack, HStack } from '@chakra-ui/react';
 
-import { useInView } from 'react-cool-inview';
-
-import { isGuest as defaultIsGuest } from '../../../containers/Layout/common/data/defaultPropValues';
 import { MediaType } from '../../../common/types';
 import DummyRating from '../../Ratings/DummyRating';
-import { LayoutContext } from '../../../containers/Layout';
-import { LayoutContext as LayoutContextType } from '../../../containers/Layout/types';
 import DummyPosterLike from '../components/DummyPosterLike';
 import DummyPosterBookmark from '../components/DummyPosterBookmark';
 import PosterGlass from '../components/PosterGlass';
-import { inView as defaultInView } from '../common/data/defaultPropValues';
+import { useUserTheme } from '../../../common/hooks';
 
 import { DummyVerticalPosterProps } from './types';
 import DummyVerticalPosterImage from './components/DummyVerticalPosterImage';
@@ -22,31 +17,22 @@ import DummyVerticalPosterTitle from './components/DummyVerticalPosterTitle';
 import DummyVerticalPosterSubtitle from './components/DummyVerticalPosterSubtitle';
 
 const DummyVerticalPoster = <MT extends MediaType>(props: DummyVerticalPosterProps<MT>): ReactElement => {
-	const { isGuest = defaultIsGuest } = useContext<LayoutContextType>(LayoutContext);
-
-	const { observe: posterRef, inView = defaultInView } = useInView<HTMLDivElement>({
-		threshold: [0.2, 0.4, 0.6, 0.8, 1],
-		unobserveOnEnter: true
-	});
+	const { colorMode } = useUserTheme();
 
 	const { mediaType, hasSubtitle = false, isFullWidth = true, isLight = true, ...rest } = props;
 
 	return (
-		<DummyCard {...rest} isFullWidth={isFullWidth} isLight={isLight}>
+		<DummyCard {...rest} colorMode={colorMode} isFullWidth={isFullWidth} isLight={isLight}>
 			<CardBody>
-				<VStack ref={posterRef} width='100%' position='relative' overflow='hidden' spacing={0}>
-					{/* Image */}
+				<VStack width='100%' position='relative' overflow='hidden' spacing={0}>
 					<DummyVerticalPosterImage />
 
 					<VStack width='100%' spacing={0.5} p={1}>
-						{/* Header */}
 						{(mediaType === 'movie' || mediaType === 'tv') && (
 							<HStack width='100%' justify='space-between' spacing={0}>
-								{/* Rating */}
-								<DummyRating size='md' />
+								<DummyRating size='sm' />
 
 								<HStack spacing={0.5}>
-									{/* Quick View */}
 									{/* {isTouchDevice && (
 											<Quickview
 												title={title}
@@ -56,16 +42,13 @@ const DummyVerticalPoster = <MT extends MediaType>(props: DummyVerticalPosterPro
 											/>
 										)} */}
 
-									{/* Like */}
 									<DummyPosterLike size='xs' />
 
-									{/* Bookmark */}
 									<DummyPosterBookmark size='xs' />
 								</HStack>
 							</HStack>
 						)}
 
-						{/* Text */}
 						<VStack width='100%' alignItems='flex-start' spacing={0.5}>
 							<DummyVerticalPosterTitle />
 
@@ -73,17 +56,13 @@ const DummyVerticalPoster = <MT extends MediaType>(props: DummyVerticalPosterPro
 						</VStack>
 					</VStack>
 
-					{/* Like / Quick View */}
 					{(mediaType === 'person' || mediaType === 'company' || mediaType === 'collection') && (
 						<>
-							{/* Quick View */}
 							{/* {isTouchDevice && mediaType !== 'company' && (
 									<PosterGlass
 										position='absolute'
-										top={1}
-										left={2}
-										onMouseEnter={() => setIsFixed.on()}
-										onMouseLeave={() => setIsFixed.off()}
+										top={1.5}
+										left={1.5}
 									>
 										<Quickview
 											title={title}
@@ -94,11 +73,9 @@ const DummyVerticalPoster = <MT extends MediaType>(props: DummyVerticalPosterPro
 									</PosterGlass>
 								)} */}
 
-							{!isGuest && (
-								<PosterGlass position='absolute' top={1.5} right={1.5}>
-									<DummyPosterLike size='xs' />
-								</PosterGlass>
-							)}
+							<PosterGlass position='absolute' top={1.5} right={1.5}>
+								<DummyPosterLike size='xs' />
+							</PosterGlass>
 						</>
 					)}
 				</VStack>
