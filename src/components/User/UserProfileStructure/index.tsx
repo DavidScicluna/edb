a5c41d8b2d6e4781176c75623b9e997b2c608ee2
David Scicluna <dave_nostra@hotmail.com>
2022-10-04 22:53:21 +0200

    Feature: User - UserProfileStructure

---
+++ src/components/User/UserProfileStructure/index.tsx
@@ -0,0 +1,87 @@
+import { FC } from 'react';
+
+import { useTheme, utils } from '@davidscicluna/component-library';
+
+import { useMediaQuery, VStack, Center } from '@chakra-ui/react';
+
+import { round } from 'lodash';
+import { useElementSize } from 'usehooks-ts';
+
+import { color as defaultColor, colorMode as defaultColorMode } from '../../../common/data/defaultPropValues';
+
+import { UserProfileStructureProps } from './types';
+
+const { convertREMToPixels, convertStringToNumber, getColor } = utils;
+
+const UserProfileStructure: FC<UserProfileStructureProps> = (props) => {
+	const theme = useTheme();
+	const [isMd] = useMediaQuery(`(min-width: ${theme.breakpoints.md})`);
+
+	const [backgroundRef, { height: backgroundHeight }] = useElementSize();
+	const [avatarRef, { width: avatarWidth, height: avatarHeight }] = useElementSize();
+	const [detailsRef, { height: detailsHeight }] = useElementSize();
+
+	const {
+		color = defaultColor,
+		colorMode = defaultColorMode,
+		renderUserAvatar,
+		renderUserBackground,
+		renderUserDetails
+	} = props;
+
+	return (
+		<VStack
+			width='100%'
+			height='auto'
+			minHeight={`${round((backgroundHeight + avatarHeight + detailsHeight) / 1.75)}px`}
+			alignItems='stretch'
+			justifyContent='stretch'
+			position='relative'
+			borderWidth='2px'
+			borderStyle='solid'
+			borderColor={getColor({ theme, colorMode, type: 'divider' })}
+			borderRadius='xl'
+			overflowX='hidden'
+			overflowY='hidden'
+			spacing={0}
+		>
+			<Center
+				ref={avatarRef}
+				position={['relative', 'relative', 'absolute']}
+				zIndex={5}
+				bottom={[0, 0, 2, 4]}
+				left={[0, 0, 2, 4]}
+				p={[2, 2, 0]}
+			>
+				{renderUserAvatar({ color, colorMode })}
+			</Center>
+
+			<VStack width='100%' alignItems='stretch' justifyContent='stretch' spacing={0}>
+				{isMd && (
+					<Center ref={backgroundRef} width='100%'>
+						{renderUserBackground({ color, colorMode })}
+					</Center>
+				)}
+
+				<Center
+					ref={detailsRef}
+					width={[
+						'100%',
+						'100%',
+						`calc(100% - ${
+							avatarWidth + convertREMToPixels(convertStringToNumber(theme.space[2], 'rem'))
+						}px)`,
+						`calc(100% - ${
+							avatarWidth + convertREMToPixels(convertStringToNumber(theme.space[4], 'rem'))
+						}px)`
+					]}
+					alignSelf='flex-end'
+				>
+					{renderUserDetails({ color, colorMode })}
+				</Center>
+			</VStack>
+		</VStack>
+	);
+};
+
+export default UserProfileStructure;
---
+++ src/components/User/UserProfileStructure/types.ts
@@ -0,0 +1,15 @@
+import { ReactNode } from 'react';
+
+import { ColorMode } from '@chakra-ui/react';
+
+import { UserThemeColor } from '../../../store/slices/Users/types';
+
+type RenderProps = Pick<UserProfileStructureProps, 'color' | 'colorMode'>;
+
+export type UserProfileStructureProps = {
+	color?: UserThemeColor;
+	colorMode?: ColorMode;
+	renderUserAvatar: (props: RenderProps) => ReactNode;
+	renderUserBackground: (props: RenderProps) => ReactNode;
+	renderUserDetails: (props: RenderProps) => ReactNode;
+};
