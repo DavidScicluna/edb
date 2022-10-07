9d90279ab8445d235170a61519743e0a9fb01b73
David Scicluna <dave_nostra@hotmail.com>
2022-10-07 21:39:36 +0200

    Fix: Page - PageHeader - Breadcrumbs

--- src/containers/Page/components/PageHeader/components/Breadcrumbs/index.tsx
+++ src/containers/Page/components/PageHeader/components/Breadcrumbs/index.tsx
@@ -1,29 +1,16 @@
 import { FC, useState } from 'react';
 
-import { useTheme, Icon } from '@davidscicluna/component-library';
-
-import { useColorMode, useBreakpointValue, Breadcrumb as CUIBreadcrumb } from '@chakra-ui/react';
+import { Breadcrumb as CUIBreadcrumb, BreadcrumbItem } from '@chakra-ui/react';
 
 import useBreadcrumbs, { BreadcrumbData } from 'use-react-router-breadcrumbs';
 import { useDebounce, useTimeout } from 'usehooks-ts';
 import { range } from 'lodash';
 
 import Breadcrumb from './components/Breadcrumb';
-// import { allRoutes as routes } from '../../../../../Routes';
+import Separator from './components/Separator';
+import DummyBreadcrumb from './components/DummyBreadcrumb';
 
 const Breadcrumbs: FC = () => {
-	const theme = useTheme();
-	const { colorMode } = useColorMode();
-
-	const iconFontSize = useBreakpointValue({
-		'base': theme.fontSizes.md,
-		'sm': theme.fontSizes.md,
-		'md': theme.fontSizes.lg,
-		'lg': theme.fontSizes.lg,
-		'xl': theme.fontSizes.lg,
-		'2xl': theme.fontSizes.lg
-	});
-
 	const getBreadcrumbs = useBreadcrumbs();
 	// routes.map((route) => omit(route, 'element'))
 
@@ -33,28 +20,21 @@ const Breadcrumbs: FC = () => {
 	useTimeout(() => setBreadcrumbs(getBreadcrumbs), 2500);
 
 	return (
-		<CUIBreadcrumb
-			width='100%'
-			separator={
-				<Icon
-					icon='chevron_right'
-					category='outlined'
-					color={theme.colors.gray[colorMode === 'light' ? 400 : 500]}
-					fontSize={iconFontSize}
-				/>
-			}
-			spacing={1}
-		>
+		<CUIBreadcrumb separator={<Separator />} spacing={2}>
 			{debouncedBreadcrumbs && debouncedBreadcrumbs.length > 0
 				? debouncedBreadcrumbs.map((breadcrumb, index) => (
-						<Breadcrumb
-							key={`ds-edb-breadcrumb-${index}`}
-							breadcrumb={breadcrumb}
-							isCurrentPage={index === debouncedBreadcrumbs.length - 1}
-							isLoading={false}
-						/>
+						<BreadcrumbItem key={index} isCurrentPage={index === debouncedBreadcrumbs.length - 1}>
+							<Breadcrumb
+								breadcrumb={breadcrumb}
+								isCurrentPage={index === debouncedBreadcrumbs.length - 1}
+							/>
+						</BreadcrumbItem>
 				  ))
-				: range(0, 3).map((_dummy, index) => <Breadcrumb key={`ds-edb-dummy-breadcrumb-${index}`} isLoading />)}
+				: range(0, 3).map((_dummy, index) => (
+						<BreadcrumbItem key={index} isCurrentPage>
+							<DummyBreadcrumb />
+						</BreadcrumbItem>
+				  ))}
 		</CUIBreadcrumb>
 	);
 };
