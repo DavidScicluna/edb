b47a27a52027db8d51d3d6bfc171391d69632af8
David Scicluna <dave_nostra@hotmail.com>
2022-10-07 21:39:21 +0200

    Feature: Page - PageHeader - Breadcrumbs - DummyBreadcrumb

---
+++ src/containers/Page/components/PageHeader/components/Breadcrumbs/components/DummyBreadcrumb/index.tsx
@@ -0,0 +1,38 @@
+import { FC } from 'react';
+
+import { useTheme, Skeleton, utils } from '@davidscicluna/component-library';
+
+import { useBreakpointValue, Text } from '@chakra-ui/react';
+
+import { useUserTheme } from '../../../../../../../../common/hooks';
+
+const { getColor } = utils;
+
+const DummyBreadcrumb: FC = () => {
+	const theme = useTheme();
+	const { colorMode } = useUserTheme();
+
+	const breadcrumbFontSize = useBreakpointValue({
+		'base': theme.fontSizes.sm,
+		'sm': theme.fontSizes.sm,
+		'md': theme.fontSizes.md,
+		'lg': theme.fontSizes.md,
+		'xl': theme.fontSizes.md,
+		'2xl': theme.fontSizes.md
+	});
+
+	return (
+		<Skeleton colorMode={colorMode} isLoaded={false} variant='text'>
+			<Text
+				align='left'
+				color={getColor({ theme, colorMode, type: 'text.primary' })}
+				fontSize={breadcrumbFontSize}
+				fontWeight='medium'
+			>
+				Dummy Breadcrumb
+			</Text>
+		</Skeleton>
+	);
+};
+
+export default DummyBreadcrumb;
