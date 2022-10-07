145ce919a91524351b0cebd4cd701deb6a13916a
David Scicluna <dave_nostra@hotmail.com>
2022-10-07 21:39:58 +0200

    Fix: Page

--- src/containers/Page/index.tsx
+++ src/containers/Page/index.tsx
@@ -2,13 +2,26 @@ import { FC } from 'react';
 
 import { Divider } from '@davidscicluna/component-library';
 
-import { VStack } from '@chakra-ui/react';
+import { VStack, Box } from '@chakra-ui/react';
+
+import { useLayoutContext } from '../Layout/common/hooks';
 
 import { PageProps } from './types';
 
 const Page: FC<PageProps> = ({ children, ...rest }) => {
+	const { spacing } = useLayoutContext();
+
 	return (
-		<VStack {...rest} width='100%' divider={<Divider />} spacing={0}>
+		<VStack
+			{...rest}
+			width='100%'
+			divider={
+				<Box width='100%' border='none' px={spacing}>
+					<Divider />
+				</Box>
+			}
+			spacing={0}
+		>
 			{children}
 		</VStack>
 	);
--- src/containers/Page/types.ts
+++ src/containers/Page/types.ts
@@ -1,3 +1,3 @@
 import { StackProps } from '@chakra-ui/react';
 
-export type PageProps = Omit<StackProps, 'direction'>;
+export type PageProps = Omit<StackProps, 'direction' | 'title'>;
