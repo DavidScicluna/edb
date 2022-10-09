855fe8408902f2af3e774b716f770688363512f8
David Scicluna <dave_nostra@hotmail.com>
2022-10-09 22:35:22 +0200

    Feature: Search - OriginalSearch - SearchForm - SearchInput - Utils

---
+++ src/pages/Search/OriginalSearch/components/SearchForm/components/SearchInput/common/utils/index.ts
@@ -0,0 +1,39 @@
+import { memoize, sample, shuffle } from 'lodash';
+
+import { Form } from '../../../../../../types';
+import { movie, tv, people, collections, companies, combined } from '../data/placeholders';
+
+type GetPlaceholderProps = { searchTypes: Form['searchTypes'] };
+
+export const getPlaceholder = memoize(({ searchTypes = [] }: GetPlaceholderProps): string => {
+	let placeholders = [];
+
+	if (searchTypes.length === 1) {
+		const searchType = searchTypes[0];
+
+		switch (searchType) {
+			case 'movie':
+				placeholders = [...movie];
+				break;
+			case 'tv':
+				placeholders = [...tv];
+				break;
+			case 'person':
+				placeholders = [...people];
+				break;
+			case 'collection':
+				placeholders = [...collections];
+				break;
+			case 'company':
+				placeholders = [...companies];
+				break;
+			default:
+				placeholders = [...combined];
+				break;
+		}
+	} else {
+		placeholders = [...combined];
+	}
+
+	return sample(placeholders) || shuffle(placeholders)[0];
+});
