90b2956e43afb9f9c4739df40757fa1900b574d8
David Scicluna <dave_nostra@hotmail.com>
2022-10-09 22:34:12 +0200

    Fix: Search - OriginalSearch - SearchForm - SearchInput - placeholders

---
+++ src/pages/Search/OriginalSearch/components/SearchForm/components/SearchInput/common/data/placeholders/index.ts
@@ -0,0 +1,8 @@
+import collections from './collections';
+import combined from './combined';
+import companies from './companies';
+import movie from './movie';
+import people from './people';
+import tv from './tv';
+
+export { collections, combined, companies, movie, people, tv };
