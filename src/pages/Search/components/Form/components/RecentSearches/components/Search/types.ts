63e38e07cdd15bd871c9278d42376019df829edf
David Scicluna <dave_nostra@hotmail.com>
2022-10-14 23:30:47 +0200

    Fix: Search - Removed Form

--- src/pages/Search/components/Form/components/RecentSearches/components/Search/types.ts
+++
@@ -1,7 +0,0 @@
-import { Search, SearchType } from '../../../../../../../../store/slices/Users/types';
-
-export type SearchProps = {
-	isDisabled: boolean;
-	onDelete: (id: Search['id']) => void;
-	onClick: (label: Search['label'], searchTypes?: SearchType[]) => void;
-} & Search;
--- src/pages/Search/components/Form/components/RecentSearches/types.ts
+++
@@ -1,5 +0,0 @@
-import { Search, SearchType } from '../../../../../../store/slices/Users/types';
-
-export type RecentSearchesProps = {
-	onSearchClick: (label: Search['label'], searchTypes?: SearchType[]) => void;
-};
