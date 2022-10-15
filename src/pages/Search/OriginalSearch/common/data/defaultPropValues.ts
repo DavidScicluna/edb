f9f4a79ab4aeec7ef523dc4e773ab830daa63f66
David Scicluna <dave_nostra@hotmail.com>
2022-10-15 08:16:48 +0200

    Feature: Search - OriginalSearch - defaultPropValues

---
+++ src/pages/Search/OriginalSearch/common/data/defaultPropValues.ts
@@ -0,0 +1,7 @@
+import { SearchQueryDataStatus } from '../../types';
+
+export const activeTab = 0;
+
+export const isFocused = false;
+
+export const queryDataStatus: SearchQueryDataStatus = 'hidden';
