66fbe2e0298af7763e29f3ca39efe774fce201c3
David Scicluna <dave_nostra@hotmail.com>
2022-10-16 16:33:41 +0200

    Fix: Trending - DummyTrending

--- src/pages/Trending/DummyTrending/index.tsx
+++ src/pages/Trending/DummyTrending/index.tsx
@@ -6,20 +6,19 @@ import { Tabs, DummyTabList, TabPanels, Skeleton } from '@davidscicluna/componen
 
 import { VStack, Text } from '@chakra-ui/react';
 
-import { MediaType } from '../../../common/types';
 import Page from '../../../containers/Page';
 import PageHeader from '../../../containers/Page/components/PageHeader';
 import PageBody from '../../../containers/Page/components/PageBody';
 import { useUserTheme } from '../../../common/hooks';
-import { formatMediaTypeLabel } from '../../../common/utils';
+import { formatMediaTypeLabel, getMediaTypeIcon } from '../../../common/utils';
 import { useLayoutContext } from '../../../containers/Layout/common/hooks';
 import { DummyDisplayMode } from '../../../components';
-import TrendingDummyMovies from '../components/TrendingDummyMovies';
-import TrendingDummyPeople from '../components/TrendingDummyPeople';
-import TrendingDummyTV from '../components/TrendingDummyTV';
+import TrendingDummyMoviesTab from '../components/TrendingDummyMoviesTab';
+import TrendingDummyPeopleTab from '../components/TrendingDummyPeopleTab';
+import TrendingDummyTVTab from '../components/TrendingDummyTVTab';
 import { getActiveTabFromHash } from '../common/utils';
-
-const mediaTypes: Exclude<MediaType, 'company' | 'collection'>[] = ['movie', 'tv', 'person'];
+import TrendingDummyAllTab from '../components/TrendingDummyAllTab';
+import TrendingTabIcon from '../components/TrendingTabIcon';
 
 const DummyTrending: FC = () => {
 	const { color, colorMode } = useUserTheme();
@@ -40,15 +39,16 @@ const DummyTrending: FC = () => {
 		<Page>
 			<PageHeader
 				renderTitle={(props) => (
-					<Skeleton isLoaded={false} variant='text'>
+					<Skeleton colorMode={colorMode} isLoaded={false} variant='text'>
 						<Text {...props}>Trending Media-Type</Text>
 					</Skeleton>
 				)}
 				renderSubtitle={(props) => (
-					<Skeleton isLoaded={false} variant='text'>
+					<Skeleton colorMode={colorMode} isLoaded={false} variant='text'>
 						<Text {...props}>A list containing the most trending media-type this week.</Text>
 					</Skeleton>
 				)}
+				actions={activeTab !== 0 ? <DummyDisplayMode /> : undefined}
 				direction='row'
 				p={spacing}
 			/>
@@ -56,18 +56,52 @@ const DummyTrending: FC = () => {
 				<Tabs width='100%' activeTab={activeTab} color={color} colorMode={colorMode} size='xl'>
 					<VStack width='100%' spacing={spacing}>
 						<DummyTabList
-							tabs={mediaTypes.map((mediaType) => {
-								return { label: formatMediaTypeLabel({ type: 'multiple', mediaType }) };
-							})}
-							renderRight={() => <DummyDisplayMode />}
+							tabs={[
+								{ label: 'All' },
+
+								{
+									label: formatMediaTypeLabel({ type: 'multiple', mediaType: 'movie' }),
+									renderLeft: (props) => (
+										<Skeleton isLoaded={false} variant='rectangle'>
+											<TrendingTabIcon
+												{...props}
+												icon={getMediaTypeIcon({ mediaType: 'movie' })}
+											/>
+										</Skeleton>
+									)
+								},
+
+								{
+									label: formatMediaTypeLabel({ type: 'multiple', mediaType: 'tv' }),
+									renderLeft: (props) => (
+										<Skeleton isLoaded={false} variant='rectangle'>
+											<TrendingTabIcon {...props} icon={getMediaTypeIcon({ mediaType: 'tv' })} />
+										</Skeleton>
+									)
+								},
+
+								{
+									label: formatMediaTypeLabel({ type: 'multiple', mediaType: 'person' }),
+									renderLeft: (props) => (
+										<Skeleton isLoaded={false} variant='rectangle'>
+											<TrendingTabIcon
+												{...props}
+												icon={getMediaTypeIcon({ mediaType: 'person' })}
+											/>
+										</Skeleton>
+									)
+								}
+							]}
 						/>
 
 						<TabPanels>
-							<TrendingDummyMovies />
+							<TrendingDummyAllTab />
 
-							<TrendingDummyTV />
+							<TrendingDummyMoviesTab />
 
-							<TrendingDummyPeople />
+							<TrendingDummyTVTab />
+
+							<TrendingDummyPeopleTab />
 						</TabPanels>
 					</VStack>
 				</Tabs>
