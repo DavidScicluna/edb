9c7a9828f2cbbc2b85ee8bdb3efe622e05073eb1
David Scicluna <dave_nostra@hotmail.com>
2022-10-17 21:06:48 +0200

    Fix: People - Orientation - Removed Vertical

--- src/pages/People/components/Orientation/Vertical/types.ts
+++
@@ -1,8 +0,0 @@
-import { PartialPerson } from '../../../../../common/types/person';
-
-export type VerticalPeopleProps = {
-	isError?: boolean;
-	isSuccess?: boolean;
-	isLoading?: boolean;
-	people?: PartialPerson[];
-};
