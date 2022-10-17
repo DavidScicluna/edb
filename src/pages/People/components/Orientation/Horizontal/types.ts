4fdc80b19f4c0e650a3979cd866a40e565bb65e1
David Scicluna <dave_nostra@hotmail.com>
2022-10-17 21:06:38 +0200

    Fix: People - Orientation - Removed Horizontal

--- src/pages/People/components/Orientation/Horizontal/types.ts
+++
@@ -1,8 +0,0 @@
-import { PartialPerson } from '../../../../../common/types/person';
-
-export type HorizontalPeopleProps = {
-	isError?: boolean;
-	isSuccess?: boolean;
-	isLoading?: boolean;
-	people?: PartialPerson[];
-};
