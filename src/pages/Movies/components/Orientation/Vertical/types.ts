d1cd7ca9ca6324ec1645d7c4a91300ca601882a3
David Scicluna <dave_nostra@hotmail.com>
2022-10-21 21:56:24 +0200

    Fix: Movies - Orientation components

--- src/pages/Movies/components/Orientation/Vertical/types.ts
+++
@@ -1,8 +0,0 @@
-import { PartialMovie } from '../../../../../common/types/movie';
-
-export type VerticalMoviesProps = {
-	isError?: boolean;
-	isSuccess?: boolean;
-	isLoading?: boolean;
-	movies?: PartialMovie[];
-};
