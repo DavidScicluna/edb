6dbd557d87913f8d52a305ae74c1f466388cd1d6
David Scicluna <dave_nostra@hotmail.com>
2022-10-21 21:53:43 +0200

    Fix Query Keys

--- src/common/keys/index.ts
+++ src/common/keys/index.ts
@@ -1,7 +1,5 @@
 import { QueryKey } from '@tanstack/react-query';
 
-import { memoize } from 'lodash';
-
 import { UseCertificationsQueryProps } from '../queries/useCertificationsQuery';
 import { UseGenresQueryProps } from '../queries/useGenresQuery';
 import { UseKeywordsInfiniteQueryProps } from '../queries/useKeywordsInfiniteQuery';
@@ -9,37 +7,63 @@ import { UsePopularQueryProps } from '../queries/usePopularQuery';
 import { UseSearchInfiniteQueryProps } from '../queries/useSearchInfiniteQuery';
 import { UseTopRatedQueryProps } from '../queries/useTopRatedQuery';
 import { UseTrendingQueryProps } from '../queries/useTrendingQuery';
+import { AxiosConfigParams } from '../types';
+import { memoizeDebounce } from '../scripts';
 
-export const certificationsQueryKey = memoize(
-	({ mediaType }: UseCertificationsQueryProps): QueryKey => [`${mediaType}_certifications`]
+const timeout = 500;
+
+export const certificationsQueryKey = memoizeDebounce(
+	({ mediaType }: UseCertificationsQueryProps): QueryKey => [`${mediaType}_certifications`],
+	timeout
 );
 
-export const countriesQueryKey: QueryKey = ['countries'];
+export const countriesQueryKey = memoizeDebounce((): QueryKey => ['countries'], timeout);
 
-export const genresQueryKey = memoize(({ mediaType }: UseGenresQueryProps): QueryKey => [`${mediaType}_genres`]);
-
-export const jobsQueryKey: QueryKey = ['jobs'];
-
-export const keywordsInfiniteQueryKey = memoize(
-	({ query }: UseKeywordsInfiniteQueryProps): QueryKey => [`${query}_search_keywords_infinite`]
+export const genresQueryKey = memoizeDebounce(
+	({ mediaType }: UseGenresQueryProps): QueryKey => [`${mediaType}_genres`],
+	timeout
 );
 
-export const languagesQueryKey: QueryKey = ['languages'];
+export const jobsQueryKey = memoizeDebounce((): QueryKey => ['jobs'], timeout);
 
-export const peopleInfiniteQueryKey: QueryKey = ['people'];
-
-export const popularQueryKey = memoize(({ mediaType }: UsePopularQueryProps): QueryKey => [`popular_${mediaType}`]);
-
-export const searchInfiniteQueryKey = memoize(
-	({ mediaType, query }: UseSearchInfiniteQueryProps): QueryKey => [`${query}_${mediaType}_search_infinite`]
+export const keywordsInfiniteQueryKey = memoizeDebounce(
+	({ query }: UseKeywordsInfiniteQueryProps): QueryKey => [`${query}_search_keywords_infinite`],
+	timeout
 );
 
-export const topRatedQueryKey = memoize(({ mediaType }: UseTopRatedQueryProps): QueryKey => [`top_rated_${mediaType}`]);
+export const languagesQueryKey = memoizeDebounce((): QueryKey => ['languages'], timeout);
 
-export const trendingQueryKey = memoize(
-	({ mediaType, time }: UseTrendingQueryProps): QueryKey => [`${time}_${mediaType}_trending`]
+export const moviesInfiniteQueryKey = memoizeDebounce(
+	({ params }: AxiosConfigParams): QueryKey => ['movies', params],
+	timeout
 );
 
-export const trendingInfiniteQueryKey = memoize(
-	({ mediaType, time }: UseTrendingQueryProps): QueryKey => [`${time}_${mediaType}_trending_infinite`]
+export const peopleInfiniteQueryKey = memoizeDebounce(
+	({ params }: AxiosConfigParams): QueryKey => ['people', params],
+	timeout
+);
+
+export const popularQueryKey = memoizeDebounce(
+	({ mediaType }: UsePopularQueryProps): QueryKey => [`popular_${mediaType}`],
+	timeout
+);
+
+export const searchInfiniteQueryKey = memoizeDebounce(
+	({ mediaType, query }: UseSearchInfiniteQueryProps): QueryKey => [`${query}_${mediaType}_search_infinite`],
+	timeout
+);
+
+export const topRatedQueryKey = memoizeDebounce(
+	({ mediaType }: UseTopRatedQueryProps): QueryKey => [`top_rated_${mediaType}`],
+	timeout
+);
+
+export const trendingQueryKey = memoizeDebounce(
+	({ mediaType, time }: UseTrendingQueryProps): QueryKey => [`${time}_${mediaType}_trending`],
+	timeout
+);
+
+export const trendingInfiniteQueryKey = memoizeDebounce(
+	({ mediaType, time }: UseTrendingQueryProps): QueryKey => [`${time}_${mediaType}_trending_infinite`],
+	timeout
 );
