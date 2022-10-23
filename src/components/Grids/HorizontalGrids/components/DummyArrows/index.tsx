d04aa6d949c7ab90408d391228ea5f612f4acd55
David Scicluna <dave_nostra@hotmail.com>
2022-10-23 18:21:16 +0200

    Fix: Grids - HorizontalGrids - DummyArrows - Added ButtonGroupItem to ButtonGroup

--- src/components/Grids/HorizontalGrids/components/DummyArrows/index.tsx
+++ src/components/Grids/HorizontalGrids/components/DummyArrows/index.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 
-import { ButtonGroup } from '@davidscicluna/component-library';
+import { ButtonGroup, ButtonGroupItem } from '@davidscicluna/component-library';
 
 import DummyArrow from './components/DummyArrow';
 import { DummyArrowsProps } from './types';
@@ -8,8 +8,12 @@ import { DummyArrowsProps } from './types';
 const DummyArrows: FC<DummyArrowsProps> = (props = {}) => {
 	return (
 		<ButtonGroup isAttached spacing={1}>
-			<DummyArrow {...props} direction='left' />
-			<DummyArrow {...props} direction='right' />
+			<ButtonGroupItem index={0} total={1}>
+				<DummyArrow {...props} direction='left' />
+			</ButtonGroupItem>
+			<ButtonGroupItem index={1} total={1}>
+				<DummyArrow {...props} direction='right' />
+			</ButtonGroupItem>
 		</ButtonGroup>
 	);
 };
