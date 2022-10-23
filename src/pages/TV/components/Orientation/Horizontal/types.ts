dda09c10297c72354bb91cd2129cfc62a5f3db23
David Scicluna <dave_nostra@hotmail.com>
2022-10-23 21:35:06 +0200

    Fix: TV - Removed Orientation components

--- src/pages/TV/components/Orientation/Horizontal/types.ts
+++
@@ -1,8 +0,0 @@
-import { PartialTV } from '../../../../../common/types/tv';
-
-export type HorizontalTVProps = {
-	isError?: boolean;
-	isSuccess?: boolean;
-	isLoading?: boolean;
-	shows?: PartialTV[];
-};
