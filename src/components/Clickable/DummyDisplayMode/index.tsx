ced151826353380b21d91f373604aaaa93e8dfd6
David Scicluna <dave_nostra@hotmail.com>
2022-10-23 18:20:46 +0200

    Fix: DummyDisplayMode - Added ButtonGroupItem to ButtonGroup

--- src/components/Clickable/DummyDisplayMode/index.tsx
+++ src/components/Clickable/DummyDisplayMode/index.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 
-import { ButtonGroup, DummyIconButton, IconButtonIcon } from '@davidscicluna/component-library';
+import { ButtonGroup, ButtonGroupItem, DummyIconButton, IconButtonIcon } from '@davidscicluna/component-library';
 
 import { useSelector, useUserTheme } from '../../../common/hooks';
 
@@ -13,29 +13,33 @@ const DummyDisplayMode: FC<DummyDisplayModeProps> = (props) => {
 
 	return (
 		<ButtonGroup>
-			<DummyIconButton
-				{...props}
-				aria-label={
-					displayMode === 'grid' ? 'Display-mode set to Grid mode' : 'Switch display-mode to Grid mode'
-				}
-				color={displayMode === 'grid' ? color : 'gray'}
-				colorMode={colorMode}
-				variant='outlined'
-			>
-				<IconButtonIcon icon='grid_on' category='outlined' />
-			</DummyIconButton>
+			<ButtonGroupItem index={0} total={1}>
+				<DummyIconButton
+					{...props}
+					aria-label={
+						displayMode === 'grid' ? 'Display-mode set to Grid mode' : 'Switch display-mode to Grid mode'
+					}
+					color={displayMode === 'grid' ? color : 'gray'}
+					colorMode={colorMode}
+					variant='outlined'
+				>
+					<IconButtonIcon icon='grid_on' category='outlined' />
+				</DummyIconButton>
+			</ButtonGroupItem>
 
-			<DummyIconButton
-				{...props}
-				aria-label={
-					displayMode === 'list' ? 'Display-mode set to List mode' : 'Switch display-mode to List mode'
-				}
-				color={displayMode === 'list' ? color : 'gray'}
-				colorMode={colorMode}
-				variant='outlined'
-			>
-				<IconButtonIcon icon='view_agenda' category='outlined' />
-			</DummyIconButton>
+			<ButtonGroupItem index={1} total={1}>
+				<DummyIconButton
+					{...props}
+					aria-label={
+						displayMode === 'list' ? 'Display-mode set to List mode' : 'Switch display-mode to List mode'
+					}
+					color={displayMode === 'list' ? color : 'gray'}
+					colorMode={colorMode}
+					variant='outlined'
+				>
+					<IconButtonIcon icon='view_agenda' category='outlined' />
+				</DummyIconButton>
+			</ButtonGroupItem>
 		</ButtonGroup>
 	);
 };
