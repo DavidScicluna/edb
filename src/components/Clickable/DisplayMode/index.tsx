212057c5aa14b1dff50674c8b3737ec816c2812a
David Scicluna <dave_nostra@hotmail.com>
2022-10-23 18:20:37 +0200

    Fix: DisplayMode - Added ButtonGroupItem to ButtonGroup

--- src/components/Clickable/DisplayMode/index.tsx
+++ src/components/Clickable/DisplayMode/index.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 
-import { ButtonGroup, Tooltip, IconButton, IconButtonIcon } from '@davidscicluna/component-library';
+import { ButtonGroup, ButtonGroupItem, Tooltip, IconButton, IconButtonIcon } from '@davidscicluna/component-library';
 
 import { useBoolean } from '@chakra-ui/react';
 
@@ -22,63 +22,75 @@ const DisplayMode: FC<DisplayModeProps> = ({ isDisabled, ...rest }) => {
 
 	return (
 		<ButtonGroup>
-			<Tooltip
-				aria-label={
-					displayMode === 'grid'
-						? 'Display-mode set to Grid mode (tooltip)'
-						: 'Switch display-mode to Grid mode (tooltip)'
-				}
-				colorMode={colorMode}
-				isOpen={!isDisabled && isHoveringGrid}
-				isDisabled={isDisabled}
-				placement='top'
-				label={displayMode === 'grid' ? 'Display-mode set to Grid mode' : 'Switch display-mode to Grid mode'}
-			>
-				<IconButton
-					{...rest}
+			<ButtonGroupItem index={0} total={1}>
+				<Tooltip
 					aria-label={
+						displayMode === 'grid'
+							? 'Display-mode set to Grid mode (tooltip)'
+							: 'Switch display-mode to Grid mode (tooltip)'
+					}
+					colorMode={colorMode}
+					isOpen={!isDisabled && isHoveringGrid}
+					isDisabled={isDisabled}
+					placement='top'
+					label={
 						displayMode === 'grid' ? 'Display-mode set to Grid mode' : 'Switch display-mode to Grid mode'
 					}
-					color={displayMode === 'grid' ? color : 'gray'}
-					colorMode={colorMode}
-					isActive={displayMode === 'grid'}
-					onMouseEnter={() => setIsHoveringGrid.on()}
-					onMouseLeave={() => setIsHoveringGrid.off()}
-					onClick={displayMode === 'list' ? () => dispatch(toggleDisplayMode('grid')) : undefined}
-					variant='outlined'
 				>
-					<IconButtonIcon icon='grid_on' category='outlined' />
-				</IconButton>
-			</Tooltip>
+					<IconButton
+						{...rest}
+						aria-label={
+							displayMode === 'grid'
+								? 'Display-mode set to Grid mode'
+								: 'Switch display-mode to Grid mode'
+						}
+						color={displayMode === 'grid' ? color : 'gray'}
+						colorMode={colorMode}
+						isActive={displayMode === 'grid'}
+						onMouseEnter={() => setIsHoveringGrid.on()}
+						onMouseLeave={() => setIsHoveringGrid.off()}
+						onClick={displayMode === 'list' ? () => dispatch(toggleDisplayMode('grid')) : undefined}
+						variant='outlined'
+					>
+						<IconButtonIcon icon='grid_on' category='outlined' />
+					</IconButton>
+				</Tooltip>
+			</ButtonGroupItem>
 
-			<Tooltip
-				aria-label={
-					displayMode === 'list'
-						? 'Display-mode set to List mode (tooltip)'
-						: 'Switch display-mode to List mode (tooltip)'
-				}
-				colorMode={colorMode}
-				isOpen={!isDisabled && isHoveringList}
-				isDisabled={isDisabled}
-				placement='top'
-				label={displayMode === 'list' ? 'Display-mode set to List mode' : 'Switch display-mode to List mode'}
-			>
-				<IconButton
-					{...rest}
+			<ButtonGroupItem index={1} total={1}>
+				<Tooltip
 					aria-label={
+						displayMode === 'list'
+							? 'Display-mode set to List mode (tooltip)'
+							: 'Switch display-mode to List mode (tooltip)'
+					}
+					colorMode={colorMode}
+					isOpen={!isDisabled && isHoveringList}
+					isDisabled={isDisabled}
+					placement='top'
+					label={
 						displayMode === 'list' ? 'Display-mode set to List mode' : 'Switch display-mode to List mode'
 					}
-					color={displayMode === 'list' ? color : 'gray'}
-					colorMode={colorMode}
-					isActive={displayMode === 'list'}
-					onMouseEnter={() => setIsHoveringList.on()}
-					onMouseLeave={() => setIsHoveringList.off()}
-					onClick={displayMode === 'grid' ? () => dispatch(toggleDisplayMode('list')) : undefined}
-					variant='outlined'
 				>
-					<IconButtonIcon icon='view_agenda' category='outlined' />
-				</IconButton>
-			</Tooltip>
+					<IconButton
+						{...rest}
+						aria-label={
+							displayMode === 'list'
+								? 'Display-mode set to List mode'
+								: 'Switch display-mode to List mode'
+						}
+						color={displayMode === 'list' ? color : 'gray'}
+						colorMode={colorMode}
+						isActive={displayMode === 'list'}
+						onMouseEnter={() => setIsHoveringList.on()}
+						onMouseLeave={() => setIsHoveringList.off()}
+						onClick={displayMode === 'grid' ? () => dispatch(toggleDisplayMode('list')) : undefined}
+						variant='outlined'
+					>
+						<IconButtonIcon icon='view_agenda' category='outlined' />
+					</IconButton>
+				</Tooltip>
+			</ButtonGroupItem>
 		</ButtonGroup>
 	);
 };
