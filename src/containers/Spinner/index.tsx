c0caa02a24665850658e0581ce8b0f7c076d1fd4
David Scicluna <dave_nostra@hotmail.com>
2022-10-23 21:33:51 +0200

    Fix: Spinner - Added ModalStack

--- src/containers/Spinner/index.tsx
+++ src/containers/Spinner/index.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 
-import { useTheme, Modal, ModalBody, ScaleFade, SlideFade, utils } from '@davidscicluna/component-library';
+import { useTheme, Modal, ModalStack, ModalBody, ScaleFade, SlideFade, utils } from '@davidscicluna/component-library';
 
 import { useBreakpointValue, useConst, Center, Show } from '@chakra-ui/react';
 
@@ -36,82 +36,73 @@ const Spinner: FC<SpinnerProps> = ({ isOpen = false, onClose }) => {
 
 	return (
 		<Modal isOpen={isOpen} closeOnEsc={false} closeOnOverlayClick={false} onClose={onClose} size='full' spacing={0}>
-			<ModalBody backgroundColor={getColor({ theme, colorMode, type: 'background' })} sx={{ transition: 'none' }}>
-				<Center width='100%' height='100vh'>
-					{/* Top */}
-					<Center position='absolute' top={0} left='50%' transform='translateX(-50%)' p={2}>
-						<ScaleFade
-							in
-							transition={{ enter: { duration, delay: delayLabel }, exit: { duration } }}
-							unmountOnExit
-						>
-							<SpinnerLabel colorMode={colorMode} />
-						</ScaleFade>
-					</Center>
-
-					{/* Bottom */}
-					<Center
-						position='absolute'
-						bottom={0}
-						left='50%'
-						transform='translateX(-50%) rotate(-180deg)'
-						p={2}
-					>
-						<ScaleFade
-							in
-							transition={{ enter: { duration, delay: delayLabel }, exit: { duration } }}
-							unmountOnExit
-						>
-							<SpinnerLabel colorMode={colorMode} />
-						</ScaleFade>
-					</Center>
-
-					<Show breakpoint='(min-width: 600px)'>
-						{/* Left */}
-						<Center
-							position='absolute'
-							top='50%'
-							left={-(labelWidth / 2 - labelHeight / 2)}
-							transform='translateY(-50%) rotate(-90deg)'
-							p={2}
-						>
-							<ScaleFade
-								in
-								transition={{ enter: { duration, delay: delayLabel }, exit: { duration } }}
-								unmountOnExit
-							>
+			<ModalStack>
+				<ModalBody background={getColor({ theme, colorMode, type: 'background' })} sx={{ transition: 'none' }}>
+					<Center width='100%' height='100vh'>
+						{/* Top */}
+						<Center position='absolute' top={0} left='50%' transform='translateX(-50%)' p={2}>
+							<ScaleFade in transition={{ enter: { duration, delay: delayLabel }, exit: { duration } }}>
 								<SpinnerLabel colorMode={colorMode} />
 							</ScaleFade>
 						</Center>
 
-						{/* Right */}
+						{/* Bottom */}
 						<Center
 							position='absolute'
-							top='50%'
-							right={-(labelWidth / 2 - labelHeight / 2)}
-							transform='translateY(-50%) rotate(-270deg)'
+							bottom={0}
+							left='50%'
+							transform='translateX(-50%) rotate(-180deg)'
 							p={2}
 						>
-							<ScaleFade
-								in
-								transition={{ enter: { duration, delay: delayLabel }, exit: { duration } }}
-								unmountOnExit
-							>
+							<ScaleFade in transition={{ enter: { duration, delay: delayLabel }, exit: { duration } }}>
 								<SpinnerLabel colorMode={colorMode} />
 							</ScaleFade>
 						</Center>
-					</Show>
 
-					<SlideFade
-						in
-						transition={{ enter: { duration, delay: delayLogo }, exit: { duration } }}
-						unmountOnExit
-						offsetY={logoOffsetY}
-					>
-						<SpinnerLogo />
-					</SlideFade>
-				</Center>
-			</ModalBody>
+						<Show breakpoint='(min-width: 600px)'>
+							{/* Left */}
+							<Center
+								position='absolute'
+								top='50%'
+								left={-(labelWidth / 2 - labelHeight / 2)}
+								transform='translateY(-50%) rotate(-90deg)'
+								p={2}
+							>
+								<ScaleFade
+									in
+									transition={{ enter: { duration, delay: delayLabel }, exit: { duration } }}
+								>
+									<SpinnerLabel colorMode={colorMode} />
+								</ScaleFade>
+							</Center>
+
+							{/* Right */}
+							<Center
+								position='absolute'
+								top='50%'
+								right={-(labelWidth / 2 - labelHeight / 2)}
+								transform='translateY(-50%) rotate(-270deg)'
+								p={2}
+							>
+								<ScaleFade
+									in
+									transition={{ enter: { duration, delay: delayLabel }, exit: { duration } }}
+								>
+									<SpinnerLabel colorMode={colorMode} />
+								</ScaleFade>
+							</Center>
+						</Show>
+
+						<SlideFade
+							in
+							transition={{ enter: { duration, delay: delayLogo }, exit: { duration } }}
+							offsetY={logoOffsetY}
+						>
+							<SpinnerLogo />
+						</SlideFade>
+					</Center>
+				</ModalBody>
+			</ModalStack>
 		</Modal>
 	);
 };
