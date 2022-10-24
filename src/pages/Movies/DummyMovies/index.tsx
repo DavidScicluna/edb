abcb3336fee2673fc28dc4ebe2eb7616cc989f01
David Scicluna <dave_nostra@hotmail.com>
2022-10-24 19:33:10 +0200

    Feature: Movies - DummyMovies

---
+++ src/pages/Movies/DummyMovies/index.tsx
@@ -0,0 +1,60 @@
+import { FC } from 'react';
+
+import { useTheme, Skeleton, DummyButton } from '@davidscicluna/component-library';
+
+import { useMediaQuery, HStack, Text } from '@chakra-ui/react';
+
+import { useLayoutContext } from '../../../containers/Layout/common/hooks';
+import Page from '../../../containers/Page';
+import PageBody from '../../../containers/Page/components/PageBody';
+import PageHeader from '../../../containers/Page/components/PageHeader';
+import { formatMediaTypeLabel } from '../../../common/utils';
+import { DummyDisplayMode } from '../../../components';
+import VerticalDummyMovies from '../components/VerticalDummyMovies';
+import { useUserTheme } from '../../../common/hooks';
+
+const DummyMovies: FC = () => {
+	const theme = useTheme();
+	const { colorMode } = useUserTheme();
+
+	const [isLg] = useMediaQuery(`(max-width: ${theme.breakpoints.lg})`);
+
+	const { spacing } = useLayoutContext();
+
+	return (
+		<Page>
+			<PageHeader
+				renderTitle={(props) => (
+					<Skeleton colorMode={colorMode} isLoaded={false} variant='text'>
+						<Text {...props}>{formatMediaTypeLabel({ type: 'multiple', mediaType: 'movie' })}</Text>
+					</Skeleton>
+				)}
+				renderSubtitle={(props) => (
+					<Skeleton colorMode={colorMode} isLoaded={false} variant='text'>
+						<Text {...props}>
+							A list containing all the MEDIATYPE that have been released or will be in the coming months.
+						</Text>
+					</Skeleton>
+				)}
+				actions={
+					<HStack width={isLg ? '100%' : 'auto'} spacing={2}>
+						<DummyButton isFullWidth={isLg} variant='outlined'>
+							Filter
+						</DummyButton>
+						<DummyButton isFullWidth={isLg} variant='outlined'>
+							Sort By
+						</DummyButton>
+						<DummyDisplayMode />
+					</HStack>
+				}
+				direction={isLg ? 'column' : 'row'}
+				p={spacing}
+			/>
+			<PageBody p={spacing}>
+				<VerticalDummyMovies />
+			</PageBody>
+		</Page>
+	);
+};
+
+export default DummyMovies;
