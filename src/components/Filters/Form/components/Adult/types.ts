8040ae051b1d172a4b344db061d246a94eaa4389
David Scicluna <dave_nostra@hotmail.com>
2022-10-25 18:31:31 +0200

    Fix: Filters - Form - Removed Adult

--- src/components/Filters/Form/components/Adult/types.ts
+++
@@ -1,9 +0,0 @@
-import { UseFormReturn } from 'react-hook-form';
-
-import { MediaType } from '../../../../../common/types';
-import { Filters } from '../../../types';
-
-export type AdultProps = {
-	form: UseFormReturn<Filters>;
-	mediaType: Omit<MediaType, 'person'>;
-};
