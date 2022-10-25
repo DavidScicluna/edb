59a977d2f8c0a02c3df3f0086f0701e6a102e775
David Scicluna <dave_nostra@hotmail.com>
2022-10-25 18:38:40 +0200

    Fix: Filters - Form - utils

--- src/components/Filters/Form/common/utils/index.ts
+++ src/components/Filters/Form/common/utils/index.ts
@@ -1,14 +1,26 @@
-/**
- * This method will check whether the number passed is in between the 2 active values
- *
- * @param number number - The number to check
- * @param value array of number - The active numbers
- * @returns boolean - If number is between the 2 active numbers
- */
-export const handleCheckIfInRange = (number: number, value?: number[]): boolean => {
-	if (value && value.length > 0) {
-		return value[0] < number && value[1] > number;
+import { sort } from 'fast-sort';
+import { memoize } from 'lodash';
+
+import { FiltersFormNumbers } from '../../../types';
+
+type FiltersFormNumbersProps = { list?: FiltersFormNumbers; number: number };
+
+export const getIsFiltersFormNumbersInList = memoize(({ list = [], number }: FiltersFormNumbersProps): boolean => {
+	if (list.some((num) => num === number)) {
+		return true;
+	} else if (list && list[0] && list[1]) {
+		return list[0] < number && list[1] > number;
 	} else {
 		return false;
 	}
-};
+});
+
+export const getFiltersFormNumbers = memoize(({ list = [], number }: FiltersFormNumbersProps) => {
+	return sort(
+		list.some((num) => num === number)
+			? [...list].filter((num) => num !== number)
+			: list.length > 1
+			? [...list, number].filter((_num, index) => index !== 0)
+			: [...list, number]
+	).asc();
+});
