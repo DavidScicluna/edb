2ac034f40e7082308b9e247b2c8d55dded18f9b1
David Scicluna <dave_nostra@hotmail.com>
2022-10-25 22:11:39 +0200

    Fix: Movies - OriginalMovies - MoviesDisplayFilters

--- src/pages/Movies/OriginalMovies/components/MoviesDisplayFilters/index.tsx
+++ src/pages/Movies/OriginalMovies/components/MoviesDisplayFilters/index.tsx
@@ -1,26 +1,14 @@
-import React, { FC, useState } from 'react';
-
-import { useLocation } from 'react-router';
+import { FC } from 'react';
 
 import { Collapse } from '@davidscicluna/component-library';
 
-import { useDebounce, useUpdateEffect } from 'usehooks-ts';
-
-import { getTotalFilters } from '../../../../../components/Filters/common/utils';
 import { DisplayFilters } from '../../../../../components';
 
 import { MoviesDisplayFiltersProps } from './types';
 
-const MoviesDisplayFilters: FC<MoviesDisplayFiltersProps> = ({ onTagDelete, onClear }) => {
-	const location = useLocation();
-
-	const [total, setTotal] = useState<number>(getTotalFilters({ location, mediaType: 'movie' }) || 0);
-	const totalDebounced = useDebounce<number>(total, 500);
-
-	useUpdateEffect(() => setTotal(getTotalFilters({ location, mediaType: 'movie' }) || 0), [location.search]);
-
+const MoviesDisplayFilters: FC<MoviesDisplayFiltersProps> = ({ total, onTagDelete, onClear }) => {
 	return (
-		<Collapse in={totalDebounced > 0} style={{ width: '100%' }}>
+		<Collapse in={total > 0} style={{ width: '100%' }}>
 			<DisplayFilters mediaType='movie' onTagDelete={onTagDelete} onClear={onClear} />
 		</Collapse>
 	);
--- src/pages/Movies/OriginalMovies/components/MoviesDisplayFilters/types.ts
+++ src/pages/Movies/OriginalMovies/components/MoviesDisplayFilters/types.ts
@@ -1,3 +1,5 @@
 import { DisplayFiltersProps } from '../../../../../components/Filters/DisplayFilters/types';
 
-export type MoviesDisplayFiltersProps = Pick<DisplayFiltersProps, 'onTagDelete' | 'onClear'>;
+export type MoviesDisplayFiltersProps = Pick<DisplayFiltersProps, 'onTagDelete' | 'onClear'> & {
+	total: number;
+};
