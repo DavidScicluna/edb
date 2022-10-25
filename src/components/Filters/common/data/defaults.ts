609729e3d18f6b6fef7bcb6dcb1e0e70df4257e7
David Scicluna <dave_nostra@hotmail.com>
2022-10-25 20:35:08 +0200

    Feature: Filters - defaults

---
+++ src/components/Filters/common/data/defaults.ts
@@ -0,0 +1,10 @@
+import { FiltersForm } from '../../types';
+
+export default {
+	dates: { gte: null, lte: null },
+	genres: [],
+	certifications: [],
+	rating: [],
+	count: [],
+	runtime: []
+} as FiltersForm;
