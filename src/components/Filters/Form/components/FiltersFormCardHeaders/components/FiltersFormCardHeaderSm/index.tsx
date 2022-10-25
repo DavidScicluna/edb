1877a29353818a3f581b99a5799d5614ff43e896
David Scicluna <dave_nostra@hotmail.com>
2022-10-25 18:41:58 +0200

    Feature: Filters - Form - FiltersFormCardHeaders - FiltersFormCardHeaderSm

---
+++ src/components/Filters/Form/components/FiltersFormCardHeaders/components/FiltersFormCardHeaderSm/index.tsx
@@ -0,0 +1,75 @@
+import { FC, useContext, useCallback } from 'react';
+
+import { Space, useTheme, utils } from '@davidscicluna/component-library';
+
+import { VStack, HStack, Center, Text } from '@chakra-ui/react';
+
+import { useElementSize } from 'usehooks-ts';
+
+import {
+	CommonFiltersFormCardHeadersProps as FiltersFormCardHeaderSmProps,
+	FiltersFormCardHeadersContext as FiltersFormCardHeadersContextType
+} from '../../common/types';
+import { FiltersFormCardHeadersContext } from '../..';
+import { useUserTheme } from '../../../../../../../common/hooks';
+
+const { convertREMToPixels, convertStringToNumber, getColor } = utils;
+
+const spacing: Space = 2;
+
+const FiltersFormCardHeaderSm: FC<FiltersFormCardHeaderSmProps> = (props) => {
+	const theme = useTheme();
+	const { colorMode } = useUserTheme();
+
+	const { renderButtonProps, renderMessageProps } =
+		useContext<FiltersFormCardHeadersContextType>(FiltersFormCardHeadersContext);
+
+	const [buttonRef, { width: buttonWidth }] = useElementSize();
+
+	const { title, subtitle, renderButton, renderMessage } = props;
+
+	const handleCalculateTextWidth = useCallback((): string => {
+		const spacingWidth = convertREMToPixels(convertStringToNumber(theme.space[spacing], 'rem'));
+
+		return `calc(100% - ${buttonWidth + spacingWidth}px)`;
+	}, [theme, spacing, buttonWidth]);
+
+	return (
+		<VStack width='100%' alignItems='flex-start' justifyContent='center' spacing={spacing}>
+			<HStack width='100%' alignItems='center' justifyContent='space-between' spacing={spacing}>
+				<VStack width={handleCalculateTextWidth()} alignItems='flex-start' spacing={0.5}>
+					{/* Title */}
+					<Text
+						align='left'
+						color={getColor({ theme, colorMode, type: 'text.primary' })}
+						fontSize='xl'
+						fontWeight='bold'
+						lineHeight='normal'
+						noOfLines={1}
+					>
+						{title}
+					</Text>
+
+					{/* Subtitle */}
+					{subtitle && (
+						<Text
+							align='left'
+							color={getColor({ theme, colorMode, type: 'text.secondary' })}
+							fontSize='sm'
+							lineHeight='normal'
+							noOfLines={1}
+						>
+							{subtitle}
+						</Text>
+					)}
+				</VStack>
+
+				<Center ref={buttonRef}>{renderButton({ ...renderButtonProps })}</Center>
+			</HStack>
+
+			{renderMessage && renderMessage({ ...renderMessageProps })}
+		</VStack>
+	);
+};
+
+export default FiltersFormCardHeaderSm;
