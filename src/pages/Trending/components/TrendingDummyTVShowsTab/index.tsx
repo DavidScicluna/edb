9da9d1f6f65c8c4e36894f37997d9c4084f8192d
David Scicluna <dave_nostra@hotmail.com>
2022-10-25 22:06:54 +0200

    Fix: Trending - Renamed TrendingDummyTVTab to TrendingDummyTVShowsTab

--- src/pages/Trending/components/TrendingDummyTVShowsTab/index.tsx
+++ src/pages/Trending/components/TrendingDummyTVShowsTab/index.tsx
@@ -9,7 +9,7 @@ import { range } from 'lodash';
 import { useLayoutContext } from '../../../../containers/Layout/common/hooks';
 import { DummyLoadMore, DummyHorizontalPoster, DummyVerticalPoster, VerticalGrid } from '../../../../components';
 
-const TrendingDummyTVTab: FC = () => {
+const TrendingDummyTVShowsTab: FC = () => {
 	const theme = useTheme();
 
 	const [isSm] = useMediaQuery(`(max-width: ${theme.breakpoints.sm})`);
@@ -37,4 +37,4 @@ const TrendingDummyTVTab: FC = () => {
 	);
 };
 
-export default TrendingDummyTVTab;
+export default TrendingDummyTVShowsTab;
