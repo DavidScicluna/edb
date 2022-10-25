9e1558a99236b9b1c89992ed95caa233587db8b6
David Scicluna <dave_nostra@hotmail.com>
2022-10-25 18:39:56 +0200

    Fix: Filters - Form - RuntimeRange

--- src/components/Filters/Form/components/RuntimeRange/index.tsx
+++ src/components/Filters/Form/components/RuntimeRange/index.tsx
@@ -1,130 +1,102 @@
-import { ReactElement } from 'react';
+import { FC } from 'react';
 
-import { Card, CardBody, ButtonGroup, Button } from '@davidscicluna/component-library';
+import {
+	useTheme,
+	Card,
+	CardBody,
+	ButtonGroup,
+	ButtonGroupItem,
+	Button,
+	SlideFade
+} from '@davidscicluna/component-library';
 
-import { useTheme, useMediaQuery, useConst, Text, ScaleFade } from '@chakra-ui/react';
+import { useMediaQuery, useConst, Text } from '@chakra-ui/react';
 
 import { Controller } from 'react-hook-form';
 import compact from 'lodash/compact';
 import range from 'lodash/range';
+import { round, toString } from 'lodash';
 
-import { defaultValues } from '../..';
-import { useSelector } from '../../../../../common/hooks';
-import { defaultUser, getUser } from '../../../../../store/slices/Users';
-import { Filters } from '../../../types';
-import { handleCheckIfInRange } from '../../common/utils';
-import Header from '../Header';
+import { useUserTheme } from '../../../../../common/hooks';
+import { getIsFiltersFormNumbersInList, getFiltersFormNumbers } from '../../common/utils';
+import FiltersFormCardHeader from '../FiltersFormCardHeaders';
+import defaultValues from '../../../common/data/defaults';
+import { CommonFiltersFormProps as RuntimeRangeProps } from '../../common/types';
+import { getFontSizeHeight } from '../../../../../common/utils';
 
-import { RuntimeRangeProps } from './types';
-
-const RuntimeRange = ({ form }: RuntimeRangeProps): ReactElement => {
+const RuntimeRange: FC<RuntimeRangeProps> = ({ form }) => {
 	const theme = useTheme();
-	const [isMd] = useMediaQuery('(max-width: 760px)');
+	const { color, colorMode } = useUserTheme();
 
-	const color = useSelector(
-		(state) => getUser(state.users.data.users, state.app.data.user)?.ui.theme.color || defaultUser.ui.theme.color
-	);
+	const [isMd] = useMediaQuery(`(min-width: ${theme.breakpoints.md})`);
+
+	const { control, setValue } = form;
 
 	const runtimes = useConst(range(0, 475, 45));
 
-	const handleOnChange = (value: Filters['runtime'], number: number): void => {
-		const runtimes = compact(value);
-
-		if (runtimes.some((num) => num === number)) {
-			form.setValue(
-				'runtime',
-				[...runtimes].filter((num) => num !== number).sort((a, b) => a - b),
-				{ shouldDirty: true }
-			);
-		} else {
-			form.setValue(
-				'runtime',
-				runtimes.length > 1
-					? [...runtimes, number].filter((_num, index) => index !== 0).sort((a, b) => a - b)
-					: [...runtimes, number].sort((a, b) => a - b),
-				{
-					shouldDirty: true
-				}
-			);
-		}
-	};
-
 	return (
 		<Controller
-			control={form.control}
+			control={control}
 			name='runtime'
-			render={({ field }) => {
-				const value = compact(field.value);
-
-				return (
-					<Card isFullWidth>
-						<Header
-							label='Runtime Range'
-							renderMessage={({ color, fontSize, fontWeight }) => (
-								<ScaleFade in={value.length > 0} unmountOnExit>
-									<Text color={color} fontSize={fontSize} fontWeight={fontWeight}>
-										{value.map((runtime) => `${runtime} minutes`).join(' -> ')}
-									</Text>
-								</ScaleFade>
-							)}
-							renderButton={({ color, size, variant }) => (
-								<Button
-									color={color}
-									isDisabled={value.length === 0}
-									onClick={() =>
-										form.setValue('runtime', defaultValues.runtime, { shouldDirty: true })
-									}
-									size={size}
-									variant={variant}
+			render={({ field: { onBlur, value, name } }) => (
+				<Card colorMode={colorMode} isFullWidth onBlur={onBlur} p={2}>
+					<FiltersFormCardHeader
+						title='Runtime Range'
+						subtitle=''
+						renderMessage={({ fontSize, ...rest }) => (
+							<SlideFade
+								in={value.length > 0}
+								offsetY={round(getFontSizeHeight({ theme, fontSize }) / 4)}
+							>
+								<Text {...rest} fontSize={fontSize}>
+									{value.map((runtime) => `${runtime} minutes`).join(' -> ')}
+								</Text>
+							</SlideFade>
+						)}
+						renderButton={(props) => (
+							<Button
+								{...props}
+								isDisabled={value.length === 0}
+								onClick={() => setValue(name, defaultValues.runtime, { shouldDirty: true })}
+							>
+								Clear
+							</Button>
+						)}
+					/>
+					<CardBody>
+						<ButtonGroup sx={{ width: '100%', flexWrap: isMd ? 'wrap' : 'nowrap' }}>
+							{runtimes.map((runtime, index) => (
+								<ButtonGroupItem
+									index={index}
+									total={runtimes.length - 1}
+									sx={{ flex: isMd ? 1 : undefined, width: isMd ? `${100 / 6}%` : 'auto' }}
 								>
-									Clear
-								</Button>
-							)}
-						/>
-						<CardBody>
-							<ButtonGroup isAttached sx={{ width: '100%', flexWrap: isMd ? 'wrap' : 'nowrap' }}>
-								{runtimes.map((number) => (
 									<Button
-										key={number}
+										key={runtime}
 										color={
-											value.some((runtime) => runtime === number) ||
-											handleCheckIfInRange(number, value)
+											getIsFiltersFormNumbersInList({ list: compact(value), number: runtime })
 												? color
 												: 'gray'
 										}
+										colorMode={colorMode}
 										isFullWidth
-										onClick={() => handleOnChange(value, number)}
-										variant={value.some((count) => count === number) ? 'contained' : 'outlined'}
-										sx={{
-											back: {
-												flex: isMd ? 1 : '',
-												minWidth: isMd ? `${100 / 6}%` : 'auto',
-												borderRadius:
-													number === runtimes[0]
-														? `${theme.radii.base} 0 0 ${theme.radii.base}`
-														: number === runtimes[runtimes.length - 1]
-														? `0 ${theme.radii.base} ${theme.radii.base} 0`
-														: 0
-											},
-											front: {
-												px: 0.5,
-												borderRadius:
-													number === runtimes[0]
-														? `${theme.radii.base} 0 0 ${theme.radii.base}`
-														: number === runtimes[runtimes.length - 1]
-														? `0 ${theme.radii.base} ${theme.radii.base} 0`
-														: 0
-											}
-										}}
+										onClick={() =>
+											setValue(
+												name,
+												getFiltersFormNumbers({ list: compact(value), number: runtime }),
+												{ shouldDirty: true }
+											)
+										}
+										variant={value.some((num) => num === runtime) ? 'contained' : 'outlined'}
 									>
-										{String(number)}
+										{toString(runtime)}
 									</Button>
-								))}
-							</ButtonGroup>
-						</CardBody>
-					</Card>
-				);
-			}}
+								</ButtonGroupItem>
+							))}
+						</ButtonGroup>
+					</CardBody>
+				</Card>
+			)}
 		/>
 	);
 };
--- src/components/Filters/Form/components/RuntimeRange/types.ts
+++
@@ -1,7 +0,0 @@
-import { UseFormReturn } from 'react-hook-form';
-
-import { Filters } from '../../../types';
-
-export type RuntimeRangeProps = {
-	form: UseFormReturn<Filters>;
-};
