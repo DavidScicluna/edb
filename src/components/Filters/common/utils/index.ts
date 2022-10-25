3461827ebc3cdb76157ce14ad5c5e65e4036d9fb
David Scicluna <dave_nostra@hotmail.com>
2022-10-25 20:35:16 +0200

    Fix: Filters - utils

--- src/components/Filters/common/utils/index.ts
+++ src/components/Filters/common/utils/index.ts
@@ -1,23 +1,15 @@
+import { isEmpty, isNil, memoize } from 'lodash';
 import compact from 'lodash/compact';
 import qs from 'query-string';
 
-import { Filters, FiltersMediaTypes } from '../../types';
+import { FiltersForm, FiltersMediaType } from '../../types';
+import defaultValues from '../data/defaults';
 
-export const handleReturnDefaultValues = (): Filters => {
-	return {
-		dates: { gte: null, lte: null },
-		genres: [],
-		certifications: [],
-		rating: [],
-		count: [],
-		runtime: []
-	};
-};
+type GetProps = { location: Partial<Location>; mediaType: FiltersMediaType };
 
-export const handlePopulateFilters = (locationSearch: string, mediaType: FiltersMediaTypes): Filters => {
-	const search = qs.parse(locationSearch);
-
-	const filters: Filters = handleReturnDefaultValues();
+export const getFiltersForm = memoize(({ location, mediaType }: GetProps): FiltersForm => {
+	const filters: FiltersForm = { ...defaultValues };
+	const search = qs.parse(location.search || '');
 
 	if (mediaType === 'movie') {
 		if (
@@ -33,7 +25,7 @@ export const handlePopulateFilters = (locationSearch: string, mediaType: Filters
 		} else if (search['primary_release_date.gte'] && typeof search['primary_release_date.gte'] === 'string') {
 			filters.dates.gte = search['primary_release_date.gte'];
 		}
-	} else if (mediaType === 'tv') {
+	} else {
 		if (
 			search['first_air_date.gte'] &&
 			typeof search['first_air_date.gte'] === 'string' &&
@@ -49,50 +41,50 @@ export const handlePopulateFilters = (locationSearch: string, mediaType: Filters
 		}
 	}
 
-	if (search['with_genres'] && typeof search['with_genres'] === 'string') {
+	if (search && search['with_genres'] && typeof search['with_genres'] === 'string') {
 		filters.genres = search['with_genres'].split(',').map((genre) => Number(genre));
 	}
 
-	if (search['certification'] && typeof search['certification'] === 'string') {
+	if (search && search['certification'] && typeof search['certification'] === 'string') {
 		filters.certifications = search['certification'].split('|').map((certification) => certification);
 	}
 
-	if (search['vote_average.gte'] && typeof search['vote_average.gte'] === 'string') {
+	if (search && search['vote_average.gte'] && typeof search['vote_average.gte'] === 'string') {
 		filters.rating = compact([
 			Number(search['vote_average.gte']),
 			filters.rating && filters.rating[1] ? Number(filters.rating[1]) : null
 		]);
 	}
 
-	if (search['vote_average.lte'] && typeof search['vote_average.lte'] === 'string') {
+	if (search && search['vote_average.lte'] && typeof search['vote_average.lte'] === 'string') {
 		filters.rating = compact([
 			filters.rating && filters.rating[0] ? Number(filters.rating[0]) : null,
 			Number(search['vote_average.lte'])
 		]);
 	}
 
-	if (search['vote_count.gte'] && typeof search['vote_count.gte'] === 'string') {
+	if (search && search['vote_count.gte'] && typeof search['vote_count.gte'] === 'string') {
 		filters.count = compact([
 			Number(search['vote_count.gte']),
 			filters.count && filters.count[1] ? Number(filters.count[1]) : null
 		]);
 	}
 
-	if (search['vote_count.lte'] && typeof search['vote_count.lte'] === 'string') {
+	if (search && search['vote_count.lte'] && typeof search['vote_count.lte'] === 'string') {
 		filters.count = compact([
 			filters.count && filters.count[0] ? Number(filters.count[0]) : null,
 			Number(search['vote_count.lte'])
 		]);
 	}
 
-	if (search['with_runtime.gte'] && typeof search['with_runtime.gte'] === 'string') {
+	if (search && search['with_runtime.gte'] && typeof search['with_runtime.gte'] === 'string') {
 		filters.runtime = compact([
 			Number(search['with_runtime.gte']),
 			filters.runtime && filters.runtime[1] ? Number(filters.runtime[1]) : null
 		]);
 	}
 
-	if (search['with_runtime.lte'] && typeof search['with_runtime.lte'] === 'string') {
+	if (search && search['with_runtime.lte'] && typeof search['with_runtime.lte'] === 'string') {
 		filters.runtime = compact([
 			filters.runtime && filters.runtime[0] ? Number(filters.runtime[0]) : null,
 			Number(search['with_runtime.lte'])
@@ -100,4 +92,24 @@ export const handlePopulateFilters = (locationSearch: string, mediaType: Filters
 	}
 
 	return filters;
-};
+});
+
+export const getTotalFilters = memoize(({ location, mediaType }: GetProps): number => {
+	const filters: FiltersForm = getFiltersForm({ location, mediaType });
+	let key: keyof FiltersForm;
+	let total = 0;
+
+	for (key in filters) {
+		if (
+			key === 'dates' &&
+			(!(isNil(filters.dates.gte) || isEmpty(filters.dates.gte)) ||
+				!(isNil(filters.dates.lte) || isEmpty(filters.dates.lte)))
+		) {
+			total = total + 1;
+		} else if (key !== 'dates' && !(isNil(filters[key]) || isEmpty(filters[key]))) {
+			total = total + 1;
+		}
+	}
+
+	return total;
+});
