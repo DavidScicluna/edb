217551f548efddcadf1e9c328cff81154399e126
David Scicluna <dave_nostra@hotmail.com>
2022-11-02 21:36:27 +0100

    Feature: User - UserProfile - UserProfileTabs - MyLikesTab - MyLikesTabDummyTabs - DummyAllTab - DummyAllTabPeople

---
+++ src/pages/User/pages/UserProfile/components/UserProfileTabs/components/MyLikesTab/components/MyLikesTabDummyTabs/components/DummyAllTab/components/DummyAllTabPeople/index.tsx
@@ -0,0 +1,18 @@
+import { FC } from 'react';
+
+import { range } from 'lodash';
+
+import AllTabDummyHorizontalGrid, { width } from '../DummyAllTabHorizontalGrid';
+import { DummyVerticalPoster } from '../../../../../../../../../../../../../../components';
+
+const AllTabDummyPeople: FC = () => {
+	return (
+		<AllTabDummyHorizontalGrid>
+			{range(20).map((_dummy, index) => (
+				<DummyVerticalPoster key={index} mediaType='person' hasSubtitle sx={{ width }} />
+			))}
+		</AllTabDummyHorizontalGrid>
+	);
+};
+
+export default AllTabDummyPeople;
