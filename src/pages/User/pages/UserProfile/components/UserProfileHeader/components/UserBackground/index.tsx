4dd5fc396729ac830b788c4ec91e694ca1ea8e24
David Scicluna <dave_nostra@hotmail.com>
2022-11-02 21:16:14 +0100

    Feature: User - UserProfile - UserProfileHeader - UserBackground

---
+++ src/pages/User/pages/UserProfile/components/UserProfileHeader/components/UserBackground/index.tsx
@@ -0,0 +1,28 @@
+import { FC } from 'react';
+
+import { Box, AspectRatio } from '@chakra-ui/react';
+
+import Image from '../../../../../../../../components/Image';
+import { useSelector } from '../../../../../../../../common/hooks';
+
+import { UserBackgroundProps } from './types';
+
+const UserBackground: FC<UserBackgroundProps> = ({ alt }) => {
+	const background_path = useSelector((state) => state.users.data.activeUser.data.info.background_path);
+
+	return (
+		<Box width='inherit' height='inherit'>
+			<AspectRatio borderRadius='none' ratio={20 / 5}>
+				<Image
+					alt={`${alt} Background`}
+					width='inherit'
+					height='inherit'
+					borderRadius='none'
+					src={{ full: background_path }}
+				/>
+			</AspectRatio>
+		</Box>
+	);
+};
+
+export default UserBackground;
