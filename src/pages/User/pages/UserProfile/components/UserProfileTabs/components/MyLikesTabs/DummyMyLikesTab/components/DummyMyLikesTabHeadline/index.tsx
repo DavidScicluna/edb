7f8db8983b62d6394190e753692dcb3922c6b06d
David Scicluna <dave_nostra@hotmail.com>
2022-11-02 22:18:25 +0100

    Feature: User - UserProfile - UserProfileTabs - MyLikesTabs - DummyMyLikesTab - DummyMyLikesTabHeadline

---
+++ src/pages/User/pages/UserProfile/components/UserProfileTabs/components/MyLikesTabs/DummyMyLikesTab/components/DummyMyLikesTabHeadline/index.tsx
@@ -0,0 +1,40 @@
+import { FC } from 'react';
+
+import { Badge, BadgeLabel, Skeleton } from '@davidscicluna/component-library';
+
+import { Text } from '@chakra-ui/react';
+
+import { Headline } from '../../../../../../../../../../../components';
+import { useUserTheme } from '../../../../../../../../../../../common/hooks';
+
+const DummyMyLikesTabHeadline: FC = () => {
+	const { color, colorMode } = useUserTheme();
+
+	return (
+		<Headline
+			renderCaption={() => (
+				// TODO: Replace with DummyBadge
+				<Skeleton color={color} colorMode={colorMode} isLoaded={false} variant='text'>
+					<Badge color={color} colorMode={colorMode} size='xs'>
+						<BadgeLabel>Total of # likes</BadgeLabel>
+					</Badge>
+				</Skeleton>
+			)}
+			renderTitle={(props) => (
+				<Skeleton colorMode={colorMode} isLoaded={false} variant='text'>
+					<Text {...props}>My Likes</Text>
+				</Skeleton>
+			)}
+			renderSubtitle={(props) => (
+				<Skeleton colorMode={colorMode} isLoaded={false} variant='text'>
+					<Text {...props}>
+						This Tab contains all likes that have been added to the likes list and all likes are separated
+						into tabs depending on the media type.
+					</Text>
+				</Skeleton>
+			)}
+		/>
+	);
+};
+
+export default DummyMyLikesTabHeadline;
