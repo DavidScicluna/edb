8f1def00925ba073b53c6a0797d43a7f873068cb
David Scicluna <dave_nostra@hotmail.com>
2022-11-05 12:52:07 +0100

    Feature: User - UserProfile - UserProfileDummyHeader - DummyUserBackground

---
+++ src/pages/User/pages/UserProfile/components/UserProfileDummyHeader/components/DummyUserBackground/types.ts
@@ -0,0 +1,3 @@
+import { UserProfileStructureProps } from '../../../../../../../../components/User/UserProfileStructure/types';
+
+export type DummyUserBackgroundProps = Pick<UserProfileStructureProps, 'colorMode'>;
