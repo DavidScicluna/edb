623bba69acab696690c632eed4cc864a1c82affc
David Scicluna <dave_nostra@hotmail.com>
2022-11-05 16:31:56 +0100

    Fix: User - Lists - Removed ListInfo

--- src/pages/User/pages/Lists/components/ListInfo/components/Stats/types.ts
+++
@@ -1,9 +0,0 @@
-export type Stat = {
-	label: string;
-	number: number;
-};
-
-export type StatsProps = {
-	totalMovies: number;
-	totalTvs: number;
-};
--- src/pages/User/pages/Lists/components/ListInfo/types.ts
+++
@@ -1,9 +0,0 @@
-import { List } from '../../../../../../store/slices/Users/types';
-
-export type ListInfoProps = {
-	id?: List['id'];
-	isOpen: boolean;
-	onEdit: () => void;
-	onDelete: () => void;
-	onClose: () => void;
-};
