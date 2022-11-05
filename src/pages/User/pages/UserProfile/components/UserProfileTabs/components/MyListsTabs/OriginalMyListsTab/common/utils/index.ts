613a718bcb21c1710912556f6c851bb78eb9d743
David Scicluna <dave_nostra@hotmail.com>
2022-11-05 18:43:09 +0100

    Feature: User - UserProfile - UserProfileTabs - MyListsTabs - OriginalMyListsTab - utils

---
+++ src/pages/User/pages/UserProfile/components/UserProfileTabs/components/MyListsTabs/OriginalMyListsTab/common/utils/index.ts
@@ -0,0 +1,19 @@
+import { Undefinable } from '@davidscicluna/component-library';
+
+import { memoize } from 'lodash';
+
+import { UserList } from '../../../../../../../../../../../store/slices/Users/types';
+
+export const getListTotal = memoize(({ mediaItems: { movie = [], tv = [] } }: UserList): number => {
+	return movie.length + tv.length;
+});
+
+type GetListProps = { lists: UserList[]; list: UserList };
+
+export const getListIndex = memoize(({ lists = [], list }: GetListProps): number => {
+	return lists.findIndex(({ id }) => id === list.id) + 1;
+});
+
+export const getList = memoize(({ lists = [], list }: GetListProps): Undefinable<UserList> => {
+	return lists.find(({ id }) => id === list.id);
+});
