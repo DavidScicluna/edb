732ec0c23d6246eafad729a713dd174d3796d518
David Scicluna <dave_nostra@hotmail.com>
2022-11-05 16:31:21 +0100

    Fix: User - Lists - Removed Toast

--- src/pages/User/pages/Lists/components/Toast/types.ts
+++
@@ -1,8 +0,0 @@
-import { List as ListType } from '../../../../../../store/slices/Users/types';
-
-export type ToastProps = {
-	list?: ListType;
-	onEdit: () => void;
-	onDelete: () => void;
-	onClose: () => void;
-};
