3cc693113f7418d6b2c3c55e402c82af18f6d36f
David Scicluna <dave_nostra@hotmail.com>
2022-11-05 16:31:44 +0100

    Fix: User - Lists - Removed ListPicker

--- src/pages/User/pages/Lists/components/ListPicker/components/ListItem/types.ts
+++
@@ -1,7 +0,0 @@
-import { List } from '../../../../../../../../store/slices/Users/types';
-
-export type ListItemProps = {
-	isSelected?: boolean;
-	onSelected: (id: List['id']) => void;
-	onClick: (id: List['id']) => void;
-} & List;
--- src/pages/User/pages/Lists/components/ListPicker/types.ts
+++
@@ -1,8 +0,0 @@
-import { List } from '../../../../../../store/slices/Users/types';
-
-export type ListPickerProps = {
-	lists: List[];
-	selectedListID?: List['id'];
-	onSelected: (id: List['id']) => void;
-	onOpenList: (index: number) => void;
-};
