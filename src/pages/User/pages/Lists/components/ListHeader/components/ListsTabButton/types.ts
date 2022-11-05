d4fd5d940c822cbc76e54675581ae884143131cb
David Scicluna <dave_nostra@hotmail.com>
2022-11-05 16:32:25 +0100

    Fix: User - Lists - Removed ListHeader

--- src/pages/User/pages/Lists/components/ListHeader/components/ListsTabButton/types.ts
+++
@@ -1,5 +0,0 @@
-import { TabsProps } from '../../../../../../../../components/Tabs/components/TabList/components/Tab/types';
-
-export type ListsTabButtonProps = {
-	onClick: () => void;
-} & Omit<TabsProps, 'label'>;
--- src/pages/User/pages/Lists/components/ListHeader/types.ts
+++
@@ -1,7 +0,0 @@
-import { List } from '../../../../../../store/slices/Users/types';
-
-export type ListHeaderProps = {
-	activeTab?: number;
-	lists: List[];
-	onListsClick: () => void;
-};
