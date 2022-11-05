7d8b0f29d35a24a2e1170b75b11a45ab50256215
David Scicluna <dave_nostra@hotmail.com>
2022-11-05 16:08:44 +0100

    Feature: User - UserProfile - UserProfileTabs - UserProfileTabsTabs - AllTab - AllTabPeople

---
+++ src/pages/User/pages/UserProfile/components/UserProfileTabs/components/UserProfileTabsTabs/components/AllTab/components/AllTabPeople/types.ts
@@ -0,0 +1,6 @@
+import { MediaItems } from '../../../../../../../../../../../../store/slices/Users/types';
+import { AllTabProps } from '../../types';
+
+export type AllTabPeopleProps = Pick<AllTabProps, 'type' | 'onSetActiveTab'> & {
+	people: MediaItems['person'];
+};
