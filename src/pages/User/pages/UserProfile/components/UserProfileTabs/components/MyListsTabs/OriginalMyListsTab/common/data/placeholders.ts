3f947fb8cffce036fe2202bcbe8a8c6a66225217
David Scicluna <dave_nostra@hotmail.com>
2022-11-05 18:43:00 +0100

    Feature: User - UserProfile - UserProfileTabs - MyListsTabs - OriginalMyListsTab - placeholders

---
+++ src/pages/User/pages/UserProfile/components/UserProfileTabs/components/MyListsTabs/OriginalMyListsTab/common/data/placeholders.ts
@@ -0,0 +1,13 @@
+const placeholders = [
+	'Action Movies',
+	'DC Movies',
+	'Leonardo DiCaprio',
+	'Classics',
+	'Comedy',
+	'Mafia Movies & TV Shows',
+	'Jennifer Lawrence',
+	'Johnny Depp',
+	'Angelina Jolie'
+];
+
+export default placeholders;
