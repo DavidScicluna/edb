77557394ca3575cb3f870affc455dd642f92276a
David Scicluna <dave_nostra@hotmail.com>
2022-11-11 21:56:08 +0100

    Fix: useCheckColorMode - changed file type from tsx to ts

--- src/common/hooks/useCheckColorMode.ts
+++ src/common/hooks/useCheckColorMode.ts
@@ -3,12 +3,13 @@ import { useCallback, useEffect } from 'react';
 import { ColorMode, useColorMode } from '@chakra-ui/react';
 
 import { useMediaMatch } from 'rooks';
-// import { useDispatch } from 'react-redux';
 import { debounce, memoize } from 'lodash';
+import { useDispatch } from 'react-redux';
+import { useUpdateEffect } from 'usehooks-ts';
 
 import { guest } from '../../store/slices/Users';
 import { UserThemeColorMode } from '../../store/slices/Users/types';
-// import { toggleSpinnerModal } from '../../store/slices/Modals';
+import { toggleSpinnerModal } from '../../store/slices/Modals';
 
 import { useSelector } from '.';
 
@@ -26,30 +27,33 @@ const getMode = memoize(({ colorMode, isDarkMode = false }: GetModeProps): Color
 });
 
 const useCheckColorMode = (): void => {
-	const { setColorMode } = useColorMode();
+	const { colorMode: colorModeHook, setColorMode } = useColorMode();
 
-	// const dispatch = useDispatch();
+	const dispatch = useDispatch();
 	const colorMode = useSelector(
 		(state) => state.users.data.activeUser.ui.theme.colorMode || guest.ui.theme.colorMode
 	);
 
-	// Checks user OS color-scheme mode
 	const isDarkMode = useMediaMatch('(prefers-color-scheme: dark)');
 
 	const handleSetColorMode = useCallback(
 		debounce(() => {
 			const mode = getMode({ colorMode, isDarkMode });
 
-			setColorMode(mode);
-			// TODO: Maybe show spinner
-			// if(colorMode === 'system'){
+			// if (colorMode === 'system' && mode !== colorModeHook) {
 			// 	dispatch(toggleSpinnerModal(true));
+
+			// 	setTimeout(() => dispatch(toggleSpinnerModal(false)), 2500);
 			// }
+
+			setTimeout(() => setColorMode(mode), 250);
 		}, 1000),
 		[colorMode, isDarkMode]
 	);
 
 	useEffect(() => handleSetColorMode(), [isDarkMode]);
+
+	useUpdateEffect(() => handleSetColorMode(), [colorModeHook]);
 };
 
 export default useCheckColorMode;
