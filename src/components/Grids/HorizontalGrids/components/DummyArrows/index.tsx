c0d9727bac669ce451e20fc1cab8bbc2b77d9b22
David Scicluna <dave_nostra@hotmail.com>
2022-11-12 21:41:59 +0100

    Fix: Grids - HorizontalGrids - DummyArrows

--- src/components/Grids/HorizontalGrids/components/DummyArrows/index.tsx
+++ src/components/Grids/HorizontalGrids/components/DummyArrows/index.tsx
@@ -5,14 +5,14 @@ import { ButtonGroup, ButtonGroupItem } from '@davidscicluna/component-library';
 import DummyArrow from './components/DummyArrow';
 import { DummyArrowsProps } from './types';
 
-const DummyArrows: FC<DummyArrowsProps> = (props = {}) => {
+const DummyArrows: FC<DummyArrowsProps> = ({ dummyArrowProps }) => {
 	return (
 		<ButtonGroup isAttached spacing={1}>
 			<ButtonGroupItem index={0} total={1}>
-				<DummyArrow {...props} direction='left' />
+				<DummyArrow {...dummyArrowProps} direction='left' />
 			</ButtonGroupItem>
 			<ButtonGroupItem index={1} total={1}>
-				<DummyArrow {...props} direction='right' />
+				<DummyArrow {...dummyArrowProps} direction='right' />
 			</ButtonGroupItem>
 		</ButtonGroup>
 	);
