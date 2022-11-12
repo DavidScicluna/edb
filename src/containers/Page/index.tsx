2fc22c84f9b45fea5138bd67bf5b7aec71a2cd3b
David Scicluna <dave_nostra@hotmail.com>
2022-11-12 21:44:18 +0100

    Fix: Page

--- src/containers/Page/index.tsx
+++ src/containers/Page/index.tsx
@@ -5,10 +5,13 @@ import { Divider } from '@davidscicluna/component-library';
 import { VStack, Box } from '@chakra-ui/react';
 
 import { useLayoutContext } from '../Layout/common/hooks';
+import { useUserTheme } from '../../common/hooks';
 
 import { PageProps } from './types';
 
 const Page: FC<PageProps> = ({ children, ...rest }) => {
+	const { colorMode } = useUserTheme();
+
 	const { spacing } = useLayoutContext();
 
 	return (
@@ -17,7 +20,7 @@ const Page: FC<PageProps> = ({ children, ...rest }) => {
 			width='100%'
 			divider={
 				<Box width='100%' border='none' px={spacing}>
-					<Divider />
+					<Divider colorMode={colorMode} />
 				</Box>
 			}
 			spacing={0}
