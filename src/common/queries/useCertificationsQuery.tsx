3f7914a86b44d0229d374767cfc44bae9653f2bd
David Scicluna <dave_nostra@hotmail.com>
2022-11-14 23:42:13 +0100

    Fix: useCertificationsQuery - Added Alert onError

--- src/common/queries/useCertificationsQuery.tsx
+++ src/common/queries/useCertificationsQuery.tsx
@@ -1,11 +1,17 @@
+import { useToast } from '@chakra-ui/react';
+
 import { UseQueryResult, UseQueryOptions, useQueryClient, useQuery } from '@tanstack/react-query';
 
 import { AxiosError } from 'axios';
+import { compact } from 'lodash';
 import { useWillUnmount } from 'rooks';
 
+import { Alert } from '../../components';
+import { convertDurationToMS } from '../../components/Alert/common/utils';
 import { certificationsQueryKey } from '../keys';
 import { axios as axiosInstance } from '../scripts';
 import { AxiosConfig, Certifications, MediaType, QueryError } from '../types';
+import { formatMediaTypeLabel } from '../utils';
 
 export type UseCertificationsQueryProps = { mediaType: Exclude<MediaType, 'person' | 'company' | 'collection'> };
 
@@ -21,12 +27,14 @@ type UseCertificationsQueryParams = {
 	options?: UseCertificationsQueryOptions;
 };
 
+const toastID = 'ds-edb-use-certifications-query-toast';
+
 const useCertificationsQuery = ({
 	props: { mediaType },
 	config = {},
 	options = {}
 }: UseCertificationsQueryParams): UseCertificationsQueryResult => {
-	// const toast = useToast();
+	const toast = useToast();
 
 	const key = certificationsQueryKey({ mediaType }) || [`${mediaType}_certifications`];
 
@@ -48,9 +56,29 @@ const useCertificationsQuery = ({
 			onError: (error) => {
 				console.error(error.toJSON());
 
-				// TODO: ADD Toast | Set Toast with action to refetch & add progress bar
-				// & time label to show when notification will close
-				// const {} = error.response?.data || {};
+				const { status_code, status_message } = error.response?.data || {};
+
+				if (!toast.isActive(toastID)) {
+					toast({
+						id: toastID,
+						duration: convertDurationToMS(),
+						position: 'bottom-left',
+						render: () => (
+							<Alert
+								description={compact([
+									status_code ? `${status_code}.` : null,
+									`Unfortunately, something went wrong when trying to fetch ${formatMediaTypeLabel({
+										type: 'single',
+										mediaType
+									})} certifications.`,
+									status_message ? `(${status_message})` : null
+								]).join(' ')}
+								status='error'
+								onClose={() => toast.close(toastID)}
+							/>
+						)
+					});
+				}
 
 				if (options.onError) {
 					options.onError(error);
