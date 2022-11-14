bf237d609599fbeb759f130136640f6528fccf4f
David Scicluna <dave_nostra@hotmail.com>
2022-11-14 22:54:35 +0100

    Fix: Layout

--- src/containers/Layout/index.tsx
+++ src/containers/Layout/index.tsx
@@ -1,41 +1,71 @@
-import { FC, useEffect } from 'react';
+import { FC, createContext, useCallback, useEffect } from 'react';
+
+import { useLocation } from 'react-router';
 
 import { useTheme, utils } from '@davidscicluna/component-library';
 
-import { useMediaQuery, useBoolean, useConst, Container } from '@chakra-ui/react';
+import { useMediaQuery, useToast, useBoolean, useConst, Container } from '@chakra-ui/react';
 
-import { useDispatch } from 'react-redux';
-import { useUpdateEffect } from 'usehooks-ts';
+import { useIsFirstRender, useUpdateEffect } from 'usehooks-ts';
+import { useOnline, useWindowSize } from 'rooks';
+import { debounce } from 'lodash';
 
-import { toggleSidebarMode } from '../../store/slices/App';
-import { useSelector } from '../../common/hooks';
+import { useCheckColorMode, useSelector, useUserTheme } from '../../common/hooks';
 import { guest } from '../../store/slices/Users';
+import { Alert } from '../../components';
+import { convertDurationToMS } from '../../components/Alert/common/utils';
 
-import { LayoutProps } from './types';
+import { LayoutContext as LayoutContextType, LayoutProps } from './types';
 import { sidebar } from './common/data/sidebar';
 import useStyles from './common/styles';
 // import Footer from './components/Footer';
 // import Header from './components/Header';
+import InternationalizationModal from './components/Modals/InternationalizationModal';
+import AuthenticationConfirmModal from './components/Modals/AuthenticationConfirmModal';
 import UserThemeModal from './components/Modals/UserThemeModal';
-// import ListsModal from './components/Modals/Lists';
+import BookmarkModal from './components/Modals/BookmarkModal';
 // import QuickViewModal from './components/Modals/QuickView';
-// import UserSwitcherModal from './components/Modals/UserSwitcher';
 // import ProgressBar from './components/ProgressBar';
 import Structure from './components/Structure';
 import ProgressBar from './components/ProgressBar';
+import {
+	isGuest as defaultIsGuest,
+	isAuthenticationRoute as defaultIsAuthenticationRoute,
+	spacing as defaultSpacing
+} from './common/data/defaultPropValues';
 import ScrollToTop from './components/ScrollToTop';
 // import Sidebar from './components/Sidebar';
 
-const { convertREMToPixels, convertStringToNumber } = utils;
+const { convertREMToPixels, convertStringToNumber, getColor } = utils;
+
+export const LayoutContext = createContext<LayoutContextType>({
+	isGuest: defaultIsGuest,
+	isAuthenticationRoute: defaultIsAuthenticationRoute,
+	spacing: defaultSpacing
+});
+
+const onlineToastID = 'ds-edb-is-online-toast';
+const offlineToastID = 'ds-edb-is-offline-toast';
 
 const Layout: FC<LayoutProps> = ({ children }) => {
 	const theme = useTheme();
+	const { colorMode } = useUserTheme();
 
-	const [isMd] = useMediaQuery('(min-width: 600px)');
-	const [isLg] = useMediaQuery('(min-width: 1280px)');
+	const [isSm] = useMediaQuery(`(min-width: ${theme.breakpoints.sm})`);
+	const [isXl] = useMediaQuery(`(min-width: ${theme.breakpoints.xl})`);
 
-	const dispatch = useDispatch();
+	const location = useLocation();
+
 	const activeUser = useSelector((state) => state.users.data.activeUser);
+
+	const { innerWidth: windowWidth = 0 } = useWindowSize();
+
+	const toast = useToast();
+
+	const isOnline = useOnline();
+	const isFirstRender = useIsFirstRender();
+
+	useCheckColorMode();
 
 	// const users = useSelector((state) => state.users.data.users);
 	// const sidebarMode = useSelector((state) => state.app.ui.sidebarMode);
@@ -43,21 +73,54 @@ const Layout: FC<LayoutProps> = ({ children }) => {
 	// const isQuickViewOpen = useSelector((state) => state.modals.ui.quickViewModal.open);
 
 	const [isGuest, setIsGuest] = useBoolean(guest.data.id === activeUser.data.id);
+	const [isAuthenticationRoute, setIsAuthenticationRoute] = useBoolean();
 
 	// const isFetching = useIsFetching();
 	// const isMutating = useIsMutating();
 
 	const style = useStyles({ theme });
 
-	const containerMaxWidth = useConst<string>(
-		`${convertREMToPixels(convertStringToNumber(theme.breakpoints.xl, 'em')) + sidebar.expanded}px`
+	const containerMaxWidth = useConst<number>(
+		convertREMToPixels(convertStringToNumber(theme.breakpoints.xl, 'em')) + sidebar.expanded
 	);
 
+	const handleCheckIsOnline = useCallback(
+		debounce(() => {
+			if (!isOnline && !toast.isActive(offlineToastID)) {
+				toast.close(onlineToastID);
+				toast({
+					id: offlineToastID,
+					duration: null,
+					position: 'bottom-left',
+					render: () => (
+						<Alert description='You are currently offline! Please connect to view data.' status='error' />
+					)
+				});
+			} else if (!isFirstRender) {
+				toast.close(offlineToastID);
+				toast({
+					id: onlineToastID,
+					duration: convertDurationToMS({ duration: 10 }),
+					position: 'bottom-left',
+					render: () => (
+						<Alert
+							duration={10}
+							description='You have successfully reconnected to the app!'
+							status='success'
+							onClose={() => toast.close(onlineToastID)}
+						/>
+					)
+				});
+			}
+		}, 500),
+		[toast, isOnline, offlineToastID, onlineToastID, isFirstRender]
+	);
+
+	useEffect(() => handleCheckIsOnline(), [isOnline]);
+
 	useEffect(() => {
-		if (!isLg) {
-			dispatch(toggleSidebarMode('expanded'));
-		}
-	}, [isLg]);
+		setIsAuthenticationRoute[location.pathname.includes('authentication') ? 'on' : 'off']();
+	}, [location.pathname]);
 
 	useUpdateEffect(() => setIsGuest[guest.data.id === activeUser.data.id ? 'on' : 'off'](), [activeUser]);
 
@@ -65,30 +128,36 @@ const Layout: FC<LayoutProps> = ({ children }) => {
 		<>
 			<Container
 				width='100%'
-				maxWidth={containerMaxWidth}
+				maxWidth={!isAuthenticationRoute ? `${containerMaxWidth}px` : '100%'}
 				minHeight='100vh'
 				centerContent
-				position='relative'
-				m={0}
+				borderLeftWidth={!isAuthenticationRoute && (windowWidth || 0) >= containerMaxWidth ? '2px' : 0}
+				borderRightWidth={!isAuthenticationRoute && (windowWidth || 0) >= containerMaxWidth ? '2px' : 0}
+				borderStyle='solid'
+				borderColor={getColor({ theme, colorMode, type: 'divider' })}
 				p={0}
 				sx={{ ...style }}
 			>
-				{/* <ProgressBar /> */}
+				<ProgressBar maxWidth={`${containerMaxWidth}px`} />
 
-				<ScrollToTop device={isMd && !isLg ? 'tablet' : isLg ? 'desktop' : 'mobile'} />
+				<ScrollToTop />
 
-				<Structure device={isMd && !isLg ? 'tablet' : isLg ? 'desktop' : 'mobile'} isGuest={isGuest}>
-					{children}
-				</Structure>
+				<LayoutContext.Provider value={{ isGuest, isAuthenticationRoute, spacing: isXl ? 4 : isSm ? 3 : 2 }}>
+					<Structure device={isXl ? 'desktop' : isSm ? 'tablet' : 'mobile'}>{children}</Structure>
+				</LayoutContext.Provider>
 			</Container>
 
 			{/* <QuickViewModal /> */}
 
+			<InternationalizationModal />
+
 			{!isGuest && <UserThemeModal />}
 
-			{/* {!isGuest && users.length > 1 && <UserSwitcherModal />} */}
+			{!isGuest && <BookmarkModal />}
 
-			{/* <ListsModal /> */}
+			{isGuest && <AuthenticationConfirmModal />}
+
+			{/* !isGuest && <ListsModal /> */}
 		</>
 	);
 };
--- src/containers/Layout/types.ts
+++ src/containers/Layout/types.ts
@@ -1,3 +1,9 @@
 import { ReactNode } from 'react';
 
+import { Space } from '@davidscicluna/component-library';
+
+export type LayoutSpacing = Space;
+
+export type LayoutContext = { isGuest?: boolean; isAuthenticationRoute?: boolean; spacing?: LayoutSpacing };
+
 export type LayoutProps = { children: ReactNode };
