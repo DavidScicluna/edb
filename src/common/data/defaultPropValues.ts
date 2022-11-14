f74fbf368d4315cadc96a958974773f2d54c9ae3
David Scicluna <dave_nostra@hotmail.com>
2022-11-14 22:55:14 +0100

    Fix: defaultPropValues

--- src/common/data/defaultPropValues.ts
+++ src/common/data/defaultPropValues.ts
@@ -5,3 +5,5 @@ import { UserThemeColor } from '../../store/slices/Users/types';
 export const color: UserThemeColor = 'blue';
 
 export const colorMode: ColorMode = 'light';
+
+export const inView = false;
