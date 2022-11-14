09a4f6364ea82acfe9d6378ad094998f0590d077
David Scicluna <dave_nostra@hotmail.com>
2022-11-14 23:42:21 +0100

    Fix: useCountriesQuery - Added Alert onError

--- src/common/queries/useCountriesQuery.tsx
+++ src/common/queries/useCountriesQuery.tsx
@@ -1,10 +1,15 @@
 import { Undefinable } from '@davidscicluna/component-library';
 
+import { useToast } from '@chakra-ui/react';
+
 import { UseQueryResult, UseQueryOptions, useQueryClient, useQuery } from '@tanstack/react-query';
 
 import { AxiosError } from 'axios';
 import { useWillUnmount } from 'rooks';
+import { compact } from 'lodash';
 
+import { Alert } from '../../components';
+import { convertDurationToMS } from '../../components/Alert/common/utils';
 import { countriesQueryKey } from '../keys';
 import { axios as axiosInstance } from '../scripts';
 import { AxiosConfig, Country, QueryError } from '../types';
@@ -20,8 +25,10 @@ type UseCountriesQueryParams = Undefinable<{
 	options?: UseCountriesQueryOptions;
 }>;
 
+const toastID = 'ds-edb-use-countries-query-toast';
+
 const useCountriesQuery = ({ config = {}, options = {} }: UseCountriesQueryParams = {}): UseCountriesQueryResult => {
-	// const toast = useToast();
+	const toast = useToast();
 
 	const key = countriesQueryKey() || ['countries'];
 
@@ -40,8 +47,26 @@ const useCountriesQuery = ({ config = {}, options = {} }: UseCountriesQueryParam
 			onError: (error) => {
 				console.error(error.toJSON());
 
-				// TODO: ADD Toast
-				// const {} = error.response?.data || {};
+				const { status_code, status_message } = error.response?.data || {};
+
+				if (!toast.isActive(toastID)) {
+					toast({
+						id: toastID,
+						duration: convertDurationToMS(),
+						position: 'bottom-left',
+						render: () => (
+							<Alert
+								description={compact([
+									status_code ? `${status_code}.` : null,
+									'Unfortunately, something went wrong when trying to fetch countries.',
+									status_message ? `(${status_message})` : null
+								]).join(' ')}
+								status='error'
+								onClose={() => toast.close(toastID)}
+							/>
+						)
+					});
+				}
 
 				if (options.onError) {
 					options.onError(error);
