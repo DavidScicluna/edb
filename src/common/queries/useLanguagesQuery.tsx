305362208a627c759e36fe70e959b6158be41e1e
David Scicluna <dave_nostra@hotmail.com>
2022-11-14 23:42:50 +0100

    Fix: useLanguagesQuery - Added Alert onError

--- src/common/queries/useLanguagesQuery.tsx
+++ src/common/queries/useLanguagesQuery.tsx
@@ -1,13 +1,18 @@
 import { Undefinable } from '@davidscicluna/component-library';
 
+import { useToast } from '@chakra-ui/react';
+
 import { UseQueryResult, UseQueryOptions, useQueryClient, useQuery } from '@tanstack/react-query';
 
 import { AxiosError } from 'axios';
 import { useWillUnmount } from 'rooks';
+import { compact } from 'lodash';
 
 import { languagesQueryKey } from '../keys';
 import { axios as axiosInstance } from '../scripts';
 import { AxiosConfig, Language, QueryError } from '../types';
+import { convertDurationToMS } from '../../components/Alert/common/utils';
+import { Alert } from '../../components';
 
 export type UseLanguagesQueryResponse = Language[];
 
@@ -20,8 +25,10 @@ type UseLanguagesQueryParams = Undefinable<{
 	options?: UseLanguagesQueryOptions;
 }>;
 
+const toastID = 'ds-edb-use-languages-query-toast';
+
 const useLanguagesQuery = ({ config = {}, options = {} }: UseLanguagesQueryParams = {}): UseLanguagesQueryResult => {
-	// const toast = useToast();
+	const toast = useToast();
 
 	const key = languagesQueryKey() || ['languages'];
 
@@ -40,8 +47,26 @@ const useLanguagesQuery = ({ config = {}, options = {} }: UseLanguagesQueryParam
 			onError: (error) => {
 				console.error(error.toJSON());
 
-				// TODO: ADD Toast
-				// const {} = error.response?.data || {};
+				const { status_code, status_message } = error.response?.data || {};
+
+				if (!toast.isActive(toastID)) {
+					toast({
+						id: toastID,
+						duration: convertDurationToMS(),
+						position: 'bottom-left',
+						render: () => (
+							<Alert
+								description={compact([
+									status_code ? `${status_code}.` : null,
+									'Unfortunately, something went wrong when trying to fetch languages.',
+									status_message ? `(${status_message})` : null
+								]).join(' ')}
+								status='error'
+								onClose={() => toast.close(toastID)}
+							/>
+						)
+					});
+				}
 
 				if (options.onError) {
 					options.onError(error);
