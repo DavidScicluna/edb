a5a265ee5d506226e8edabc6eab096f9498fe1b0
David Scicluna <dave_nostra@hotmail.com>
2022-11-17 23:29:49 +0100

    Fix: Search - OriginalSearch - SearchForm - Fixed on hover keep container expanded

--- src/pages/Search/OriginalSearch/components/SearchForm/index.tsx
+++ src/pages/Search/OriginalSearch/components/SearchForm/index.tsx
@@ -2,7 +2,7 @@ import { FC, useRef } from 'react';
 
 import { Nullable, Card, CardBody, CardDivider, Collapse } from '@davidscicluna/component-library';
 
-import { useOutsideClick, VStack } from '@chakra-ui/react';
+import { useBoolean, useOutsideClick, VStack } from '@chakra-ui/react';
 
 import { useUserTheme } from '../../../../../common/hooks';
 import { isFocused as defaultIsFocused } from '../../common/data/defaultPropValues';
@@ -12,21 +12,32 @@ import { SearchFormProps } from './types';
 const SearchForm: FC<SearchFormProps> = (props) => {
 	const { colorMode } = useUserTheme();
 
-	const ref = useRef<Nullable<HTMLInputElement>>(null);
+	const formRef = useRef<Nullable<HTMLInputElement>>(null);
 
 	const { children, isFocused = defaultIsFocused, onFocus, onBlur } = props;
 	const { input, collapsible, info } = children;
 
-	useOutsideClick({ ref, handler: () => onBlur() });
+	const [isHovering, setIsHovering] = useBoolean();
+
+	useOutsideClick({ ref: formRef, handler: !isHovering ? () => onBlur() : undefined });
 
 	return (
-		<VStack ref={ref} width='100%' spacing={0.5}>
-			<Card colorMode={colorMode} isFullWidth onClick={() => onFocus()} p={2}>
+		<VStack width='100%'>
+			<Card
+				ref={formRef}
+				spacing={0.5}
+				colorMode={colorMode}
+				isFullWidth
+				onClick={() => onFocus()}
+				onMouseEnter={() => setIsHovering.on()}
+				onMouseLeave={() => setIsHovering.off()}
+				p={2}
+			>
 				<CardBody>
 					<VStack width='100%' spacing={2}>
 						{input}
 
-						<Collapse in={isFocused} style={{ width: '100%' }}>
+						<Collapse in={isFocused || isHovering} style={{ width: '100%' }}>
 							<VStack width='100%' spacing={2}>
 								<CardDivider />
 
