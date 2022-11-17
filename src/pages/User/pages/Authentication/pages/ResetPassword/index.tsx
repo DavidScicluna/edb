bec382772c5eae707dc7e8aad19d16128ebc7a11
David Scicluna <dave_nostra@hotmail.com>
2022-11-17 20:38:04 +0100

    Fix: User - Authentication - ResetPassword - Added prompt logic

--- src/pages/User/pages/Authentication/pages/ResetPassword/index.tsx
+++ src/pages/User/pages/Authentication/pages/ResetPassword/index.tsx
@@ -1,24 +1,10 @@
 import { FC } from 'react';
 
-import { useLocation, useNavigate, useOutletContext } from 'react-router';
+import { useLocation, useNavigate } from 'react-router';
 
-import {
-	useTheme,
-	useDebounce,
-	ConfirmModal,
-	ConfirmModalStack,
-	ConfirmModalIcon,
-	ConfirmModalBody,
-	ConfirmModalTitle,
-	ConfirmModalSubtitle,
-	ConfirmModalFooter,
-	Button,
-	IconButton,
-	IconButtonIcon,
-	Icon
-} from '@davidscicluna/component-library';
+import { useTheme, useDebounce } from '@davidscicluna/component-library';
 
-import { useMediaQuery, useDisclosure, useToast, HStack, Center } from '@chakra-ui/react';
+import { useMediaQuery, useToast, HStack, Center } from '@chakra-ui/react';
 
 import qs from 'query-string';
 import { useForm, useFormState } from 'react-hook-form';
@@ -27,13 +13,10 @@ import { useElementSize, useWindowSize } from 'usehooks-ts';
 import { useDispatch } from 'react-redux';
 import sha256 from 'crypto-js/sha256';
 
-import { color as defaultColor, colorMode as defaultColorMode } from '../../../../../../common/data/defaultPropValues';
 import Illustration from '../../components/Illustration';
-import { useSelector } from '../../../../../../common/hooks';
+import { usePrompt, useSelector } from '../../../../../../common/hooks';
 import { UserCredentials } from '../../../../../../store/slices/Users/types';
 import { setUserCredentials } from '../../../../../../store/slices/Users';
-import { AuthenticationOutletContext } from '../../types';
-import { useLayoutContext } from '../../../../../../containers/Layout/common/hooks';
 import { convertDurationToMS } from '../../../../../../components/Alert/common/utils';
 import { Alert } from '../../../../../../components';
 
@@ -56,14 +39,8 @@ const ForgotPassword: FC = () => {
 	const [isLg] = useMediaQuery(`(min-width: ${theme.breakpoints.lg})`);
 	const [isXl] = useMediaQuery(`(min-width: ${theme.breakpoints.xl})`);
 
-	const { spacing } = useLayoutContext();
-
-	const { isOpen: isConfirmOpen, onOpen: onConfirmOpen, onClose: onConfirmClose } = useDisclosure();
-
 	const location = useLocation();
 	const navigate = useNavigate();
-
-	const { color = defaultColor, colorMode = defaultColorMode } = useOutletContext<AuthenticationOutletContext>();
 
 	const dispatch = useDispatch();
 	const users = useSelector((state) => state.users.data.users);
@@ -84,22 +61,12 @@ const ForgotPassword: FC = () => {
 
 	const { isDirty } = useFormState({ control });
 
-	const handleClose = (): void => {
-		navigate('/authentication/signin');
-	};
-
-	const handleCloseConfirm = (): void => {
-		onConfirmClose();
-		handleClose();
-	};
-
-	const handleCheckBack = (): void => {
-		if (!isDirty) {
-			handleClose();
-		} else {
-			onConfirmOpen();
-		}
-	};
+	usePrompt({
+		title: 'Unsubmitted Changes!',
+		subtitle:
+			'Are you sure you want to cancel resetting the password? Once you close the page you will not be able to retrieve the changed data!',
+		when: isDirty
+	});
 
 	const handleChangePassword = (values: ResetPasswordFormType): void => {
 		const { username, password, newPassword } = values;
@@ -148,79 +115,37 @@ const ForgotPassword: FC = () => {
 	};
 
 	return (
-		<>
-			<HStack
-				ref={containerRef}
-				width='100%'
+		<HStack
+			ref={containerRef}
+			width='100%'
+			minHeight='100vh'
+			position='relative'
+			alignItems='center'
+			justifyContent='flex-start'
+			spacing={0}
+		>
+			<Center
+				width={isLg ? `${debouncedContainerWidth / 2}px` : '100%'}
 				minHeight='100vh'
-				position='relative'
-				alignItems='center'
-				justifyContent='flex-start'
-				spacing={0}
+				px={[2, 2, 3, 3]}
+				py={[3, 3, 4, 4]}
 			>
-				<Center
-					width={isLg ? `${debouncedContainerWidth / 2}px` : '100%'}
-					minHeight='100vh'
-					px={[2, 2, 3, 3]}
-					py={[3, 3, 4, 4]}
-				>
-					<ResetPasswordForm form={form} onSubmit={handleChangePassword} onBack={handleCheckBack} />
-				</Center>
+				<ResetPasswordForm
+					form={form}
+					onSubmit={handleChangePassword}
+					onBack={() => navigate('/authentication/signin')}
+				/>
+			</Center>
 
-				{isLg && (
-					<Illustration
-						width={`${debouncedContainerWidth / 2}px`}
-						height='100vh'
-						position='fixed'
-						right={`${isXl ? (windowWidth - containerWidth) / 2 : 0}px`}
-					/>
-				)}
-			</HStack>
-
-			{/* TODO: Extract ConfirmModal to Prompt */}
-			<ConfirmModal
-				colorMode={colorMode}
-				renderCancel={({ icon, category, ...rest }) => (
-					<IconButton {...rest}>
-						<IconButtonIcon icon={icon} category={category} />
-					</IconButton>
-				)}
-				isOpen={isConfirmOpen}
-				onClose={onConfirmClose}
-			>
-				<ConfirmModalStack spacing={spacing} p={spacing}>
-					<ConfirmModalIcon
-						renderIcon={(props) => (
-							<Icon
-								{...props}
-								width={theme.fontSizes['6xl']}
-								height={theme.fontSizes['6xl']}
-								fontSize={theme.fontSizes['6xl']}
-								icon='help_outline'
-							/>
-						)}
-						color={color}
-						p={2}
-					/>
-
-					<ConfirmModalBody>
-						<ConfirmModalTitle>Cancel Reset Password?</ConfirmModalTitle>
-						<ConfirmModalSubtitle>
-							Are you sure you want to cancel reseting the password? Once you close the page you will not
-							be able to retrieve the data!
-						</ConfirmModalSubtitle>
-					</ConfirmModalBody>
-					<ConfirmModalFooter
-						renderCancel={(props) => <Button {...props}>Cancel</Button>}
-						renderAction={(props) => (
-							<Button {...props} color={color} onClick={handleCloseConfirm}>
-								Go Back
-							</Button>
-						)}
-					/>
-				</ConfirmModalStack>
-			</ConfirmModal>
-		</>
+			{isLg && (
+				<Illustration
+					width={`${debouncedContainerWidth / 2}px`}
+					height='100vh'
+					position='fixed'
+					right={`${isXl ? (windowWidth - containerWidth) / 2 : 0}px`}
+				/>
+			)}
+		</HStack>
 	);
 };
 
