9c9173378fade2a4f7e9f347797849d36dcc67bd
David Scicluna <dave_nostra@hotmail.com>
2022-11-17 20:31:09 +0100

    Fix: Tabs - TabIcon

--- src/components/Tabs/TabIcon/types.ts
+++ src/components/Tabs/TabIcon/types.ts
@@ -5,4 +5,4 @@ import { IconProps, TabsProps } from '@davidscicluna/component-library';
 // TODO: Use TabList Tab RenderProps type
 export type TabIconProps = Pick<TabsProps, 'color' | 'colorMode'> & {
 	height?: number;
-} & Pick<IconProps, 'icon' | 'category'>;
+} & Pick<IconProps, 'icon' | 'category' | 'sx'>;
