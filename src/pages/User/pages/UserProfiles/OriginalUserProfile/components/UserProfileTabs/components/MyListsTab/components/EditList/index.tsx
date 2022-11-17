56cc2c5f1f46e1c1cfc1e8de131c02bd5fcced0c
David Scicluna <dave_nostra@hotmail.com>
2022-11-17 22:34:12 +0100

    Fix: User - UserProfiles - OriginalUserProfile  - UserProfileTabs - MyListsTab - EditList - Added prompt logic

--- src/pages/User/pages/UserProfiles/OriginalUserProfile/components/UserProfileTabs/components/MyListsTab/components/EditList/index.tsx
+++ src/pages/User/pages/UserProfiles/OriginalUserProfile/components/UserProfileTabs/components/MyListsTab/components/EditList/index.tsx
@@ -7,23 +7,15 @@ import {
 	ModalHeader,
 	ModalBody,
 	ModalFooter,
-	ConfirmModal,
-	ConfirmModalStack,
-	ConfirmModalIcon,
-	ConfirmModalBody,
-	ConfirmModalTitle,
-	ConfirmModalSubtitle,
-	ConfirmModalFooter,
 	Form,
 	Input,
 	Textarea,
 	Button,
 	IconButton,
-	IconButtonIcon,
-	Icon
+	IconButtonIcon
 } from '@davidscicluna/component-library';
 
-import { useDisclosure, VStack, Text } from '@chakra-ui/react';
+import { VStack, Text } from '@chakra-ui/react';
 
 import { useDispatch } from 'react-redux';
 import { useForm, useFormState, Controller } from 'react-hook-form';
@@ -37,6 +29,7 @@ import { useLayoutContext } from '../../../../../../../../../../../containers/La
 import { setUserLists } from '../../../../../../../../../../../store/slices/Users';
 import placeholders from '../../common/data/placeholders';
 import { UserList } from '../../../../../../../../../../../store/slices/Users/types';
+import { defaultPromptConfirmModal, setPromptConfirmModal } from '../../../../../../../../../../../store/slices/Modals';
 
 import { EditListProps, EditListForm } from './types';
 import { schema } from './validation';
@@ -51,8 +44,6 @@ const EditList: FC<EditListProps> = ({ list, isOpen, onClose }) => {
 
 	const { spacing } = useLayoutContext();
 
-	const { isOpen: isConfirmOpen, onOpen: onConfirmOpen, onClose: onConfirmClose } = useDisclosure();
-
 	const dispatch = useDispatch();
 	const { id, lists = [] } = useSelector((state) => state.users.data.activeUser.data);
 
@@ -64,7 +55,7 @@ const EditList: FC<EditListProps> = ({ list, isOpen, onClose }) => {
 	const { isDirty } = useFormState({ control });
 
 	const handleCloseConfirm = (): void => {
-		onConfirmClose();
+		dispatch(setPromptConfirmModal({ ...defaultPromptConfirmModal }));
 		onClose();
 	};
 
@@ -72,7 +63,15 @@ const EditList: FC<EditListProps> = ({ list, isOpen, onClose }) => {
 		if (!isDirty) {
 			onClose();
 		} else {
-			onConfirmOpen();
+			dispatch(
+				setPromptConfirmModal({
+					isOpen: true,
+					title: 'Unsubmitted Changes!',
+					subtitle:
+						'Are you sure you want to cancel editing the list? Once you close the page you will not be able to retrieve the changed data!',
+					onConfirm: () => handleCloseConfirm()
+				})
+			);
 		}
 	};
 
@@ -98,118 +97,73 @@ const EditList: FC<EditListProps> = ({ list, isOpen, onClose }) => {
 	useEffect(() => handleResetModal(), [isOpen]);
 
 	return (
-		<>
-			<Modal colorMode={colorMode} isOpen={isOpen} onClose={handleCheckModal} size='lg'>
-				<ModalStack as={Form} onSubmit={handleSubmit(handleSubmitForm)}>
-					<ModalHeader
-						renderTitle={(props) => <Text {...props}>Edit List</Text>}
-						renderSubtitle={(props) => (
-							<Text {...props}>{`Edit the existing "${list.label}" list label & description`}</Text>
-						)}
-						renderCancel={({ icon, category, ...rest }) => (
-							<IconButton {...rest}>
-								<IconButtonIcon icon={icon} category={category} />
-							</IconButton>
-						)}
-					/>
-					<ModalBody>
-						<VStack width='100%' spacing={spacing}>
-							<Controller
-								control={control}
-								name='label'
-								render={({ field: { onChange, onBlur, value, name }, fieldState: { error } }) => (
-									<Input
-										color={color}
-										colorMode={colorMode}
-										label='Label'
-										name={name}
-										helper={error ? error.message : undefined}
-										placeholder={`Try "${placeholder}"`}
-										isError={isBoolean(error)}
-										isFullWidth
-										isRequired
-										onBlur={onBlur}
-										onChange={onChange}
-										value={value}
-									/>
-								)}
-							/>
-							<Controller
-								control={control}
-								name='description'
-								render={({ field: { onChange, onBlur, value, name }, fieldState: { error } }) => (
-									<Textarea
-										color={color}
-										colorMode={colorMode}
-										label='Description'
-										name={name}
-										helper={error ? error.message : undefined}
-										placeholder={`Try "A list containing ${placeholder} ..."`}
-										isError={isBoolean(error)}
-										isFullWidth
-										onBlur={onBlur}
-										onChange={onChange}
-										value={value}
-										sx={{ textarea: { height: theme.space[12.5] } }}
-									/>
-								)}
-							/>
-						</VStack>
-					</ModalBody>
-					<ModalFooter
-						renderCancel={(props) => <Button {...props}>Cancel</Button>}
-						renderAction={(props) => (
-							<Button {...props} color={color} isDisabled={!id || !isDirty} type='submit'>
-								Update List
-							</Button>
-						)}
-					/>
-				</ModalStack>
-			</Modal>
-
-			<ConfirmModal
-				colorMode={colorMode}
-				renderCancel={({ icon, category, ...rest }) => (
-					<IconButton {...rest}>
-						<IconButtonIcon icon={icon} category={category} />
-					</IconButton>
-				)}
-				isOpen={isConfirmOpen}
-				onClose={onConfirmClose}
-			>
-				<ConfirmModalStack spacing={spacing} p={spacing}>
-					<ConfirmModalIcon
-						renderIcon={(props) => (
-							<Icon
-								{...props}
-								width={theme.fontSizes['6xl']}
-								height={theme.fontSizes['6xl']}
-								fontSize={theme.fontSizes['6xl']}
-								icon='warning'
-								category='outlined'
-							/>
-						)}
-						color={color}
-						p={2}
-					/>
-
-					<ConfirmModalBody>
-						<ConfirmModalTitle>Unsaved data!</ConfirmModalTitle>
-						<ConfirmModalSubtitle>
-							Are you sure you want to close the modal? The data inserted will be lost unless you save it!
-						</ConfirmModalSubtitle>
-					</ConfirmModalBody>
-					<ConfirmModalFooter
-						renderCancel={(props) => <Button {...props}>Cancel</Button>}
-						renderAction={(props) => (
-							<Button {...props} color={color} onClick={handleCloseConfirm}>
-								Close
-							</Button>
-						)}
-					/>
-				</ConfirmModalStack>
-			</ConfirmModal>
-		</>
+		<Modal colorMode={colorMode} isOpen={isOpen} onClose={handleCheckModal} size='lg'>
+			<ModalStack as={Form} onSubmit={handleSubmit(handleSubmitForm)}>
+				<ModalHeader
+					renderTitle={(props) => <Text {...props}>Edit List</Text>}
+					renderSubtitle={(props) => (
+						<Text {...props}>{`Edit the existing "${list.label}" list label & description`}</Text>
+					)}
+					renderCancel={({ icon, category, ...rest }) => (
+						<IconButton {...rest}>
+							<IconButtonIcon icon={icon} category={category} />
+						</IconButton>
+					)}
+				/>
+				<ModalBody>
+					<VStack width='100%' spacing={spacing}>
+						<Controller
+							control={control}
+							name='label'
+							render={({ field: { onChange, onBlur, value, name }, fieldState: { error } }) => (
+								<Input
+									color={color}
+									colorMode={colorMode}
+									label='Label'
+									name={name}
+									helper={error ? error.message : undefined}
+									placeholder={`Try "${placeholder}"`}
+									isError={isBoolean(error)}
+									isFullWidth
+									isRequired
+									onBlur={onBlur}
+									onChange={onChange}
+									value={value}
+								/>
+							)}
+						/>
+						<Controller
+							control={control}
+							name='description'
+							render={({ field: { onChange, onBlur, value, name }, fieldState: { error } }) => (
+								<Textarea
+									color={color}
+									colorMode={colorMode}
+									label='Description'
+									name={name}
+									helper={error ? error.message : undefined}
+									placeholder={`Try "A list containing ${placeholder} ..."`}
+									isError={isBoolean(error)}
+									isFullWidth
+									onBlur={onBlur}
+									onChange={onChange}
+									value={value}
+									sx={{ textarea: { height: theme.space[12.5] } }}
+								/>
+							)}
+						/>
+					</VStack>
+				</ModalBody>
+				<ModalFooter
+					renderCancel={(props) => <Button {...props}>Cancel</Button>}
+					renderAction={(props) => (
+						<Button {...props} color={color} isDisabled={!id || !isDirty} type='submit'>
+							Update List
+						</Button>
+					)}
+				/>
+			</ModalStack>
+		</Modal>
 	);
 };
 
