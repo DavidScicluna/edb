a9ec1e153d5d40635b3aa8f5bbaaf4582331a35a
David Scicluna <dave_nostra@hotmail.com>
2022-11-20 19:47:30 +0100

    Fix: Filters - types

--- src/components/Filters/types.ts
+++ src/components/Filters/types.ts
@@ -9,12 +9,13 @@ export type FiltersFormCertification = Certification['certification'];
 export type FiltersFormCertifications = FiltersFormCertification[];
 
 export type FiltersFormDate = string | null;
+export type FiltersFormDates = { gte: FiltersFormDate; lte: FiltersFormDate };
 
 export type FiltersFormNumber = number | null;
 export type FiltersFormNumbers = FiltersFormNumber[];
 
 export type FiltersForm = {
-	dates: { gte: FiltersFormDate; lte: FiltersFormDate };
+	dates: FiltersFormDates;
 	genres: FiltersFormGenres;
 	certifications: FiltersFormCertifications;
 	rating: FiltersFormNumbers;
