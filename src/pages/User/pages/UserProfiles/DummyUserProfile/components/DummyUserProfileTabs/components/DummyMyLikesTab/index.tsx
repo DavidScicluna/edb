e4165f9f25b5cb04deb4704e84439cb747320678
David Scicluna <dave_nostra@hotmail.com>
2022-11-27 22:48:38 +0100

    Fix: User - UserProfiles - DummyUserProfile - DummyUserProfileTabs - DummyMyLikesTab - Moved Headline outside of its own component

--- src/pages/User/pages/UserProfiles/DummyUserProfile/components/DummyUserProfileTabs/components/DummyMyLikesTab/index.tsx
+++ src/pages/User/pages/UserProfiles/DummyUserProfile/components/DummyUserProfileTabs/components/DummyMyLikesTab/index.tsx
@@ -1,18 +1,17 @@
 import { FC } from 'react';
 
-import { useTheme, Divider } from '@davidscicluna/component-library';
+import { useTheme, Divider, Skeleton, Badge, BadgeLabel } from '@davidscicluna/component-library';
 
-import { VStack, Center } from '@chakra-ui/react';
+import { VStack, Center, Text } from '@chakra-ui/react';
 
 import DummyTabs from '../DummyUserProfileTabsTabs';
 import { useUserTheme } from '../../../../../../../../../common/hooks';
 import { useLayoutContext } from '../../../../../../../../../containers/Layout/common/hooks';
-
-import DummyMyLikesTabHeadline from './components/DummyMyLikesTabHeadline';
+import { Headline } from '../../../../../../../../../components';
 
 const DummyMyLikesTab: FC = () => {
 	const theme = useTheme();
-	const { colorMode } = useUserTheme();
+	const { color, colorMode } = useUserTheme();
 
 	const { spacing } = useLayoutContext();
 
@@ -23,7 +22,30 @@ const DummyMyLikesTab: FC = () => {
 			spacing={0}
 		>
 			<Center width='100%' py={spacing * 2}>
-				<DummyMyLikesTabHeadline />
+				<Headline
+					width='100%'
+					renderCaption={() => (
+						// TODO: Replace with DummyBadge
+						<Skeleton color={color} colorMode={colorMode} isLoaded={false} variant='text'>
+							<Badge color={color} colorMode={colorMode} size='xs'>
+								<BadgeLabel>Total of # likes</BadgeLabel>
+							</Badge>
+						</Skeleton>
+					)}
+					renderTitle={(props) => (
+						<Skeleton colorMode={colorMode} isLoaded={false} variant='text'>
+							<Text {...props}>My Likes</Text>
+						</Skeleton>
+					)}
+					renderSubtitle={(props) => (
+						<Skeleton colorMode={colorMode} isLoaded={false} variant='text'>
+							<Text {...props}>
+								This Tab contains all likes that have been added to the likes list and all likes are
+								separated into tabs depending on the media type.
+							</Text>
+						</Skeleton>
+					)}
+				/>
 			</Center>
 
 			<DummyTabs mediaTypes={['movie', 'tv', 'person', 'company', 'collection']} />
