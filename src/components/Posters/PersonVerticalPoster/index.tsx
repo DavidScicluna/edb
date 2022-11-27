7189bf49ee7bfb20ad4f00ea8cd8ae5712c432c5
David Scicluna <dave_nostra@hotmail.com>
2022-11-27 21:57:45 +0100

    Fix: Posters - PersonVerticalPoster - Added option to pass down subtitle prop

--- src/components/Posters/PersonVerticalPoster/index.tsx
+++ src/components/Posters/PersonVerticalPoster/index.tsx
@@ -1,6 +1,8 @@
 import { FC } from 'react';
 
-import { getImageSize } from '../../../common/utils';
+import { useConst } from '@chakra-ui/react';
+
+import { formatMediaTypeLabel, getImageSize } from '../../../common/utils';
 import VerticalPoster from '../VerticalPoster';
 
 import { PersonVerticalPosterProps } from './types';
@@ -9,8 +11,16 @@ const thumbnail = getImageSize({ type: 'poster', mode: 'thumbnail' });
 const full = getImageSize({ type: 'poster', mode: 'full' });
 
 const PersonVerticalPoster: FC<PersonVerticalPosterProps> = (props) => {
-	const { person, ...rest } = props;
+	const { person, subtitle, ...rest } = props;
 	const { name, profile_path, known_for_department } = person || {};
+
+	const alt = useConst<string>(
+		name
+			? `${name} ${formatMediaTypeLabel({ type: 'single', mediaType: 'person' })} poster`
+			: `${formatMediaTypeLabel({ type: 'single', mediaType: 'person' })} poster`
+	);
+
+	const defaultSubtitle = known_for_department;
 
 	return (
 		<VerticalPoster<'person'>
@@ -18,12 +28,12 @@ const PersonVerticalPoster: FC<PersonVerticalPosterProps> = (props) => {
 			mediaItem={{ ...person }}
 			mediaType='person'
 			image={{
-				alt: name ? `${name || ''} person poster` : 'Person poster',
+				alt,
 				src: profile_path || '',
 				size: { full, thumbnail }
 			}}
 			title={name || ''}
-			subtitle={known_for_department || ''}
+			subtitle={subtitle || defaultSubtitle || ''}
 		/>
 	);
 };
--- src/components/Posters/PersonVerticalPoster/types.ts
+++ src/components/Posters/PersonVerticalPoster/types.ts
@@ -1,7 +1,7 @@
 import { PartialPerson } from '../../../common/types/person';
 import { VerticalPosterProps } from '../VerticalPoster/types';
 
-type Omitted = 'mediaItem' | 'mediaType' | 'image' | 'rating' | 'title' | 'subtitle' | 'isFullWidth' | 'isLight';
+type Omitted = 'mediaItem' | 'mediaType' | 'image' | 'rating' | 'title' | 'isFullWidth' | 'isLight';
 
 export type PersonVerticalPosterProps = Omit<VerticalPosterProps<'tv'>, Omitted> & {
 	person: PartialPerson;
