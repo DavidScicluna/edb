2afc6aa0f20af3e70149725e77cd73bcb484bd21
David Scicluna <dave_nostra@hotmail.com>
2022-11-27 21:57:56 +0100

    Fix: Posters - TVShowVerticalPoster - Added option to pass down subtitle prop

--- src/components/Posters/TVShowVerticalPoster/index.tsx
+++ src/components/Posters/TVShowVerticalPoster/index.tsx
@@ -1,9 +1,12 @@
 import { FC } from 'react';
 
+import { useConst } from '@chakra-ui/react';
+
 import { compact } from 'lodash';
 
-import { getImageSize, handleReturnDate, handleReturnGenresByID } from '../../../common/utils';
+import { formatDate, formatMediaTypeLabel, getGenreLabelsByIDs, getImageSize } from '../../../common/utils';
 import VerticalPoster from '../VerticalPoster';
+import { useSelector } from '../../../common/hooks';
 
 import { TVShowVerticalPosterProps } from './types';
 
@@ -11,8 +14,24 @@ const thumbnail = getImageSize({ type: 'poster', mode: 'thumbnail' });
 const full = getImageSize({ type: 'poster', mode: 'full' });
 
 const TVShowVerticalPoster: FC<TVShowVerticalPosterProps> = (props) => {
-	const { show, ...rest } = props;
+	const genres = useSelector((state) => state.options.data.genres.tv);
+
+	const { show, subtitle, ...rest } = props;
 	const { name, poster_path, vote_average, vote_count, first_air_date, genre_ids } = show || {};
+
+	const alt = useConst<string>(
+		name
+			? `${name} ${formatMediaTypeLabel({ type: 'single', mediaType: 'tv' })} poster`
+			: `${formatMediaTypeLabel({ type: 'single', mediaType: 'tv' })} poster`
+	);
+
+	const defaultSubtitle =
+		!!first_air_date || !!genre_ids
+			? `${compact([
+					first_air_date ? formatDate({ date: first_air_date, section: 'year' }) : undefined,
+					genre_ids ? getGenreLabelsByIDs({ genres, ids: genre_ids }) : undefined
+			  ]).join(' • ')}`
+			: undefined;
 
 	return (
 		<VerticalPoster<'tv'>
@@ -20,20 +39,13 @@ const TVShowVerticalPoster: FC<TVShowVerticalPosterProps> = (props) => {
 			mediaItem={{ ...show }}
 			mediaType='tv'
 			image={{
-				alt: name ? `${name || ''} TV Show poster` : 'TV Show poster',
+				alt,
 				src: poster_path || '',
 				size: { full, thumbnail }
 			}}
 			rating={{ rating: vote_average, count: vote_count }}
 			title={name || ''}
-			subtitle={
-				!!first_air_date || !!genre_ids
-					? `${compact([
-							first_air_date ? `${handleReturnDate(first_air_date || '', 'year')}` : undefined,
-							genre_ids ? `${handleReturnGenresByID(genre_ids || [], 'tv')}` : undefined
-					  ]).join(' • ')}`
-					: undefined
-			}
+			subtitle={subtitle || defaultSubtitle || ''}
 		/>
 	);
 };
--- src/components/Posters/TVShowVerticalPoster/types.ts
+++ src/components/Posters/TVShowVerticalPoster/types.ts
@@ -1,7 +1,7 @@
 import { PartialTV } from '../../../common/types/tv';
 import { VerticalPosterProps } from '../VerticalPoster/types';
 
-type Omitted = 'mediaItem' | 'mediaType' | 'image' | 'rating' | 'title' | 'subtitle' | 'isFullWidth' | 'isLight';
+type Omitted = 'mediaItem' | 'mediaType' | 'image' | 'rating' | 'title' | 'isFullWidth' | 'isLight';
 
 export type TVShowVerticalPosterProps = Omit<VerticalPosterProps<'tv'>, Omitted> & {
 	show: PartialTV;
