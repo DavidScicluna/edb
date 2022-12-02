a3468154284fc25ca930471814e2722fcfdb3ddb
David Scicluna <dave_nostra@hotmail.com>
2022-12-02 23:28:37 +0100

    Fix: Moved Suspense to components/Lazy folder

---
+++ src/components/Lazy/Suspense/index.tsx
@@ -0,0 +1,42 @@
+import { FC, SuspenseProps, Suspense as RS, useCallback, useEffect, isValidElement } from 'react';
+
+import { useBoolean } from '@chakra-ui/react';
+
+import { debounce } from 'lodash';
+
+import ErrorBoundary from '../ErrorBoundary';
+
+let timer: ReturnType<typeof setTimeout>;
+
+const Suspense: FC<SuspenseProps> = ({ children, fallback, ...rest }) => {
+	const [isChildrenVisible, setIsChildrenVisible] = useBoolean();
+
+	const handleCheckChildren = useCallback(
+		debounce(() => {
+			if (isValidElement(children)) {
+				timer = setTimeout(() => setIsChildrenVisible.on(), 500);
+			} else {
+				setIsChildrenVisible.off();
+			}
+		}, 500),
+		[children, timer]
+	);
+
+	useEffect(() => {
+		handleCheckChildren();
+
+		return () => {
+			clearTimeout(timer);
+		};
+	}, [children]);
+
+	return (
+		<ErrorBoundary>
+			<RS {...rest} fallback={fallback}>
+				{isChildrenVisible ? children : fallback}
+			</RS>
+		</ErrorBoundary>
+	);
+};
+
+export default Suspense;
--- src/components/Suspense/components/SuspenseActions/types.ts
+++
@@ -1,3 +0,0 @@
-import { ButtonProps } from '@davidscicluna/component-library';
-
-export type SuspenseActionsProps = Pick<ButtonProps, 'color' | 'colorMode' | 'size' | 'variant'>;
