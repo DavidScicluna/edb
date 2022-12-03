f767e8efd4ecababa2314cca0891f383a19cb604
David Scicluna <dave_nostra@hotmail.com>
2022-12-03 23:44:34 +0100

    Feature: View - ViewDummyAvatar

---
+++ src/pages/View/components/ViewDummyAvatar/index.tsx
@@ -0,0 +1,22 @@
+import { FC } from 'react';
+
+import { useTheme, Skeleton } from '@davidscicluna/component-library';
+
+import { AspectRatio } from '@chakra-ui/react';
+
+import { getRatio } from '../../../../common/utils';
+import { useUserTheme } from '../../../../common/hooks';
+
+const ViewDummyAvatar: FC = () => {
+	const theme = useTheme();
+	const { colorMode } = useUserTheme();
+
+	return (
+		<AspectRatio width={theme.fontSizes['9xl']} ratio={getRatio({ orientation: 'portrait' })} borderRadius='base'>
+			{/* TODO: Go over all Skeleton check that we are passing colorMode */}
+			<Skeleton colorMode={colorMode} borderRadius='base' isLoaded={false} variant='rectangle' />
+		</AspectRatio>
+	);
+};
+
+export default ViewDummyAvatar;
