47fdd6ad163b43c0ec96f4effb164f05c8fbb758
David Scicluna <dave_nostra@hotmail.com>
2022-12-11 21:47:25 +0100

    Fix: Page - PageHeader

--- src/containers/Page/components/PageHeader/types.ts
+++ src/containers/Page/components/PageHeader/types.ts
@@ -1,16 +1,16 @@
 import { ReactNode } from 'react';
 
+import { HeadlineProps } from '@davidscicluna/component-library';
+
 import { StackProps } from '@chakra-ui/react';
 
-import { HeadlineProps } from '../../../../components/Headline/types';
-
-export type RenderProps = {
+export type PageHeaderRenderProps = {
 	width: number; // In Pixels
 	height: number; // In Pixels
 };
 
 export type PageHeaderProps = Omit<StackProps, 'children'> & {
-	renderLeftPanel?: (props: RenderProps) => ReactNode;
-	renderRightPanel?: (props: RenderProps) => ReactNode;
+	renderLeftPanel?: (props: PageHeaderRenderProps) => ReactNode;
+	renderRightPanel?: (props: PageHeaderRenderProps) => ReactNode;
 	actions?: ReactNode;
 } & Pick<HeadlineProps, 'renderTitle' | 'renderSubtitle'>;
