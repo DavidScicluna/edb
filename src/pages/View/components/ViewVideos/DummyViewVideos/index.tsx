2c959c54e944477ea67d8b2d115b6c1219e96238
David Scicluna <dave_nostra@hotmail.com>
2022-12-13 21:31:31 +0100

    Feature: View - ViewVideos - DummyViewVideos

---
+++ src/pages/View/components/ViewVideos/DummyViewVideos/index.tsx
@@ -0,0 +1,44 @@
+import { FC } from 'react';
+
+import { useTheme, DummyButton } from '@davidscicluna/component-library';
+
+import { useMediaQuery } from '@chakra-ui/react';
+
+import { range } from 'lodash';
+
+import { useUserTheme } from '../../../../../common/hooks';
+import {
+	DummyHorizontalGrid,
+	DummyHorizontalGridHeader,
+	DummyHorizontalGridBody,
+	DummyHorizontalGridScroll,
+	DummyHorizontalGridFooter
+} from '../../../../../components';
+import DummyVideo from '../components/ViewVideosDummyVideo';
+
+const ViewVideos: FC = () => {
+	const theme = useTheme();
+	const { color, colorMode } = useUserTheme();
+
+	const [isSm] = useMediaQuery(`(max-width: ${theme.breakpoints.sm})`);
+
+	return (
+		<DummyHorizontalGrid colorMode={colorMode} isFullWidth spacing={2} p={2}>
+			<DummyHorizontalGridHeader hasTitle hasSubtitle dummyArrowProps={{ variant: 'icon' }} spacing={0} />
+			<DummyHorizontalGridBody>
+				<DummyHorizontalGridScroll>
+					{range(5).map((_dummy, index) => (
+						<DummyVideo key={index} />
+					))}
+				</DummyHorizontalGridScroll>
+			</DummyHorizontalGridBody>
+			<DummyHorizontalGridFooter>
+				<DummyButton color={color} colorMode={colorMode} isFullWidth size={isSm ? 'xs' : 'sm'} variant='text'>
+					View all ## videos
+				</DummyButton>
+			</DummyHorizontalGridFooter>
+		</DummyHorizontalGrid>
+	);
+};
+
+export default ViewVideos;
