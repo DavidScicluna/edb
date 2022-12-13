a90bae3c51c9b03a97db76dddd25e6d84db105db
David Scicluna <dave_nostra@hotmail.com>
2022-12-13 20:43:34 +0100

    Fix: View - Removed Assets

--- src/pages/View/components/Assets/components/Image/types.ts
+++
@@ -1,8 +0,0 @@
-import { Image } from '../../../../../../common/types';
-import { AssetsTabProps } from '../../types';
-
-export type AssetImageProps = Image & {
-	srcSize: [string, string];
-	isLoading?: boolean;
-	onClickImage?: (path: string) => void;
-} & Omit<AssetsTabProps, 'images' | 'videos' | 'isError' | 'isSuccess' | 'isLoading' | 'onClickAsset'>;
--- src/pages/View/components/Assets/components/Video/types.ts
+++
@@ -1,8 +0,0 @@
-import { Video } from '../../../../../../common/types';
-import { AssetsTabProps } from '../../types';
-
-export type AssetVideoProps = Omit<Video, 'key'> & {
-	videoId?: Video['key'];
-	isLoading?: boolean;
-	onClickVideo?: (videoId: string) => void;
-} & Omit<AssetsTabProps, 'images' | 'videos' | 'isError' | 'isSuccess' | 'isLoading' | 'onClickAsset'>;
