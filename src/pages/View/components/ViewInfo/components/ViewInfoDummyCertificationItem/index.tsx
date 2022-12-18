97f778f9b9ad0bd32f171c70e7a60c55404abf0a
David Scicluna <dave_nostra@hotmail.com>
2022-12-18 19:21:48 +0100

    Feature: View - ViewInfo - ViewInfoDummyCertificationItem

---
+++ src/pages/View/components/ViewInfo/components/ViewInfoDummyCertificationItem/index.tsx
@@ -0,0 +1,24 @@
+import { FC } from 'react';
+
+import { Skeleton } from '@davidscicluna/component-library';
+
+import { Text } from '@chakra-ui/react';
+
+import { useUserTheme } from '../../../../../../common/hooks';
+import ViewInfoItem from '../ViewInfoItem';
+
+const ViewInfoDummyCertificationItem: FC = () => {
+	const { colorMode } = useUserTheme();
+
+	return (
+		<ViewInfoItem
+			renderLabel={(props) => (
+				<Skeleton colorMode={colorMode} isLoaded={false} variant='text'>
+					<Text {...props}>##</Text>
+				</Skeleton>
+			)}
+		/>
+	);
+};
+
+export default ViewInfoDummyCertificationItem;
