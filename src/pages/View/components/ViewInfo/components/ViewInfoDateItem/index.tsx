21158d800f455ddfb89c580cc8b9718dc69cc01f
David Scicluna <dave_nostra@hotmail.com>
2022-12-22 17:49:12 +0100

    Fix: View - ViewInfo - ViewInfoDateItem - Added color to Icon

--- src/pages/View/components/ViewInfo/components/ViewInfoDateItem/index.tsx
+++ src/pages/View/components/ViewInfo/components/ViewInfoDateItem/index.tsx
@@ -1,19 +1,47 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 
-import { Tooltip, Icon } from '@davidscicluna/component-library';
+import { useTheme, Tooltip, InternalLink, Icon, utils } from '@davidscicluna/component-library';
 
-import { useBoolean, Text } from '@chakra-ui/react';
+import { useBoolean, Center, Text } from '@chakra-ui/react';
+
+import qs from 'query-string';
+import dayjs from 'dayjs';
+import { useUpdateEffect } from 'usehooks-ts';
 
 import { useUserTheme } from '../../../../../../common/hooks';
 import ViewInfoItem from '../ViewInfoItem';
-import { formatDate } from '../../../../../../common/utils';
+import { formatDate, formatMediaType } from '../../../../../../common/utils';
+import { data as dataFormat } from '../../../../../../components/Filters/common/data/formats';
 
 import { ViewInfoDateItemProps } from './types';
 
-const ViewInfoDateItem: FC<ViewInfoDateItemProps> = ({ date }) => {
-	const { colorMode } = useUserTheme();
+const { getColor } = utils;
+
+const ViewInfoDateItem: FC<ViewInfoDateItemProps> = ({ mediaType, date }) => {
+	const theme = useTheme();
+	const { color: themeColor, colorMode } = useUserTheme();
 
 	const [isHovering, setIsHovering] = useBoolean();
+
+	const [color, setColor] = useState<string>(
+		getColor({
+			theme,
+			colorMode,
+			color: isHovering ? themeColor : 'gray',
+			type: isHovering ? 'color' : 'text.secondary'
+		})
+	);
+
+	useUpdateEffect(() => {
+		setColor(
+			getColor({
+				theme,
+				colorMode,
+				color: isHovering ? themeColor : 'gray',
+				type: isHovering ? 'color' : 'text.secondary'
+			})
+		);
+	}, [isHovering]);
 
 	return (
 		<Tooltip
@@ -24,12 +52,36 @@ const ViewInfoDateItem: FC<ViewInfoDateItemProps> = ({ date }) => {
 			label={formatDate({ date })}
 			shouldWrapChildren
 		>
-			<ViewInfoItem
-				renderIcon={(props) => <Icon {...props} icon='schedule' category='outlined' />}
-				renderLabel={(props) => <Text {...props}>{formatDate({ date, section: 'year' })}</Text>}
-				onMouseEnter={() => setIsHovering.on()}
-				onMouseLeave={() => setIsHovering.off()}
-			/>
+			<Center>
+				<InternalLink
+					colorMode={colorMode}
+					to={{
+						pathname: `/${formatMediaType({ mediaType })}`,
+						search: qs.stringify(
+							mediaType === 'movie'
+								? {
+										'primary_release_date.gte': dayjs(date).format(dataFormat),
+										'primary_release_date.lte': dayjs(new Date()).format(dataFormat)
+								  }
+								: {
+										'first_air_date.gte': dayjs(date).format(dataFormat),
+										'first_air_date.lte': dayjs(new Date()).format(dataFormat)
+								  }
+						)
+					}}
+				>
+					<ViewInfoItem
+						renderIcon={(props) => <Icon {...props} color={color} icon='schedule' category='outlined' />}
+						renderLabel={(props) => (
+							<Text {...props} color={color}>
+								{formatDate({ date, section: 'year' })}
+							</Text>
+						)}
+						onMouseEnter={() => setIsHovering.on()}
+						onMouseLeave={() => setIsHovering.off()}
+					/>
+				</InternalLink>
+			</Center>
 		</Tooltip>
 	);
 };
--- src/pages/View/components/ViewInfo/components/ViewInfoDateItem/types.ts
+++ src/pages/View/components/ViewInfo/components/ViewInfoDateItem/types.ts
@@ -1,1 +1,5 @@
-export type ViewInfoDateItemProps = { date: string };
+import { MediaType } from '../../../../../../common/types';
+
+export type ViewInfoDateItemMediaType = Exclude<MediaType, 'person' | 'company' | 'collection'>;
+
+export type ViewInfoDateItemProps = { mediaType: ViewInfoDateItemMediaType; date: string };
