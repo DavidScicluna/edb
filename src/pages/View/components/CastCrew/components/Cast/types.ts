a825aefe15509b0522bf9ef1f9050e595745f333
David Scicluna <dave_nostra@hotmail.com>
2022-12-23 15:10:16 +0100

    Fix: View - Removed CastCrew

--- src/pages/View/components/CastCrew/components/Cast/types.ts
+++
@@ -1,7 +0,0 @@
-import { AccordionType } from '@davidscicluna/component-library';
-
-import { CastCrewProps, Cast } from '../../types';
-
-export type CastProps = Omit<CastCrewProps, 'credits'> & {
-	cast?: Cast[];
-} & Omit<AccordionType<Cast[]>, 'id' | 'title' | 'subtitle' | 'total' | 'isDisabled' | 'data'>;
--- src/pages/View/components/CastCrew/components/Crew/types.ts
+++
@@ -1,7 +0,0 @@
-import { AccordionType } from '@davidscicluna/component-library';
-
-import { CastCrewProps, Crew } from '../../types';
-
-export type CrewProps = Omit<CastCrewProps, 'credits'> & {
-	crew?: Crew[];
-} & Omit<AccordionType<Crew[]>, 'id' | 'subtitle' | 'total' | 'isDisabled' | 'data'>;
