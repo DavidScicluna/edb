9b97ab41b3d1e0fd2eee720e2ac0882e9f5c0922
David Scicluna <dave_nostra@hotmail.com>
2022-12-23 15:24:30 +0100

    Feature: View - ViewHero - ViewHeroDummyCrew - ViewHeroDummyCrewPerson

---
+++ src/pages/View/components/ViewHero/components/ViewHeroDummyCrew/components/ViewHeroDummyCrewPerson/index.tsx
@@ -0,0 +1,18 @@
+import { FC } from 'react';
+
+import { Skeleton } from '@davidscicluna/component-library';
+
+import { useUserTheme } from '../../../../../../../../common/hooks';
+import ViewHeroText from '../../../ViewHeroText';
+
+const ViewHeroDummyCrewPerson: FC = () => {
+	const { colorMode } = useUserTheme();
+
+	return (
+		<Skeleton colorMode={colorMode} isLoaded={false} variant='text'>
+			<ViewHeroText textTransform='capitalize'>Person Name</ViewHeroText>
+		</Skeleton>
+	);
+};
+
+export default ViewHeroDummyCrewPerson;
