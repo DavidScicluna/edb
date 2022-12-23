8960a73cd2dc4ead14e1ca1dfa52231238af24d4
David Scicluna <dave_nostra@hotmail.com>
2022-12-23 15:20:49 +0100

    Feature: View - ViewHero - ViewHeroCover

---
+++ src/pages/View/components/ViewHero/components/ViewHeroCover/index.tsx
@@ -0,0 +1,41 @@
+import { FC } from 'react';
+
+import { useTheme } from '@davidscicluna/component-library';
+
+import { useMediaQuery, VStack, Center } from '@chakra-ui/react';
+
+import { ViewHeroCoverProps } from './types';
+
+const ViewHeroCover: FC<ViewHeroCoverProps> = ({ renderPoster, renderBackdrop }) => {
+	const theme = useTheme();
+
+	const [isMd] = useMediaQuery(`(min-width: ${theme.breakpoints.md})`);
+
+	return (
+		<VStack
+			width='100%'
+			height='auto'
+			alignItems='stretch'
+			justifyContent='stretch'
+			position='relative'
+			borderRadius={`${theme.radii.lg} ${theme.radii.lg} 0px 0px`}
+			overflowX='hidden'
+			overflowY='hidden'
+			spacing={0}
+		>
+			<Center
+				position={['relative', 'relative', 'absolute']}
+				zIndex={5}
+				bottom={[0, 0, 2, 4]}
+				left={[0, 0, 2, 4]}
+				p={[2, 2, 0]}
+			>
+				{renderPoster()}
+			</Center>
+
+			{isMd && <Center width='100%'>{renderBackdrop()}</Center>}
+		</VStack>
+	);
+};
+
+export default ViewHeroCover;
