0e7bdb30a68edf486e93ce4fd0e317e205a1c732
David Scicluna <dave_nostra@hotmail.com>
2022-12-23 15:34:48 +0100

    Feature: View - ViewVideos - ViewVideosDummyVideo

---
+++ src/pages/View/components/ViewVideos/components/ViewVideosDummyVideo/index.tsx
@@ -0,0 +1,26 @@
+import { FC } from 'react';
+
+import { Skeleton } from '@davidscicluna/component-library';
+
+import { AspectRatio } from '@chakra-ui/react';
+
+import { useUserTheme } from '../../../../../../common/hooks';
+import { getRatio } from '../../../../../../common/utils';
+
+const ViewVideosDummyVideo: FC = () => {
+	const { colorMode } = useUserTheme();
+
+	return (
+		<AspectRatio
+			width='100%'
+			height='100%'
+			overflow='hidden'
+			borderRadius='lg'
+			ratio={getRatio({ orientation: 'square' })}
+		>
+			<Skeleton colorMode={colorMode} width='inherit' height='inherit' isLoaded={false} variant='rectangle' />
+		</AspectRatio>
+	);
+};
+
+export default ViewVideosDummyVideo;
