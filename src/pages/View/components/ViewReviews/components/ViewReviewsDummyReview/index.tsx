5e3e83c615e47ec58ce855148af1675e2de9454e
David Scicluna <dave_nostra@hotmail.com>
2022-12-28 21:50:53 +0100

    Feature: View - ViewReviews - ViewReviewsDummyReview

---
+++ src/pages/View/components/ViewReviews/components/ViewReviewsDummyReview/index.tsx
@@ -0,0 +1,60 @@
+import { FC } from 'react';
+
+import {
+	useTheme,
+	DummyCard,
+	DummyCardHeader,
+	CardBody,
+	CardFooter,
+	Skeleton,
+	utils
+} from '@davidscicluna/component-library';
+
+import { AspectRatio, Text } from '@chakra-ui/react';
+
+import dayjs from 'dayjs';
+
+import { DummyParagraph, DummyRating } from '../../../../../../components';
+import { useUserTheme } from '../../../../../../common/hooks';
+import { formatDate, getRatio } from '../../../../../../common/utils';
+
+const { getColor } = utils;
+
+const ViewReviewsDummyReview: FC = () => {
+	const theme = useTheme();
+	const { colorMode } = useUserTheme();
+
+	return (
+		<DummyCard colorMode={colorMode} isFullWidth p={2}>
+			<DummyCardHeader
+				renderLeft={() => (
+					<AspectRatio
+						width={theme.fontSizes['5xl']}
+						height={theme.fontSizes['5xl']}
+						borderRadius='full'
+						ratio={getRatio({ orientation: 'square' })}
+					>
+						<Skeleton colorMode={colorMode} isLoaded={false} variant='circle' />
+					</AspectRatio>
+				)}
+				hasTitle
+				hasSubtitle
+				actions={<DummyRating size='xl' />}
+				spacing={1.5}
+			/>
+			<CardBody>
+				<DummyParagraph variant='transparent' />
+			</CardBody>
+
+			<CardFooter>
+				<Skeleton colorMode={colorMode} isLoaded={false} variant='text'>
+					<Text align='left' color={getColor({ theme, colorMode, type: 'text.secondary' })} fontSize='sm'>
+						{`* Created on: ${formatDate({ date: dayjs(new Date()).toISOString() })}`}
+					</Text>
+				</Skeleton>
+			</CardFooter>
+		</DummyCard>
+	);
+};
+
+export default ViewReviewsDummyReview;
