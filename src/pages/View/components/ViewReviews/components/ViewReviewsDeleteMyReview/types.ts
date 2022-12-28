775b00a13d84dbadcef69cba4f783944afc2cd89
David Scicluna <dave_nostra@hotmail.com>
2022-12-28 21:50:42 +0100

    Feature: View - ViewReviews - ViewReviewsDeleteMyReview

---
+++ src/pages/View/components/ViewReviews/components/ViewReviewsDeleteMyReview/types.ts
@@ -0,0 +1,18 @@
+import { ReactElement } from 'react';
+
+import { ButtonProps, IconCategory, IconType } from '@davidscicluna/component-library';
+
+import { ViewReviewsMediaType, ViewReviewsProps } from '../../types';
+import { UserReview } from '../../../../../../store/slices/Users/types';
+
+export type ViewReviewsDeleteMyReviewRenderButtonProps = Pick<ButtonProps, 'color' | 'colorMode' | 'onClick'> & {
+	icon: IconType;
+	category: IconCategory;
+};
+
+type Picked = 'mediaType' | 'mediaItem';
+
+export type ViewReviewsDeleteMyReviewProps<MT extends ViewReviewsMediaType> = Pick<ViewReviewsProps<MT>, Picked> & {
+	renderButton: (props: ViewReviewsDeleteMyReviewRenderButtonProps) => ReactElement;
+	review: UserReview;
+};
