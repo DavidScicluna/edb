42745cf0703f8a6a259e13049cbe34bb3b64e65b
David Scicluna <dave_nostra@hotmail.com>
2022-12-29 23:58:51 +0100

    Feature: View - ViewReviews - ViewReviewsDummyMyReviews

---
+++ src/pages/View/components/ViewReviews/components/ViewReviewsDummyMyReviews/index.tsx
@@ -0,0 +1,46 @@
+import { FC } from 'react';
+
+import { useTheme, DummyButton } from '@davidscicluna/component-library';
+
+import { useMediaQuery } from '@chakra-ui/react';
+
+import { range } from 'lodash';
+
+import { useUserTheme } from '../../../../../../common/hooks';
+import { useLayoutContext } from '../../../../../../containers/Layout/common/hooks';
+import DummyReview from '../ViewReviewsDummyReview';
+import DummyReviews from '../ViewReviewsDummyReviews';
+import { VerticalGrid } from '../../../../../../components';
+
+const ViewReviewsDummyMyReviews: FC = () => {
+	const theme = useTheme();
+	const { color, colorMode } = useUserTheme();
+
+	const [isSm] = useMediaQuery(`(max-width: ${theme.breakpoints.sm})`);
+
+	const { spacing } = useLayoutContext();
+
+	return (
+		<DummyReviews
+			title='My Reviews'
+			renderFooter={() => (
+				<DummyButton
+					color={color}
+					colorMode={colorMode}
+					isFullWidth
+					hasLeft
+					size={isSm ? 'xs' : 'sm'}
+					variant='text'
+				>
+					Create a new Review
+				</DummyButton>
+			)}
+		>
+			<VerticalGrid displayMode='list' spacing={spacing}>
+				{() => range(2).map((_dummy, index) => <DummyReview key={index} />)}
+			</VerticalGrid>
+		</DummyReviews>
+	);
+};
+
+export default ViewReviewsDummyMyReviews;
