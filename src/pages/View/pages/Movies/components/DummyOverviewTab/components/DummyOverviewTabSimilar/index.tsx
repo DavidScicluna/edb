bace0e7ff61ed9d15891b52d19e3392d0b08a989
David Scicluna <dave_nostra@hotmail.com>
2022-12-30 16:17:10 +0100

    Feature: View - Movies - DummyOverviewTab - DummyOverviewTabSimilar

---
+++ src/pages/View/pages/Movies/components/DummyOverviewTab/components/DummyOverviewTabSimilar/index.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+import DummyViewSimilar from '../../../../../../components/ViewSimilar/DummyViewSimilar';
+
+const DummyOverviewTabSimilar: FC = () => {
+	return <DummyViewSimilar<'movie'> mediaType='movie' />;
+};
+
+export default DummyOverviewTabSimilar;
