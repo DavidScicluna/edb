2f637ff8319b3508661177be5c8665715b132388
David Scicluna <dave_nostra@hotmail.com>
2022-12-30 16:44:37 +0100

    Fix: View - Persons - DummyOverviewTab - DummyPhotos

--- src/pages/View/pages/Persons/components/DummyOverviewTab/components/DummyPhotos/index.tsx
+++ src/pages/View/pages/Persons/components/DummyOverviewTab/components/DummyPhotos/index.tsx
@@ -1,44 +1,16 @@
 import { FC } from 'react';
 
-import { useTheme, DummyButton } from '@davidscicluna/component-library';
-
-import { useMediaQuery } from '@chakra-ui/react';
-
 import { range } from 'lodash';
 
-import {
-	DummyHorizontalGrid,
-	DummyHorizontalGridHeader,
-	DummyHorizontalGridBody,
-	DummyHorizontalGridScroll,
-	DummyHorizontalGridFooter
-} from '../../../../../../../../components';
-import { useUserTheme } from '../../../../../../../../common/hooks';
-
-import DummyPhoto from './components/DummyPhotosPhoto';
+import DummyViewPhotosHorizontalGrid from '../../../../../../components/ViewPhotosHorizontalGrid/DummyViewPhotosHorizontalGrid';
 
 const DummyPhotos: FC = () => {
-	const theme = useTheme();
-	const { color, colorMode } = useUserTheme();
-
-	const [isSm] = useMediaQuery(`(max-width: ${theme.breakpoints.sm})`);
-
 	return (
-		<DummyHorizontalGrid colorMode={colorMode} isFullWidth spacing={2} p={2}>
-			<DummyHorizontalGridHeader hasTitle hasSubtitle spacing={0} />
-			<DummyHorizontalGridBody>
-				<DummyHorizontalGridScroll>
-					{range(10).map((_dummy, index) => (
-						<DummyPhoto key={index} />
-					))}
-				</DummyHorizontalGridScroll>
-			</DummyHorizontalGridBody>
-			<DummyHorizontalGridFooter>
-				<DummyButton color={color} colorMode={colorMode} isFullWidth size={isSm ? 'xs' : 'sm'} variant='text'>
-					View all # photos
-				</DummyButton>
-			</DummyHorizontalGridFooter>
-		</DummyHorizontalGrid>
+		<DummyViewPhotosHorizontalGrid
+			photos={range(10).map(() => {
+				return { orientation: 'portrait' };
+			})}
+		/>
 	);
 };
 
