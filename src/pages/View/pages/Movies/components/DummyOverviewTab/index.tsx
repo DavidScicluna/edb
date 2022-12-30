8018145db103f1f866140ccd9c74ed62f26e703b
David Scicluna <dave_nostra@hotmail.com>
2022-12-30 16:17:30 +0100

    Feature: View - Movies - DummyOverviewTab

---
+++ src/pages/View/pages/Movies/components/DummyOverviewTab/index.tsx
@@ -0,0 +1,37 @@
+import { FC } from 'react';
+
+import { VStack } from '@chakra-ui/react';
+
+import { useLayoutContext } from '../../../../../../containers/Layout/common/hooks';
+
+import DummyOverviewTabHero from './components/DummyOverviewTabHero';
+import DummyOverviewTabPhotos from './components/DummyOverviewTabPhotos';
+import DummyOverviewTabVideos from './components/DummyOverviewTabVideos';
+import DummyOverviewTabTopCast from './components/DummyOverviewTabTopCast';
+import DummyOverviewTabReview from './components/DummyOverviewTabReview';
+import DummyOverviewTabRecommendations from './components/DummyOverviewTabRecommendations';
+import DummyOverviewTabSimilar from './components/DummyOverviewTabSimilar';
+
+const DummyOverviewTab: FC = () => {
+	const { spacing } = useLayoutContext();
+
+	return (
+		<VStack width='100%' spacing={spacing}>
+			<DummyOverviewTabHero />
+
+			<DummyOverviewTabTopCast />
+
+			<DummyOverviewTabPhotos />
+
+			<DummyOverviewTabVideos />
+
+			<DummyOverviewTabReview />
+
+			<DummyOverviewTabSimilar />
+
+			<DummyOverviewTabRecommendations />
+		</VStack>
+	);
+};
+
+export default DummyOverviewTab;
