fe66cd7b1b2757d56ee2c2f0b9cfe9c2f39bac48
David Scicluna <dave_nostra@hotmail.com>
2022-12-30 16:36:15 +0100

    Feature: View - Movies - OriginalMovie - OverviewTab - OverviewTabVideos

---
+++ src/pages/View/pages/Movies/OriginalMovie/components/OverviewTab/components/OverviewTabVideos/index.tsx
@@ -0,0 +1,47 @@
+import { FC, useState, useEffect } from 'react';
+
+import { useDebounce } from '@davidscicluna/component-library';
+
+import { shuffle } from 'lodash';
+
+import { useMovieContext } from '../../../../common/hooks';
+import { movieTabs } from '../../../..';
+import ViewVideos from '../../../../../../../components/ViewVideos/ViewVideosHorizontalGrid/OriginalViewVideosHorizontalGrid';
+import { ViewVideosVideos } from '../../../../../../../components/ViewVideos/common/types';
+import { formatMediaTypeLabel } from '../../../../../../../../../common/utils';
+
+const OverviewTabVideos: FC = () => {
+	const { movieQuery, videosQuery, onSetActiveTab } = useMovieContext();
+	const { data: movie } = movieQuery || {};
+	const { title } = movie || {};
+	const { data: videos, isFetching, isLoading, isError, isSuccess } = videosQuery || {};
+	const { results = [] } = videos || {};
+
+	const [shuffled, setShuffled] = useState<ViewVideosVideos>([]);
+	const photosDebounced = useDebounce<ViewVideosVideos>(shuffled, 'slow');
+
+	useEffect(() => {
+		setShuffled(shuffle([...results.filter((_poster, index) => index < 10)]));
+	}, [results]);
+
+	return (
+		<ViewVideos
+			mediaType='movie'
+			videos={[...photosDebounced]}
+			title='Videos'
+			subtitle={`This list is showcasing some of the trailers, teasers & featurettes that were created for ${
+				title ? title : `the ${formatMediaTypeLabel({ type: 'single', mediaType: 'movie' })}`
+			}`}
+			emptyLabel={title ? `${title} videos` : 'Videos'}
+			total={results.length}
+			isLoading={isFetching || isLoading}
+			isError={isError}
+			isSuccess={isSuccess}
+			onFooterClick={() =>
+				onSetActiveTab({ index: movieTabs.findIndex(({ path }) => path.hash === 'videos') || 5 })
+			}
+		/>
+	);
+};
+
+export default OverviewTabVideos;
