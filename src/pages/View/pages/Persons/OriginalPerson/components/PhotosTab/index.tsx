6503bf79bb33d385253ed832042a3a4da5cf989a
David Scicluna <dave_nostra@hotmail.com>
2022-12-30 16:47:04 +0100

    Fix: View - Persons - OriginalPerson - PhotosTab

--- src/pages/View/pages/Persons/OriginalPerson/components/PhotosTab/index.tsx
+++ src/pages/View/pages/Persons/OriginalPerson/components/PhotosTab/index.tsx
@@ -1,34 +1,22 @@
-import { FC } from 'react';
+import { FC, useState, useEffect } from 'react';
 
-import { useTheme, Headline, Button, Badge, BadgeLabel, Divider, Icon, utils } from '@davidscicluna/component-library';
+import { useDebounce, Headline, Divider } from '@davidscicluna/component-library';
 
-import { VStack, Text } from '@chakra-ui/react';
+import { useConst, VStack, Text } from '@chakra-ui/react';
 
-import { range } from 'lodash';
+import { range, shuffle } from 'lodash';
 
-import {
-	QueryEmpty,
-	QueryEmptyStack,
-	QueryEmptyIcon,
-	QueryEmptyBody,
-	QueryEmptyTitle,
-	QueryEmptySubtitle,
-	QueryEmptyActions,
-	TotalBadge
-} from '../../../../../../../components';
+import { TotalBadge } from '../../../../../../../components';
 import { useLayoutContext } from '../../../../../../../containers/Layout/common/hooks';
 import { useUserTheme } from '../../../../../../../common/hooks';
 import { usePersonContext } from '../../common/hooks';
-import { getEmptySubtitle } from '../../../../../../../components/QueryEmpty/common/utils';
-import Masonry from '../../../components/PersonsMasonry';
-import DummyPhoto from '../../../components/DummyPhotosTab/components/DummyPhotosTabPhoto';
-
-import Photo from './components/PhotosTabPhoto';
-
-const { getColor } = utils;
+import { formatMediaTypeLabel } from '../../../../../../../common/utils';
+import ViewPhotos from '../../../../../components/ViewPhotos/OriginalViewPhotos';
+import { ViewPhotosDummyPhotos, ViewPhotosDummyPhoto } from '../../../../../components/ViewPhotos/common/types';
+import { ViewPhotosPhotos, ViewPhotosPhoto } from '../../../../../components/ViewPhotos/OriginalViewPhotos/types';
+import { getPhotoHeight } from '../../../../../components/ViewPhotos/common/utils';
 
 const PhotosTab: FC = () => {
-	const theme = useTheme();
 	const { color, colorMode } = useUserTheme();
 
 	const { spacing } = useLayoutContext();
@@ -41,6 +29,34 @@ const PhotosTab: FC = () => {
 	const { data: images, isFetching, isLoading, isError, isSuccess, error, refetch } = imagesQuery || {};
 	const { profiles = [] } = images || {};
 
+	const [photos, setPhotos] = useState<ViewPhotosPhotos>([]);
+	const photosDebounced = useDebounce<ViewPhotosPhotos>(photos, 'slow');
+
+	const dummyPhotos = useConst<ViewPhotosDummyPhotos>(
+		range(20).map(() => {
+			return {
+				height: getPhotoHeight({ orientation: 'portrait' }),
+				orientation: 'portrait'
+			} as ViewPhotosDummyPhoto;
+		})
+	);
+
+	useEffect(() => {
+		if (photosDebounced.length === 0 && profiles.length > 0) {
+			setPhotos(
+				shuffle(
+					profiles.map((profile) => {
+						return {
+							height: getPhotoHeight({ orientation: 'portrait' }),
+							image: profile,
+							orientation: 'portrait'
+						} as ViewPhotosPhoto;
+					})
+				)
+			);
+		}
+	}, [profiles]);
+
 	return (
 		<VStack width='100%' divider={<Divider colorMode={colorMode} />} spacing={spacing}>
 			<Headline
@@ -49,110 +65,36 @@ const PhotosTab: FC = () => {
 					<TotalBadge
 						color={color}
 						colorMode={colorMode}
-						prefix={`${name ? name : 'Person'} has a total of`}
-						suffix='Photos'
-						total={profiles.length || 0}
+						prefix={`${
+							name ? name : formatMediaTypeLabel({ type: 'single', mediaType: 'person' })
+						} has a total of`}
+						suffix={`Photo${photosDebounced.length === 1 ? '' : 's'}`}
+						total={photosDebounced.length || 0}
 					/>
 				)}
 				renderTitle={(props) => <Text {...props}>Photos</Text>}
 				renderSubtitle={(props) => (
 					<Text {...props}>
-						{`This Tab contains all the photos that ${name ? name : 'the person'} has taken throughout ${
-							gender === 1 ? 'her' : gender === 2 ? 'his' : 'their'
-						} career.`}
+						{`This Tab contains all the photos that ${
+							name ? name : `the ${formatMediaTypeLabel({ type: 'single', mediaType: 'person' })}`
+						} has taken throughout ${gender === 1 ? 'her' : gender === 2 ? 'his' : 'their'} career.`}
 					</Text>
 				)}
 				py={spacing * 2}
 			/>
 
-			<VStack width='100%'>
-				{!(isFetching || isLoading) && isError ? (
-					<QueryEmpty
-						color={color}
-						colorMode={colorMode}
-						borderWidth='2px'
-						borderStyle='dashed'
-						borderColor={getColor({ theme, colorMode, type: 'divider' })}
-						borderRadius='lg'
-					>
-						<QueryEmptyStack>
-							<QueryEmptyIcon
-								renderIcon={(props) => (
-									<Icon
-										{...props}
-										width={theme.fontSizes['6xl']}
-										height={theme.fontSizes['6xl']}
-										fontSize={theme.fontSizes['6xl']}
-										icon='error_outline'
-									/>
-								)}
-								p={2}
-							/>
-							<QueryEmptyBody>
-								<QueryEmptyTitle />
-								<QueryEmptySubtitle>
-									{getEmptySubtitle({
-										type: 'error',
-										label: name ? `"${name}" Photos` : 'Photos'
-									})}
-								</QueryEmptySubtitle>
-							</QueryEmptyBody>
-
-							{error &&
-								error.response?.data &&
-								error.response.data.status_code &&
-								error.response.data.status_message && (
-									<Badge color={color} colorMode={colorMode}>
-										<BadgeLabel>{`(${error.response.data.status_code}) ${error.response.data.status_message}`}</BadgeLabel>
-									</Badge>
-								)}
-
-							{refetch && (
-								<QueryEmptyActions
-									renderActions={(props) => (
-										<Button {...props} onClick={() => refetch()}>
-											Try Again
-										</Button>
-									)}
-								/>
-							)}
-						</QueryEmptyStack>
-					</QueryEmpty>
-				) : !(isFetching || isLoading) && isSuccess && profiles.length === 0 ? (
-					<QueryEmpty
-						color={color}
-						colorMode={colorMode}
-						borderWidth='2px'
-						borderStyle='dashed'
-						borderColor={getColor({ theme, colorMode, type: 'divider' })}
-						borderRadius='lg'
-					>
-						<QueryEmptyStack>
-							<QueryEmptyBody>
-								<QueryEmptyTitle />
-								<QueryEmptySubtitle>
-									{getEmptySubtitle({
-										type: 'empty',
-										label: name ? `"${name}" Photos` : 'Photos'
-									})}
-								</QueryEmptySubtitle>
-							</QueryEmptyBody>
-						</QueryEmptyStack>
-					</QueryEmpty>
-				) : !(isFetching || isLoading) && isSuccess && profiles.length > 0 ? (
-					<Masonry>
-						{profiles.map((profile, index) => (
-							<Photo {...profile} key={profile.file_path} name={name} index={index} />
-						))}
-					</Masonry>
-				) : (
-					<Masonry>
-						{range(10).map((_dummy, index) => (
-							<DummyPhoto key={index} />
-						))}
-					</Masonry>
-				)}
-			</VStack>
+			<ViewPhotos
+				mediaType='person'
+				photos={photosDebounced}
+				dummyPhotos={dummyPhotos}
+				name={name}
+				isFetching={isFetching}
+				isLoading={isLoading}
+				isError={isError}
+				isSuccess={isSuccess}
+				error={error}
+				refetch={refetch}
+			/>
 		</VStack>
 	);
 };
