9676c72abcced6e36ea7b1d8e86d36d68b867964
David Scicluna <dave_nostra@hotmail.com>
2022-12-30 16:22:57 +0100

    Feature: View - Movies - OriginalMovie - MoviePoster

---
+++ src/pages/View/pages/Movies/OriginalMovie/components/MoviePoster/types.ts
@@ -0,0 +1,6 @@
+import { FullMovie } from '../../../../../../../common/types/movie';
+import { ViewPosterProps } from '../../../../../components/ViewPoster/types';
+
+export type MoviePosterProps = Pick<ViewPosterProps, 'onClick'> & {
+	movie: FullMovie;
+};
