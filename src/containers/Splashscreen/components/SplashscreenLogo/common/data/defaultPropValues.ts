1343ce915f0d8ae9348c9acb4c2aab1fae83479a
David Scicluna <dave_nostra@hotmail.com>
2023-01-03 22:43:22 +0100

    Feature: Splashscreen - SplashscreenLogo - defaultPropValues

---
+++ src/containers/Splashscreen/components/SplashscreenLogo/common/data/defaultPropValues.ts
@@ -0,0 +1,21 @@
+import { SplashscreenLogoColors, SplashscreenLogoSize } from '../../types';
+
+export const colors: SplashscreenLogoColors = [
+	'red',
+	'pink',
+	'purple',
+	'deep_purple',
+	'indigo',
+	'blue',
+	'light_blue',
+	'cyan',
+	'teal',
+	'green',
+	'light_green',
+	'lime',
+	'yellow',
+	'orange',
+	'deep_orange'
+];
+
+export const size: SplashscreenLogoSize = 240;
