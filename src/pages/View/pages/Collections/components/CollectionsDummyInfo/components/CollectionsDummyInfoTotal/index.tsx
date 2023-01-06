5fb6dd62ae6db6362bdf5043e6792389829012f7
David Scicluna <dave_nostra@hotmail.com>
2023-01-06 23:20:48 +0100

    Fix: View - Collections - CollectionsDummyInfo

--- src/pages/View/pages/Collections/components/CollectionsDummyInfo/components/CollectionsDummyInfoTotal/index.tsx
+++ src/pages/View/pages/Collections/components/CollectionsDummyInfo/components/CollectionsDummyInfoTotal/index.tsx
@@ -6,16 +6,19 @@ import { Text } from '@chakra-ui/react';
 
 import { useUserTheme } from '../../../../../../../../common/hooks';
 import ViewInfoItem from '../../../../../../components/ViewInfo/components/ViewInfoItem';
+import { getMediaTypeIcon } from '../../../../../../../../common/utils';
 
 const CollectionsDummyInfoTotal: FC = () => {
 	const { colorMode } = useUserTheme();
 
 	return (
 		<ViewInfoItem
-			renderIcon={(props) => <Icon {...props} icon='tag' category='outlined' />}
+			renderIcon={(props) => (
+				<Icon {...props} icon={getMediaTypeIcon({ mediaType: 'movie' })} category='outlined' />
+			)}
 			renderLabel={(props) => (
 				<Skeleton colorMode={colorMode} isLoaded={false} variant='text'>
-					<Text {...props}>## movies</Text>
+					<Text {...props}>##</Text>
 				</Skeleton>
 			)}
 		/>
