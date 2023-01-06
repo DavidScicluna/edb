cbaae48a067939fb81369d7c60e72393ecc646fc
David Scicluna <dave_nostra@hotmail.com>
2023-01-06 23:13:38 +0100

    Feature: View - Collections - DummyPhotosTab

---
+++ src/pages/View/pages/Collections/components/DummyPhotosTab/index.tsx
@@ -0,0 +1,64 @@
+import { FC } from 'react';
+
+import { Headline, Divider, Skeleton, Badge, BadgeLabel } from '@davidscicluna/component-library';
+
+import { useConst, VStack, Text } from '@chakra-ui/react';
+
+import { range, sample } from 'lodash';
+
+import DummyViewPhotos from '../../../../components/ViewPhotos/DummyViewPhotos';
+import { useUserTheme } from '../../../../../../common/hooks';
+import { useLayoutContext } from '../../../../../../containers/Layout/common/hooks';
+import {
+	ViewPhotosDummyPhoto,
+	ViewPhotosDummyPhotos,
+	ViewPhotosOrientation
+} from '../../../../components/ViewPhotos/common/types';
+import { getPhotoHeight } from '../../../../components/ViewPhotos/common/utils';
+
+const DummyPhotosTab: FC = () => {
+	const { color, colorMode } = useUserTheme();
+
+	const { spacing } = useLayoutContext();
+
+	const dummyPhotos = useConst<ViewPhotosDummyPhotos>(
+		range(20).map(() => {
+			const orientation: ViewPhotosOrientation = sample(['portrait', 'landscape']) || 'portrait';
+			return { height: getPhotoHeight({ orientation }), orientation } as ViewPhotosDummyPhoto;
+		})
+	);
+
+	return (
+		<VStack width='100%' divider={<Divider colorMode={colorMode} />} spacing={spacing}>
+			<Headline
+				width='100%'
+				renderCaption={() => (
+					// TODO: Replace with DummyBadge
+					<Skeleton color={color} colorMode={colorMode} isLoaded={false} variant='text'>
+						<Badge color={color} colorMode={colorMode} size='xs'>
+							<BadgeLabel>Collection has a total of # Photos</BadgeLabel>
+						</Badge>
+					</Skeleton>
+				)}
+				renderTitle={(props) => (
+					<Skeleton colorMode={colorMode} isLoaded={false} variant='text'>
+						<Text {...props}>Photos</Text>
+					</Skeleton>
+				)}
+				renderSubtitle={(props) => (
+					<Skeleton colorMode={colorMode} isLoaded={false} variant='text'>
+						<Text {...props}>
+							This Tab contains all the posters & backdrops that were used for the movies in the
+							collection
+						</Text>
+					</Skeleton>
+				)}
+				py={spacing * 2}
+			/>
+
+			<DummyViewPhotos dummyPhotos={dummyPhotos} />
+		</VStack>
+	);
+};
+
+export default DummyPhotosTab;
