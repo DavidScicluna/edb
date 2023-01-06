13989c0843d81521284118c41a924a46efb0b778
David Scicluna <dave_nostra@hotmail.com>
2023-01-06 23:13:27 +0100

    Feature: View - Collections - DummyPartsTab

---
+++ src/pages/View/pages/Collections/components/DummyPartsTab/index.tsx
@@ -0,0 +1,85 @@
+import { FC } from 'react';
+
+import { useTheme, Headline, Skeleton, Badge, BadgeLabel, Divider } from '@davidscicluna/component-library';
+
+import { useMediaQuery, Center, VStack, Text } from '@chakra-ui/react';
+
+import { range } from 'lodash';
+
+import { useLayoutContext } from '../../../../../../containers/Layout/common/hooks';
+import {
+	DummyHorizontalPoster,
+	DummyVerticalPoster,
+	VerticalGrid,
+	LoadMore,
+	DummyDisplayMode
+} from '../../../../../../components';
+import { useUserTheme } from '../../../../../../common/hooks';
+import { formatMediaTypeLabel } from '../../../../../../common/utils';
+
+const DummyPartsTab: FC = () => {
+	const theme = useTheme();
+	const { color, colorMode } = useUserTheme();
+
+	const [isSm] = useMediaQuery(`(max-width: ${theme.breakpoints.sm})`);
+
+	const { spacing } = useLayoutContext();
+
+	return (
+		<VStack width='100%' divider={<Divider colorMode={colorMode} />} spacing={spacing}>
+			<Headline
+				width='100%'
+				renderCaption={() => (
+					// TODO: Replace with DummyBadge
+					<Skeleton color={color} colorMode={colorMode} isLoaded={false} variant='text'>
+						<Badge color={color} colorMode={colorMode} size='xs'>
+							<BadgeLabel>Collection has a total of # Movies</BadgeLabel>
+						</Badge>
+					</Skeleton>
+				)}
+				renderTitle={(props) => (
+					<Skeleton colorMode={colorMode} isLoaded={false} variant='text'>
+						<Text {...props}>Movies</Text>
+					</Skeleton>
+				)}
+				renderSubtitle={(props) => (
+					<Skeleton colorMode={colorMode} isLoaded={false} variant='text'>
+						<Text {...props}>This Tab contains all the Movies that are part of the collection</Text>
+					</Skeleton>
+				)}
+				renderRight={() => <DummyDisplayMode />}
+				py={spacing * 2}
+			/>
+
+			<VStack width='100%' spacing={spacing}>
+				<VerticalGrid spacing={spacing}>
+					{({ displayMode }) =>
+						range(20).map((_dummy, index) =>
+							displayMode === 'list' ? (
+								<DummyHorizontalPoster key={index} mediaType='movie' hasSubtitle hasDescription />
+							) : (
+								<DummyVerticalPoster key={index} mediaType='movie' hasSubtitle />
+							)
+						)
+					}
+				</VerticalGrid>
+
+				<Center width={isSm ? '100%' : 'auto'}>
+					<LoadMore
+						amount={0}
+						total={0}
+						label={`${formatMediaTypeLabel({
+							type: 'single',
+							mediaType: 'collection'
+						})} ${formatMediaTypeLabel({ type: 'multiple', mediaType: 'movie' })}`}
+						isDisabled
+						isLoading
+						isButtonVisible
+					/>
+				</Center>
+			</VStack>
+		</VStack>
+	);
+};
+
+export default DummyPartsTab;
