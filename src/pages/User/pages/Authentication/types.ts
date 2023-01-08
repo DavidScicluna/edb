188a65b81f4626fdab213b516e26058580d0f735
David Scicluna <dave_nostra@hotmail.com>
2023-01-08 18:29:33 +0100

    Fix: User - Authentication - types

--- src/pages/User/pages/Authentication/types.ts
+++ src/pages/User/pages/Authentication/types.ts
@@ -2,6 +2,9 @@ import { ColorMode } from '@chakra-ui/react';
 
 import { UserThemeColor } from '../../../../store/slices/Users/types';
 
+export type AuthenticationIllustrationIndex = 1 | 2 | 3 | 4 | 5 | 6;
+export type AuthenticationIllustrationIndexes = AuthenticationIllustrationIndex[];
+
 export type AuthenticationOutletContext = {
 	color: UserThemeColor;
 	colorMode: ColorMode;
