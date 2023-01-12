d59c21e471f94702dd0bab5763fc495fb886753e
David Scicluna <dave_nostra@hotmail.com>
2023-01-12 19:25:21 +0100

    Feature: View - TVShows - DummyReviewsTab

---
+++ src/pages/View/pages/TVShows/components/DummyReviewsTab/index.tsx
@@ -0,0 +1,48 @@
+import { FC } from 'react';
+
+import { Headline, Divider, Skeleton, Badge, BadgeLabel } from '@davidscicluna/component-library';
+
+import { VStack, Text } from '@chakra-ui/react';
+
+import { useUserTheme } from '../../../../../../common/hooks';
+import { useLayoutContext } from '../../../../../../containers/Layout/common/hooks';
+import DummyViewReviews from '../../../../components/ViewReviews/DummyViewReviews';
+
+const DummyReviewsTab: FC = () => {
+	const { color, colorMode } = useUserTheme();
+
+	const { spacing } = useLayoutContext();
+
+	return (
+		<VStack width='100%' divider={<Divider colorMode={colorMode} />} spacing={spacing}>
+			<Headline
+				width='100%'
+				renderCaption={() => (
+					// TODO: Replace with DummyBadge
+					<Skeleton color={color} colorMode={colorMode} isLoaded={false} variant='rectangle'>
+						<Badge color={color} colorMode={colorMode} size='xs'>
+							<BadgeLabel>TV Show has a total of # Reviews</BadgeLabel>
+						</Badge>
+					</Skeleton>
+				)}
+				renderTitle={(props) => (
+					<Skeleton colorMode={colorMode} isLoaded={false} variant='text'>
+						<Text {...props}>Reviews</Text>
+					</Skeleton>
+				)}
+				renderSubtitle={(props) => (
+					<Skeleton colorMode={colorMode} isLoaded={false} variant='text'>
+						<Text {...props}>
+							This Tab contains all the reviews that were made by a number of viewers for the TV Show
+						</Text>
+					</Skeleton>
+				)}
+				py={spacing * 2}
+			/>
+
+			<DummyViewReviews />
+		</VStack>
+	);
+};
+
+export default DummyReviewsTab;
