08045da0d9adb9a91fc48c2ef267811a401668e0
David Scicluna <dave_nostra@hotmail.com>
2023-01-13 22:23:51 +0100

    Fix: View - ViewHero - ViewHeroCrew - Adding a limit to the number of crew to render

--- src/pages/View/components/ViewHero/components/ViewHeroCrew/index.tsx
+++ src/pages/View/components/ViewHero/components/ViewHeroCrew/index.tsx
@@ -1,20 +1,42 @@
 import { ReactElement } from 'react';
 
+import { useBreakpointValue } from '@chakra-ui/react';
+
+import { sort } from 'fast-sort';
+
 import ViewHeroLabel from '../ViewHeroLabel';
 import ViewHeroStack from '../ViewHeroHStack';
+import ViewHeroText from '../ViewHeroText';
 
 import ViewHeroCrewPerson from './components/ViewHeroCrewPerson';
 import { ViewHeroCrewMediaType, ViewHeroCrewProps } from './types';
 
 const ViewHeroCrew = <MT extends ViewHeroCrewMediaType>(props: ViewHeroCrewProps<MT>): ReactElement => {
+	const limit =
+		useBreakpointValue<number>({
+			'base': 4,
+			'sm': 6,
+			'md': 8,
+			'lg': 10,
+			'xl': 12,
+			'2xl': 12
+		}) || 8;
+
 	const { label, crew = [], ...rest } = props;
 
 	return (
 		<ViewHeroLabel {...rest} maxWidth='100%' label={label}>
 			<ViewHeroStack>
-				{crew.map((person) => (
-					<ViewHeroCrewPerson {...person} key={person.id} />
-				))}
+				{sort([...crew])
+					.desc(({ popularity }) => popularity)
+					.filter((_person, index) => index < limit)
+					.map((person) => (
+						<ViewHeroCrewPerson {...person} key={person.id} />
+					))}
+
+				{crew.length > limit && (
+					<ViewHeroText textTransform='capitalize'>{`+${crew.length - limit}`}</ViewHeroText>
+				)}
 			</ViewHeroStack>
 		</ViewHeroLabel>
 	);
--- src/pages/View/components/ViewHero/components/ViewHeroCrew/types.ts
+++ src/pages/View/components/ViewHero/components/ViewHeroCrew/types.ts
@@ -6,8 +6,8 @@ import { ViewHeroLabelProps } from '../ViewHeroLabel/types';
 export type ViewHeroCrewMediaType = Exclude<MediaType, 'person' | 'company' | 'collection'>;
 
 export type ViewHeroCrewGetMediaItemType<MT extends ViewHeroCrewMediaType> = MT extends 'movie'
-	? Pick<MovieCrew, 'id' | 'name'>
-	: Pick<TVShowCrew, 'id' | 'name'>;
+	? Pick<MovieCrew, 'id' | 'name' | 'popularity'>
+	: Pick<TVShowCrew, 'id' | 'name' | 'popularity'>;
 
 export type ViewHeroCrewProps<MT extends ViewHeroCrewMediaType> = Pick<ViewHeroLabelProps, 'label'> & {
 	crew?: ViewHeroCrewGetMediaItemType<MT>[];
