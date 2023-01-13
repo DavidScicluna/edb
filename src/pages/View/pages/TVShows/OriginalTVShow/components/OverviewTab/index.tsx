ead54b3354d10ed2732d7eda6ce0c2256710c5fa
David Scicluna <dave_nostra@hotmail.com>
2023-01-13 22:21:54 +0100

    Feature: View - TVShows - OriginalTVShow - OverviewTab

---
+++ src/pages/View/pages/TVShows/OriginalTVShow/components/OverviewTab/index.tsx
@@ -0,0 +1,47 @@
+import { FC } from 'react';
+
+import { VStack } from '@chakra-ui/react';
+
+import { compact } from 'lodash';
+
+import { useLayoutContext } from '../../../../../../../containers/Layout/common/hooks';
+
+import OverviewTabHero from './components/OverviewTabHero';
+import OverviewTabCreators from './components/OverviewTabCreators';
+import OverviewTabTopActor from './components/OverviewTabTopActor';
+import OverviewTabRecommendations from './components/OverviewTabRecommendations';
+import OverviewTabSimilar from './components/OverviewTabSimilar';
+import OverviewTabPhotos from './components/OverviewTabPhotos';
+import OverviewTabVideos from './components/OverviewTabVideos';
+import OverviewTabTopCast from './components/OverviewTabTopCast';
+import OverviewTabReview from './components/OverviewTabReview';
+
+const OverviewTab: FC = () => {
+	const { spacing } = useLayoutContext();
+
+	return (
+		<VStack width='100%' spacing={spacing}>
+			{compact([
+				<OverviewTabHero key='ds-edb-tv-show-overview-tab-hero' />,
+
+				<OverviewTabTopCast key='ds-edb-tv-show-overview-tab-top-cast' />,
+
+				<OverviewTabPhotos key='ds-edb-tv-show-overview-tab-photos' />,
+
+				<OverviewTabVideos key='ds-edb-tv-show-overview-tab-videos' />,
+
+				<OverviewTabReview key='ds-edb-tv-show-overview-tab-review' />,
+
+				<OverviewTabCreators key='ds-edb-tv-show-overview-tab-creators' />,
+
+				<OverviewTabTopActor key='ds-edb-tv-show-overview-tab-top-actor' />,
+
+				<OverviewTabSimilar key='ds-edb-tv-show-overview-tab-similar' />,
+
+				<OverviewTabRecommendations key='ds-edb-tv-show-overview-tab-recommendations' />
+			])}
+		</VStack>
+	);
+};
+
+export default OverviewTab;
