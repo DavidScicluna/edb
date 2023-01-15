bd836baecdc7e201b4511931fb29beca6ae7a1cc
David Scicluna <dave_nostra@hotmail.com>
2023-01-15 21:56:39 +0100

    Feature: Layout - Modals - QuickViewModal - QuickViewModalTVShow - QuickViewModalTVShowActions

---
+++ src/containers/Layout/components/Modals/QuickViewModal/components/QuickViewModalTVShow/components/QuickViewModalTVShowActions/types.ts
@@ -0,0 +1,7 @@
+import { MouseEvent as ME } from 'react';
+
+import { CommonQuickViewModalTVShowProps } from '../../common/types';
+
+export type QuickViewModalTVShowActionsMouseEvent = ME<HTMLButtonElement, globalThis.MouseEvent>;
+
+export type QuickViewModalTVShowActionsProps = CommonQuickViewModalTVShowProps;
