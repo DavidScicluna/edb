9c0d374af9b282d81ef2d570be902ce4109d0e45
David Scicluna <dave_nostra@hotmail.com>
2023-01-22 23:02:18 +0100

    Fix: View - Episodes - EpisodesDummyInfo - Added Certification & Runtime

--- src/pages/View/pages/Episodes/components/EpisodesDummyInfo/index.tsx
+++ src/pages/View/pages/Episodes/components/EpisodesDummyInfo/index.tsx
@@ -1,6 +1,8 @@
 import { FC } from 'react';
 
 import ViewInfo from '../../../../components/ViewInfo';
+import ViewInfoDummyCertificationItem from '../../../../components/ViewInfo/components/ViewInfoDummyCertificationItem';
+import ViewInfoDummyRuntimeItem from '../../../../components/ViewInfo/components/ViewInfoDummyRuntimeItem';
 
 import EpisodesDummyInfoDate from './components/EpisodesDummyInfoDate';
 import EpisodesDummyInfoNumbers from './components/EpisodesDummyInfoNumbers';
@@ -8,9 +10,13 @@ import EpisodesDummyInfoNumbers from './components/EpisodesDummyInfoNumbers';
 const EpisodesDummyInfo: FC = () => {
 	return (
 		<ViewInfo>
+			<ViewInfoDummyCertificationItem key='ds-edb-episode-dummy-info-certification' />
+
 			<EpisodesDummyInfoDate key='ds-edb-episode-dummy-info-date' />
 
 			<EpisodesDummyInfoNumbers key='ds-edb-episode-dummy-info-numbers' />
+
+			<ViewInfoDummyRuntimeItem key='ds-edb-episode-dummy-info-runtime' />
 		</ViewInfo>
 	);
 };
