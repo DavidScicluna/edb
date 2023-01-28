c1092fabcc787a6896a49b7646d5ab6262b92cb2
David Scicluna <dave_nostra@hotmail.com>
2023-01-28 18:30:30 +0100

    Feature: View - Episodes - DummyOverviewTab - DummyOverviewTabVideos

---
+++ src/pages/View/pages/Episodes/components/DummyOverviewTab/components/DummyOverviewTabVideos/index.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+import DummyViewVideos from '../../../../../../components/ViewVideos/ViewVideosHorizontalGrid/DummyViewVideosHorizontalGrid';
+
+const DummyOverviewTabVideos: FC = () => {
+	return <DummyViewVideos />;
+};
+
+export default DummyOverviewTabVideos;
