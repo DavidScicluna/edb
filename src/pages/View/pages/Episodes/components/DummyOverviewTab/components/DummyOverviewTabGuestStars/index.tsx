b2ac13ded4cc22c06fbe023b0c37844420c8bde9
David Scicluna <dave_nostra@hotmail.com>
2023-01-28 18:29:57 +0100

    Feature: View - Episodes - DummyOverviewTab - DummyOverviewTabGuestStars

---
+++ src/pages/View/pages/Episodes/components/DummyOverviewTab/components/DummyOverviewTabGuestStars/index.tsx
@@ -0,0 +1,45 @@
+import { FC } from 'react';
+
+import { useTheme, DummyButton } from '@davidscicluna/component-library';
+
+import { useMediaQuery } from '@chakra-ui/react';
+
+import { range } from 'lodash';
+
+import width from '../../../../../../../../components/Posters/common/data/width';
+import {
+	DummyHorizontalGrid,
+	DummyHorizontalGridHeader,
+	DummyHorizontalGridBody,
+	DummyHorizontalGridScroll,
+	DummyHorizontalGridFooter,
+	DummyVerticalPoster
+} from '../../../../../../../../components';
+import { useUserTheme } from '../../../../../../../../common/hooks';
+
+const DummyOverviewTabGuestStars: FC = () => {
+	const theme = useTheme();
+	const { color, colorMode } = useUserTheme();
+
+	const [isSm] = useMediaQuery(`(max-width: ${theme.breakpoints.sm})`);
+
+	return (
+		<DummyHorizontalGrid colorMode={colorMode} isFullWidth spacing={2} p={2}>
+			<DummyHorizontalGridHeader hasTitle hasSubtitle dummyArrowProps={{ variant: 'icon' }} spacing={0} />
+			<DummyHorizontalGridBody>
+				<DummyHorizontalGridScroll>
+					{range(20).map((_dummy, index) => (
+						<DummyVerticalPoster key={index} mediaType='person' hasSubtitle sx={{ width }} />
+					))}
+				</DummyHorizontalGridScroll>
+			</DummyHorizontalGridBody>
+			<DummyHorizontalGridFooter>
+				<DummyButton color={color} colorMode={colorMode} isFullWidth size={isSm ? 'xs' : 'sm'} variant='text'>
+					View all ## Guest Stars
+				</DummyButton>
+			</DummyHorizontalGridFooter>
+		</DummyHorizontalGrid>
+	);
+};
+
+export default DummyOverviewTabGuestStars;
