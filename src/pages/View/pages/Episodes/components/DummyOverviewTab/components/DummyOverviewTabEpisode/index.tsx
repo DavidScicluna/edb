8f9217408082ab11baab530c2e2b9b0853a4a7fd
David Scicluna <dave_nostra@hotmail.com>
2023-01-28 18:29:50 +0100

    Feature: View - Episodes - DummyOverviewTab - DummyOverviewTabEpisode

---
+++ src/pages/View/pages/Episodes/components/DummyOverviewTab/components/DummyOverviewTabEpisode/index.tsx
@@ -0,0 +1,21 @@
+import { FC } from 'react';
+
+import { DummyCard, DummyCardHeader, CardBody } from '@davidscicluna/component-library';
+
+import { useUserTheme } from '../../../../../../../../common/hooks';
+import ViewDummyEpisode from '../../../../../../components/ViewDummyEpisode';
+
+const DummyOverviewTabEpisode: FC = () => {
+	const { colorMode } = useUserTheme();
+
+	return (
+		<DummyCard colorMode={colorMode} isFullWidth p={2}>
+			<DummyCardHeader hasTitle />
+			<CardBody>
+				<ViewDummyEpisode hasDate hasOverview />
+			</CardBody>
+		</DummyCard>
+	);
+};
+
+export default DummyOverviewTabEpisode;
