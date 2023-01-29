653766eaba3529b5ea4665fbed2a5a00b3c63826
David Scicluna <dave_nostra@hotmail.com>
2023-01-29 23:09:04 +0100

    Feature: Search - OriginalSearch - SearchTabs - AllTab - AllTabCompanies

---
+++ src/pages/Search/OriginalSearch/components/SearchTabs/components/AllTab/components/AllTabCompanies/index.tsx
@@ -0,0 +1,44 @@
+import { FC } from 'react';
+
+import numbro from 'numbro';
+
+import AllTabHorizontalGrid from '../AllTabHorizontalGrid';
+import { formatMediaTypeLabel } from '../../../../../../../../../common/utils';
+import { CompanyVerticalPoster } from '../../../../../../../../../components';
+import dimensions from '../../../../../../../../../components/Posters/common/data/dimensions';
+
+import { AllTabCompaniesProps } from './types';
+import { getMediaTypeIndex } from '../../../../common/utils';
+import { useSearchContext } from '../../../../../../common/hooks';
+
+const AllTabCompanies: FC<AllTabCompaniesProps> = ({ mediaTypes, companies }) => {
+	const { query, onSetActiveTab } = useSearchContext();
+
+	const { total_results: total = 0, results = [] } = companies || {};
+
+	return (
+		<AllTabHorizontalGrid
+			title={formatMediaTypeLabel({ type: 'multiple', mediaType: 'company' })}
+			subtitle={`Found a total of ${numbro(total).format({ average: true })} ${formatMediaTypeLabel({
+				type: total === 1 ? 'single' : 'multiple',
+				mediaType: 'company'
+			})} with query "${query}"`}
+			footerLabel={`View all ${numbro(total).format({ average: true })} ${formatMediaTypeLabel({
+				type: total === 1 ? 'single' : 'multiple',
+				mediaType: 'company'
+			})}`}
+			isDisabled={total === 0}
+			onFooterClick={
+				total > 0 && onSetActiveTab
+					? () => onSetActiveTab({ index: getMediaTypeIndex({ mediaTypes, mediaType: 'company' }) })
+					: undefined
+			}
+		>
+			{results.map((company) => (
+				<CompanyVerticalPoster key={company.id} company={company} sx={dimensions} />
+			))}
+		</AllTabHorizontalGrid>
+	);
+};
+
+export default AllTabCompanies;
