7f4639bc49f4aa5685ecb5b5fab94c4e2a3f68d3
David Scicluna <dave_nostra@hotmail.com>
2023-01-29 23:46:49 +0100

    Feature: User - UserProfile - DummyUserProfile - DummyUserProfileTabs - DummyUserProfileTabsCompanies

---
+++ src/pages/User/pages/UserProfile/DummyUserProfile/components/DummyUserProfileTabs/components/DummyUserProfileTabsCompanies/index.tsx
@@ -0,0 +1,45 @@
+import { FC } from 'react';
+
+import { useTheme } from '@davidscicluna/component-library';
+
+import { useMediaQuery, VStack, Center } from '@chakra-ui/react';
+
+import { range } from 'lodash';
+
+import { useLayoutContext } from '../../../../../../../../../containers/Layout/common/hooks';
+import {
+	VerticalGrid,
+	DummyLoadMore,
+	DummyHorizontalPoster,
+	DummyVerticalPoster
+} from '../../../../../../../../../components';
+
+const DummyUserProfileTabsCompanies: FC = () => {
+	const theme = useTheme();
+
+	const [isSm] = useMediaQuery(`(max-width: ${theme.breakpoints.sm})`);
+
+	const { spacing } = useLayoutContext();
+
+	return (
+		<VStack width='100%' spacing={spacing}>
+			<VerticalGrid>
+				{({ displayMode }) =>
+					range(20).map((_dummy, index) =>
+						displayMode === 'list' ? (
+							<DummyHorizontalPoster key={index} mediaType='company' hasSubtitle />
+						) : (
+							<DummyVerticalPoster key={index} mediaType='company' hasSubtitle />
+						)
+					)
+				}
+			</VerticalGrid>
+
+			<Center width={isSm ? '100%' : 'auto'}>
+				<DummyLoadMore isButtonVisible />
+			</Center>
+		</VStack>
+	);
+};
+
+export default DummyUserProfileTabsCompanies;
