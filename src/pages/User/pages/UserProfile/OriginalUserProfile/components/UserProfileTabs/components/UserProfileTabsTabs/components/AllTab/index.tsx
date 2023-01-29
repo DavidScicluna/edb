f052608cf7d0a0ab2f5418924820e91df7f4ef79
David Scicluna <dave_nostra@hotmail.com>
2023-01-29 23:46:10 +0100

    Fix: User - UserProfile - OriginalUserProfile - UserProfileTabs - UserProfileTabsTabs - AllTab

--- src/pages/User/pages/UserProfile/OriginalUserProfile/components/UserProfileTabs/components/UserProfileTabsTabs/components/AllTab/index.tsx
+++ src/pages/User/pages/UserProfile/OriginalUserProfile/components/UserProfileTabs/components/UserProfileTabsTabs/components/AllTab/index.tsx
@@ -1,42 +1,57 @@
-import { FC, lazy } from 'react';
+import { FC } from 'react';
 
 import { VStack } from '@chakra-ui/react';
 
 import { useLayoutContext } from '../../../../../../../../../../../containers/Layout/common/hooks';
 import { Suspense } from '../../../../../../../../../../../components';
-import AllTabDummyMovies from '../../../../../../../DummyUserProfile/components/DummyUserProfileTabs/components/DummyUserProfileTabsTabs/components/DummyAllTab/components/DummyAllTabMovies';
-import AllTabDummyPeople from '../../../../../../../DummyUserProfile/components/DummyUserProfileTabs/components/DummyUserProfileTabsTabs/components/DummyAllTab/components/DummyAllTabPeople';
-import AllTabDummyTVShows from '../../../../../../../DummyUserProfile/components/DummyUserProfileTabs/components/DummyUserProfileTabsTabs/components/DummyAllTab/components/DummyAllTabTVShows';
+import DummyAllTabMovies from '../../../../../../../DummyUserProfile/components/DummyUserProfileTabs/components/DummyUserProfileTabsTabs/components/DummyAllTab/components/DummyAllTabMovies';
+import DummyAllTabPeople from '../../../../../../../DummyUserProfile/components/DummyUserProfileTabs/components/DummyUserProfileTabsTabs/components/DummyAllTab/components/DummyAllTabPeople';
+import DummyAllTabTVShows from '../../../../../../../DummyUserProfile/components/DummyUserProfileTabs/components/DummyUserProfileTabsTabs/components/DummyAllTab/components/DummyAllTabTVShows';
+import DummyAllTabCollections from '../../../../../../../DummyUserProfile/components/DummyUserProfileTabs/components/DummyUserProfileTabsTabs/components/DummyAllTab/components/DummyAllTabCollections';
+import DummyAllTabCompanies from '../../../../../../../DummyUserProfile/components/DummyUserProfileTabs/components/DummyUserProfileTabsTabs/components/DummyAllTab/components/DummyAllTabCompanies';
 
 import { AllTabProps } from './types';
-
-const AllTabMovies = lazy(() => import('./components/AllTabMovies'));
-const AllTabPeople = lazy(() => import('./components/AllTabPeople'));
-const AllTabTVShows = lazy(() => import('./components/AllTabTVShows'));
+import AllTabMovies from './components/AllTabMovies';
+import AllTabPeople from './components/AllTabPeople';
+import AllTabTVShows from './components/AllTabTVShows';
+import AllTabCollections from './components/AllTabCollections';
+import AllTabCompany from './components/AllTabCompany';
 
 const AllTab: FC<AllTabProps> = ({ type, mediaItems, onSetActiveTab }) => {
 	// TODO: Add Sections for Collectios & Companies
-	const { movie = [], tv = [], person = [], company = [], collection = [] } = mediaItems;
+	const { movie = [], tv = [], person = [], collection = [], company = [] } = mediaItems;
 
 	const { spacing } = useLayoutContext();
 
 	return (
 		<VStack width='100%' spacing={spacing}>
 			{movie.length > 0 && (
-				<Suspense fallback={<AllTabDummyMovies />}>
+				<Suspense fallback={<DummyAllTabMovies />}>
 					<AllTabMovies type={type} movies={movie} onSetActiveTab={onSetActiveTab} />
 				</Suspense>
 			)}
 
 			{tv.length > 0 && (
-				<Suspense fallback={<AllTabDummyTVShows />}>
+				<Suspense fallback={<DummyAllTabTVShows />}>
 					<AllTabTVShows type={type} shows={tv} onSetActiveTab={onSetActiveTab} />
 				</Suspense>
 			)}
 
 			{person.length > 0 && (
-				<Suspense fallback={<AllTabDummyPeople />}>
+				<Suspense fallback={<DummyAllTabPeople />}>
 					<AllTabPeople type={type} people={person} onSetActiveTab={onSetActiveTab} />
+				</Suspense>
+			)}
+
+			{collection.length > 0 && (
+				<Suspense fallback={<DummyAllTabCollections />}>
+					<AllTabCollections type={type} collections={collection} onSetActiveTab={onSetActiveTab} />
+				</Suspense>
+			)}
+
+			{company.length > 0 && (
+				<Suspense fallback={<DummyAllTabCompanies />}>
+					<AllTabCompany type={type} companies={company} onSetActiveTab={onSetActiveTab} />
 				</Suspense>
 			)}
 		</VStack>
