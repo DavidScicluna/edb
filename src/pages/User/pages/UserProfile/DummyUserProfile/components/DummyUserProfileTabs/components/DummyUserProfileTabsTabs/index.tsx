3c287640ed26258f08301af88823a5c145d0ec5e
David Scicluna <dave_nostra@hotmail.com>
2023-01-29 23:48:04 +0100

    Fix: User - UserProfile - DummyUserProfile - DummyUserProfileTabs - DummyUserProfileTabsTabs

--- src/pages/User/pages/UserProfile/DummyUserProfile/components/DummyUserProfileTabs/components/DummyUserProfileTabsTabs/index.tsx
+++ src/pages/User/pages/UserProfile/DummyUserProfile/components/DummyUserProfileTabs/components/DummyUserProfileTabsTabs/index.tsx
@@ -1,8 +1,10 @@
 import { FC } from 'react';
 
-import { TabListTab, Tabs, DummyTabList, TabPanels } from '@davidscicluna/component-library';
+import { Tabs, DummyTabList, TabPanels } from '@davidscicluna/component-library';
 
 import { VStack } from '@chakra-ui/react';
+
+import { compact } from 'lodash';
 
 import DummyMoviesTab from '../../../../../../../../Movies/components/VerticalDummyMovies';
 import DummyPeopleTab from '../../../../../../../../People/components/VerticalDummyPeople';
@@ -11,6 +13,8 @@ import { useUserTheme } from '../../../../../../../../../common/hooks';
 import { useLayoutContext } from '../../../../../../../../../containers/Layout/common/hooks';
 import { formatMediaTypeLabel, getMediaTypeIcon } from '../../../../../../../../../common/utils';
 import { DummyTabIcon } from '../../../../../../../../../components';
+import DummyCollectionsTab from '../DummyUserProfileTabsCollections';
+import DummyCompaniesTab from '../DummyUserProfileTabsCompanies';
 
 import DummyAllTab from './components/DummyAllTab';
 import { DummyUserProfileTabsTabsProps } from './types';
@@ -24,18 +28,69 @@ const DummyUserProfileTabsTabs: FC<DummyUserProfileTabsTabsProps> = ({ mediaType
 		<Tabs width='100%' color={color} colorMode={colorMode} isDisabled size='lg'>
 			<VStack width='100%' spacing={spacing}>
 				<DummyTabList
-					tabs={[
+					tabs={compact([
 						{ label: 'All' },
 
-						...(mediaTypes.map((mediaType) => {
-							return {
-								label: formatMediaTypeLabel({ type: 'multiple', mediaType }),
-								renderLeft: ({ colorMode }) => (
-									<DummyTabIcon colorMode={colorMode} icon={getMediaTypeIcon({ mediaType })} />
-								)
-							};
-						}) as TabListTab[])
-					]}
+						mediaTypes.some((mediaType) => mediaType === 'movie')
+							? {
+									label: formatMediaTypeLabel({ type: 'multiple', mediaType: 'movie' }),
+									renderLeft: ({ colorMode }) => (
+										<DummyTabIcon
+											colorMode={colorMode}
+											icon={getMediaTypeIcon({ mediaType: 'movie' })}
+										/>
+									)
+							  }
+							: null,
+
+						mediaTypes.some((mediaType) => mediaType === 'tv')
+							? {
+									label: formatMediaTypeLabel({ type: 'multiple', mediaType: 'tv' }),
+									renderLeft: ({ colorMode }) => (
+										<DummyTabIcon
+											colorMode={colorMode}
+											icon={getMediaTypeIcon({ mediaType: 'tv' })}
+										/>
+									)
+							  }
+							: null,
+
+						mediaTypes.some((mediaType) => mediaType === 'person')
+							? {
+									label: formatMediaTypeLabel({ type: 'multiple', mediaType: 'person' }),
+									renderLeft: ({ colorMode }) => (
+										<DummyTabIcon
+											colorMode={colorMode}
+											icon={getMediaTypeIcon({ mediaType: 'person' })}
+										/>
+									)
+							  }
+							: null,
+
+						mediaTypes.some((mediaType) => mediaType === 'collection')
+							? {
+									label: formatMediaTypeLabel({ type: 'multiple', mediaType: 'collection' }),
+									renderLeft: ({ colorMode }) => (
+										<DummyTabIcon
+											colorMode={colorMode}
+											icon={getMediaTypeIcon({ mediaType: 'collection' })}
+										/>
+									)
+							  }
+							: null,
+
+						mediaTypes.some((mediaType) => mediaType === 'company')
+							? {
+									label: formatMediaTypeLabel({ type: 'multiple', mediaType: 'company' }),
+									renderLeft: ({ colorMode }) => (
+										<DummyTabIcon
+											colorMode={colorMode}
+											icon={getMediaTypeIcon({ mediaType: 'company' })}
+										/>
+									)
+							  }
+							: null
+					])}
 				/>
 
 				<TabPanels>
@@ -47,7 +102,9 @@ const DummyUserProfileTabsTabs: FC<DummyUserProfileTabsTabsProps> = ({ mediaType
 
 					{mediaTypes.some((mediaType) => mediaType === 'person') && <DummyPeopleTab />}
 
-					{/* TODO: Add company & collection */}
+					{mediaTypes.some((mediaType) => mediaType === 'collection') && <DummyCollectionsTab />}
+
+					{mediaTypes.some((mediaType) => mediaType === 'company') && <DummyCompaniesTab />}
 				</TabPanels>
 			</VStack>
 		</Tabs>
