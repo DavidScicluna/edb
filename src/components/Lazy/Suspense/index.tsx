7d68ba884bcea883c8a3fb1a5ecb8ba8c4d509dc
David Scicluna <dave_nostra@hotmail.com>
2023-01-29 18:20:16 +0100

    Fix: Lazy - Suspense - Commented out some logic for now

--- src/components/Lazy/Suspense/index.tsx
+++ src/components/Lazy/Suspense/index.tsx
@@ -1,39 +1,41 @@
-import { FC, SuspenseProps, Suspense as RS, useCallback, useEffect, isValidElement } from 'react';
+// import { FC, SuspenseProps, Suspense as RS, useCallback, useEffect, isValidElement } from 'react';
+import { FC, SuspenseProps, Suspense as RS } from 'react';
 
-import { useBoolean } from '@chakra-ui/react';
+// import { useBoolean } from '@chakra-ui/react';
 
-import { debounce } from 'lodash';
+// import { debounce } from 'lodash';
 
 import ErrorBoundary from '../ErrorBoundary';
 
-let timer: ReturnType<typeof setTimeout>;
+// let timer: ReturnType<typeof setTimeout>;
 
 const Suspense: FC<SuspenseProps> = ({ children, fallback, ...rest }) => {
-	const [isChildrenVisible, setIsChildrenVisible] = useBoolean();
+	// const [isChildrenVisible, setIsChildrenVisible] = useBoolean();
 
-	const handleCheckChildren = useCallback(
-		debounce(() => {
-			if (isValidElement(children)) {
-				timer = setTimeout(() => setIsChildrenVisible.on(), 500);
-			} else {
-				setIsChildrenVisible.off();
-			}
-		}, 500),
-		[children, timer]
-	);
+	// const handleCheckChildren = useCallback(
+	// 	debounce(() => {
+	// 		if (isValidElement(children)) {
+	// 			timer = setTimeout(() => setIsChildrenVisible.on(), 500);
+	// 		} else {
+	// 			setIsChildrenVisible.off();
+	// 		}
+	// 	}, 500),
+	// 	[children, timer]
+	// );
 
-	useEffect(() => {
-		handleCheckChildren();
+	// useEffect(() => {
+	// 	handleCheckChildren();
 
-		return () => {
-			clearTimeout(timer);
-		};
-	}, [children]);
+	// 	return () => {
+	// 		clearTimeout(timer);
+	// 	};
+	// }, [children]);
 
 	return (
 		<ErrorBoundary>
 			<RS {...rest} fallback={fallback}>
-				{isChildrenVisible ? children : fallback}
+				{/* {isChildrenVisible ? children : fallback} */}
+				{children}
 			</RS>
 		</ErrorBoundary>
 	);
