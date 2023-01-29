c2404dcba220d5442ec08d5e0b4f04f40d16a2c7
David Scicluna <dave_nostra@hotmail.com>
2023-01-29 23:02:57 +0100

    Feature: Search - OriginalSearch - SearchForm - SearchInfo index

---
+++ src/pages/Search/OriginalSearch/components/SearchForm/components/SearchInfo/index.tsx
@@ -0,0 +1,16 @@
+import { useMediaQuery } from '@chakra-ui/react';
+import { useTheme } from '@davidscicluna/component-library';
+import { FC } from 'react';
+import SearchInfoSm from './components/SearchInfoSm';
+import SearchInfoXl from './components/SearchInfoXl';
+import { SearchInfosCommonProps as SearchInfoProps } from './common/types';
+
+const SearchInfo: FC<SearchInfoProps> = ({ total }) => {
+	const theme = useTheme();
+
+	const [isSm] = useMediaQuery(`(max-width: ${theme.breakpoints.sm})`);
+
+	return isSm ? <SearchInfoSm total={{ ...total }} /> : <SearchInfoXl total={{ ...total }} />;
+};
+
+export default SearchInfo;
