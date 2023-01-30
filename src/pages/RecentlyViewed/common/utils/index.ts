9119dc0adb9cd2aff66c4db22f36210e25b871e7
David Scicluna <dave_nostra@hotmail.com>
2023-01-30 22:46:15 +0100

    Feature: RecentlyViewed - utils

---
+++ src/pages/RecentlyViewed/common/utils/index.ts
@@ -0,0 +1,27 @@
+import { Location } from 'react-router';
+
+import { Undefinable } from '@davidscicluna/component-library';
+
+import { memoize } from 'lodash';
+
+import { formatMediaType } from '../../../../common/utils';
+
+type GetActiveTabFromHashProps = { location: Location };
+type GetActiveTabFromHashReturn = Undefinable<0 | 1 | 2 | 3 | 4>;
+
+export const getActiveTabFromHash = memoize(({ location }: GetActiveTabFromHashProps): GetActiveTabFromHashReturn => {
+	const hash = location.hash.replaceAll('#', '');
+
+	switch (hash) {
+		case formatMediaType({ mediaType: 'movie' }):
+			return 1;
+		case formatMediaType({ mediaType: 'tv' }):
+			return 2;
+		case formatMediaType({ mediaType: 'person' }):
+			return 3;
+		case formatMediaType({ mediaType: 'collection' }):
+			return 4;
+		default:
+			return 0;
+	}
+});
