922ed154f3edccb5c566793530ee3ea71acb09ae
David Scicluna <dave_nostra@hotmail.com>
2023-01-30 22:41:44 +0100

    Feature: RecentlyViewed - RecentlyViewedDummyTabs - DummyAllTab - DummyAllTabMovies

---
+++ src/pages/RecentlyViewed/components/RecentlyViewedDummyTabs/components/DummyAllTab/components/DummyAllTabMovies/index.tsx
@@ -0,0 +1,19 @@
+import { FC } from 'react';
+
+import { range } from 'lodash';
+
+import AllTabDummyHorizontalGrid from '../DummyAllTabHorizontalGrid';
+import { DummyVerticalPoster } from '../../../../../../../../components';
+import dimensions from '../../../../../../../../components/Posters/common/data/dimensions';
+
+const AllTabDummyMovies: FC = () => {
+	return (
+		<AllTabDummyHorizontalGrid>
+			{range(20).map((_dummy, index) => (
+				<DummyVerticalPoster key={index} mediaType='movie' hasSubtitle sx={dimensions} />
+			))}
+		</AllTabDummyHorizontalGrid>
+	);
+};
+
+export default AllTabDummyMovies;
