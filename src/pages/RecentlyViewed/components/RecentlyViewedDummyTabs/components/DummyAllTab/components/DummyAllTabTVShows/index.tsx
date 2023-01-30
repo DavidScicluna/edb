40ebf662b6a41c5653c8248b3c36a75094752a70
David Scicluna <dave_nostra@hotmail.com>
2023-01-30 22:44:00 +0100

    Feature: RecentlyViewed - RecentlyViewedDummyTabs - DummyAllTab - DummyAllTabTVShows

---
+++ src/pages/RecentlyViewed/components/RecentlyViewedDummyTabs/components/DummyAllTab/components/DummyAllTabTVShows/index.tsx
@@ -0,0 +1,19 @@
+import { FC } from 'react';
+
+import { range } from 'lodash';
+
+import dimensions from '../../../../../../../../components/Posters/common/data/dimensions';
+import AllTabDummyHorizontalGrid from '../DummyAllTabHorizontalGrid';
+import { DummyVerticalPoster } from '../../../../../../../../components';
+
+const AllTabDummyTVShows: FC = () => {
+	return (
+		<AllTabDummyHorizontalGrid>
+			{range(20).map((_dummy, index) => (
+				<DummyVerticalPoster key={index} mediaType='tv' hasSubtitle sx={dimensions} />
+			))}
+		</AllTabDummyHorizontalGrid>
+	);
+};
+
+export default AllTabDummyTVShows;
