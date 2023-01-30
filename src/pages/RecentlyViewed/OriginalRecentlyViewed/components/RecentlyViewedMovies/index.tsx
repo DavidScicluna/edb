8b70613e6f4a9133b6f528d7bad3b6d62855dd77
David Scicluna <dave_nostra@hotmail.com>
2023-01-30 22:47:36 +0100

    Feature: RecentlyViewed - OriginalRecentlyViewed - RecentlyViewedMovies

---
+++ src/pages/RecentlyViewed/OriginalRecentlyViewed/components/RecentlyViewedMovies/index.tsx
@@ -0,0 +1,94 @@
+import { FC, useState } from 'react';
+
+import { useDebounce, useTheme, utils } from '@davidscicluna/component-library';
+
+import { useMediaQuery, VStack, Center } from '@chakra-ui/react';
+
+import { sort } from 'fast-sort';
+
+import { useLayoutContext } from '../../../../../containers/Layout/common/hooks';
+import {
+	QueryEmpty,
+	QueryEmptyStack,
+	QueryEmptyBody,
+	QueryEmptyTitle,
+	QueryEmptySubtitle,
+	VerticalGrid,
+	LoadMore,
+	MovieHorizontalPoster,
+	MovieVerticalPoster
+} from '../../../../../components';
+import { useSelector, useUserTheme } from '../../../../../common/hooks';
+import { getEmptySubtitle } from '../../../../../components/QueryEmpty/common/utils';
+import { formatMediaTypeLabel } from '../../../../../common/utils';
+
+const { getColor } = utils;
+
+const limit = 20;
+
+const RecentlyViewedMovies: FC = () => {
+	const theme = useTheme();
+	const { color, colorMode } = useUserTheme();
+
+	const [isSm] = useMediaQuery(`(max-width: ${theme.breakpoints.sm})`);
+
+	const { spacing } = useLayoutContext();
+
+	const recentlyViewedMovies = useSelector((state) => state.users.data.activeUser.data.recentlyViewed.movie);
+
+	const [visible, setVisible] = useState<number>(limit);
+	const visibleDebounced = useDebounce<number>(visible, 'slow');
+
+	return recentlyViewedMovies.length === 0 ? (
+		<QueryEmpty
+			color={color}
+			colorMode={colorMode}
+			borderWidth='2px'
+			borderStyle='dashed'
+			borderColor={getColor({ theme, colorMode, type: 'divider' })}
+			borderRadius='lg'
+		>
+			<QueryEmptyStack>
+				<QueryEmptyBody>
+					<QueryEmptyTitle />
+					<QueryEmptySubtitle>
+						{getEmptySubtitle({
+							type: 'empty',
+							label: formatMediaTypeLabel({ type: 'multiple', mediaType: 'movie' })
+						})}
+					</QueryEmptySubtitle>
+				</QueryEmptyBody>
+			</QueryEmptyStack>
+		</QueryEmpty>
+	) : (
+		<VStack width='100%' spacing={spacing}>
+			<VerticalGrid>
+				{({ displayMode }) =>
+					sort(recentlyViewedMovies)
+						.desc(({ addedAt }) => addedAt)
+						.filter((_movie, index) => index < visibleDebounced)
+						.map(({ mediaItem }) =>
+							displayMode === 'list' ? (
+								<MovieHorizontalPoster key={mediaItem.id} movie={mediaItem} />
+							) : (
+								<MovieVerticalPoster key={mediaItem.id} movie={mediaItem} />
+							)
+						)
+				}
+			</VerticalGrid>
+
+			<Center width={isSm ? '100%' : 'auto'}>
+				<LoadMore
+					amount={visibleDebounced}
+					total={recentlyViewedMovies.length}
+					label={formatMediaTypeLabel({ type: 'multiple', mediaType: 'movie' })}
+					isLoading={false}
+					isButtonVisible={visibleDebounced <= recentlyViewedMovies.length}
+					onClick={() => setVisible((total) => total + limit)}
+				/>
+			</Center>
+		</VStack>
+	);
+};
+
+export default RecentlyViewedMovies;
